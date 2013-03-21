c079cea48095f314c7d5ac4b2f8d66e79818ade6
Gavin Price <gaprice@lbl.gov>
2013-03-21 09:21:36 -0700

    Add python touch imp

---
+++ lib/biokbase/common/filetools.py
@@ -0,0 +1,10 @@
+import os
+
+def touch(fname, times=None):
+  '''Python implementation of the unix touch command. See os.utime for the
+  format of the times argument. Reference: 
+  http://stackoverflow.com/questions/1158076/implement-touch-using-python
+  '''
+  
+  with file(fname, 'a'):
+    os.utime(fname, times)

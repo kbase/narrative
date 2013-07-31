96aa7e9925e2f8868d5db68fd0c260c277a972ba
Matt Henderson <mlhenderson@Matthews-MacBook-Pro.local>
2013-07-31 16:15:02 -0700

    updated ipython.sh run script

--- src/notebook/bin/ipython.sh
+++ src/notebook/bin/ipython.sh
@@ -3,8 +3,9 @@
 # Script to run ipython from the src directory so that you don't have to install
 # everything - intended solely for development work in the src repo
 #
-export IPYTHONSRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=$IPYTHONSRC:$IPYTHONSRC/KBNB:$PYTHONPATH
-export IPYTHONDIR=$IPYTHONSRC/ipythondir
+export IPYTHONSRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../../
+export PYTHONPATH=$IPYTHONSRC/ipython:$PYTHONPATH
+export IPYTHONDIR=$IPYTHONSRC/notebook/ipython_profiles
 
-KBNB/ipython.py $* --NotebookManager.notebook_dir=$IPYTHONSRC/notebooks --profile=narrative
+cd $IPYTHONSRC/ipython
+python -m IPython $* --notebook-dir=~/.narrative --profile=narrative

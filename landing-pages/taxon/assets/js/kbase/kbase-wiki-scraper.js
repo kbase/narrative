44d007284ac29d91b98e266a201ad027739914ef
Bill Riehl <wjriehl@lbl.gov>
2013-08-07 15:52:30 -0400

    Added the taxon page prototype to landing-pages as a prototype guidepoint.

---
+++ landing-pages/taxon/assets/js/kbase/kbase-wiki-scraper.js
@@ -0,0 +1,122 @@
+
+
+function WikiScraper(url, auth, auth_cb) {
+
+    var _url = url;
+    var deprecationWarningSent = false;
+    
+    function deprecationWarning() {
+        if (!deprecationWarningSent) {
+            deprecationWarningSent = true;
+            if (!window.console) return;
+            console.log(
+                "DEPRECATION WARNING: '*_async' method names will be removed",
+                "in a future version. Please use the identical methods without",
+                "the'_async' suffix.");
+        }
+    }
+
+    if (typeof(_url) != "string" || _url.length == 0) {
+        _url = "http://kbase.us/services/experiment";
+    }
+    var _auth = auth ? auth : { 'token' : '', 'user_id' : ''};
+    var _auth_cb = auth_cb;
+
+
+    this.scrape = function (term, param, _callback, _errorCallback) {
+    return json_call_ajax("WikiScraper.scrape",
+        [term, param], 1, _callback, _errorCallback);
+};
+
+    this.scrape_async = function (term, param, _callback, _error_callback) {
+        deprecationWarning();
+        return json_call_ajax("WikiScraper.scrape", [term, param], 1, _callback, _error_callback);
+    };
+
+    this.scrape_first_hit = function (term_list, param, _callback, _errorCallback) {
+    return json_call_ajax("WikiScraper.scrape_first_hit",
+        [term_list, param], 1, _callback, _errorCallback);
+};
+
+    this.scrape_first_hit_async = function (term_list, param, _callback, _error_callback) {
+        deprecationWarning();
+        return json_call_ajax("WikiScraper.scrape_first_hit", [term_list, param], 1, _callback, _error_callback);
+    };
+ 
+
+    /*
+     * JSON call using jQuery method.
+     */
+    function json_call_ajax(method, params, numRets, callback, errorCallback) {
+        var deferred = $.Deferred();
+
+        if (typeof callback === 'function') {
+           deferred.done(callback);
+        }
+
+        if (typeof errorCallback === 'function') {
+           deferred.fail(errorCallback);
+        }
+
+        var rpc = {
+            params : params,
+            method : method,
+            version: "1.1",
+            id: String(Math.random()).slice(2),
+        };
+        
+        var beforeSend = null;
+        var token = (_auth_cb && typeof _auth_cb === 'function') ? _auth_cb()
+            : (_auth.token ? _auth.token : null);
+        if (token != null) {
+            beforeSend = function (xhr) {
+                xhr.setRequestHeader("Authorization", _auth.token);
+            }
+        }
+
+        jQuery.ajax({
+            url: _url,
+            dataType: "text",
+            type: 'POST',
+            processData: false,
+            data: JSON.stringify(rpc),
+            beforeSend: beforeSend,
+            success: function (data, status, xhr) {
+                var result;
+                try {
+                    var resp = JSON.parse(data);
+                    result = (numRets === 1 ? resp.result[0] : resp.result);
+                } catch (err) {
+                    deferred.reject({
+                        status: 503,
+                        error: err,
+                        url: _url,
+                        resp: data
+                    });
+                    return;
+                }
+                deferred.resolve(result);
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                var error;
+                if (xhr.responseText) {
+                    try {
+                        var resp = JSON.parse(xhr.responseText);
+                        error = resp.error;
+                    } catch (err) { // Not JSON
+                        error = "Unknown error - " + xhr.responseText;
+                    }
+                } else {
+                    error = "Unknown Error";
+                }
+                deferred.reject({
+                    status: 500,
+                    error: error
+                });
+            }
+        });
+        return deferred.promise();
+    }
+}
+
+

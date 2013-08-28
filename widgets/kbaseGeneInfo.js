22a4e57bba8089e7fb8df820b13c8d59987a52e9
Bill Riehl <wjriehl@lbl.gov>
2013-08-28 12:16:21 -0700

    More work on the gene info widget.

--- widgets/kbaseGeneInfo.js
+++ widgets/kbaseGeneInfo.js
@@ -49,9 +49,57 @@
 			 * annotation(s)
 			 * publication(s)
 			 */
+			var self = this;
 
-			this.$elem.append(this.options.featureID);
+			this.$table = $("<table />");
+			this.entityClient.get_entity_Feature(
+				[this.options.featureID], 
+				['feature_type', 'function', 'alias'],
+
+				function(feature) {
+					feature = feature[self.options.featureID];
+					self.$table.append("<tr><td>ID</td><td>" + feature.id + "</td></tr>");
+					self.$table.append("<tr><td>Function</td><td>" + (feature.function ? feature.function : "Unknown function") + "</td></tr>");
+
+					if (feature.alias) {
+						self.$table.append("<tr><td>Alias</td><td>" + feature.alias + "</td></tr>");
+					}
+
+					self.cdmiClient.fids_to_feature_data(
+						[feature.id],
+
+						function(featureData) {
+							featureData = featureData[self.options.featureID];
+							if (featureData.feature_publications && featureData.feature_publications.length > 0) {
+								var pubStr = "";
+								for (var i=0; i<featureData.feature_publications.length; i++) {
+									pubStr += "<a href='" + featureData.feature_publications[i][1] + "' target='_new'>" + featureData.feature_publications[i][2] + "</a><br>";
+								}
+								self.$table.append("<tr><td>Publications</td><td>" + pubStr + "</td></tr>");
+							}
+							self.$elem.append(self.$table);
+						},
+						self.clientError
+					);
+				},
+
+				this.clientError
+			);
+
+			// this.cdmiClient.fids_to_feature_data(
+			// 	[this.options.featureID],
+			// 	function(featureData) {
+			// 		console.log(featureData);
+			// 	},
+			// 	this.clientError
+			// );
+
+
 			return this;
+		},
+
+		clientError: function(error) {
+
 		}
 
 

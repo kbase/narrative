00cda2277feb1ae193653ed3a9ed9040a60a8cb9
Dan Gunter <dkgunter@lbl.gov>
2013-09-18 09:47:06 -0700

    parameterized the script, inputs are now in JSON file (added to repo)

--- src/biokbase/narrative/demo/sac/coex_workflow/coex_network.py
+++ src/biokbase/narrative/demo/sac/coex_workflow/coex_network.py
@@ -9,9 +9,11 @@ __version__ = '0.1'
 
 ## Imports
 
-# system
+# std lib
 import json
 import logging
+import argparse
+import os
 from string import Template
 import sys
 import time
@@ -98,58 +100,45 @@ def get_output_files(uri, id_):
 
 
 def submit_awe_job(uri, awe_job_document):
-    _log.debug("Processed document:\n{}".format(awe_job_document))
+    _log.debug("job.script value=\n{}".format(awe_job_document))
     content = {'upload': ("awe_job", awe_job_document)}
     r = requests.post("{}/job".format(uri), files=content)
     response = json.loads(r.text)
     if response['data'] is None:
         raise SubmitException("Response from job submit has no data: {}".format(response))
     try:
-        return(response['data']['id'])
+        return response['data']['id']
     except Exception as e:
         raise SubmitException("Parsing response from job submit: {}".format(e))
 
-## Global (configuration) variables
-
-# Server URLs
-
-
-class URLS:
-    main = "http://140.221.84.236:8000/node"
-    shock = "http://140.221.84.236:8000"
-    awe = "http://140.221.84.236:8001/"
-
-sessionID = str(uuid.uuid4())
-
-# File types, names, and descriptions
-
-files = {"expression": "Expression_Table.csv",
-         "sample_id": "Sample_id.csv",
-         "annotation": "Gene_Annotation.csv"}
-files_desc = dict(expression="Expression data",
-                  sample_id="Sample file",
-                  annotation="Annotation file")
-
-# Create metadata for each file type
-
-metadata = {}
-for file_type, file_name in files.iteritems():
-    metadata[file_type] = {
-        "pipeline": "coexpression network",
-        "file_name": file_name,
-        "file_type": "expression_file",
-        "description": files_desc[file_type],
-        "sessionID": sessionID
-    }
-
-coex_args = dict(
-    coex_filter="-m anova -p 0.05 -u n -r y -d y",
-    coex_net="-m simple -t edge -c 0.75 -r 0.8 -k 40 -p 50",
-    coex_cluster2="-s 20 -c hclust -n simple -r 0.8 -k 40 -p 50 -d 0.99")
-
 ## MAIN ##
 
-def main():
+
+# Run options
+DEFAULTS = dict(
+    config_file="config.json",
+    job_file="awe_job.json")
+
+
+def parse_args():
+    d = DEFAULTS    # alias
+    op = argparse.ArgumentParser()
+    op.add_argument("-c", "--config", dest="config_file", default=d["config_file"],
+                  help="JSON configuration file with urls, files, etc. "
+                        "(default={})".format(d["config_file"]))
+    op.add_argument("-j", "--job", dest="job_file", default=d["job_file"],
+                  help="AWE job JSON configuration file "
+                       "(default={})".format(d["job_file"]))
+    options = op.parse_args()
+    # check that input files exist
+    for key in 'config_file', 'job_file':
+        f = getattr(options, key)
+        if not os.path.exists(f):
+            flag = '--' + key.split('_')[0]
+            op.error("File for {} not found: {}".format(flag, f))
+    return options
+
+def main(options):
     """Create a narrative for co-expression network workflow
 
     1. User uploads multiple files to shock and remembers shock node IDs
@@ -157,29 +146,51 @@ def main():
     3. Job is submitted to awe service
     4. Node ids of output files are provided
     """
-    _log.info("Uploading files to shock")
+    sessionID = str(uuid.uuid4())
+
+    # Configure from JSON
+    _log.info("config.read.begin")
+    cfg = json.load(open(options.config_file))
+    _log.info("config.read.end")
+    # local aliases
+    files, descs, urls = cfg['files'], cfg['files_desc'], cfg['urls']
+
+    # Create metadata for each file type
+    metadata = {}
+    for file_type, file_name in files.iteritems():
+        metadata[file_type] = {
+            "pipeline": "coexpression network",
+            "file_name": file_name,
+            "file_type": "expression_file",
+            "description": descs[file_type],
+            "sessionID": sessionID
+        }
+
+    # Upload data to Shock
+    _log.info("upload.shock.begin")
     shock_ids = {}
     for file_type, file_name in files.iteritems():
-        _log.info("Uploading to {} :: {} = {}".format(URLS.shock, file_type, file_name))
+        _log.info("upload.shock.file url={} type={} name={}".format(
+            urls['shock'], file_type, file_name))
         file_meta = str(metadata[file_type])
-        shock_ids[file_type] = upload_file(URLS.shock, file_name, file_meta)
-    _log.debug("Uploaded to shock. ids = {}".format(','.join(shock_ids.values())))
+        shock_ids[file_type] = upload_file(urls['shock'], file_name, file_meta)
+    _log.debug("upload.shock.end ids={}".format(','.join(shock_ids.values())))
 
     # Read & substitute values into job spec
-    awe_job = json.load(open("awe_job.json"))
+    awe_job = json.load(open(options.job_file))
     subst = shock_ids.copy()
-    subst.update(coex_args)
-    subst.update(dict(shock_uri=URLS.shock, session_id=sessionID))
+    subst.update(cfg['args'])
+    subst.update(dict(shock_uri=urls['shock'], session_id=sessionID))
     awe_job_str = Template(json.dumps(awe_job)).substitute(subst)
 
     # Submit job
-    job_id = submit_awe_job(URLS.awe, awe_job_str)
+    job_id = submit_awe_job(urls['awe'], awe_job_str)
 
     # Wait for job to complete
     _log.info("job.begin")
     while 1:
         time.sleep(5)
-        count = check_job_status(URLS.awe, job_id)
+        count = check_job_status(urls['awe'], job_id)
         if count == 0:
             break
         _log.debug("job.run tasks_remaining={:d}".format(count))
@@ -187,17 +198,19 @@ def main():
 
     #XXX: Never get past this point
 
-    print("\n##############################\nDownload and visualize network output\n")
+    sep = '#' * 40
+    print(sep + "Download and visualize network output\n" + sep)
 
-    print("\nURLs to download output files\n")
-    download_urls = get_output_files(URLS.awe, job_id)
+    print("\nURLs to download output files")
+    download_urls = get_output_files(urls['awe'], job_id)
     print('\n'.join(['\t\t' + s for s in download_urls]))
 
-    print("URL to visualize the network\n")
-    viz_urls = get_url_visualization(URLS.awe, job_id)
+    print("\nURL to visualize the network")
+    viz_urls = get_url_visualization(urls['awe'], job_id)
     print('\n'.join(['\t\t' + s for s in viz_urls]))
 
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main())
+    options = parse_args()
+    sys.exit(main(options))

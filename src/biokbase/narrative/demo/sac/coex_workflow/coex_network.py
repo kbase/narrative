6d532df10862762ec0365f7a171bdba01a73b348
Dan Gunter <dkgunter@lbl.gov>
2013-09-17 17:14:21 -0700

    got as far as job submission, hitting an error there

--- src/biokbase/narrative/demo/sac/coex_workflow/coex_network.py
+++ src/biokbase/narrative/demo/sac/coex_workflow/coex_network.py
@@ -3,13 +3,24 @@
    This a conversion of Ranjan Priya's coex_network script into Python 2.7+
 
    sychan@lbl.gov
-
+    dkgunter@lbl.gov
 """
-
-import pprint
+# system
 import json
+import logging
+import pprint
+import sys
+import time
+# third-party
 import requests
-import base64
+
+## configure logging
+_log = logging.getLogger("coex_network")
+_log.setLevel(logging.DEBUG)
+_log.addHandler(logging.StreamHandler())
+
+class UploadException(Exception): pass
+
 
 """
 sub upload_file {
@@ -36,22 +47,21 @@ sub upload_file {
 
 """
 
-def upload_file( uri, filename, att_content):
+def upload_file(uri, filename, att_content):
+    f = open(filename)
+    file_contents = ''.join((s.strip() for s in f))
+    data = {'upload': (filename, file_contents),
+            'attributes': ('', filename, {'Content': att_content})}
+    _log.debug("upload.request data={}".format(data))
+    r = requests.post("%s/node" % uri, files=data)
+    response = json.loads(r.text)
+    if response['data'] is None:
+        raise UploadException("Response from upload has no data: {}".format(response))
+    _log.debug("Response.json={}".format(response))
     try:
-        with open( filename, 'r') as f:
-            file_contents = f.read()
-    except Exception, e:
-        raise
-    # Not clear why we are reading it in first before uploading when the API
-    # supports directly uploading it. But maybe Ranjan has a reason for it?
-    data = { 'upload' : (filename, file_contents),
-             'attributes' : ( filename, att_content)}
-    r = requests.post( "%s/node" % uri, files = data)
-    response = json.reads(r.text)
-    try:
-        return( response['data']['id'])
-    except Exception, e:
-        raise( "Problem with parsing response from upload: %s" % e)
+        return response['data']['id']
+    except Exception as err:
+        raise UploadException("Problem with parsing response from upload: {}".format(err))
     
 
 """
@@ -147,10 +157,9 @@ def get_output_files( uri, id):
                               response["data"]["tasks"][1]["outputs"]["net_edge_csv"]["url"])
         download_urls.append( "module_csv: %s" %
                               response["data"]["tasks"][2]["outputs"]["module_csv"]["url"])
-        return( download_urls)
     except Exception, e:
         raise Exception( "Could not parse results properly: %s" % e)
-
+    return download_urls
 
 
 """
@@ -178,14 +187,16 @@ sub submit_awe_job {
 }
 """
 
-def submit_awe_job( uri, awe_job_document):
-    data = { 'upload' : (filename, awe_job_document) }
-    r = requests.post( "%s/job" % uri, files = data)
-    response = json.reads(r.text)
+def submit_awe_job(uri, awe_job_document):
+    data = {'upload': ('', "awe_job", {'Content': awe_job_document})}
+    r = requests.post("{}/job".format(uri), files=data)
+    response = json.loads(r.text)
+    if response['data'] is None:
+        raise UploadException("Response from upload has no data: {}".format(response))
     try:
-        return( response['data']['id'])
+        return(response['data']['id'])
     except Exception, e:
-        raise( "Problem with parsing response from upload: %s" % e)
+        raise UploadException("Problem with parsing response from upload: {}".format(e))
 
 #########################
 ### End of SUB PROCEDURES
@@ -215,10 +226,12 @@ my $sessionID = "xyz-123-abc"; #Randome string generated
 
 """
 
-uri = "http://140.221.84.236:8000/node"
-shock_uri = "http://140.221.84.236:8000"
-awe_uri = "http://140.221.84.236:8001/"
-sessionID = "xyz-123-abc"  #Randome string generated
+class URLS:
+   main = "http://140.221.84.236:8000/node"
+   shock = "http://140.221.84.236:8000"
+   awe = "http://140.221.84.236:8001/"
+
+sessionID = "xyz-123-abc"  #Random string generated
 
 
 """
@@ -233,13 +246,18 @@ my $user_input_coex_cluster2  = "-s 20 -c hclust -n simple -r 0.8 -k 40 -p 50 -d
 
 """
 
-expression_file = "Expression_Table.csv"
-sample_id_file  = "Sample_id.csv"
-annotation_file = "Gene_Annotation.csv"
+files = {"expression": "Expression_Table.csv",
+         "sample_id": "Sample_id.csv",
+         "annotation": "Gene_Annotation.csv"}
+files_desc = dict(expression="Expression data",
+                  sample_id="Sample file",
+                  annotation="Annotation file")
 
-user_input_coex_filter    = "-m anova -p 0.05 -u n -r y -d y"
-user_input_coex_net       = "-m simple -t edge -c 0.75 -r 0.8 -k 40 -p 50"
-user_input_coex_cluster2  = "-s 20 -c hclust -n simple -r 0.8 -k 40 -p 50 -d 0.99"
+
+class ARGS:
+    coex_filter = "-m anova -p 0.05 -u n -r y -d y"
+    coex_net = "-m simple -t edge -c 0.75 -r 0.8 -k 40 -p 50"
+    coex_cluster2 = "-s 20 -c hclust -n simple -r 0.8 -k 40 -p 50 -d 0.99"
 
 
 
@@ -255,20 +273,6 @@ my $expression_file_metadata=<<END;
 		"sessionID": "$sessionID"
 }
 END
-"""
-expression_file_metadata="""
-{
-	"pipeline":"coexpression network",
-		"file_name": "%s",
-		"file_type": "expression_file",
-		"description": "Input Expression data",
-		"sessionID": "%s"
-}
-""" % ( expression_file, sessionID)
-
-
-"""
-#Preparing metadata for sample_id_file for upload to shock
 my $sample_id_file_metadata=<<END;
 {
 	"pipeline":"coexpression network",
@@ -276,13 +280,8 @@ my $sample_id_file_metadata=<<END;
 		"file_type": "sample_id_file",
 		"description": "Input Sample file",
 		"sessionID": "$sessionID"
-}  
+}
 END
-"""
-
-
-"""
-#Preparing metadata for annotation_file for upload to shock
 my $annotation_file_metadata=<<END;
 {
 	"pipeline":"coexpression network",
@@ -292,158 +291,34 @@ my $annotation_file_metadata=<<END;
 		"sessionID": "$sessionID"
 }
 END
+"""
 
-
-
-
+metadata = {}
+for file_type, file_name in files.iteritems():
+    metadata[file_type] = {
+        "pipeline": "coexpression network",
+        "file_name": file_name,
+        "file_type": "expression_file",
+        "description": files_desc[file_type],
+        "sessionID": sessionID
+    }
 
 #my $att_content = join('',@lines2);
-
+"""
 print "Uploading files to shock\n";
-
 print "Uploading Expression Table: $expression_file\n";
 my $expression_file_shock_id = upload_file ($shock_uri, $expression_file, $expression_file_metadata);      
-
-
 print "Uploading Sample ID file: $sample_id_file\n";
 my $sample_id_file_shock_id = upload_file ($shock_uri, $sample_id_file, $sample_id_file_metadata);      
-
-
 print "Uploading Annotation file: $annotation_file \n";
 my $annotation_file_shock_id = upload_file ($shock_uri, $annotation_file, $annotation_file_metadata);      
-
 print "Input File uploads complete\n";
-
-
 
 my $awe_job_document =<<END;
 
-
-{
-	"info": {
-		"pipeline": "coex-example",
-			"name": "testcoex",
-			"project": "default",
-			"user": "default",
-			"clientgroups":"",
-			"sessionId":"$sessionID"
-	}, 
-		"tasks": [
-		{
-			"cmd": {
-				"args": "-i \@data_csv --output=data_filtered_csv  --sample_index=\@sample_id_csv  $user_input_coex_filter", 
-				"description": "filtering", 
-				"name": "coex_filter"
-			}, 
-			"dependsOn": [], 
-			"inputs": {
-				"data_csv": {
-					"host": "$shock_uri",
-					"node": "$expression_file_shock_id"
-				},
-				"sample_id_csv": {
-					"host": "$shock_uri",
-					"node": "$sample_id_file_shock_id"
-				}
-			}, 
-			"outputs": {
-				"data_filtered_csv": {
-					"host": "$shock_uri"
-				}
-			},
-			"taskid": "0",
-			"skip": 0,
-			"totalwork": 1                
-		},
-		{
-			"cmd": {
-				"args": "-i \@data_filtered_csv -o net_edge_csv -m simple -t edge $user_input_coex_net", 
-				"description": "coex network", 
-				"name": "coex_net"
-			}, 
-			"dependsOn": ["0"], 
-			"inputs": {
-				"data_filtered_csv": {
-					"host": "$shock_uri",
-					"origin": "0"
-				}
-			}, 
-			"outputs": {
-				"net_edge_csv": {
-					"host": "$shock_uri"
-				}
-			},
-			"taskid": "1",
-			"skip": 0, 
-			"totalwork": 1
-		},
-		{
-			"cmd": {
-				"args": "-i \@data_filtered_csv -o module_csv  -c hclust -n simple $user_input_coex_cluster2", 
-				"description": "clustering", 
-				"name": "coex_cluster2"
-			}, 
-			"dependsOn": ["1"], 
-			"inputs": {
-				"data_filtered_csv": {
-					"host": "$shock_uri",
-					"origin": "0"
-				}
-			}, 
-			"outputs": {
-				"hclust_tree_method=hclust.txt": {
-					"host": "$shock_uri"
-				},
-				"module_network_edgelist_method=hclust.csv": {
-					"host": "$shock_uri"
-				},
-				"module_csv": {
-					"host": "$shock_uri"
-				}
-			},
-			"taskid": "2",
-			"skip": 0,
-			"totalwork": 1
-		},
-		{
-			"cmd": {
-				"args": "-m \@module_csv -e \@net_edge_csv -a \@annotation_csv -o merged_list_json -u $shock_uri/node -s $sessionID", 
-				"description": "clustering", 
-				"name": "merge_csv"
-			}, 
-			"dependsOn": ["2"], 
-			"inputs": {
-				"module_csv": {
-					"host": "$shock_uri",
-					"origin": "2"
-				},
-				"net_edge_csv": {
-					"host": "$shock_uri",
-					"origin": "1"
-				},
-				"annotation_csv": {
-					"host": "$shock_uri",
-					"node": "$annotation_file_shock_id"
-				}
-			}, 
-			"outputs": {
-				"merged_list_json": {
-					"host": "$shock_uri"
-				}
-			},
-			"taskid": "3",
-			"skip": 0,
-			"totalwork": 1
-		}
-	]
-}
-END
-
-
+.. see awe_job.json ..
 
 my $id = submit_awe_job($awe_uri, $awe_job_document);
-
-
 print "Submitted Job to awe server\n";
 my $task_remain = -1;
 
@@ -452,21 +327,61 @@ while ($task_remain !=0){
 	print "Tasks remaining: $task_remain\n";
 	sleep(5);
 }
-
 #my $id = "c544f64e-2572-4da3-8e9b-cabd184a1ed8";
 my $url_array_ref = get_output_files ($awe_uri, $id);
 print "Job successfully completed\n";
-
-
-print "\n##############################\nDownload and visualize network output\n";
-print "\nURLs to download output files\n";
 
  foreach my $url (@$url_array_ref){
   print "\t\t$url\n";
  }
 
-
-print "URL to visualize the network\n";
- my $url_viz = get_url_visualization($awe_uri, $id);
-  print "\t\t$url_viz\n";
+    my $url_viz = get_url_visualization($awe_uri, $id);
+      print "\t\t$url_viz\n";
 """
+def main():
+    _log.info("Uploading files to shock")
+    shock_ids = {}
+    for file_type, file_name in files.iteritems():
+        _log.info("Uploading to {} :: {} = {}".format(URLS.shock, file_type, file_name))
+        file_meta = str(metadata[file_type])
+        shock_ids[file_type] = upload_file(URLS.shock, file_name, file_meta)
+    _log.debug("Uploaded to shock. ids = {}".format(','.join(shock_ids.values())))
+
+    awe_job = json.load(open("awe_job.json"))
+    final_tasks = []
+    subst = shock_ids.copy()
+    subst['shock_uri'] = URLS.shock
+    subst['session_id'] = sessionID
+    for task in awe_job["tasks"]:
+        for key1 in ('inputs', 'outputs'):
+            for key2 in task[key1]:
+                for key3 in task[key1][key2]:
+                    task[key1][key2][key3] = task[key1][key2][key3].format(**subst)
+        final_tasks.append(task)
+    awe_job['tasks'] = final_tasks
+    pprint.pprint(awe_job)
+    job_id = submit_awe_job(URLS.awe, awe_job)
+
+    _log.info("job.begin")
+    while 1:
+        time.sleep(5)
+        count = check_job_status(URLS.awe, job_id)
+        if count == 0:
+            break
+        _log.debug("job tasks remaining: {:d}".format(count))
+    _log.info("job.end")
+
+    print("\n##############################\nDownload and visualize network output\n")
+
+    print("\nURLs to download output files\n")
+    download_urls = get_output_files(URLS.awe, job_id)
+    print('\n'.join(['\t\t' + s for s in download_urls]))
+
+    print("URL to visualize the network\n")
+    viz_urls = get_url_visualization(URLS.awe, job_id)
+    print('\n'.join(['\t\t' + s for s in viz_urls]))
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())

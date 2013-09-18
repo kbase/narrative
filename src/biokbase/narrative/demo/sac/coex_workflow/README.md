e7e133bb8dec9c3cc6a5ecee28b67f907b74daff
Dan Gunter <dkgunter@lbl.gov>
2013-09-17 21:48:29 -0700

    upload & submit seem to work; job runs forever, so getting results not tested yet

---
+++ src/biokbase/narrative/demo/sac/coex_workflow/README.md
@@ -0,0 +1,6 @@
+# install notes
+
+To install 'requests' package
+
+pip install requests
+
--- src/biokbase/narrative/demo/sac/coex_workflow/coex_network.py
+++ src/biokbase/narrative/demo/sac/coex_workflow/coex_network.py
@@ -1,58 +1,54 @@
 """
+This a conversion of Ranjan Priya's coex_network script into Python 2.7+
 
-   This a conversion of Ranjan Priya's coex_network script into Python 2.7+
-
-   sychan@lbl.gov
-    dkgunter@lbl.gov
+Authors:
+    Steve Chan <sychan@lbl.gov>
+    Dan Gunter <dkgunter@lbl.gov>
 """
+__version__ = '0.1'
+
+## Imports
+
 # system
 import json
 import logging
-import pprint
+from string import Template
 import sys
 import time
+import uuid
 # third-party
 import requests
 
-## configure logging
+## Configure logging
+
 _log = logging.getLogger("coex_network")
 _log.setLevel(logging.DEBUG)
-_log.addHandler(logging.StreamHandler())
+_h = logging.StreamHandler()
+_h.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
+_log.addHandler(_h)
 
-class UploadException(Exception): pass
+## Exception classes
 
 
-"""
-sub upload_file {
-	my ($uri, $filename, $att_content) = @_;
-	open (FH,$filename) || die "Could not find $filename";
-	my @lines = <FH>;
-	close FH;
-	my $file_contents = join('',@lines);
-	my $json = new JSON;
-        # Perl UserAgent code
-	my $browser= LWP::UserAgent->new; # http version 
-	my $req = HTTP::Request->new( 'POST', $uri );
-        #  Send POST request - was it a redirect as expected
-	my $response = $browser->post(
-			"$uri/node",                                # URI should start with   https to use https service protocol 
-			Content_Type => 'multipart/form-data',
-			Content => [ upload => [undef, $filename, Content=>$file_contents ], attributes => [undef, $filename, Content=>$att_content] ]
-			);
+class UploadException(Exception):
+    pass
 
-	my $response_hash = $json->decode( $response->content );
-	my $id = $response_hash->{"data"}->{"id"};
-	return $id;
-}
 
-"""
+class SubmitException(Exception):
+    pass
+
+
+## Functions
+
+def join_stripped(iterable):
+    return ''.join((s.strip() for s in iterable))
+
 
 def upload_file(uri, filename, att_content):
-    f = open(filename)
-    file_contents = ''.join((s.strip() for s in f))
+    file_contents = join_stripped(open(filename))
     data = {'upload': (filename, file_contents),
-            'attributes': ('', filename, {'Content': att_content})}
-    _log.debug("upload.request data={}".format(data))
+            'attributes': ('', att_content)}
+    #_log.debug("upload.request data={}".format(data))
     r = requests.post("%s/node" % uri, files=data)
     response = json.loads(r.text)
     if response['data'] is None:
@@ -64,50 +60,18 @@ def upload_file(uri, filename, att_content):
         raise UploadException("Problem with parsing response from upload: {}".format(err))
     
 
-"""
-sub check_job_status {
-	my ($uri, $id) = @_;
-	my $json = new JSON;
-# Perl UserAgent code
-	my $browser = LWP::UserAgent->new; # http version
-#		my $req = HTTP::Request->new( 'POST', "$uri/job/$id" );
-	my $url = "$uri/job/$id";
-	my $response = $browser->get( $url );
-
-	my $response_hash = $json->decode( $response->content );
-	my $remain_tasks = $response_hash->{"data"}->{"remaintasks"};
-	return $remain_tasks;
-}
-"""
-
-def check_job_status( uri, id):
-    url = "%s/job/%s" % (uri,id)
+def check_job_status(uri, id_):
+    url = "%s/job/%s" % (uri, id_)
     r = requests.get(url)
-    response = json.reads( r.text)
+    response = json.loads(r.text)
     remain_tasks = response.get("data",dict()).get("remaintasks")
     return remain_tasks
 
-"""
-sub get_url_visualization {
 
-        my ($uri, $id) = @_;
-        my $json = new JSON;
-# Perl UserAgent code
-        my $browser = LWP::UserAgent->new; # http version
-        my $url = "$uri/job/$id";
-        my $response = $browser->get( $url );
-
-        my $response_hash = $json->decode( $response->content );
-	my $merged_csv_node = $response_hash->{"data"}->{"tasks"}->[3]->{"outputs"}->{"merged_list_json"}->{"node"};
-        my $url_viz = "http://140.221.85.95/gvisualize/$merged_csv_node";
-        return $url_viz;
-}
-"""
-
-def get_url_visualization( uri, id):
-    url = "%s/job/%s" % (uri,id)
+def get_url_visualization(uri, id_):
+    url = "%s/job/%s" % (uri, id_)
     r = requests.get(url)
-    response = json.reads( r.text)
+    response = json.loads(r.text)
     try:
         merged_csv_node = response["data"]["tasks"][3]["outputs"]["merged_list_json"]["node"]
     except Exception,e:
@@ -115,136 +79,49 @@ def get_url_visualization( uri, id):
     url_viz = "http://140.221.85.95/gvisualize/%s" % merged_csv_node
     return url_viz
 
-"""
-sub get_output_files {
-	my ($uri, $id) = @_;
-	my $json = new JSON;
-# Perl UserAgent code
-	my $browser = LWP::UserAgent->new; # http version
-#		my $req = HTTP::Request->new( 'POST', "$uri/job/$id" );
-	my $url = "$uri/job/$id";
-	my $response = $browser->get( $url );
 
-	my $response_hash = $json->decode( $response->content );
-#	my $remain_tasks = $response_hash->{"data"}->{"remaintasks"};
-
-#Get download urls for all outfile types
-my @download_urls = ();
-
-my $data_filtered_csv=$response_hash->{"data"}->{"tasks"}->[0]->{"outputs"}->{"data_filtered_csv"}->{"url"};
-push (@download_urls, "data_filtered_csv: $data_filtered_csv");
-
-my $net_edge_csv=$response_hash->{"data"}->{"tasks"}->[1]->{"outputs"}->{"net_edge_csv"}->{"url"};
-push (@download_urls, "net_edge_csv: $net_edge_csv");
-
-my $module_csv=$response_hash->{"data"}->{"tasks"}->[2]->{"outputs"}->{"module_csv"}->{"url"};
-push (@download_urls, "module_csv: $module_csv");
-
-
-	return \@download_urls;
-}
-"""
-
-def get_output_files( uri, id):
-    url = "%s/job/%s" % (uri,id)
+def get_output_files(uri, id_):
+    url = "%s/job/%s" % (uri, id_)
     r = requests.get(url)
-    response = json.reads( r.text)
-    download_urls = ()
+    response = json.loads(r.text)
+    download_urls = []
     try:
-        download_urls.append( "data_filtered_csv: %s" %
+        download_urls.append("data_filtered_csv: %s" %
                               response["data"]["tasks"][0]["outputs"]["data_filtered_csv"]["url"])
-        download_urls.append( "net_edge_csv: %s" %
+        download_urls.append("net_edge_csv: %s" %
                               response["data"]["tasks"][1]["outputs"]["net_edge_csv"]["url"])
-        download_urls.append( "module_csv: %s" %
+        download_urls.append("module_csv: %s" %
                               response["data"]["tasks"][2]["outputs"]["module_csv"]["url"])
     except Exception, e:
-        raise Exception( "Could not parse results properly: %s" % e)
+        raise Exception("Parsing results: %s" % e)
     return download_urls
 
 
-"""
-sub submit_awe_job {
-
-	my ($uri, $awe_job_document) = @_;
-
-
-	my $json = new JSON;
-
-
-# Perl UserAgent code
-	my $browser = LWP::UserAgent->new; # http version
-	#my $req = HTTP::Request->new( 'POST', "$uri/job" );
-#  Send POST request - was it a redirect as expected
-	my $response = $browser->post(
-			"$uri/job",                                # URI should start with   https to use https service protocol
-			Content_Type => 'multipart/form-data',
-			Content => [ upload => [undef, "awe_job", Content=>$awe_job_document ] ]
-			);
-
-	my $response_hash = $json->decode( $response->content );
-	my $id = $response_hash->{"data"}->{"id"};
-	return $id;
-}
-"""
-
 def submit_awe_job(uri, awe_job_document):
-    data = {'upload': ('', "awe_job", {'Content': awe_job_document})}
-    r = requests.post("{}/job".format(uri), files=data)
+    _log.debug("Processed document:\n{}".format(awe_job_document))
+    content = {'upload': ("awe_job", awe_job_document)}
+    r = requests.post("{}/job".format(uri), files=content)
     response = json.loads(r.text)
     if response['data'] is None:
-        raise UploadException("Response from upload has no data: {}".format(response))
+        raise SubmitException("Response from job submit has no data: {}".format(response))
     try:
         return(response['data']['id'])
-    except Exception, e:
-        raise UploadException("Problem with parsing response from upload: {}".format(e))
+    except Exception as e:
+        raise SubmitException("Parsing response from job submit: {}".format(e))
 
-#########################
-### End of SUB PROCEDURES
-#########################
+## Global (configuration) variables
 
+# Server URLs
 
-#This script will be used to create a narrative for co-expression network workflow
-
-# Step 1
-#User uploads multiple files to shock and remembers shock node IDs
-
-#Step 2
-#An awe job script is created
-
-#Step 3 
-#Job is submitted to awe service
-
-#Step 4
-#Node ids of output files are provided
-
-"""
-
-my $uri = "http://140.221.84.236:8000/node";
-my $shock_uri = "http://140.221.84.236:8000";
-my $awe_uri = "http://140.221.84.236:8001/";
-my $sessionID = "xyz-123-abc"; #Randome string generated
-
-"""
 
 class URLS:
-   main = "http://140.221.84.236:8000/node"
-   shock = "http://140.221.84.236:8000"
-   awe = "http://140.221.84.236:8001/"
+    main = "http://140.221.84.236:8000/node"
+    shock = "http://140.221.84.236:8000"
+    awe = "http://140.221.84.236:8001/"
 
-sessionID = "xyz-123-abc"  #Random string generated
+sessionID = str(uuid.uuid4())
 
-
-"""
-#Getting user input. Both for file uploads and parameters to submit for awe
-my $expression_file = "Expression_Table.csv";
-my $sample_id_file  = "Sample_id.csv";
-my $annotation_file = "Gene_Annotation.csv";
-
-my $user_input_coex_filter    = "-m anova -p 0.05 -u n -r y -d y";
-my $user_input_coex_net       = "-m simple -t edge -c 0.75 -r 0.8 -k 40 -p 50";
-my $user_input_coex_cluster2  = "-s 20 -c hclust -n simple -r 0.8 -k 40 -p 50 -d 0.99";
-
-"""
+# File types, names, and descriptions
 
 files = {"expression": "Expression_Table.csv",
          "sample_id": "Sample_id.csv",
@@ -253,45 +130,7 @@ files_desc = dict(expression="Expression data",
                   sample_id="Sample file",
                   annotation="Annotation file")
 
-
-class ARGS:
-    coex_filter = "-m anova -p 0.05 -u n -r y -d y"
-    coex_net = "-m simple -t edge -c 0.75 -r 0.8 -k 40 -p 50"
-    coex_cluster2 = "-s 20 -c hclust -n simple -r 0.8 -k 40 -p 50 -d 0.99"
-
-
-
-"""
-#Preparing metadata for expression_file for upload to shock
-
-my $expression_file_metadata=<<END;
-{
-	"pipeline":"coexpression network",
-		"file_name": "$expression_file",
-		"file_type": "expression_file",
-		"description": "Input Expression data",
-		"sessionID": "$sessionID"
-}
-END
-my $sample_id_file_metadata=<<END;
-{
-	"pipeline":"coexpression network",
-		"file_name": "$sample_id_file",
-		"file_type": "sample_id_file",
-		"description": "Input Sample file",
-		"sessionID": "$sessionID"
-}
-END
-my $annotation_file_metadata=<<END;
-{
-	"pipeline":"coexpression network",
-		"file_name": "$annotation_file",
-		"file_type": "annotation_file",
-		"description": "Input Annotation file",
-		"sessionID": "$sessionID"
-}
-END
-"""
+# Create metadata for each file type
 
 metadata = {}
 for file_type, file_name in files.iteritems():
@@ -303,42 +142,21 @@ for file_type, file_name in files.iteritems():
         "sessionID": sessionID
     }
 
-#my $att_content = join('',@lines2);
-"""
-print "Uploading files to shock\n";
-print "Uploading Expression Table: $expression_file\n";
-my $expression_file_shock_id = upload_file ($shock_uri, $expression_file, $expression_file_metadata);      
-print "Uploading Sample ID file: $sample_id_file\n";
-my $sample_id_file_shock_id = upload_file ($shock_uri, $sample_id_file, $sample_id_file_metadata);      
-print "Uploading Annotation file: $annotation_file \n";
-my $annotation_file_shock_id = upload_file ($shock_uri, $annotation_file, $annotation_file_metadata);      
-print "Input File uploads complete\n";
+coex_args = dict(
+    coex_filter="-m anova -p 0.05 -u n -r y -d y",
+    coex_net="-m simple -t edge -c 0.75 -r 0.8 -k 40 -p 50",
+    coex_cluster2="-s 20 -c hclust -n simple -r 0.8 -k 40 -p 50 -d 0.99")
 
-my $awe_job_document =<<END;
+## MAIN ##
 
-.. see awe_job.json ..
-
-my $id = submit_awe_job($awe_uri, $awe_job_document);
-print "Submitted Job to awe server\n";
-my $task_remain = -1;
-
-while ($task_remain !=0){
-	$task_remain = check_job_status($awe_uri, $id);
-	print "Tasks remaining: $task_remain\n";
-	sleep(5);
-}
-#my $id = "c544f64e-2572-4da3-8e9b-cabd184a1ed8";
-my $url_array_ref = get_output_files ($awe_uri, $id);
-print "Job successfully completed\n";
-
- foreach my $url (@$url_array_ref){
-  print "\t\t$url\n";
- }
-
-    my $url_viz = get_url_visualization($awe_uri, $id);
-      print "\t\t$url_viz\n";
-"""
 def main():
+    """Create a narrative for co-expression network workflow
+
+    1. User uploads multiple files to shock and remembers shock node IDs
+    2. An AWE job script is created
+    3. Job is submitted to awe service
+    4. Node ids of output files are provided
+    """
     _log.info("Uploading files to shock")
     shock_ids = {}
     for file_type, file_name in files.iteritems():
@@ -347,29 +165,27 @@ def main():
         shock_ids[file_type] = upload_file(URLS.shock, file_name, file_meta)
     _log.debug("Uploaded to shock. ids = {}".format(','.join(shock_ids.values())))
 
+    # Read & substitute values into job spec
     awe_job = json.load(open("awe_job.json"))
-    final_tasks = []
     subst = shock_ids.copy()
-    subst['shock_uri'] = URLS.shock
-    subst['session_id'] = sessionID
-    for task in awe_job["tasks"]:
-        for key1 in ('inputs', 'outputs'):
-            for key2 in task[key1]:
-                for key3 in task[key1][key2]:
-                    task[key1][key2][key3] = task[key1][key2][key3].format(**subst)
-        final_tasks.append(task)
-    awe_job['tasks'] = final_tasks
-    pprint.pprint(awe_job)
-    job_id = submit_awe_job(URLS.awe, awe_job)
+    subst.update(coex_args)
+    subst.update(dict(shock_uri=URLS.shock, session_id=sessionID))
+    awe_job_str = Template(json.dumps(awe_job)).substitute(subst)
 
+    # Submit job
+    job_id = submit_awe_job(URLS.awe, awe_job_str)
+
+    # Wait for job to complete
     _log.info("job.begin")
     while 1:
         time.sleep(5)
         count = check_job_status(URLS.awe, job_id)
         if count == 0:
             break
-        _log.debug("job tasks remaining: {:d}".format(count))
+        _log.debug("job.run tasks_remaining={:d}".format(count))
     _log.info("job.end")
+
+    #XXX: Never get past this point
 
     print("\n##############################\nDownload and visualize network output\n")
 

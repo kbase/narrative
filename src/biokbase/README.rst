2ca2b1e811b368f07b2bba998e8567f4777fef1b
Dan Gunter <dkgunter@lbl.gov>
2013-09-20 13:27:49 -0700

    reorg and setup.py added for installation of biokbase python code

---
+++ src/biokbase/README.rst
@@ -0,0 +1,15 @@
+# Description
+
+This Python package contains all the KBase Python libraries to support the Python narrative UI, which is built on the IPython notebook.
+
+# Install
+
+Installation uses setuptools, the standard Python mechanism.
+
+    python setup.py install
+
+
+# Running
+
+Demos are in biokbase/narrative/demo/
+
---
+++ src/biokbase/setup.py
@@ -0,0 +1,44 @@
+import os
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+with open("README.rst") as f:
+    long_desc_lines = []
+    for line in f:
+        if line.strip().startswith('#'):
+            for line2 in f:
+                if line2.strip().startswith('#'):
+                    break
+                long_desc_lines.append(line2.strip())
+            break
+    long_desc = ' '.join(long_desc_lines)
+
+setup(
+    name="biokbase",
+    packages=find_packages(exclude=["biokbase.*Service", "biokbase.Genotype_PhenotypeAPI"]),
+    version="0.0.1",
+    install_requires=["requests>=1.0"],
+    extras_require={},
+    package_data={"": ["*.json"]},
+    author="Steve Chan, Dan Gunter",
+    author_email="sychan@lbl.gov, dkgunter@lbl.gov",
+    maintainer="Steve Chan, Dan Gunter",
+    url="https://kbase.us/",
+    license="Other",
+    description="Source code for IPython/Python KBase narrative UI.",
+    long_description=long_desc,
+    keywords=["kbase", "narrative", "UI"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    ext_modules=[],
+    #scripts=[os.path.join("scripts", f) for f in os.listdir("scripts")]
+)

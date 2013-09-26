ca9a3d7288c00e60b3e8078edc835471ced87f68
Shiran Pasternak <shiranpasternak@gmail.com>
2013-09-26 17:07:15 -0400

    Added skeletal network widget.

---
+++ src/widgets/networks/force-directed.js
@@ -0,0 +1,30 @@
+/**
+ * @class ForceDirectedNetwork
+ *
+ * Implements a force-directed network widget
+ * 
+ * @extends KBWidget
+ *
+ * And here's an example:
+ *
+ *     @example
+ *     var widget = $.ForceDirectedNetwork({
+ *         workspaceID: "workspace.1",
+ *     });
+ */
+(function ($) {
+    $.KBWidget({
+        name: "ForceDirectedNetwork",
+        version: "0.1.0",
+        options: {},
+        init: function (options) {
+            this._super(options);
+            this.render();
+            return this;
+        },
+        render: function () {
+            this.$elem.append($("<h1>").text("Hello World"));
+            return this;
+        }
+    });
+})(jQuery);

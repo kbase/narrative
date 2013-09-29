5eaa5b6d693937088eb3a3e691713b1a7044efc7
Steve Chan <sychan@lbl.gov>
2013-09-28 22:08:51 -0700

    Modified the names a little to make them more reasonable, also added in a new method to fetch a workspace object based on a workspace id

--- src/biokbase/narrative/ws_util.py
+++ src/biokbase/narrative/ws_util.py
@@ -13,7 +13,7 @@ from biokbase.workspaceService.Client import workspaceService
 
 # regex for parsing out workspace_id and object_id from
 # a "kb|ws.{workspace}.{object}" string
-ws_regex = re.compile( '^kb\|ws\.(\w+).(\w+)')
+ws_regex = re.compile( '^kb\|ws\.(\w+)\.(\w+)')
 
 # Exception for a malformed workspace ID see regex above
 class BadWorkspaceID( Exception ):
@@ -25,13 +25,17 @@ list_ws_obj_fields = ['id','type','moddate','instance','command',
                       'metadata']
 
 
-def get_wsobj_meta( wsclient, token, wstype="Narrative", perm=None ):
+def get_wsobj_meta( wsclient, token, objtype="Narrative", perm=None, ws=None ):
     """
     Takes an initialized workspace client and a token. Defaults to searching for
     Narrative types in any workspace that the token has at least read access to.
+
     If the perm field is specified then the workspaces will be filtered by that
     permission value - if perm='a' then search all workspaces where the token
     provides administrative access
+
+    If the ws field is specified then it will return the workspace metadata
+    for only the workspace specified
 
     Returns a dictionary of object descriptions - the key is a workspace id of
     the form "kb|ws.{workspace_id}.{object_id}" and the values are dictionaries
@@ -42,21 +46,24 @@ def get_wsobj_meta( wsclient, token, wstype="Narrative", perm=None ):
     # but the slowdown is mostly due to network latency for all the
     # requests that have to go over the wire. Try to be space efficient
     # by using generators and simple for loops
-    wslist1 = wsclient.list_workspaces( { 'auth' : token })
-    if perm is None:
-        wslist2 = (w[0] for w in wslist1 if w[4] != u'n')
+    if ws is None:
+        wslist1 = wsclient.list_workspaces( { 'auth' : token })
+        if perm is None:
+            wslist2 = (w[0] for w in wslist1 if w[4] != u'n')
+        else:
+            wslist2 = (w[0] for w in wslist1 if w[4] == perm)
     else:
-        wslist2 = (w[0] for w in wslist1 if w[4] == perm)
+        wslist2 = [ws]
     my_narratives = {}
     list_ws_obj = wsclient.list_workspace_objects
     fields = list_ws_obj_fields
     for w in wslist2:
-        res = list_ws_obj({ 'auth' : token, 'type' : wstype, 'workspace' : w})
+        res = list_ws_obj({ 'auth' : token, 'type' : objtype, 'workspace' : w})
         for obj in res:
             my_narratives["kb|ws.%s.%s" % (w,obj[0])] = dict(zip(fields,obj))
     return my_narratives
 
-def get_wsob( wsclient, token, ws_id ):
+def get_wsobj( wsclient, token, ws_id, objtype=None):
     """
     This is just a wrapper for the workspace get_object call.
 
@@ -64,11 +71,21 @@ def get_wsob( wsclient, token, ws_id ):
     of the form "kb|ws.{ws_id}.{object id}" and returns the following:
     { 'data' : {actual data contained in the object},
       'metadata' : { a dictionary version of the object metadata }}
+
+    if type is not specified then an extra lookup for object metadata
+    is required, this can be shortcut by passing in the object type
     """
     match = ws_regex.match( ws_id)
     if not match:
         raise BadWorkspaceID( "%s does not match workspace ID format kb|ws.{workspace}.{object}" % ws_id)
-    ws = match.group(0)
-    objid = match.group(1)
-
-    return
+    ws = match.group(1)
+    objid = match.group(2)
+    if objtype is None:
+        obj_meta = get_wsobj_meta( wsclient, token, ws=ws )
+        if ws_id in obj_meta:
+            objtype = obj_meta[ws_id]['type']
+        else:
+            raise BadWorkspaceID( "Could not find object named %s in workspace %s" % (objid,ws))
+    res = wsclient.get_object( dict( auth=token, workspace=ws, id=objid, type=objtype))
+    res['metadata'] = dict(zip(list_ws_obj_fields, res['metadata']))
+    return res

5909d50888cecdc1ed12b1a9d3113dddf2ef11ad
Shiran Pasternak <shiranpasternak@gmail.com>
2013-10-02 21:54:24 -0400

    Now using Workspace Service.

--- src/widgets/networks/force-directed.js
+++ src/widgets/networks/force-directed.js
@@ -2,7 +2,7 @@
  * @class ForceDirectedNetwork
  *
  * Implements a force-directed network widget
- * 
+ *
  * @extends KBWidget
  *
  * And here's an example:
@@ -12,52 +12,66 @@
  *         workspaceID: "workspace.1",
  *     });
  */
-(function ($) {
+(function($) {
     var URL_ROOT = "http://140.221.84.142/objects/coexpr_test/Networks";
+    var WS_URL   = "http://kbase.us/services/workspace_service/";
     $.KBWidget({
         name: "ForceDirectedNetwork",
         version: "0.1.0",
         options: {},
-        init: function (options) {
+        init: function(options) {
             this._super(options);
             this.render();
-/*
-            requirejs.config({
-                baseUrl: "../js-dev/datavis",
-                shim: {
-                    jquery:      { exports: ["$", "jQuery"] },
-                    d3:          { exports: "d3"          },
-                    underscore:  { exports: "_"           },
-                },
-            });
-*/
-            
             return this;
         },
         render: function () {
             var self = this;
-            $.ajax({
-                dataType: "json",
-                url: URL_ROOT + "/" +
-                    encodeURIComponent(this.options.workspaceID) + ".json"
-            }).done(function (result) {
+            var fetchAjax = function () { return 1; };
+            if (this.options.token) {
+                var wsRegex = /^(\w+)\.(.+)/;
+                var wsid = wsRegex.exec(this.options.workspaceID);
+                console.log(wsid);
+                if (wsid[1] && wsid[2]) {
+                    var kbws = new workspaceService(WS_URL);
+                    fetchAjax = kbws.get_object({
+                        auth: this.options.token,
+                        workspace: wsid[1],
+                        id: wsid[2],
+                        type: 'Networks'
+                    });
+                }
+            } else {
+                fetchAjax = $.ajax({
+                    dataType: "json",
+                    url: URL_ROOT + "/" +
+                        encodeURIComponent(this.options.workspaceID) + ".json"
+                });
+            }
+            $.when(fetchAjax).done(function (result) {
                 var data = transformNetwork(result.data);
-                datavis.require(["renderers/network"], function (Network) {
+                datavis.require([
+                    "renderers/network"
+                ], function(Network) {
                     var network = new Network({
                         element: self.$elem,
                         dock: false,
-                        nodeLabel: { type: "GENE" },
+                        nodeLabel: {
+                            type: "GENE"
+                        }
                     });
                     network.setData(data);
                     network.render();
                 });
-            })
+            });
             return self;
         }
     });
 
     function transformNetwork(networkJson) {
-        var json = { nodes: [], edges: [] };
+        var json = {
+            nodes: [],
+            edges: []
+        };
         var nodeMap = {};
         for (var i = 0; i < networkJson.nodes.length; i++) {
             var node = $.extend({}, networkJson.nodes[i]);
@@ -82,4 +96,3 @@
         return json;
     }
 })(jQuery);
-

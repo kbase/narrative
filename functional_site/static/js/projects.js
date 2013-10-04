d67c67ea9d5bc96cac8988e9dc28192707e94253
Steve Chan <sychan@lbl.gov>
2013-10-04 01:27:04 -0700

    Started making some of the project wrapper functions functional. Can search for all project workspaces (slow as frozen shit) and create new project workspaces.

--- functional_site/static/js/projects.js
+++ functional_site/static/js/projects.js
@@ -25,14 +25,32 @@
     project.ws_url = "https://kbase.us/services/workspace";
     project.ws_client = new workspaceService(project.ws_url);
 
+    var legit_ws_id = /^\w+$/;
+
     // Fields in a workspace metadata object
     project.ws_meta_fields = ['id','owner','moddate','objects',
 		      'user_permission','global_permission'];
+
+    // function used in reduce() to map ws_meta array into a dict
+    var ws_meta_dict = function (ws_meta){
+	return ws_meta.reduce ( function( prev, curr, index){
+				    prev[project.ws_meta_fields[index]] = curr;
+				    return(prev);
+				},{});
+    };
 
     // Fields in an workspace object metadata object
     project.obj_meta_fields = ['id','type','moddate','instance','command',
 		       'lastmodifier','owner','workspace','ref','chsum',
 		       'metadata'];
+
+    // function to map obj_meta array into a dict
+    var obj_meta_dict = function (obj_meta) {
+	return obj_meta.reduce( function( prev, curr, index){
+				    prev[project.obj_meta_fields[index]] = curr;
+				    return(prev);
+				},{});
+    };
 
     // Fields in an empty narrative
     var empty_narrative = {
@@ -55,9 +73,22 @@
     // project workspaces from other containers. Using this in case we
     // decide to include other types of workspaces
     
-    var ws_tag = {
+    project.ws_tag = {
 	project : '_project'
     };
+
+    project.ws_tag_type = 'workspace_meta';
+
+    // Empty project tag template
+    var empty_proj_tag = {
+	id : project.ws_tag.project,
+	type : project.ws_tag_type,
+	data : { description : 'Tag! You\'re a project!' },
+	workspace : undefined,
+	metadata : {},
+	auth : undefined
+    };
+
 
     // id of the div containing the kbase login widget to query for auth
     // info. Set this to a different value if the div has been named differently.
@@ -72,12 +103,45 @@
     var filter_wsobj = function (p_in) {
 	var def_params = { callback : undefined,
 			   perms : ['a'],
-			   workspace_id : undefined,
-			   filter_tag : ws_tag.project };
+			   filter_tag : project.ws_tag.project };
 	var p = $.extend( def_params, p_in);
-	var auth = $(project.auth_div).kbaseLogin('get_kbase_cookie');
 
-        	
+	var ignore = /^core/;
+	var token = $(project.auth_div).kbaseLogin('get_kbase_cookie').token;
+
+	// ignore any workspace with the name prefix of "core"
+	// and ignore workspace that doesn't match perms
+	var reduce_ws_meta = function ( prev, curr) {
+	    if ( ignore.test(curr[0])) {
+		return( prev);
+	    }
+	    if ( p.perms.indexOf(curr[4]) >= 0 ) {
+		return( prev.concat([curr]));
+	    } else {
+		return( prev);
+	    }
+	};
+	var ws_match = p.res.reduce(reduce_ws_meta,[]);
+	var ws_ids = ws_match.map( function(v) { return v[0]});
+	var ws_obj_fn = ws_ids.map( function( wsid) {
+					return project.ws_client.has_object(
+					    { auth: token,
+					      workspace: wsid,
+					      id : project.ws_tag.project,
+					      type : project.ws_tag_type });
+				    });
+	$.when.apply(null, ws_obj_fn).done( function() {
+						var results = [].slice.call(arguments);
+						var reduce_ws_proj = function(prev,curr,index){
+						    if (curr) {
+							// convert array into dict and then bind to ws_id
+							prev[ ws_match[index][0]] = ws_meta_dict(ws_match[index]);
+						    } 
+						    return( prev);
+						};
+						var proj_list = results.reduce( reduce_ws_proj,{});
+						p.callback( proj_list );
+					    });
     };
 
     // Get all the workspaces that match the values of the
@@ -92,19 +156,45 @@
 			   perms : ['a'],
 			   workspace_id : undefined};
 	var p = $.extend( def_params, p_in);
-	var auth = $(project.auth_div).kbaseLogin('get_kbase_cookie');
+	var token = $(project.auth_div).kbaseLogin('get_kbase_cookie').token;
+	var META_ws;
+	if ( p.workspace_id ) {
+	    META_ws = project.ws_client.get_workspacemeta( { auth : token,
+							      workspace : p.workspace_id } );
+	    $.when( META_ws).done( function(result) {
+				       filter_wsobj( { res: [result],
+						       callback : p.callback,
+						       perms: p.perms });
+				   });
+	} else {
+	    META_ws = project.ws_client.list_workspaces( { auth : token} );
+	    $.when( META_ws).done( function(result) {
+				       filter_wsobj( { res: result,
+						       callback : p.callback,
+						       perms: p.perms });
+				   });
+	}
     };
 
 
-    // Get the objects in the specified workspace id and return a
+    // Get the object metadata for the specified workspace id and return a
     // dictionary keyed on "objectId" where the
     // value is a dictionary keyed on obj_meta_fields
     project.get_project = function( p_in ) {
 	var def_params = { callback : undefined,
 			   workspace_id : undefined};
 	var p = $.extend( def_params, p_in);
-	var auth = $(project.auth_div).kbaseLogin('get_kbase_cookie');
-	
+	var token = $(project.auth_div).kbaseLogin('get_kbase_cookie').token;
+        var ws_meta = project.ws_client.list_workspace_objects( { auth: token,
+								  workspace: p.workspace_id});
+	$.when( ws_meta).done( function (results) {
+				   var res = {};
+				   $.each( results, function (index, val) {
+					       var obj_meta = obj_meta_dict( val);
+					       res[val[0]] = obj_meta;
+					   });
+				   p.callback( res)
+			       });
     };
 
     // Get the individual workspace object named. Takes names
@@ -133,6 +223,55 @@
 	var p = $.extend( def_params, p_in);
 	var auth = $(project.auth_div).kbaseLogin('get_kbase_cookie');
 	
+    };
+
+    // Create an new workspace and tag it with a project tag. The name
+    // *must" conform to the future workspace rules about object naming,
+    // the it can only contain [a-zA-Z0-9_]. We show the error prompt
+    // if it fails to conform
+
+    project.new_project = function( p_in ) {
+	var def_params = { callback : undefined,
+			   project_id : undefined,
+			   def_perm : 'n'
+			 };
+	var p = $.extend( def_params, p_in);
+	var token = $(project.auth_div).kbaseLogin('get_kbase_cookie').token;
+
+        if ( legit_ws_id.test(p.project_id)) {
+	    // create workspace and populate with _project object
+	    var ws_fn = project.ws_client.create_workspace( {
+							    auth: token,
+							    workspace : p.project_id,
+							    default_permission : p.def_perm
+							    });
+	    $.when( ws_fn).done( function(ws_meta) {
+				     var proj = empty_proj_tag;
+				     proj.auth = token;
+				     proj.workspace = p.project_id;
+				     var ws_fn2 = project.ws_client.save_object( proj);
+				     $.when( ws_fn2 ).done( function(obj_meta) {
+								p.callback( obj_meta_dict(obj_meta));
+							    });
+				 });
+	} else {
+	    console.log( "Bad project id: ",p.project_id);
+	}
+    };
+
+    // Delete a workspace(project)
+    project.delete_project = function( p_in ) {
+	var def_params = { callback : undefined,
+			   project_id : undefined
+			 };
+	var p = $.extend( def_params, p_in);
+	var auth = $(project.auth_div).kbaseLogin('get_kbase_cookie');
+
+        if ( legit_ws_id.test(p.project_id)) {
+	    // create workspace and populate with _project object
+	} else {
+	    console.log( "Bad project id: ",p.project_id);
+	}
     };
 
     // Create an empty narrative object with the specified name. The name

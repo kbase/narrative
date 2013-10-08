14672ddd0bdacc995faaf63ddaa896386e5baf00
Shiran Pasternak <shiranpasternak@gmail.com>
2013-10-08 02:31:54 -0400

    Added UI components to network widget. Still needs work.

--- src/widgets/networks/force-directed.js
+++ src/widgets/networks/force-directed.js
@@ -34,7 +34,8 @@
         name: "ForceDirectedNetwork",
         version: "0.1.0",
         options: {
-            minHeight: "300px"
+            minHeight: "300px",
+            minStrength: 0.7
         },
         init: function(options) {
             this._super(options);
@@ -43,61 +44,139 @@
         },
         render: function () {
             var self = this;
-            var fetchAjax = function () { return 1; };
-            if (self.options.minHeight) {
-                self.$elem.css("min-height", self.options.minHeight);
-            }
-            if (self.options.token) {
-                var wsRegex = /^(\w+)\.(.+)/;
-                var wsid = wsRegex.exec(self.options.workspaceID);
-                if (wsid !== null && wsid[1] && wsid[2]) {
-                    var kbws = new workspaceService(WS_URL);
-                    fetchAjax = kbws.get_object({
-                        auth: self.options.token,
-                        workspace: wsid[1],
-                        id: wsid[2],
-                        type: 'Networks'
-                    });
-                } else {
-                    self.trigger("error", ["Cannot parse workspace ID " +
-                        self.options.workspaceID ]);
-                    return self;
-                }
-            } else {
-                fetchAjax = $.ajax({
-                    dataType: "json",
-                    url: URL_ROOT + "/" +
-                        encodeURIComponent(self.options.workspaceID) + ".json"
+            KBVis.require(["renderers/network", "util/viewport", "underscore"],
+            function (Network, Viewport, _) {
+                self.minStrength = self.options.minStrength;
+                var viewport = new Viewport({
+                    parent: self.$elem,
+                    title: "Network",
+                    maximize: true
                 });
-            }
-            $.when(fetchAjax).done(function (result) {
-                var data = transformNetwork(result.data);
-                KBVis.require(["renderers/network", "util/viewport"],
-                function (Network, Viewport) {
-                    var viewport = new Viewport({
-                        parent: self.$elem,
-                        title: "Network",
-                        maximize: true
+                viewport.css("min-height", "700px");
+                var toolbox = viewport.toolbox();
+                viewport.addTool($("<a/>", { href: "#" }).html("Click me!"));
+                addSlider(toolbox, self);
+                addSearch(toolbox);
+                
+                var fetchAjax = function () { return 1; };
+                if (self.options.minHeight) {
+                    self.$elem.css("min-height", self.options.minHeight);
+                }
+                if (self.options.token) {
+                    var wsRegex = /^(\w+)\.(.+)/;
+                    var wsid = wsRegex.exec(self.options.workspaceID);
+                    if (wsid !== null && wsid[1] && wsid[2]) {
+                        var kbws = new workspaceService(WS_URL);
+                        fetchAjax = kbws.get_object({
+                            auth: self.options.token,
+                            workspace: wsid[1],
+                            id: wsid[2],
+                            type: 'Networks'
+                        });
+                    } else {
+                        self.trigger("error", ["Cannot parse workspace ID " +
+                            self.options.workspaceID ]);
+                        return self;
+                    }
+                } else {
+                    fetchAjax = $.ajax({
+                        dataType: "json",
+                        url: URL_ROOT + "/" +
+                            encodeURIComponent(self.options.workspaceID) + ".json"
                     });
-                    viewport.css("min-height", "700px");
+                }
+                $.when(fetchAjax).done(function (result) {
+                    var data = transformNetwork(result.data);
+                    var minStrength = 0.7;
                     var network = new Network({
                         element: viewport,
                         dock: false,
-                        nodeLabel: { type: "GENE" }
+                        nodeLabel: { type: "GENE" },
+                        infoOn: "hover",
+                        edgeFilter: function (edge) {
+                            return edge.source != edge.target &&
+                            edge.strength >= minStrength;
+                        }
                     });
+                    self.network = network;
                     network.setData(data);
                     network.render();
+
+                    addDatasetDropdown(toolbox, data);
                 });
             });
             return self;
         }
     });
 
+    function addSlider($container, widget) {
+        var tipTitle = "Minimum edge strength: ";
+        var wrapper = $("<div/>", {
+            id: "strength-slider",
+            class: "btn btn-default tool"
+        });
+        var slider = $("<div/>", { style: "min-width:70px" });
+        wrapper
+            .append($("<div/>", { class: "btn-pad" })
+                .append($("<i/>", { class: "icon-adjust" })))
+            .append($("<div/>", { class: "btn-pad" })
+                .append(slider));
+        $container.prepend(wrapper);
+        $("#strength-slider").tooltip({
+           title: tipTitle + widget.minStrength.toFixed(2),
+           placement: "bottom"
+        });
+        slider.slider({
+            min: 0, max: 1, step: 0.05, value: 0.8,
+            slide: function (event, ui) {
+                widget.minStrength = ui.value;
+                widget.network.update();
+                $("#strength-slider").next().find(".tooltip-inner")
+                    .text(tipTitle + widget.minStrength.toFixed(2));
+            }
+        });
+    }
+
+    function addSearch($container) {
+        var wrapper = $("<div/>", { class: "btn btn-default tool" });
+        wrapper
+            .append($("<div/>", { class: "btn-pad" })
+                .append($("<input/>", { type: "text", class: " input-xs" })))
+            .append($("<div/>", { class: "btn-pad" })
+                .append($("<i/>", { class: "icon-search" })));
+        $container.prepend(wrapper);
+    }
+
+    function addDatasetDropdown($container, data) {
+        var wrapper = $("<div/>", { class: "btn-group tool" });
+        var list = $("<ul/>", { class: "dropdown-menu", role: "menu" });
+        _.each(data.datasets, function (ds) {
+            var dsStr = ds.id.replace(/^kb.*\.ws\/\//, "");
+            list.append($("<li/>")
+                .append($("<a/>", {
+                    href: "#",
+                    "data-toggle": "tooltip",
+                    "data-container": "body",
+                    "title": ds.description,
+                    "data-original-title": ds.description
+                }).html(dsStr)));
+        })
+        wrapper
+            .append($("<div/>", {
+                class: "btn btn-default btn-sm dropdown-toggle",
+                "data-toggle": "dropdown"
+            }).text("Data Set ").append($("<span/>", { class: "caret"})))
+            .append(list);
+        $container.prepend(wrapper);
+    }
+
+
     function transformNetwork(networkJson) {
-        var json = {
-            nodes: [],
-            edges: []
-        };
+        var json = {};
+        for (var property in networkJson) {
+            json[property] = networkJson[property];
+        }
+        json.nodes = []; json.edges = [];
         var nodeMap = {};
         for (var i = 0; i < networkJson.nodes.length; i++) {
             var node = $.extend({}, networkJson.nodes[i]);

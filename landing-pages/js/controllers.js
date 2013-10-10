7005646e988476972cf06a8b0da87cb429179da5
Neal Conrad <nealconrad@gmail.com>
2013-10-10 15:58:07 -0500

    moving card directives to card-directives

--- landing-pages/js/controllers.js
+++ landing-pages/js/controllers.js
@@ -87,3 +87,16 @@ function CpdDetail($scope, $routeParams, $location) {
     $scope.ids = $routeParams.ids.split('&');
 }
 
+
+function ScrollCtrl($scope, $location, $anchorScroll) {
+  $scope.gotoBottom = function (){
+    // set the location.hash to the id of
+    // the element you wish to scroll to.
+    $location.hash('bottom');
+    
+    // call $anchorScroll()
+    $anchorScroll();
+  }
+}
+
+
--- landing-pages/js/directives.js
+++ landing-pages/js/directives.js
@@ -56,36 +56,6 @@ angular.module('lp-directives')
             
         };
     })
-    .directive('genomecards', function($rootScope) {
-        return {
-            link: function(scope, element, attrs) {
-                $(element).KBaseCardLayoutManager({template: "genome", 
-                                                   data: scope.params, 
-                                                   auth: $rootScope.USER_TOKEN,
-                                                   userId: $rootScope.USER_ID});
-            }
-        };
-    })
-    .directive('genecards', function($rootScope) {
-        return {
-            link: function(scope, element, attrs) {
-                $(element).KBaseCardLayoutManager({template: "gene", 
-                                                   data: scope.params, 
-                                                   auth: $rootScope.USER_TOKEN,
-                                                   userId: $rootScope.USER_ID});
-            }
-        };
-    })
-    .directive('memecards', function($rootScope) {
-        return {
-            link: function(scope, element, attrs) {
-                $(element).KBaseCardLayoutManager({template: "meme", 
-                                                   data: scope.params, 
-                                                   auth: $rootScope.USER_TOKEN,
-                                                   userId: $rootScope.USER_ID});
-            }
-        };
-    })
     .directive('modelmeta', function() {
         return {
             link: function(scope, element, attrs) {
@@ -141,100 +111,6 @@ angular.module('lp-directives')
             }
         };
     })
-    .directive('modelcards', function($rootScope) {
-        return {
-            link: function(scope, element, attrs) {
-                var prom = wsGet('objectMeta', 'Model', scope.ws, scope.id);
-                $.when(prom).done(function(data){
-                    $(element).KBaseCardLayoutManager().addNewCard("kbaseModelMeta", 
-                        { title: 'Model Info',
-                          data: data,
-                          id: scope.id,
-                          ws: scope.ws },
-                        { my: "left top+50",
-                          at: "left bottom",
-                          of: "#app"
-                    });
-                });
-
-                var prom = fbaGet('Model', scope.ws, scope.id);
-                $.when(prom).done(function(data) {
-                    $(element).KBaseCardLayoutManager().addNewCard("kbaseModelTabs", 
-                        { modelsData: data,
-                          title: 'Model Details',
-                          id: scope.id,
-                          ws: scope.ws,
-                          width: 700 },
-                        { my: "left+400 top+50",
-                          at: "left bottom",
-                          of: "#app"
-                    });
-                    $(element).KBaseCardLayoutManager().addNewCard("kbaseModelCore", 
-                        { title: 'Central Carbon Core Metabolic Pathway',
-                          modelsData: data,
-                          ids: [scope.id],
-                          workspaces: [scope.ws],
-                          width: 900 },
-                        { my: "left+800 top+600",
-                          at: "left bottom",
-                          of: "#app"
-                    });                    
-                });
-
-            }
-        }
- 
-    })
-    .directive('fbacards', function($rootScope) {
-        return {
-            link: function(scope, element, attrs) {
-                var prom = wsGet('objectMeta', 'FBA', scope.ws, scope.id);
-                $.when(prom).done(function(data){
-                    $(element).KBaseCardLayoutManager().addNewCard("kbaseFbaMeta", 
-                        { title: 'Model Info',
-                          data: data,
-                          id: scope.id,
-                          ws: scope.ws },
-                        { my: "left top+50",
-                          at: "left bottom",
-                          of: "#app"
-                    });
-                });
-
-                var prom = fbaGet('FBA', scope.ws, scope.id);
-                $.when(prom).done(function(fbas_data) {
-                    $(element).KBaseCardLayoutManager().addNewCard("kbaseFbaTabs", 
-                        { title: 'FBA Details',
-                          fbaData: fbas_data,
-                          id: scope.id,
-                          ws: scope.ws,
-                          width: 700 },
-                        { my: "left+400 top+50",
-                          at: "left bottom",
-                          of: "#app"
-                    });
-                    var model_ws = fbas_data[0].model_workspace;
-                    var model_id = fbas_data[0].model;
-
-                    var prom2 = fbaGet('Model', model_ws, model_id);
-                    $.when(prom2).done(function(models_data){
-                        $(element).KBaseCardLayoutManager().addNewCard("kbaseModelCore", 
-                            { title: 'Central Carbon Core Metabolic Pathway',
-                              modelsData: models_data,
-                              fbasData: fbas_data,
-                              ids: [scope.id],
-                              workspaces: [scope.ws],
-                              width: 900 },
-                            { my: "left+800 top+600",
-                              at: "left bottom",
-                              of: "#app"
-                        });
-                    });
-                });
-            }
-        }
-    })
-
     .directive('fbameta', function() {
         return {
             link: function(scope, element, attrs) {

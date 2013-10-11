43e39c95d80d889189f6a46f49a30710d14a9c8c
Dan Gunter <dkgunter@lbl.gov>
2013-10-11 00:40:35 -0700

    wrap console logging more safely with this utility function

---
+++ functional_site/static/js/kbaseLogging.js
@@ -0,0 +1,23 @@
+/**
+ * Absolute minimal support for logging.
+ *
+ * Author: Dan Gunter <dkgunter@lbl.gov>
+ * Date: 14 September 2013
+ */
+
+/*jshint sub:true*/
+/*global window:true,define:true, module:true*/
+(function(window) {
+    "use strict";
+    /* Make console logging a no-op where not defined */
+	if (typeof console === 'undefined' || !console.log) {
+	  window.console = {
+	    debug: function() {},
+	    trace: function() {},
+	    log: function() {},
+	    info: function() {},
+	    warn: function() {},
+	    error: function() {}
+	  };
+	}
+})(window);

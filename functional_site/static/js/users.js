84ab89c6ba729a8f63fedf077dfd3a5905a0e6f7
Steve Chan <sychan@lbl.gov>
2013-10-11 14:00:46 -0700

    Added code to set kbase_session cookie with login widget

--- functional_site/static/js/users.js
+++ functional_site/static/js/users.js
@@ -1,13 +1,26 @@
-/*
- * Create a users object
- * Uses namespacing technique from here:
- * http://enterprisejquery.com/2010/10/how-good-c-habits-can-encourage-bad-javascript-habits-part-1/
- * 
- */
 (function($, undefined) {
     var workspaceURL = "https://kbase.us/services/workspace";
     var workspaceClient = new workspaceService(workspaceURL);
     notLoggedIn();
+
+    // Function that sets a cookie compatible with the current narrative
+    // (it expects to find user_id and token in the cookie)
+    var set_cookie = function() {
+        var c = $("#login-widget").kbaseLogin('get_kbase_cookie');
+        console.log( 'Setting kbase_session cookie');
+        $.cookie('kbase_session',
+                 'un=' + c.user_id
+                 + '|'
+                 + 'kbase_sessionid=' + c.kbase_sessionid
+                 + '|'
+                 + 'user_id=' + c.user_id
+                 + '|'
+                 + 'token=' + c.token.replace(/=/g, 'EQUALSSIGN').replace(/\|/g,'PIPESIGN'),
+                 { path: '/',
+                   domain: 'kbase.us' });
+    };
+
+
 
 
     $(function() {
@@ -27,16 +40,17 @@
             rePrompt: false,
 
             login_callback: function(args) {
-            	alert("callback call");
+		set_cookie();
                 loadPage();
             },
 
             logout_callback: function(args) {
+		$.removeCookie( 'kbase_session');
             	notLoggedIn();
-
             },
 
             prior_login_callback: function(args) {
+		set_cookie();
                 loadPage();
             },
         });
@@ -55,15 +69,16 @@
         			console.log(args);
         			if (args.success === 1) {
         				
-        				this.registerLogin(args);
-                        loadPage();
-	        			doneLoading();
-    	    			$("#login-widget").show();
-    	    		} else {
-    	    			$("#loading-indicator").hide();
-						$("#login_error").html(args.message);
-						$("#login_error").show();
-    	    		}
+        			    this.registerLogin(args);
+				    set_cookie();
+				    loadPage();
+	        		    doneLoading();
+    	    			    $("#login-widget").show();
+    	    			} else {
+    	    			    $("#loading-indicator").hide();
+				    $("#login_error").html(args.message);
+				    $("#login_error").show();
+    	    			}
         		}
         	);
         });
@@ -78,7 +93,7 @@
     });
 
     function notLoggedIn() {
-    	console.debug("Not logged in");
+    	console.log("got here");
     	$("#header_banner").hide();
     	$("#alt_banner").show();
      	$("#login-widget").hide();
@@ -105,13 +120,8 @@
         var userId = $("#login-widget").kbaseLogin("get_kbase_cookie", "user_id");
         var userName = $("#login-widget").kbaseLogin("get_kbase_cookie", "name");
 
-        if (!userName) {
-            console.warn("No user name found");
+        if (!userName)
             userName = "KBase User";
-        }
-        else {
-            console.debug("user name = " + userName);
-        }
         $("#kb_name").html(userName);
 
         loadProjectFeed(token, userId);
@@ -121,14 +131,6 @@
     };
 
 
-    /** Get current user id */
-    function _user_id() {
-        return $("#login-widget").kbaseLogin("get_kbase_cookie", "user_id");
-    }
-    /** Get current token */
-    function _token() {
-        return $("#login-widget").kbaseLogin("token");
-    }
 
     function clientError(error) {
         console.debug(error);
@@ -143,7 +145,8 @@
 
 		//get auth token
 		if (token !== "null") {
-			wsClient.list_workspaces({ auth: token }, 
+
+			wsClient.list_workspaces_async({ auth: token }, 
 					function(results) {
 						var data = { rows: []};
 						var count = 0;
@@ -153,16 +156,18 @@
 						    return ws[2];
 						});
 						results.reverse();
-
+						
 						//populate the data structure for the template
 						_.every(results, function(workspace){
 
 							if (userId != workspace[1]) {
                                 var moddate = workspace[2];
                                 moddate = moddate.replace(/T/g," ");
-                                var row = {username: workspace[1], 
-                                    workspace: workspace[0], date: moddate};
-								data.rows.push(row);
+								data.rows.push({
+									"username": workspace[1],
+									"workspace": workspace[0],
+									"date": moddate
+								});		
 								count++;
 							}
 							return count !== 10;
@@ -183,135 +188,59 @@
 		}	
 	}
 
-    /** Make a workspace array into a mapping */
-    function _ws_arr2obj(arr) {
-        var obj = {}, fld = project.obj_meta_fields;
-        for (i=0; i < fld.length; i++) {
-            obj[fld[i]] = arr[i];
-        }
-        return obj;
-    }
-
-    /**
-     * Get all objects in home workspace that satisfy 'params'.
-     *
-     * @return mapping, keyed by object id.
-     */
-    function getHomeWorkspaceObjects(params, callback) {
-        var home = _user_id() + '_home';
-        $.extend(params, {auth: _token(), workspace: home});
-        //console.debug("list_workspace_objects, params=", params);
-        project.ws_client.list_workspace_objects(params,
-            function(obj_list) {
-                var result = {};
-                //console.debug("Obj list=",obj_list);
-                $.each(obj_list, function(idx, val) {
-                    //console.debug("Home workspace narrative at " + idx + ":", val);
-                    var obj_meta = _ws_arr2obj(val);
-                    result[obj_meta.id] = obj_meta;
-                });
-                callback(result);
-            },
-            function() {
-                var args = [].slice.call(arguments);
-                console.warn("Error getting home workspace objects. ", args);
-                callback({});
-            }
-        );
-    };
-
-	/** 
-     * Loads data and populates the recent narratives section of the user home page.
-     *
-     * Calls:
-     *    project.get_narratives(), to load narratives in a project
-     *    getHomeWorkspaceObjects(), to add narratives from the user's home ws
-     *    _showRecentNarratives(), when done loading, to show the results
-     */
+	//populates the recent narratives section of the user home page
 	function loadRecentNarratives() {
-        // show user that we are loading
 		$("#no_narratives").hide();
         $("#narratives_loading").show();
-        // get the data & then show it
-        getAllNarratives(
-            _showRecentNarratives,
-            function() {
-                $("#no_narratives").show(); 
-                $("#narratives_loading").hide();
-            }
-        );
-    }
-
-    /**
-     * Get all narratives for this user.
-     *
-     * @param show_results_cb - Called with object containing results.
-     * @param no_results_cb - Called if no results
-     */
-    function getAllNarratives(show_results_cb, no_results_cb) {
         project.get_narratives({
             callback: function(results) {
-                console.debug("Project narratives:", results);
-                // augment project narratives with user's home objects
-                getHomeWorkspaceObjects({type: project.narrative_type},
-                    function(home_narr) {
-                        $.extend(results, home_narr);
-                        console.debug("Project + home narratives:", results);
-                        // show combined results
-                        if (Object.keys(results).length > 0) {
-                            show_results_cb(results);
-                        }
-                        // no data? show that, too
-                        else {
-                            no_results_cb();
-                        }
-                    }
-                );
+                if (Object.keys(results).length > 0) {
+                    var data = { rows: []};
+                    var userId = $("#login-widget").kbaseLogin("get_kbase_cookie", "user_id");
+
+					//first sort
+					results = _.sortBy(results, function(narrative) {
+					    return narrative.moddate;
+					});
+					results.reverse();
+					
+					//populate the data structure for the template
+					var count = 0;
+					_.every(results, function(narrative){
+
+						var name = narrative.id.replace(/_/g," ");
+						//var project_id = narrative.workspace.replace(/_/g," ");
+
+
+						data.rows.push({
+                            "name": name,
+                            "narrative_id": narrative.id,
+                            "project_id": narrative.workspace,
+                            "userId": userId
+                        });
+                        count++;
+						return count !== 5;
+					});
+
+
+
+                    //populate the html template
+                    var rows = ich.recent_narratives(data)
+                    $('#recent_narratives_list').append(rows);
+
+                    $("#narratives_loading").hide();
+
+
+
+                } else {
+                    $("#narratives_loading").hide();
+                    $("#no_narratives").show();
+
+                }
+                
             }
-        });        
-    }
-
-    /**
-     * Update HTML to show >0 recent narratives.
-     *
-     * @pre results is non-empty
-     * @return undefined
-     */
-    function _showRecentNarratives(results) {
-        var userId = _user_id();
-
-		// make a list that can be sorted
-        //console.debug("results:", results);
-        var indexed = [];
-        _.each(Object.keys(results), function(key, i, lst) {
-            indexed.push({'id': results[key].id, 'date': results[key].moddate});
         });
-        //console.debug("indexed:",indexed);
-        // sort the list by the date
-		indexed = _.sortBy(indexed, 'date');
-        indexed.reverse();
-
-        // build data from first <= 5 items in the sorted list
-        var rows = [], n = indexed.length;
-        var limit = _.min([n, 5]);
-        for (var i=0; i < limit; i++) {
-            var narr = results[indexed[i].id];
-            var name = narr.id.replace(/_/g," ");
-            //var project_id = narrative.workspace.replace(/_/g," ");
-            rows.push({
-                "name": name,
-                "narrative_id": narr.id,
-                "project_id": narr.workspace,
-                "userId": userId
-            });
-		}
-
-        //populate the html template
-        var rows2 = ich.recent_narratives({'rows': rows});
-        $('#recent_narratives_list').append(rows2);
-
-        $("#narratives_loading").hide();
-    }
+    };
 
     //populates the recent projects portion of the user home
 	function loadRecentProjects() {
@@ -404,5 +333,5 @@
     });
 
 
-})(jQuery);
+})( jQuery );
 

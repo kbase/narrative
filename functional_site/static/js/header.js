000918502f413422167ee6cb2feb4a90fbbab541
Annette Greiner <amgreiner@lbl.gov>
2013-10-14 15:29:51 -0700

    getting rid of duplicate id for search_terms input

--- functional_site/static/js/header.js
+++ functional_site/static/js/header.js
@@ -1,7 +1,7 @@
 $(function() {
 	$("#search_terms").on("keypress", function (evt) {
         if (evt.keyCode === 13) {
-            var input = $.trim($('#search_terms')[0].value);
+            var input = $.trim($('#searchspan input')[0].value);
             if (input !== null && input !== '') {
                 var einput = encodeURIComponent(input);
 	            var myUrl = "/search.shtml?q=" + einput;

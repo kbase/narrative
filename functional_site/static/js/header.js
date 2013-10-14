b9a107fb9e7a4a73c18e4eeb8c3b2588c0f31f7e
Annette Greiner <amgreiner@lbl.gov>
2013-10-14 16:01:48 -0700

    get the search term from whichever input triggered it

--- functional_site/static/js/header.js
+++ functional_site/static/js/header.js
@@ -1,7 +1,8 @@
 $(function() {
-	$("#search_terms").on("keypress", function (evt) {
+	$("#searchspan input").on("keypress", function (evt) {
         if (evt.keyCode === 13) {
-            var input = $.trim($('#searchspan input')[0].value);
+            var input = $.trim($(this).val());
+            console.log(input);
             if (input !== null && input !== '') {
                 var einput = encodeURIComponent(input);
 	            var myUrl = "/search.shtml?q=" + einput;

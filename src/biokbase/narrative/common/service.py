99a94d9baf1efe20c27e346a47db61f427e313d0
Dan Gunter <dkgunter@lbl.gov>
2013-11-13 06:10:32 -0800

    refactored a bit more to use traitlets, added an example too

--- src/biokbase/narrative/common/service.py
+++ src/biokbase/narrative/common/service.py
@@ -9,16 +9,17 @@ and the run() method to perform the desired work.
 
 Sample usage::
 
-    from biokbase.common.service import IpService
+    from biokbase.common.service import Service
 
-    class MyService(IpService):
+    class TaxiService(Service):
     
         def __init__(self):
-            IpService.__init__(self)
+            Service.__init__(self)
             self.name = "groovalicious"
             self.desc = "This service makes me want to dance"
             self.version = (1,0,1)
-        
+
+    class PickUp(ServiceMethod):
         
         def run(self, params):
             # do the work of the service.
@@ -34,12 +35,14 @@ __version__ = "0.0.1"
 
 ## Imports
 # Stdlib
+from collections import deque
+import json
 import logging
 import os
 import re
-import sys
+import time
 # Third-party
-from IPython.utils.traitlets import HasTraits, Unicode, TraitType
+import IPython.utils.traitlets as trt
 
 ## Logging boilerplate
 
@@ -58,7 +61,8 @@ _log.addHandler(_h)
 
 ## Classes and functions
 
-class VersionNumber(TraitType):
+
+class VersionNumber(trt.TraitType):
     """A trait for a (major, minor, patch) version tuple
     See http://semver.org/
     
@@ -83,16 +87,38 @@ class VersionNumber(TraitType):
         self.error(obj, value)
     
 
-class Service(HasTraits):
+class Service(trt.HasTraits):
     """Base Service class.
     """
 
     #: Name of the service; should be short identifier
-    name = Unicode()
+    name = trt.Unicode()
     #: Description of the service
-    desc = Unicode()
+    desc = trt.Unicode()
     #: Version number of the service, see :class:`VersionNumber` for format
     version = VersionNumber()
+
+    def __init__(self, **meta):
+        trt.HasTraits.__init__(self)
+        # set traits from 'meta', if present
+        for key, val in meta.iteritems():
+            if hasattr(self, key):
+                setattr(self, key, val)
+        # list of all methods
+        self.methods = []
+
+    def add(self, m):
+        self.methods.append(m)
+
+    def as_json(self):
+        d = {
+            'name': self.name,
+            'desc': self.desc,
+            'version': self.version,
+            'methods': [m.as_json() for m in self.methods]
+        }
+        return d
+
 
 class LifecycleSubject(object):
     """Contains the current status of a running process.
@@ -113,29 +139,28 @@ class LifecycleSubject(object):
         self.obs = []
         
     def register(self, observer):
-        self.obs.append(observers)
+        self.obs.append(observer)
         
     def _event(self, name, *args):
-        if name in events:
-            for obs in self.obs:
-                getattr(obs, name)(*args)
+        for obs in self.obs:
+            getattr(obs, name)(*args)
 
     ## Events
 
     def advance(self):
         """Increments stage."""
         if not self._done:
-            self.stage = self.stage + 1
+            self._stage += 1
             self._event('stage', self._stage, self._stages)
 
-    def started(self):
+    def started(self, params):
         """Start the process.
         Sets stage to 1.
         Idempotent.
         """
         if self._stage == 0 and not self._done:
             self._stage = 1
-        self._event('started')
+        self._event('started', params)
         self._event('stage', self._stage, self._stages)
 
     def done(self):
@@ -196,7 +221,7 @@ class LifecycleObserver(object):
     :class:`IpService` to communicate with the IPython kernel,
     but they can also be extended to perform service-specific actions.    
     """
-    def started(self):
+    def started(self, params):
         """Called before execution starts"""
         pass
         
@@ -214,86 +239,168 @@ class LifecycleObserver(object):
 
 
 class ServiceMethodHistory(LifecycleObserver):
-    
-    XXX: make this only one per service/method combination,
-    XXX: so it is really a history..
-    
+    """Record duration between start/end in lifecycle events.
+    """
     def __init__(self, method, max_save=1000):
         self._method = method
         self._t = [None, None]
-        self._dur = -1
-        
-    def get_durations(self):
-        This should return a list of tuples
-          (start_time, duration, (params))
+        self._p = None
+        self._hist = deque()  # each item: (t0, t1, dur, [params])
+        self._maxlen = max_save
 
-    def started(self):
-        """Called before execution starts"""
+    def get_durations(self):
+        """Past durations of the method.
+
+        :return: All the past durations, in seconds
+        :rtype: iterable of double
+        """
+        return (x[2] for x in self._hist)
+
+    def started(self, params):
+        """Called when execution starts
+        """
         self._t[0] = time.time()
+        self._p = params
         
     def done(self):
-        """Called on successful completion"""
+        """Called on successful completion
+        """
         self._t[1] = time.time()
+        dur = self._t[1] - self._t[0]
+        self._hist.append(tuple(self._t + [dur, self._p]))
+        if len(self._hist) > self._maxlen:
+            self._hist.popleft()
     
     def error(self, code, msg):
         """Called on fatal error"""
         pass
 
+    def estimated_runtime(self, params):
+        """Based on history and params, estimate runtime for service.
+
+        """
+        dur = self._history.get_durations()
+        if len(dur) == 0:
+            estimate = -1  # no @!$%# idea
+        else:
+            # dumb: ignore params
+            estimate = sum(dur) / len(dur)
+        return estimate
 
 
-class ServiceMethod(HasTraits, LifecycleSubject):
+class ServiceMethod(trt.HasTraits, LifecycleSubject):
     """A method of a service.
     
     Defines standard attributes, a run() method, and
     a status property. For behavior/usage of the status property,
     see :class:`Status`.
     """
-    def __init__(self, service):
+
+    #: Name of the method; should be short identifier
+    name = trt.Unicode()
+    #: Description of the method
+    desc = trt.Unicode()
+    #: Parameters of method, a Tuple of traits
+    params = trt.Tuple()
+
+    def __init__(self, parent, history_class=ServiceMethodHistory, **meta):
         """Constructor.
-        
-        :param service: Parent service
-        :type service: Service
         """
         LifecycleSubject.__init__(self)
-        self.service = service
-        self._history = ServiceMethodHistory()
+        self._parent = parent
+        self._history = history_class(self)
         self.register(self._history)
+        # set traits from 'meta', if present
+        for key, val in meta.iteritems():
+            if hasattr(self, key):
+                setattr(self, key, val)
 
-    def execute(self, params):
-        """Wrapper for running the service. Subclasses
-        should not redefine this unless they know what they are doing;
-        instead override :meth:`run` in this class.
+    def estimated_runtime(self, params=()):
+        return self._history.estimated_runtime(params)
+
+    def execute(self, *params):
+        """Wrapper for running the service.
         """
-        self.event('started')
+        self._validate(params)
+        self.started(params)
         result = []
         try:
             result = self.run(params)
-            self.event('done')
+            self.done()
         except Exception, err:
-            self.event('error', -1, "Fatal: {}".format(err))
+            self.error(-1, "Fatal: {}".format(err))
         return result
 
-    def run(self, params):
-        """Actually run the service method.
-        
-        :param params: Dictionary of parameters, form is up to method to define
-        :type params: dict
-        :result: A result object
-        :rtype: Result
+    def set_func(self, fn, params):
+        """Set the main function to run.
+
+        :param fn: Function object to run
+        :param params: Tuple() of traits
+
+        :raise: ValueError, if function signature does not match
         """
-        raise NotImplemented()
+        self.run = fn
+        self.name = fn.__name__
+        for i, p in enumerate(params):
+            p.name = "param{:d}".format(i)
+        self.params = params
+
+    def _validate(self, p):
+        if len(p) != len(self.params):
+            raise ValueError("Wrong number of arguments. got={} wanted={}".format(len(p), len(self.params)))
+        for obj, spec in zip(p, self.params):
+            try:
+                spec.validate(spec, obj)
+            except trt.TraitError, err:
+                raise ValueError("Argument type error: {}".format(err))
+
+    def as_json(self):
+        d = {
+            'name': self.name,
+            'desc': self.desc,
+            'params': [(p.name, p.info_text, p.get_metadata('desc')) for p in self.params]
+        }
+        return d
+
+#############################################################################
 
 
-    def estimate_runtime(self, params):
-        """Based on history and params, estimate runtime for service.
-        
-        """
-        return -1  # no @!$%# idea
-        
-        
-##################################################################################
+def pick_up_people(num, where_from, where_to):
+    print("Pick up {:d} people at {} and drive to {}".format(num, where_from, where_to))
 
-class IpService(Service):
-    """Service that communicates with IPython kernel.
-    """
-    pass #XXX: Implement this
+
+class Person(trt.Unicode):
+    default_value = "Joe Schmoe"
+    info_text = 'the name of a person'
+
+    def validate(self, obj, value):
+        trt.Unicode.validate(self, obj, value)
+
+
+def example():
+    service = Service(name="taxicab", desc="yellow cab", version="0.0.1-alpha")
+    method = ServiceMethod(service, name="pickup", desc="Pick up people in a taxi")
+    method.set_func(pick_up_people,
+                    (trt.Int(1, desc="number of people"), trt.Unicode("", desc="Pick up location"),
+                     trt.Unicode("", desc="main drop off location"),
+                     Person("", desc="Person who called the taxi")))
+    service.add(method)
+    service.add(ServiceMethod(service, name="circle", desc="Drive around in circles"))
+    #
+    print(json.dumps(service.as_json(), indent=2))
+    #
+    try:
+        method.execute(1)
+    except ValueError, err:
+        print("as expected, validation failed: {}".format(err))
+    #
+    try:
+        method.execute(1, "here", 3.14, "me")
+    except ValueError, err:
+        print("as expected, validation failed: {}".format(err))
+    #
+    method.execute(1, "here", "there", "dang")
+    print("it worked!")
+
+if __name__ == '__main__':
+    example()

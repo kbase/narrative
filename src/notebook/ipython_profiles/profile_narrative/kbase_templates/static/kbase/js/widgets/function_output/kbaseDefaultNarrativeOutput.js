c86bed4f107f2b4350a4ece818883502cfd783fe
Bill Riehl <wjriehl@lbl.gov>
2013-12-10 22:39:20 -0800

    Added default input/output widgets.

---
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseDefaultNarrativeOutput.js
@@ -0,0 +1,100 @@
+/**
+ * This is a simple template for building KBase Narrative Widgets.
+ * KBase Widgets are based around the jQuery widget extension architecture,
+ * and are also instantiated as such.
+ *
+ * Your widget will need (at minimum) a unique name, a parent to inherit 
+ * from, a semantic version, an 'options' structure, and an init() function
+ * that returns itself.
+ *
+ * Details are described below.
+ *
+ * Instantiating a widget is done using a code form like this:
+ * $("#myElement").MyWidget({ option1: value1, option2:value2 });
+ *
+ * Instantiating this widget within the narrative just requires the output
+ * of a function that is run in the IPython Kernel to output the widget and
+ * the set of options it requires. Examples to follow.
+ *
+ * This version of the widget template includes authentication options for
+ * free - you shouldn't need to handle passing in user ids or auth tokens.
+ * These are referenced by the functions:
+ * this.user_id();
+ * and
+ * this.authToken();
+ *
+ * @see kbaseAuthenticatedWidget.js
+ * @public
+ */
+(function($, undefined) {
+    $.KBWidget({
+        /* 
+         * (required) Your widget should be named in CamelCase.
+         */
+        name: 'kbaseDefaultNarrativeOutput',
+
+        /*
+         * Extending kbaseAuthenticatedWidget lets you use auth tokens
+         * semi-automatically, assuming the page this is used in fires
+         * the loggedIn.kbase, loggedOut.kbase, and loggedInQuery events.
+         * These are usually fired by the kbaseLogin widget.
+         *
+         * this.user_id() = the logged in user id
+         * this.authToken() = the current authentication token
+         */
+        parent: 'kbaseWidget',
+
+        /*
+         * (optional) Widgets should be semantically versioned.
+         * See http://semver.org
+         */
+        version: '1.0.0',
+
+        /*
+         * (optional) Widgets are implied to include an options structure.
+         * It's useful to put default values here.
+         */
+        options: {
+            stubString: 'This is a stub!',
+            data: null,
+        },
+
+        /**
+         * (required) This is the only required function for a KBase Widget.
+         * @param {object} options - a structure containing the set of 
+         * options to be passed to this widget.
+         * @private
+         */
+        init: function(options) {
+            /*
+             * This should be the first line of your init function.
+             * It registers the new widget, overriding existing options.
+             *
+             * The members of the options structure will become members of 
+             * this.options, overriding any existing members.
+             */
+            this._super(options);
+
+            /*
+             * It is required to return this.
+             */
+            return this.render();
+        },
+
+        /**
+         * (not required)
+         * I prefer to keep initialization and rendering code separate, but
+         * that's just a style thing. You can do whatever the widget requires.
+         */
+        render: function() {
+            this.$elem.append(
+                $('<div>')
+                .append(this.options.data)
+            );
+
+            return this;
+        },
+
+
+    });
+})(jQuery);
---
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/kbaseDefaultNarrativeInput.js
@@ -0,0 +1,87 @@
+/**
+ * @author Bill Riehl <wjriehl@lbl.gov>
+ * @public
+ */
+
+(function( $, undefined ) {
+    $.KBWidget({
+        name: "kbaseDefaultNarrativeInput", 
+        parent: "kbaseWidget",
+        version: "1.0.0",
+        options: {
+            loadingImage: "../images/ajax-loader.gif",
+            method: null,
+        },
+
+        init: function(options) {
+            this._super(options);
+
+            // expects the method as a JSON string
+            if (this.options.method)
+                this.options.method = JSON.parse(this.options.method);
+            
+            this.render();
+            return this;
+        },
+
+        /**
+         * Builds the input div for a function cell, based on the given method object.
+         * @param {Object} method - the method being constructed around.
+         * @returns {String} an HTML string describing the available parameters for the cell.
+         * @private
+         */
+        render: function() {
+
+            // figure out all types from the method
+            var method = this.options.method;
+            
+            var params = method.properties.parameters;
+            var lookupTypes = [];
+            for (var p in params) {
+                lookupTypes.push(params[p].type);
+            }
+            this.trigger('dataLoadedQuery.Narrative', [lookupTypes, $.proxy(
+                function(objects) {
+                    var inputDiv = "<div class='kb-cell-params'><table class='table'>";
+                    var params = method.properties.parameters;
+                    for (var i=0; i<Object.keys(params).length; i++) {
+                        var pid = 'param' + i;
+                        var p = params[pid];
+
+                        var input = "";
+                        console.log(objects);
+                        if (objects[p.type] && objects[p.type].length > 0) {
+                            var objList = objects[p.type];
+                            objList.sort(function(a, b) {
+                                if (a[0] < b[0])
+                                    return -1;
+                                if (a[0] > b[0])
+                                    return 1;
+                                return 0;
+                            });
+
+                            input = "<select name='" + pid + "' > ";
+                            for (var j=0; j<objects[p.type].length; j++) {
+                                input += "<option value='" + objList[j][0] + "'>" + objList[j][0] + "</option>";
+                            }
+                            input += "</select>";
+                        }
+
+                        else {
+                            input = "<input name='" + pid + "' value='' type='text'></input>";
+                        }
+                        inputDiv += "<tr style='border:none'>" + 
+                                        "<td style='border:none'>" + p.ui_name + "</td>" + 
+                                        "<td style='border:none'>" + input + "</td>" +
+                                        "<td style='border:none'>" + p.description + "</td>" +
+                                    "</tr>";
+                    }
+                    inputDiv += "</table></div>";
+                    this.$elem.append(inputDiv);
+                },
+                this
+            )]);
+        },
+    });
+
+})( jQuery );

7e9342916099905dd36247e32be1a69dde51ec8c
Dan Gunter <dkgunter@lbl.gov>
2013-12-12 02:18:53 -0800

    Improved top-level description

--- src/biokbase/narrative/common/service_root.py
+++ src/biokbase/narrative/common/service_root.py
@@ -1,5 +1,6 @@
 """
-Description.
+Root of all services.
+
 """
 __author__ = 'Dan Gunter <dkgunter@lbl.gov>'
 __date__ = '12/11/13'
@@ -8,4 +9,3 @@ import biokbase.narrative.common.service as service
 # load all modules in services dir (see services/__init__.py),
 # which will register themselves on import
 from biokbase.narrative.services import *
-

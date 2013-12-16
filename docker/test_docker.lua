17d78fd0defec30adbd4e3d7c86bbf77b33c08be
vagrant <sychan@lbl.gov>
2013-12-16 23:09:03 +0000

    Some more test code to exercise the API: grab the ports where services are listening, etc...

--- docker/test_docker.lua
+++ docker/test_docker.lua
@@ -1,6 +1,8 @@
 PROXY=nil
 d = require('docker')
 p=require('pl.pretty')
+comp = require('pl.comprehension').new()
+
 c=d.config()
 c.image='sychan/narrative:latest'
 c.Cmd={'test_narrative'}
@@ -18,7 +20,19 @@ else
    print("Container failed to start")
    p.dump( res.body)
 end
-p.dump(d.client:containers().body)
+res = d.client:containers()
+--p.dump(res.body)
+print "Listing of ports in containers"
+portmap = {}
+for index,container in pairs(res.body) do
+   name = string.sub(container.Names[1],2,-1)
+   p.dump( name)
+   portmap[name]={}
+   for i, v in pairs(container.Ports) do
+      portmap[name][v.PublicPort] = v.PrivatePort
+   end
+end
+p.dump(portmap)
 p.dump(d.client:top{id=id,ps_args="auxww"}.body)
 res = d.client:stop_container{ id = id}
 if (res.status == 204) then

8593b6c38b55b51ccf5b772d989adaa45067f4e3
Bill Riehl <wjriehl@lbl.gov>
2014-01-06 12:29:14 -0800

    NAR-69 #comment Replaced <select> input with a HTML5 datalist linked to an input text.

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_input/kbaseDefaultNarrativeInput.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_input/kbaseDefaultNarrativeInput.js
@@ -54,11 +54,23 @@
                                 return 0;
                             });
 
-                            input = "<select name='" + pid + "' > ";
+                            var datalistUUID = this.genUUID();
+                            input = "<input type='text' name='" + pid + "' list='" + datalistUUID + "'>" +
+                                    "<datalist id='" + datalistUUID + "'>";
+
                             for (var j=0; j<objects[p.type].length; j++) {
                                 input += "<option value='" + objList[j][0] + "'>" + objList[j][0] + "</option>";
                             }
-                            input += "</select>";
+
+                            input += "</datalist>";
+
+
+
+                            // input = "<select name='" + pid + "' > ";
+                            // for (var j=0; j<objects[p.type].length; j++) {
+                            //     input += "<option value='" + objList[j][0] + "'>" + objList[j][0] + "</option>";
+                            // }
+                            // input += "</select>";
                         }
 
                         else {
@@ -137,5 +149,12 @@
                 }
             });
         },
+
+        genUUID: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);});
+        },
+
     });
 })( jQuery );

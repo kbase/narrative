92c93e40485a98de44c6973d721b7107ef083e41
Sarah Poon <sarahpoon@0-26-bb-4-64-c3.dhcp.lbnl.us>
2014-02-05 14:21:03 -0800

    adding a link to help@kbase in the Help menu, per Adam's request

--- functional-site/js/app.js
+++ functional-site/js/app.js
@@ -294,6 +294,7 @@ app.run(function ($rootScope, $state, $stateParams, $location) {
     var HELP_DROPDOWN = '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Help <b class="caret"></b></a> \
                  <ul class="dropdown-menu"> \
                  <li><a href="#/landing-pages-help">Landing Page Documentation</a></li> \
+                 <li><a href="mailto:help@kbase.us">Email help@kbase.us</a></li> \
               </ul>';
     $('.help-dropdown').html(HELP_DROPDOWN);              
 

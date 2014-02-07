37c6e16cb89f26dc81e2d0f0887030ffac9758bd
msneddon <mwsneddon@lbl.gov>
2014-02-07 14:42:35 -0800

    added running loader image in genome comparison output widget

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseGenomeComparison.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseGenomeComparison.js
@@ -251,8 +251,12 @@ $.KBWidget({
         			var wasError = data[6];
     				var tdElem = $('#'+self.pref+'job');
     				tdElem.html(status);
+				if (status === 'running') {
+					tdElem.html(status+"... &nbsp &nbsp <img src=\""+self.loadingImage+"\">");
+                                }
         			if (complete === 1) {
         				clearInterval(self.timer);
+					
         				if (wasError === 0) {
             				dataIsReady();
         				}

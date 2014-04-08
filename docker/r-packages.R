dcb8e864978f954be8b1b011e32dd19142f47133
root <sychan@lbl.gov>
2014-04-08 21:54:18 +0000

    NAR-143 #comment A somewhat dumbed down version of R configs from before. The ubuntu package maintainer screwed up the version string on the 3.0.3 release, making it 3.1-beta instead, and making it incompatible with many packages in CRAN. Will wait for it to settle down and go back to the more ambitiuus R installation

--- docker/r-packages.R
+++ docker/r-packages.R
@@ -1,30 +1,28 @@
 options(repos = c(CRAN = "http://cran.r-project.org/"))
-install.packages("ggplot2")  # powerful printing system
-install.packages("devtools")  # make dev life easier
+# ggplot2 not supported under the older version of R available via ubuntu repo
+# will need to wait until CRAN repo version/dependency mess for 3.1-beta is resolved
+#install.packages("ggplot2")  # powerful printing system
 
 install.packages("knitr")  # elegant report generator
 install.packages("stringr")
 install.packages("rgl")
 install.packages("data")
 install.packages("table")
-install.packages("parallel")
  
 install.packages("Rglpk")  # powerful solver for mixed integer linear programming
 install.packages("goalprog")  # goal programming
-install.packages("Rdonlp2", repos="http://R-Forge.R-project.org")  # powerful solver for smooth nonlinear minimization problem
 install.packages("tseries")  # processing time series 
 install.packages("zoo")  # no standar time series
-install.packages("xts")  # extend ts
+#install.packages("xts")  # extend ts
 install.packages("timeSeries")  # another time series format
 install.packages("lubridate")  # dealing with dates
 install.packages("fACD", repos="http://R-Forge.R-project.org")  # ACD model
  
-install.packages("ggpmap")  # access Google Maps
 install.packages("googleVis")  # access Google Visualisation API
 install.packages("Quandl")  # access Quandl
-install.packages("rdatamarket")  # access http://datamarket.com/
+#install.packages("rdatamarket")  # access http://datamarket.com/
 
-require(devtools)
-install_github('rCharts', 'ramnathv')
+#require(devtools)
+#install_github('rCharts', 'ramnathv')
 #source("http://bioconductor.org/biocLite.R")
 #biocLite()

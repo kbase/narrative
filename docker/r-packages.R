d83a5c82c0fe8763fd9376332fced8f87e8aba9b
Steve Chan <sychan@lbl.gov>
2014-04-10 15:14:29 -0500

    NAR-143 #resolve   Marcin suggested that we could get around the module version issue by building all the R modules from source, so this commit includes a working 3.1-beta version of R, a working rpy2 module (installed via apt-get - pip module doesn't work with 3.1-beta) and the R modules recommended by Marcin for this R tasks

--- docker/r-packages.R
+++ docker/r-packages.R
@@ -3,26 +3,31 @@ options(repos = c(CRAN = "http://cran.r-project.org/"))
 # will need to wait until CRAN repo version/dependency mess for 3.1-beta is resolved
 #install.packages("ggplot2")  # powerful printing system
 
-install.packages("knitr")  # elegant report generator
-install.packages("stringr")
-install.packages("rgl")
-install.packages("data")
-install.packages("table")
+install.packages("knitr", type="source")  # elegant report generator
+install.packages("stringr", type="source")
+install.packages("rgl", type="source")
+install.packages("data", type="source")
+install.packages("table", type="source")
+install.packages("ggplot2", type="source")
+install.packages("pheatmap", type="source")
+install.packages("RCcolorBrewer", type="source")
  
-install.packages("Rglpk")  # powerful solver for mixed integer linear programming
-install.packages("goalprog")  # goal programming
-install.packages("tseries")  # processing time series 
-install.packages("zoo")  # no standar time series
+install.packages("Rglpk", type="source")  # powerful solver for mixed integer linear programming
+install.packages("goalprog", type="source")  # goal programming
+install.packages("tseries", type="source")  # processing time series 
+install.packages("zoo", type="source")  # no standar time series
 #install.packages("xts")  # extend ts
-install.packages("timeSeries")  # another time series format
-install.packages("lubridate")  # dealing with dates
+install.packages("timeSeries", type="source")  # another time series format
+install.packages("lubridate", type="source")  # dealing with dates
 install.packages("fACD", repos="http://R-Forge.R-project.org")  # ACD model
  
-install.packages("googleVis")  # access Google Visualisation API
-install.packages("Quandl")  # access Quandl
+install.packages("googleVis", type="source")  # access Google Visualisation API
+install.packages("Quandl", type="source")  # access Quandl
 #install.packages("rdatamarket")  # access http://datamarket.com/
 
-#require(devtools)
-#install_github('rCharts', 'ramnathv')
+install.packages("codetools", type="source")
+install.packages("devtools", type="source")
+require(devtools)
+install_github('rCharts', 'ramnathv')
 #source("http://bioconductor.org/biocLite.R")
 #biocLite()

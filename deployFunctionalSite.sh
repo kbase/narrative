0413aab66884c6369074aadb45f591e021d77592
kkeller <kkeller@lbl.gov>
2014-05-29 15:30:44 -0700

    Excluding deployment files in deploy script

--- deployFunctionalSite.sh
+++ deployFunctionalSite.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -avP --exclude=.git\* * /kb/deployment/ui-common/
+# should be more selective what to deploy here
+rsync -avP --exclude=.git\* --exclude=README\* --exclude=deployFunctionalSite\* --exclude=Makefile * /kb/deployment/ui-common/

5e3044fe523098022b5c86b4ea1890f6c5a484aa
Travis Harrison <guumby@gmail.com>
2014-06-20 13:15:24 -0500

    pixal pushing

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseCategoryView.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseCategoryView.js
@@ -18,32 +18,32 @@
             var main  = $('<div>').attr('id', index);
             // Create accordian
             if (this.options.data !== null) {
-                main.addClass('panel-group');
+                main.addClass('panel-group').css({'margin':'0','padding':'0'});
                 var i, j;
                 for (i = 0; i < this.options.data.length; i++) {
                     var catid = index+'_'+i;
                     var cnum  = this.options.data[i]['items'].length
-                    var clist = $('<ul>').addClass('list-group').css({'margin':'2','padding':'2'});
+                    var clist = $('<ul>').addClass('list-group').css({'margin':'0','padding':'0'});
                     for (j = 0; j < cnum; j++) {
                         clist.append(
                             $('<li>').addClass('list-group-item')
-                                .css({'margin':'2','padding':'2'})
+                                .css({'margin':'0','padding':'0'})
                                 .html(this.options.data[i]['items'][j])
                         );
                     }
                     main.append(
-                        $('<div>').addClass('panel panel-default').css({'margin':'2','padding':'2'})
+                        $('<div>').addClass('panel panel-default').css({'margin':'0','padding':'0'})
                         .append(
-                            $('<div>').addClass('panel-heading').css({'margin':'2','padding':'2'}).append(
-                                $('<h4>').addClass('panel-title').css({'margin':'2','padding':'2'}).append(
+                            $('<div>').addClass('panel-heading').css({'margin':'0','padding':'0'}).append(
+                                $('<h4>').addClass('panel-title').css({'margin':'0','padding':'0'}).append(
                                     $('<a>').attr({'data-toggle':'collapse', 'data-parent':'#'+index, 'href':'#'+catid})
                                         .append($('span').addClass('badge').html(cnum))
                                         .html(this.options.data[i]['title'])
                                 )
                             )
                         ).append(
-                           $('<div>').attr('id', catid).css({'margin':'2','padding':'2'}).addClass('panel-collapse collapse').append(
-                               $('<div>').addClass('panel-body').css({'margin':'2','padding':'2'}).append(clist)
+                           $('<div>').attr('id', catid).css({'margin':'0','padding':'0'}).addClass('panel-collapse collapse').append(
+                               $('<div>').addClass('panel-body').css({'margin':'0','padding':'0'}).append(clist)
                             )
                         )
                     );

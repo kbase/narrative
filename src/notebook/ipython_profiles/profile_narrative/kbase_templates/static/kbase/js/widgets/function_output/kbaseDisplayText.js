c1b7b3a839523e9810eca65e8607964c3eb61a59
Travis Harrison <guumby@gmail.com>
2014-07-08 14:01:41 -0500

    output stout even if there is stderr

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseDisplayText.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseDisplayText.js
@@ -17,24 +17,23 @@
         render: function() {
             // creater main comtainer
             var main = $('<div>');
-            // this is an error message
+            // add header
+            if (this.options.header !== '') {
+                main.append($('<p>')
+                    .css({'padding': '10px 20px'})
+                    .append($('<h3>').text(this.options.header)));
+            }
+            // add text
+            if (this.options.text !== '') {
+                main.append($('<div>')
+                    .css({'font-size': 'small'})
+                    .append($('<pre>').text(this.options.text)));
+            }
+            // add error
             if (this.options.error !== '') {
                 main.append($('<div>')
                     .css({'padding': '10px 20px', 'background-color': 'rgba(255,0,0,0.25)'})
                     .append($('<pre>').text('ERROR:\n'+this.options.error)));
-            } else {
-                // add header
-                if (this.options.header !== '') {
-                    main.append($('<p>')
-                        .css({'padding': '10px 20px'})
-                        .append($('<h3>').text(this.options.header)));
-                }
-                // add text
-                if (this.options.text !== '') {
-                    main.append($('<div>')
-                        .css({'font-size': 'small'})
-                        .append($('<pre>').text(this.options.text)));
-                }
             }
             // put container in cell
             this.$elem.append(main);

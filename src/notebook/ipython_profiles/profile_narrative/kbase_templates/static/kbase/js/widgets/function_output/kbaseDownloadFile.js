166c2ceb0a5d099f11f55dff699710043d352617
Travis Harrison <guumby@gmail.com>
2014-07-10 11:20:06 -0500

    javascript fix

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseDownloadFile.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseDownloadFile.js
@@ -18,36 +18,39 @@
             var main = $('<div>');
             // add download button
             if ((this.options.data !== null) && (this.options.name !== null)) {
+                var downloadFn = function (data, filename) {
+                    try {
+                        data = window.btoa(data);
+                    } catch (err) {
+                    	var utftext = "";
+                    	for (var n=0; n<data.length; n++) {
+                            var c=data.charCodeAt(n);
+                    	    if (c<128) {
+                    		    utftext += String.fromCharCode(c);
+                            } else if ((c>127) && (c<2048)) {
+                                utftext += String.fromCharCode((c>>6)|192);
+                                utftext += String.fromCharCode((c&63)|128);
+                    		} else {
+                    		    utftext += String.fromCharCode((c>>12)|224);
+                                utftext += String.fromCharCode(((c>>6)&63)|128);
+                                utftext += String.fromCharCode((c&63)|128);
+                            }
+                        }
+                    	data = window.btoa(utftext);
+                    }
+                    data = 'data:application/octet-stream;base64,'+data;
+                    var anchor = document.createElement('a');
+                    anchor.setAttribute('download', filename);
+                    anchor.setAttribute('href', data);
+                    document.body.appendChild(anchor);
+                    anchor.click();
+                    document.body.removeChild(anchor);
+                };
                 main.append($('<button>')
                     .text("Save "+name)
-                    .on('click', function (data, filename) {
-                        try {
-                            data = window.btoa(data);
-                        } catch (err) {
-                        	var utftext = "";
-                        	for (var n=0; n<data.length; n++) {
-                                var c=data.charCodeAt(n);
-                        	    if (c<128) {
-                        		    utftext += String.fromCharCode(c);
-                                } else if ((c>127) && (c<2048)) {
-                                    utftext += String.fromCharCode((c>>6)|192);
-                                    utftext += String.fromCharCode((c&63)|128);
-                        		} else {
-                        		    utftext += String.fromCharCode((c>>12)|224);
-                                    utftext += String.fromCharCode(((c>>6)&63)|128);
-                                    utftext += String.fromCharCode((c&63)|128);
-                                }
-                            }
-                        	data = window.btoa(utftext);
-                        }
-                        data = 'data:application/octet-stream;base64,'+data;
-                        var anchor = document.createElement('a');
-                        anchor.setAttribute('download', filename);
-                        anchor.setAttribute('href', data);
-                        document.body.appendChild(anchor);
-                        anchor.click();
-                        document.body.removeChild(anchor);
-                    }(this.options.data, this.options.name))
+                    .on('click', function () {
+                        downloadFn(this.options.data, this.options.name);
+                    })
                 );
             } else {
                 main.append($('<p>').text("Error: file content is empty."));

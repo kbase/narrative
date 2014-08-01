c076687b94aa3a4586096c2f938f3c933bde0147
Shinjae Yoo <sjyoo@bnl.gov>
2014-08-01 09:37:44 +0000

    Added FeatureSet supports

--- src/biokbase/narrative/services/plants_gwas.py
+++ src/biokbase/narrative/services/plants_gwas.py
@@ -9,6 +9,8 @@ __date__ = '12/12/13'
 
 # Stdlib
 import json
+from string import Template
+from operator import itemgetter
 # Third party
 # Service framework
 from biokbase.narrative.common.service import init_service, method, finalize_service
@@ -18,7 +20,9 @@ from biokbase.narrative.common.util import AweJob, Workspace2
 from biokbase.KBaseNetworksService2.Client import KBaseNetworks
 from biokbase.cdmi.client import CDMI_API,CDMI_EntityAPI
 from biokbase.IdMap.Client import IdMap
+from biokbase.OntologyService.Client import Ontology
 import sys
+import os
 
 ## Exceptions
 
@@ -35,24 +39,24 @@ GENE_NETWORK_OBJECT_TYPE = "KBaseGwasData.GwasGeneList"
 
 
 class URLS:
-    _host = '140.221.84.248'
-    main = "http://140.221.84.236:8000/node"
-    shock = "http://140.221.84.236:8000"
-    #awe = "http://140.221.85.182:7080"
-    awe = "https://kbase.us/services/awe-api/"
+    #_host = '140.221.84.248'
+    #main = "http://140.221.84.236:8000/node"
+    #shock = "http://140.221.84.236:8000"
+    awe = "http://140.221.85.182:7080"
+    #awe = "https://kbase.us/services/awe-api/"
     #workspace = "https://kbase.us/services/ws/"
     ids = "http://kbase.us/services/idserver"
     cdmi = "http://kbase.us/services/cdmi_api"
     #cdmi = "http://140.221.85.181:7032"
     ontology = "http://kbase.us/services/ontology_service"
-    #gwas = "http://140.221.85.182:7086"
-    gwas = "https://kbase.us/services/GWAS"
+    gwas = "http://140.221.85.182:7086"
+    #gwas = "https://kbase.us/services/GWAS"
     gwas1 = "http://140.221.85.95:7086"
     ujs = "https://kbase.us/services/userandjobstate"
     #networks = "http://kbase.us/services/networks"
     networks = "http://140.221.85.172:7064/KBaseNetworksRPC/networks"
-    #idmap = "http://kbase.us/services/id_map"
-    idmap = "http://140.221.85.181:7111"
+    idmap = "http://kbase.us/services/id_map"
+    #idmap = "http://140.221.85.181:7111"
 
 AweJob.URL = URLS.awe
 
@@ -71,8 +75,72 @@ def _workspace_output(wsid):
 
 
 
+def ids2cds(ql):
+    cdmic = CDMI_API(URLS.cdmi)
+    idm = IdMap(URLS.idmap)
 
+    gl = set()
+    rd = {}
+    eids = []
+    lids = set()
+    mids = set()
+    for gid in ql:
+      rd[gid] = gid
+      if 'kb|g.' in gid:
+        if 'locus' in gid:
+          lids.add(gid)
+        elif 'mRNA' in gid:
+          mids.add(gid)
+      else:
+        eids.append(gid)
 
+    sid2fids = cdmic.source_ids_to_fids(eids)
+    for sid in sid2fids:
+      for fid in sid2fids[sid]:
+        rd[sid] = fid
+        if 'locus' in fid:
+          lids.add(fid)
+        elif 'mRNA' in fid:
+          mids.add(fid)
+    lidmap = ()
+    if len(lids) > 0: lidmap = idm.longest_cds_from_locus(list(lids))
+    for lid in lidmap:
+      for k in lidmap[lid]:
+        gl.add(k)
+    midl = list(mids)
+    midmap = ()
+    if len(mids) > 0: lidmap = idm.longest_cds_from_mrna(list(mids))
+    for lid in midmap:
+      for k in midmap[lid]:
+        gl.add(k)
+
+    for gid in ql:
+      if 'kb|g.' in gid:
+        if 'locus' in gid:
+          for k in lidmap[gid]:
+            rd[gid] = k
+        elif 'mRNA' in gid:
+          for k in midmap[gid]:
+            rd[gid] = k
+      else:
+        if 'locus' in rd[gid]:
+            for k in lidmap[rd[gid]]:
+              rd[gid] = k
+        elif 'mRNA' in rd[gid]:
+            for k in midmap[rd[gid]]:
+              rd[gid] = k
+    return rd
+
+def cds2locus(gids):
+    cdmie = CDMI_EntityAPI(URLS.cdmi)
+    mrnas_l = cdmie.get_relationship_IsEncompassedIn(gids, [], ['to_link'], [])
+    mrnas = dict((i[1]['from_link'], i[1]['to_link']) for i in mrnas_l)
+    locus_l = cdmie.get_relationship_IsEncompassedIn(mrnas.values(), [], ['to_link'], [])
+    locus = dict((i[1]['from_link'], i[1]['to_link']) for i in locus_l)
+    lgids = dict((i,locus[mrnas[i]]) for i in gids if i in mrnas and mrnas[i] in locus)
+    return lgids
+    
+    
 @method(name="Prepare Variation data for GWAS")
 def maf(meth, maf=0.05, variation=None, out=None, comment=None):
     """Perform filtering on Minor allele frequency (MAF).
@@ -322,13 +390,133 @@ def gene_network2ws(meth, obj_id=None, out_id=None):
     meth.advance("Returning object")
     return _workspace_output(out_id)
 
-@method(name="User genelist to Networks")
-def genelist_network2ws(meth, gene_ids=None, out_id=None):
-    """This method displays a gene list
-    along with functional annotation in a table.
+      
+@method(name="User genelist to FeatureSet")
+def genelist_to_featureset(meth, gene_ids=None, out_id=None):
+    """This method converts user gene list to FeatureSet typed object.
 
     :param gene_ids: List of genes (comma separated)
     :type gene_ids: kbtypes.Unicode
+    :param out_id: Output FeatureSet object identifier
+    :type out_id: kbtypes.KBaseSearch.FeatureSet
+    :return: New workspace object
+    :rtype: kbtypes.Unicode
+    :output_widget: ValueListWidget
+    """
+    meth.stages = 2
+    meth.advance("Retrieve genes from Central Store")
+    ws = Workspace2(token=meth.token, wsid=meth.workspace_id)
+    
+    gene_ids_ns = gene_ids.replace(" ","")
+    qid2cds = ids2cds(gene_ids_ns.split(','))
+    fs = {"description" : "User generated feature set by " + gene_ids, 
+          "elements" : {}
+         }
+    cdmie = CDMI_EntityAPI(URLS.cdmi)
+    cdmic = CDMI_API(URLS.cdmi)
+    cds_ids = qid2cds.values()
+    cds2l = cds2locus(cds_ids);
+    lfunc = cdmic.fids_to_functions(cds2l.values())
+    
+    fm = cdmie.get_entity_Feature(cds_ids,['feature_type', 'source_id', 'sequence_length', 'function', 'alias'])
+    for i in cds_ids: 
+      if i in fm:
+        if not fm[i]['function'] and cds2l[i] in lfunc:
+          fm[i]['function'] = lfunc[cds2l[i]]
+        fs['elements'][i] = {"data" : { 'type' : fm[i]['feature_type'], 'id' : i, 'dna_sequence_length' : int(fm[i]['sequence_length']), 'function' : fm[i]['function'], 'aliases' : fm[i]['alias']}}
+
+    ws.save_objects({'workspace' : meth.workspace_id, 'objects' :[{'type' : 'KBaseSearch.FeatureSet', 'data' : fs, 'name' : out_id, 'meta' : {'original' : gene_ids}}]})
+
+    meth.advance("Returning object")
+    #return _workspace_output(out_id)
+    return json.dumps({'values': [
+                                   ["Workspace object", out_id],
+                                   ["CDS IDs", ",".join(qid2cds.values())],
+                                   ["User IDs", ",".join(qid2cds.keys())]
+                                 ]})
+
+@method(name="FeatureSet GO Analysis")
+def featureset_go_anal(meth, feature_set_id=None, p_value=0.05, ec='IEA', domain='biological_process', out_id=None):
+    """This method annotate GO terms and execute GO enrichment test 
+
+    :param feature_set_id: FeatureSet workspace object id
+    :type feature_set_id: kbtypes.KBaseSearch.FeatureSet
+    :param p_value: p-value cutoff
+    :type p_value: kbtypes.Unicode
+    :param ec: Evidence code list (comma separated, IEA, ...)
+    :type ec:kbtypes.Unicode
+    :param domain: Domain list (comma separated, biological_process, ...)
+    :type domain: kbtypes.Unicode
+    :param out_id: Output FeatureSet object identifier
+    :type out_id: kbtypes.KBaseSearch.FeatureSet
+    :return: New workspace object
+    :rtype: kbtypes.Unicode
+    :output_widget: ValueListWidget
+    """
+    # :param workspace_id: Workspace name (if empty, defaults to current workspace)
+    # :type workspace_id: kbtypes.Unicode
+    meth.stages = 3
+    meth.advance("Prepare Enrichment Test")
+
+    oc = Ontology(url=URLS.ontology)
+    ws = Workspace2(token=meth.token, wsid=meth.workspace_id)
+    fs = ws.get(feature_set_id)
+    qid2cds = ids2cds(fs['elements'].keys())
+    
+    meth.advance("Execute Enrichment Test")
+    ec = ec.replace(" ","")
+    domain = domain.replace(" ","")
+    ec_list = [ i for i in ec.split(',')]
+    domain_list = [ i for i in domain.split(',')]
+    ots = oc.get_goidlist(list(set(qid2cds.values())), domain_list, ec_list)
+    enr_list = oc.get_go_enrichment(list(set(qid2cds.values())), domain_list, ec_list, 'hypergeometric', 'GO')
+    enr_list = sorted(enr_list, key=itemgetter('pvalue'), reverse=False)
+    go_enr_smry = ""
+    for i in range(len(enr_list)):
+      goen = enr_list[i]
+      if goen['pvalue'] > float(p_value) : continue
+      #fs['fse.'+goen['goID']+".desc" ] = goen['goDesc'][0]
+      #fs['fse.'+goen['goID']+".domain" ] = goen['goDesc'][1]
+      #fs['fse.'+goen['goID']+".p_value" ] = `goen['pvalue']`
+      if i < 3 :
+        go_enr_smry += goen['goID']+"(" + "{:6.4f}".format(goen['pvalue']) + ")" + goen['goDesc'][0] + "\n"
+        #go_enr_anns[i] = goen['goID']+"(" + "{:6.4f}".format(goen['pvalue']) + ")" + goen['goDesc'][0]
+    go_enr_smry
+    
+    meth.advance("Annotate GO Term")
+    go_key = lambda go, i, ext: "go.{}.{:d}.{}".format(go, i, ext)
+    for gid in fs['elements']:
+      if 'metadata' not in fs['elements'][gid]: fs['elements'][gid]['metadata'] = {}
+      lid = qid2cds[gid]
+      if lid in ots:
+          go_enr_list = []
+          for lcnt, go in enumerate(ots[lid].keys()):
+              #if lcnt < 0:
+                  #fs['elements'][gid]['data']['annotations'].append(go + "(go)" + ots[lid][go][0]['desc'] + '\n')
+              for i, goen in enumerate(ots[lid][go]):
+                  for ext in "domain", "ec", "desc":
+                      fs['elements'][gid]['metadata'][go_key(go, i, ext)] = goen[ext]
+                      fs['elements'][gid]['metadata'][go_key(go, i, ext)] = goen[ext]
+      
+    meth.advance("Saving output to Workspace")
+
+    ws.save_objects({'workspace' : meth.workspace_id, 'objects' :[{'type' : 'KBaseSearch.FeatureSet', 'data' : fs, 'name' : out_id, 'meta' : {'original' : feature_set_id, 'enr_summary' : go_enr_smry}}]})
+
+
+    meth.advance("Returning object")
+    return json.dumps({'values': [
+                                   ["Workspace object", out_id],
+                                   ["Enrichment Summary", go_enr_smry]
+                                 ]})
+
+
+@method(name="FeatureSet to Networks")
+def gene_network2ws(meth, feature_set_id=None, out_id=None):
+    """This method displays a gene list
+    along with functional annotation in a table.
+
+    :param feature_set_id: FeatureSet workspace object id
+    :type feature_set_id: kbtypes.KBaseSearch.FeatureSet
     :param out_id: Output Networks object identifier
     :type out_id: kbtypes.KBaseNetworks.Network
     :return: New workspace object
@@ -341,55 +529,12 @@ def genelist_network2ws(meth, gene_ids=None, out_id=None):
     meth.advance("init GWAS service")
     gc = GWAS(URLS.gwas, token=meth.token)
 
-    meth.advance("Converting external ids to internal ids")
-    # if not workspace_id:
-    #     meth.debug("Workspace ID is empty, setting to current ({})".format(meth.workspace_id))
-    #     workspace_id = meth.workspace_id
-    
-    cdmic = CDMI_API(URLS.cdmi)
-    idm = IdMap(URLS.idmap)
+    meth.advance("Retrieve genes from workspace")
+    ws = Workspace2(token=meth.token, wsid=meth.workspace_id)
+    fs = ws.get(feature_set_id)
+    qid2cds = ids2cds(fs['elements'].keys())
 
-    gl = set()
-    eids = []
-    lids = set()
-    mids = set()
-    for gid in gene_ids.split(','):
-      if 'kb|g.' in gid:
-        if 'CDS' in gid:
-          gl.add(gid)
-        elif 'locus' in gid:
-          lids.add(gid)
-        elif 'mRNA' in gid:
-          mids.add(gid)
-        else:
-          gl.add(gid)
-      else:
-        eids.append(gid)
-    sid2fids = cdmic.source_ids_to_fids(eids)
-    for sid in sid2fids:
-      for fid in sid2fids[sid]:
-        if 'locus' in fid:
-          lids.add(fid)
-        elif 'CDS' in fid:
-          gl.add(fid)
-        elif 'mRNA' in fid:
-          mids.add(fid)
-        else:
-          gl.add(fid)
-    lidmap = ()
-    if len(lids) > 0: lidmap = idm.longest_cds_from_locus(list(lids))
-    for lid in lidmap:
-      for k in lidmap[lid]:
-        gl.add(k)
-    midl = list(mids)
-    
-    lidmap = ()
-    if len(mids) > 0: lidmap = idm.longest_cds_from_mrna(list(mids))
-    for lid in lidmap:
-      for k in lidmap[lid]:
-        gl.add(k)
-    
-    gl_str = ",".join(gl);
+    gl_str = ",".join(list(set(qid2cds.values())));
 
     meth.advance("Running GeneList to Networks")
     argsx = {"ws_id" : meth.workspace_id, "inobj_id" : gl_str,  "outobj_id": out_id}
@@ -402,6 +547,74 @@ def genelist_network2ws(meth, gene_ids=None, out_id=None):
 
     meth.advance("Returning object")
     return _workspace_output(out_id)
+
+@method(name="FeatureSet Network Enrichment")
+def featureset_net_enr(meth, feature_set_id=None, p_value=None, ref_wsid="KBasePublicNetwork", ref_network=None, out_id=None):
+    """This method annotate GO terms and execute GO enrichment test 
+
+    :param feature_set_id: FeatureSet workspace object id
+    :type feature_set_id: kbtypes.KBaseSearch.FeatureSet
+    :param p_value: p-value cutoff
+    :type p_value: kbtypes.Unicode
+    :param ref_wsid: Reference Network workspace id (optional, default to current workspace)
+    :type ref_wsid: kbtypes.Unicode
+    :param ref_network: Reference Network object name
+    :type ref_network:kbtypes.KBaseNetworks.Network
+    :param out_id: Output FeatureSet object identifier
+    :type out_id: kbtypes.KBaseSearch.FeatureSet
+    :return: New workspace object
+    :rtype: kbtypes.Unicode
+    :output_widget: ValueListWidget
+    """
+    # :param workspace_id: Workspace name (if empty, defaults to current workspace)
+    # :type workspace_id: kbtypes.Unicode
+    meth.stages = 3
+    meth.advance("Prepare Enrichment Test")
+
+    # if not workspace_id:
+    #     meth.debug("Workspace ID is empty, setting to current ({})".format(meth.workspace_id))
+    #     workspace_id = meth.workspace_id
+    
+    oc = Ontology(url=URLS.ontology)
+    ws = Workspace2(token=meth.token, wsid=meth.workspace_id)
+    fs = ws.get(feature_set_id)
+    if  not ref_wsid : ref_wsid = meth.workspace_id
+    ws2 = Workspace2(token=meth.token, wsid=ref_wsid)
+    net = ws2.get(ref_network)
+    qid2cds = ids2cds(fs['elements'].keys())
+    
+    meth.advance("Execute Enrichment Test")
+    enr_dict = oc.association_test(list(set(qid2cds.values())), ref_wsid, ref_network, '', 'hypergeometric', 'none', p_value)
+    enr_list = sorted([(value,key) for (key,value) in enr_dict.items()])
+ 
+
+    nid2name = {}
+    for ne in net['nodes']:
+      nid2name[ne['entity_id']] = ne['name'] 
+
+    pwy_enr_smry = ""
+    for i in range(len(enr_list)):
+      pwy_en = enr_list[i]
+      if float(pwy_en[0]) > float(p_value) : continue
+      #fs['fse.'+goen['goID']+".desc" ] = goen['goDesc'][0]
+      #fs['fse.'+goen['goID']+".domain" ] = goen['goDesc'][1]
+      #fs['fse.'+goen['goID']+".p_value" ] = `goen['pvalue']`
+      if i < 3 :
+        pwy_enr_smry += pwy_en[1]+"(" + "{:6.4f}".format(float(pwy_en[0])) + ")" + nid2name[pwy_en[1]] + "\n"
+        #go_enr_anns[i] = goen['goID']+"(" + "{:6.4f}".format(goen['pvalue']) + ")" + goen['goDesc'][0]
+    
+      
+    meth.advance("Saving output to Workspace")
+
+    #ws.save_objects({'workspace' : meth.workspace_id, 'objects' :[{'type' : 'KBaseSearch.FeatureSet', 'data' : fs, 'name' : out_id, 'meta' : {'original' : feature_set_id, 'ref_wsid' : ref_wsid, 'ref_net' : ref_network, 'network_enrichment' : enr_dict}}]})
+    ws.save_objects({'workspace' : meth.workspace_id, 'objects' :[{'type' : 'KBaseSearch.FeatureSet', 'data' : fs, 'name' : out_id, 'meta' : {'original' : feature_set_id, 'ref_wsid' : ref_wsid, 'ref_net' : ref_network, 'pwy_enr_summary' :pwy_enr_smry}}]})
+
+
+    meth.advance("Returning object")
+    return json.dumps({'values': [
+                                   ["Workspace object", out_id],
+                                   ["Enrichment Summary", pwy_enr_smry]
+                                 ]})
 
 
 @method(name="Gene network")

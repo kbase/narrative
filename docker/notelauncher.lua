2ac78d36441fa7b33d0dd6d9b647305cec53618e
Bill Riehl <wjriehl@lbl.gov>
2014-08-04 19:18:54 -0700

    Added some logging.

--- docker/notelauncher.lua
+++ docker/notelauncher.lua
@@ -63,78 +63,79 @@ end
 --    Actually launch a new docker container.
 --
 local function launch_notebook( name )
-   -- don't wrap this in a pcall, if it fails let it propagate to
-   -- the caller
-   portmap = get_notebooks()
-   assert(portmap[name] == nil, "Notebook by this name already exists: " .. name)
-   local conf = docker.config()
-   local bind_syslog = nil
-   conf.Image = string.format("%s:%s",M.repository_image,M.repository_version)
-   conf.Cmd={name}
-   conf.PortSpecs = {tostring(M.private_port)}
-   ngx.log(ngx.INFO,string.format("Spinning up instance of %s on port %d",conf.Image, M.private_port))
-   -- we wrap the next call in pcall because we want to trap the case where we get an
-   -- error and try deleting the old container and creating a new one again
-   local ok,res = pcall(docker.client.create_container, docker.client, { payload = conf, name = name})
-   if not ok and res.response.status >= 409 then
-      -- conflict, try to delete it and then create it again
-      ngx.log(ngx.ERR,string.format("conflicting notebook, removing notebook named: %s",name))   
-      ok, res = pcall( docker.client.remove_container, docker.client, { id = name })
-      ngx.log(ngx.ERR,string.format("response from remove_container: %s", p.write(res.response)))
-      -- ignore the response and retry the create, and if it still errors, let that propagate
-      ok, res = pcall(docker.client.create_container, docker.client, { payload = conf, name = name})
-   end
-   if ok then
-      assert(res.status == 201, "Failed to create container: " .. json.encode(res.body))
-      local id = res.body.Id
-      if M.syslog_src then
-	 -- Make sure it exists and is writeable
-	 local stat = lfs.attributes(M.syslog_src)
-	 if stat ~= nil and stat.mode == 'socket' then
-	    bind_syslog = { string.format("%s:%s",M.syslog_src,"/dev/log") }
-	    --ngx.log(ngx.ERR,string.format("Binding %s in container %s", bind_syslog[1], name))
-	 else
-	    --ngx.log(ngx.ERR,string.format("%s is not writeable, not mounting in container %s",M.syslog_src, name))
-	 end
-      end
-      if bind_syslog ~= nil then
-	 res = docker.client:start_container{ id = id, payload = { PublishAllPorts = true, Binds = bind_syslog }}
-      else
-	 res = docker.client:start_container{ id = id, payload = { PublishAllPorts = true }}
-      end      
-      assert(res.status == 204, "Failed to start container " .. id .. " : " .. json.encode(res.body))
-      -- get back the container info to pull out the port mapping
-      res = docker.client:inspect_container{ id=id}
-      --p.dump(res)
-      assert(res.status == 200, "Could not inspect new container: " .. id)
-      local ports = res.body.NetworkSettings.Ports
-      local ThePort = string.format("%d/tcp", M.private_port)
+    -- don't wrap this in a pcall, if it fails let it propagate to
+    -- the caller
+    portmap = get_notebooks()
+    assert(portmap[name] == nil, "Notebook by this name already exists: " .. name)
+    local conf = docker.config()
+    local bind_syslog = nil
+    conf.Image = string.format("%s:%s",M.repository_image,M.repository_version)
+    conf.Cmd={name}
+    conf.PortSpecs = {tostring(M.private_port)}
+    ngx.log(ngx.INFO,string.format("Spinning up instance of %s on port %d",conf.Image, M.private_port))
+    -- we wrap the next call in pcall because we want to trap the case where we get an
+    -- error and try deleting the old container and creating a new one again
+    local ok,res = pcall(docker.client.create_container, docker.client, { payload = conf, name = name})
+    if not ok and res.response.status >= 409 then
+        -- conflict, try to delete it and then create it again
+        ngx.log(ngx.ERR,string.format("conflicting notebook, removing notebook named: %s",name))   
+        ok, res = pcall( docker.client.remove_container, docker.client, { id = name })
+        ngx.log(ngx.ERR,string.format("response from remove_container: %s", p.write(res.response)))
+        -- ignore the response and retry the create, and if it still errors, let that propagate
+        ok, res = pcall(docker.client.create_container, docker.client, { payload = conf, name = name})
+    end
+    if ok then
+        assert(res.status == 201, "Failed to create container: " .. json.encode(res.body))
+        local id = res.body.Id
+        if M.syslog_src then
+            -- Make sure it exists and is writeable
+            local stat = lfs.attributes(M.syslog_src)
+            if stat ~= nil and stat.mode == 'socket' then
+                bind_syslog = { string.format("%s:%s",M.syslog_src,"/dev/log") }
+                --ngx.log(ngx.ERR,string.format("Binding %s in container %s", bind_syslog[1], name))
+            else
+                --ngx.log(ngx.ERR,string.format("%s is not writeable, not mounting in container %s",M.syslog_src, name))
+            end
+        end
+        if bind_syslog ~= nil then
+            res = docker.client:start_container{ id = id, payload = { PublishAllPorts = true, Binds = bind_syslog }}
+        else
+            res = docker.client:start_container{ id = id, payload = { PublishAllPorts = true }}
+        end      
+        assert(res.status == 204, "Failed to start container " .. id .. " : " .. json.encode(res.body))
+        -- get back the container info to pull out the port mapping
+        res = docker.client:inspect_container{ id=id}
+        --p.dump(res)
+        assert(res.status == 200, "Could not inspect new container: " .. id)
+        local ports = res.body.NetworkSettings.Ports
+        local ThePort = string.format("%d/tcp", M.private_port)
 
-      local log, occ = string.gsub(res.body.HostsPath,"hosts","root/tmp/kbase-narrative.log")
-      local ct=5
-      local ready=0
-      while (ct and not ready) do
-         local f=io.open(name,"r")
-         if f ~= nil then 
-            io.close(f) 
-            ready = 1 
-         end
-         ct = ct - 1
-         ngx.sleep(2)
-      end
-      if not ready then
-         local msg = "Time out starting container: " .. id
-         ngx.log(ngx.ERR,msg)
-         error(msg)
-      end
+        local log, occ = string.gsub(res.body.HostsPath, "hosts", "root/tmp/kbase-narrative.log")
+        local count = 5
+        local ready = 0
+        while (count and not ready) do
+            ngx.log(ngx.INFO, "Testing for presense of kbase-narrative log file.")
+            local f = io.open(name, "r")
+            if f ~= nil then 
+                io.close(f) 
+                ready = 1 
+            end
+            ct = ct - 1
+            ngx.sleep(2)
+        end
+        if not ready then
+            local msg = "Time out starting container: " .. id
+            ngx.log(ngx.ERR,msg)
+            error(msg)
+        end
 
-      assert(ports[ThePort] ~= nil, string.format("Port binding for port %s not found!",ThePort))
-      return(string.format("%s:%d","127.0.0.1", ports[ThePort][1].HostPort))
-   else
-      local msg = "Failed to create container: " .. p.write(res)
-      ngx.log(ngx.ERR,msg)
-      error(msg)
-   end
+        assert(ports[ThePort] ~= nil, string.format("Port binding for port %s not found!",ThePort))
+        return(string.format("%s:%d","127.0.0.1", ports[ThePort][1].HostPort))
+    else
+        local msg = "Failed to create container: " .. p.write(res)
+        ngx.log(ngx.ERR,msg)
+        error(msg)
+    end
 end
 
 --

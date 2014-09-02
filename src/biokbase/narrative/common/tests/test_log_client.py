48cc9a14c7f095a1f84a86189f3effcfbba21384
Dan Gunter <dkgunter@lbl.gov>
2014-09-02 15:03:51 -0700

    added client for testing, though testing isnt very automated yet

---
+++ src/biokbase/narrative/common/tests/test_log_client.py
@@ -0,0 +1,15 @@
+"""
+Test log proxy and kblogging
+"""
+__author__ = 'Dan Gunter <dkgunter@lbl.gov>'
+
+import unittest
+
+class TestCase(unittest.TestCase):
+#    def test_something(self):
+    pass
+
+# Not entirely clear what to do here..
+
+if __name__ == '__main__':
+    unittest.main()

12aa05fcabcae36b78c2f0d5b629d7a46d04f411
Dan Gunter <dkgunter@lbl.gov>
2014-11-05 12:30:36 -0800

    tweaks to sample PHP

--- src/scripts/query.php
+++ src/scripts/query.php
@@ -2,17 +2,31 @@
 <head>PHP test</head>
 <body>
 <?php
-function total_by_user($db, $from_date, $to_date) {
+function agg($db, $from_date, $to_date, $group1, $group2) {
+    $groups = "";
+    $groups_pre = "";
+    if ($group1 !== null) {
+        $groups .= $group1;
+        if ($group2 !== null) {
+            $groups .= "," . $group2;
+        }
+        $groups_pre = $groups . ",";
+    }
     $where = "event = 'F' and date >= '" . $from_date . "' and date <= '" . $to_date . "'";
-    $qry = "select user,date,sum(dur) as 'total', count(*) as 'count' from narrative where " . $where . " group by user";
+    $qry = "select " . $groups_pre . "date,sum(dur) as 'total', count(*) as 'count' from narrative";
+    $qry .= " where " . $where;
+    if ($groups != "") {
+        $qry .= " group by " . $groups;
+    }
+    echo '<h2>Query</h2><p>' . $qry . '</p>';
     return $db->query($qry);
 }
 // Show a table of data
-echo '<table><tr><td>User</td><td>Date</td><td>Total (sec)</td><td>Count</td></tr>';
+echo '<table><tr><td>User</td><td>Method</td><td>Date</td><td>Total (sec)</td><td>Count</td></tr>';
 $db = new SQLite3("logs.db");
-$rows = total_by_user($db, '2014-10-01', '2014-11-01');
+$rows = agg($db, '2014-10-01', '2014-11-01', 'name', 'user');
 while ($row = $rows->fetchArray()) {
-    echo '<tr><td>' . $row['user'] . '</td><td>' . $row['date'] . '</td><td>' . $row['total'] . '</td><td>' . $row['count'] . '</td></tr>';
+    echo '<tr><td>' . $row['user'] . '</td><td>' . $row['name'] . '</td><td>' . $row['date'] . '</td><td>' . $row['total'] . '</td><td>' . $row['count'] . '</td></tr>';
 }
  echo '</table>';
 ?>

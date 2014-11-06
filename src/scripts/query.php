58e6e6af62bf1b31c784b56e4994dc14fecd94d2
Dan Gunter <dkgunter@lbl.gov>
2014-11-06 09:26:05 -0800

    query update

--- src/scripts/query.php
+++ src/scripts/query.php
@@ -12,23 +12,45 @@ function agg($db, $from_date, $to_date, $group1, $group2) {
         }
         $groups_pre = $groups . ",";
     }
-    $where = "event = 'F' and date >= '" . $from_date . "' and date <= '" . $to_date . "'";
-    $qry = "select " . $groups_pre . "date,sum(dur) as 'total', count(*) as 'count' from narrative";
+    $where = "date >= '" . $from_date . "' and date <= '" . $to_date . "'";
+    $qry = "select " . $groups_pre . "date,event,sum(dur) as 'total', count(*) as 'count' from narrative";
     $qry .= " where " . $where;
-    if ($groups != "") {
-        $qry .= " group by " . $groups;
+    if ($groups == "") {
+        $qry .= " group by event,date";
     }
+    else {
+        $qry .= " group by " . $groups . ",event,date";
+    }
+    $qry .= " order by date,event";
     echo '<h2>Query</h2><p>' . $qry . '</p>';
     return $db->query($qry);
 }
+function total_by_day($rows) {
+    $tbd = [];
+    $i = 0;
+    $rec = array();
+    while ($row = $rows->fetchArray()) {
+        if ($row['event'] == 'F') {
+            /* create new record */
+            $rec = array('date' => $row['date'],
+                'meta' => array('generated' => 'whenever'),
+                'methodCount' => $row['count'],
+                'methodRunTime' => $row['total']);
+        }
+        elseif ($row['event'] == 'O') {
+            /* add existing record to result */
+            $rec['narrativeCount'] = $row['count'];
+            $tbd[$i++] = $rec;
+        }
+    }
+    return array('total_by_day' => $tbd);
+}
+
 // Show a table of data
 echo '<table><tr><td>User</td><td>Method</td><td>Date</td><td>Total (sec)</td><td>Count</td></tr>';
 $db = new SQLite3("logs.db");
-$rows = agg($db, '2014-10-01', '2014-11-01', 'name', 'user');
-while ($row = $rows->fetchArray()) {
-    echo '<tr><td>' . $row['user'] . '</td><td>' . $row['name'] . '</td><td>' . $row['date'] . '</td><td>' . $row['total'] . '</td><td>' . $row['count'] . '</td></tr>';
-}
- echo '</table>';
+$rows = agg($db, '2014-11-01', '2014-12-01');
+echo json_encode(total_by_day($rows));
 ?>
 </body>
 </html>

1c88875f6faa619b1f8066029b1980bc64d12eaa
Dan Gunter <dkgunter@lbl.gov>
2014-11-26 14:29:38 -0800

    fixed logging to remote (eg syslog) to *finally* have the right information in it

--- src/biokbase/narrative/common/kblogging.py
+++ src/biokbase/narrative/common/kblogging.py
@@ -1,21 +1,12 @@
 """
 Common narrative logging functions.
 
-Other biokbase modules can use the logging like this:
 
-    from biokbase.narrative.common.kblogging import get_logger
-    _log = get_logger(__name__)
+To log an event with proper metadata and formatting use 'log_event':
 
-Log messages can be free-form, *but* the desired format is:
+You can also do free-form logs, but these will be ignored by
+most upstream consumers.
 
-    Event_name;Key1=value1 Key2=Value2 (..etc..)
-
-In this format the "Event_name" is taken as the canonical name of what event in the
-system this is logging and the Key/value pairs are parsed into a mapping.
-Any non-key/value text will be combined into a single text field.
-
-Logging to MongoDB will be enabled, via proxy, if the proxy is
-running on the pre-configured host/port
 """
 __author__ = 'Dan Gunter <dkgunter@lbl.gov>'
 __date__ = '2014-07-31'
@@ -24,7 +15,6 @@ import collections
 import logging
 from logging import handlers
 import os
-import re
 import threading
 import time
 # Local
@@ -42,14 +32,13 @@ KBASE_PROXY_ENV = 'KBASE_PROXY_CONFIG'
 
 ## Internal logging
 
-_log = logging.getLogger("kblogging")
-_h = logging.StreamHandler()
-_h.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s %(name)s: %(message)s"))
-_log.addHandler(_h)
-lvl = logging.DEBUG if os.environ.get('KBASE_DEBUG', False) else logging.WARN
-_log.setLevel(lvl)
+_log = logging.getLogger("tornado.application")
 
-## Functions
+# WTF is going on logging
+#def _logdbg(m):
+#    open("/tmp/wtf", "a").write(m + "\n")
+
+## External functions
 
 def get_logger(name="", init=False):
     """Get a given KBase log obj.
@@ -63,10 +52,23 @@ def get_logger(name="", init=False):
     """
     if init:
         reset_handlers()
-    log = logging.getLogger(_kbase_log_name(name))
-    return LogAdapter(log, _get_meta())
+    return logging.getLogger(_kbase_log_name(name))
+
+def log_event(log, event, mapping):
+    """Log an event and a mapping.
+
+    For example::
+
+        log_event(_log, "collision", {"who":"unstoppable force",
+                  "with":"immovable object", "where":"kbase"})
+    """
+    msg = format_event(event, mapping)
+    log.info(msg)
+
+## Internal functions and classes
 
 def _kbase_log_name(name):
+    """Smarter name of KBase logger."""
     # no name => root
     if not name:
         return "biokbase"
@@ -76,97 +78,28 @@ def _kbase_log_name(name):
     # relative name
     return "biokbase." + name
 
-def log_event(log, event, mapping):
-    """Log an event and a mapping."""
-    log.info(format_event(event, mapping))
+def _has_handler_type(log, type_):
+    return any(map(lambda h: isinstance(h, type_), log.handlers))
 
-class LogAdapter(logging.LoggerAdapter):
-    """Add some extra methods to the stock LoggerAdapter."""
-    def __init__(self, log, extra):
-        logging.LoggerAdapter.__init__(self, log, extra)
-        self.handlers = log.handlers
-        self.addHandler = log.addHandler
-        self.removeHandler = log.removeHandler
-        self.setLevel = log.setLevel
-        self.isEnabledFor = log.isEnabledFor
-        self.file_handler, self.socket_handler = None, None
-
-    def addHandler(self, h):
-        """Track known handlers for efficient access later."""
-        if isinstance(h, logging.FileHandler):
-            self.file_handler = h
-        elif isinstance(h, handlers.SocketHandler):
-            self.socket_handler = h
-        self.logger.addHandler(h)
-
-    def removeHandler(self, h):
-        """Track known handlers for efficient access later."""
-        if isinstance(h, logging.FileHandler):
-            self.file_handler = None
-        elif isinstance(h, handlers.SocketHandler):
-            self.socket_handler = None
-        self.logger.removeHandler(h)
-
-    def shutdown(self):
-        """Close and remove all handlers."""
-        map(self.removeHandler, self.handlers)
-
-
-def _get_meta():
-    meta = {}
-
-    # Auth values
-    token = kbase_env.auth_token
-
-    if token:
-        # User
-        m = re.search('un=([^|]+)', token)
-        if m is not None:
-            meta['user'] = m.group(1)
-
-    # Session id
-    sess = kbase_env.session
-    if sess:
-        meta['session_id'] = sess
-
-    # Notebook name
-    if kbase_env.narrative:
-        meta['narr'] = kbase_env.narrative
-
-    # Client IP
-    meta['client_ip'] = kbase_env.client_ip or "0.0.0.0"
-
-    return meta
-
-
-class MetaFormatter(logging.Formatter):
-    def __init__(self):
-        """Format with metadata in the mix."""
-        logging.Formatter.__init__(
-            self,
-            "%(levelname)s %(asctime)s %(name)s %(message)s")
-
-    def format(self, record):
-        s = logging.Formatter.format(self, record)
-        return s
-        # XXX: This version adds env crap
-        # return "{} {}".format(s, ' '.join(["env.{}={}".format(k, v)
-        #                                    for k, v in os.environ.items()
-        #                                    if k.startswith('KB_')]))
+## Custom handlers
 
 class BufferedSocketHandler(handlers.SocketHandler):
-    """Proxy for another handler that always returns immediately
-    and queues up messages to send.
+    """Buffer up messages to a socket, sending them asynchronously.
+    Starts a separate thread to pull messages off and send them.
+    Ignores any messages that did not come from `log_event()`, above.
     """
     def __init__(self, *args):
         handlers.SocketHandler.__init__(self, *args)
-        _log.debug("Created SocketHandler with args = {}".format(args))
+        self._dbg = _log.isEnabledFor(logging.DEBUG)
+        if self._dbg:
+            _log.debug("Created SocketHandler with args = {}".format(args))
         self.buf = collections.deque([], 100)
         self.buf_lock = threading.Lock()
         # start thread to send data from buffer
         self.thr = threading.Thread(target=self.emitter)
         self.thr.daemon = True
         self._stop = False
+        self.extra = {}
         self.thr.start()
 
     def close(self):
@@ -192,13 +125,26 @@ class BufferedSocketHandler(handlers.SocketHandler):
                 time.sleep(0.1)
 
     def emit(self, record):
-        if _log.isEnabledFor(logging.DEBUG):
-            _log.debug("Emit 1 record")
+        if self._skip(record):
+            return
+        # stuff 'extra' from environment into record
+        #_logdbg("@@ stuffing into record: {}".format(kbase_env))
+        record.__dict__.update(kbase_env)
         self.buf_lock.acquire()
         try:
             self.buf.append(record)
         finally:
             self.buf_lock.release()
+
+    def _skip(self, record):
+        """Return True if this record should not go to a socket"""
+        # Do not forward records that didn't get logged through
+        # kblogging.log_event
+        if record.funcName != 'log_event':
+            if self._dbg:
+                _log.debug("Skip: funcName {} != log_event"
+                           .format(record.funcName))
+            return
 
     def _emit(self, record):
         """Re-implement to return a success code."""
@@ -218,19 +164,23 @@ class BufferedSocketHandler(handlers.SocketHandler):
 
 
 def init_handlers():
-    """Initialize and add the log handlers."""
+    """Initialize and add the log handlers.
+    We only allow one FileHandler and one SocketHandler to exist,
+    no matter how many times this is called.
+    """
     # Turn on debugging by setting environment variable KBASE_DEBUG.
     if os.environ.get("KBASE_DEBUG", None):
         g_log.setLevel(logging.DEBUG)
     else:
         g_log.setLevel(logging.INFO)
 
-    if not g_log.file_handler:
+    if not _has_handler_type(g_log, logging.FileHandler):
         hndlr = logging.FileHandler(KBASE_TMP_LOGFILE)
-        hndlr.setFormatter(MetaFormatter())
+        fmtr = logging.Formatter("%(levelname)s %(asctime)s %(name)s %(message)s")
+        hndlr.setFormatter(fmtr)
         g_log.addHandler(hndlr)
 
-    if not g_log.socket_handler:
+    if not _has_handler_type(g_log, handlers.SocketHandler):
         cfg = get_proxy_config()
         g_log.debug("Opening socket to proxy at {}:{}".format(
             cfg.host, cfg.port))

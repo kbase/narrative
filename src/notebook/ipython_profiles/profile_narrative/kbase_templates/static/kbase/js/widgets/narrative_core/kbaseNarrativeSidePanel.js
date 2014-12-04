560fc5f9d01bdfb088c58ea1cffc649c5448cd4e
nconrad <nealconrad@gmail.com>
2014-12-03 17:00:13 -0800

    prototype of data owned data import

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/narrative_core/kbaseNarrativeSidePanel.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/narrative_core/kbaseNarrativeSidePanel.js
@@ -240,10 +240,22 @@
             })
 
             var self = this;
-            var user = 'nconrad';
+            var user = $("#signin-button").kbaseLogin('session', 'user_id');
 
             var body = this.$overlayBody;
             var footer = this.$overlayFooter;
+
+            // models
+            var myData = [],
+                sharedData = [],
+                publicData = [];
+
+            // models for filter selections
+            var query;
+
+            // models for options in type and workspace dropdowns
+            var types = [];
+                workspaces =[];
 
             // tab panels
             var minePanel = $('<div class="kb-import-panel">'),
@@ -262,9 +274,12 @@
                     {tabName: 'Import', content: importPanel},
                 ]);
 
+            sharedPanel.append('<div class="kb-import-content"><br>coming soon.</div>');
+            publicPanel.append('<div class="kb-import-content"><br>coming soon.</div>');
+            importPanel.append('<div class="kb-import-content"><br>coming soon.</div>');
+
             body.addClass('kb-side-panel');
             body.append($tabs.header).append($tabs.body);
-
 
             // It is silly to invoke a new object for each widget
             var auth = {token: $("#signin-button").kbaseLogin('session', 'token')}
@@ -272,6 +287,7 @@
 
 
             // get possible types (not used)
+            /*
             ws.list_all_types({}).done(function(res) {
                 console.log('types', res)
 
@@ -283,44 +299,8 @@
                     }
                 }
                 console.log('type_names', types)
-            })
+            })*/
             
-            // useable types
-            var types = ['Data type...',
-                         'KBaseGenomes.Genome', 
-                         'KBaseFBA.FBAModel',
-                         'KBaseFBAModeling.FBA',
-                         'KBaseGenomes.ContigSet',
-                         'KBaseGenomes.Pangenome',                         
-                         'KBaseFBA.Media'];
-
-            // add search box
-            var search = $('<input type="text" class="form-control kb-import-search" placeholder="Search objects" >');
-            search = $('<div class="col-md-4">').append(search);
-            minePanel.append(row);
-
-            search.keyup(function(e, v){
-                console.log(e, v, $(this).val())
-            })
-
-            // add type dropdown
-            var typeFilter = $('<select class="form-control kb-import-type-filter">');
-            typeFilter.append('<option>All types...</option>');
-            for (var i=1; i<types.length -1; i++) {
-                typeFilter.append('<option data-type="'+types[i]+'">'+
-                                    types[i].split('.')[1]+
-                                '</option>');
-            }
-            typeFilter = $('<div class="col-md-3">').append(typeFilter);
-
-            var row = $('<div class="row">').append(search, typeFilter);
-            minePanel.append(row);
-
-            // evengt for type dropdown
-            typeFilter.change(function(blah) {
-                var type = $(this).children('option:selected').data('type');
-                if (type) updateView(type);
-            })
 
             // add footer status container and button
             var importStatus = $('<div class="pull-left kb-import-status">');
@@ -349,107 +329,91 @@
             });
 
             // function used to update my data list
-            function getMyData(ids, type, user) {
+            function getMyData(workspaces) {
                 // clear view
                 content.html('');
                 content.loading();
 
-                if (type)
-                    var p = ws.list_objects({ids: ids, type: type});
-                else 
-                    var p = ws.list_objects({ids: ids});
+                var ws_ids = []
+                for (var i in workspaces) ws_ids.push(workspaces[i].id);
 
-                $.when(p).done(function(d) {
-                        content.rmLoading();
-                        console.log('heres the data', d);
-                        if (type) content.append('<i>'+d.length+'</i> '+ type.split('.')[1] + 's');
 
-                        // add each item
-                        for (var i in d) {
-                            var obj = d[i];
-                            var id = obj[0];
-                            var name = obj[1];
-                            var mod_type = obj[2].split('-')[0];
-                            var kind = mod_type.split('.')[1]
-                            var module = mod_type.split('.')[0];
-                            var wsID = obj[6];
-                            var ws = obj[7];
-                            var relativeTime = self.prettyTimestamp(obj[3]);
+                var p = ws.list_objects({ids: ws_ids});
+                return $.when(p).then(function(d) {
+                    content.rmLoading();                    
 
-                            var item = $('<div class="kb-import-item">')
-                                            .data('ref', wsID+'.'+id)
-                                            .data('obj-name', name);                                    
-                            item.append('<input type="checkbox" value="" class="pull-left kb-import-checkbox">');
-                            item.append('<a class="h4" href="'+
-                                            objURL(module, kind, ws, name)+
-                                            '" target="_blank">'+name+'</a>');
-                            item.append('<br>');
-                            item.append('<i>'+relativeTime+'</i>');
+                    myData = d;
+                    renderAllData();
 
-                            content.append(item);
-                        }
-
-                        // update view
-                        minePanel.append(content);
-
-                        // some events
-                        var selected = [];
-                        $('.kb-import-checkbox').change(function(){
-                            $(this).is(":checked")
-
-                            var item = $(this).parent('.kb-import-item');
-                            var ref = item.data('ref').replace(/\./g, '/');
-                            var name = item.data('obj-name');
-
-                            // update model for selected items
-                            if ($(this).is(":checked")) {
-                                selected.push({ref: ref, name: name});
-                            }
-                            else {
-                                for (var i=0; i<selected.length; i++) {
-                                    if (selected[0].ref == ref) 
-                                        selected.splice(i, 1);
-                                }
-                            }
-
-                            // disable/enable button
-                            if (selected.length > 0) btn.prop('disabled', false);
-                            else btn.prop('disabled', true);
-                        });
-
-                        // import items on button click
-                        btn.click(function() {
-                            if (selected.length == 0) return;
-
-                            var proms = copyObjects(selected, narWSName);
-                            $.when.apply($, proms).done(function(data) {
-                                importStatus.html('');
-                                var status = $('<span class="text-success">done.</span>');
-                                importStatus.append(status);
-                                status.delay(1000).fadeOut();
-
-                                // update sidebar data list
-                                self.trigger('updateDataList.Narrative');
-                            });
-                        });
+                    // update view
+                    minePanel.append(content);
+                    
                 })
+
             }
 
 
             function updateView(type) {
-                getMyWSIDs().done(function(ids) {
-                    getMyData(ids, (type ? type : null), user);
+                getMyWS().done(function(workspaces) {
+
+                    // update workspace dropdown model
+                    workspaces = workspaces; 
+
+                    getMyData(workspaces).done(function(filterOptions) {
+                        // possible filters via input
+                        var type, ws, query;
+
+                        // create workspace filter
+                        var wsInput = $('<select class="form-control kb-import-filter">');
+                        wsInput.append('<option>All narratives...</option>');
+                        for (var i=1; i < workspaces.length-1; i++) {
+                            wsInput.append('<option data-id="'+workspaces[i].id+'" data-name="'+workspaces[i].name+'">'+
+                                                  workspaces[i].name+
+                                            '</option>');
+                        }
+                        var wsFilter = $('<div class="col-md-4">').append(wsInput);
+
+                        // event for type dropdown
+                        wsInput.change(function() {
+                            ws = $(this).children('option:selected').data('name');
+                            renderFilteredData(type, ws, query);
+                        })
+
+
+                        // create type filter
+                        var typeInput = $('<select class="form-control kb-import-filter">');
+                        typeInput.append('<option>All types...</option>');
+                        for (var i=1; i < types.length-1; i++) {
+                            typeInput.append('<option data-type="'+types[i]+'">'+
+                                                  types[i].split('.')[1]+
+                                             '</option>');
+                        }
+                        var typeFilter = $('<div class="col-md-3">').append(typeInput);
+
+                        // event for type dropdown
+                        typeInput.change(function() {
+                            type = $(this).children('option:selected').data('type');
+                            renderFilteredData(type, ws, query);
+                        })
+
+
+                        // create filter (search)
+                        var filterInput = $('<input type="text" class="form-control kb-import-search" placeholder="Filter objects">');
+                        var searchFilter = $('<div class="col-md-4">').append(filterInput);
+
+                        // event for filter (search)
+                        filterInput.keyup(function(e){
+                            renderFilteredData(type, ws, this.value);
+                        });
+
+                        // add search, type, ws filter to dom
+                        var row = $('<div class="row">').append(searchFilter, typeFilter, wsFilter);
+                        minePanel.prepend(row);     
+                    });
+
+
                 });
             }
-
-            function objURL(module, type, ws, name) {
-                if (self.landingPageMap[module])
-                    return self.options.landingPageURL+self.landingPageMap[module][type]+'/'+ws+'/'+name;
-            }
-
-            function wsURL(ws) {
-                return self.options.landingPageURL+'ws/'+ws;
-            }            
 
             function copyObjects(objs, nar_ws_name) {
                 importStatus.html('Adding <i>'+objs.length+'</i> objects to narrative...');
@@ -458,26 +422,170 @@
                 for (var i in objs) {
                     var ref = objs[i].ref;
                     var name = objs[i].name;
-                    console.log('copying ', ref, 'to', nar_ws_name)
+                    console.log('copying ', ref, 'to', nar_ws_name);
                     proms.push( ws.copy_object({to: {workspace: nar_ws_name, name: name},
                                                 from: {ref: ref} }) );
                 }
                 return proms;
             }
 
-            function getMyWSIDs() {
+            function getMyWS() {
                 return ws.list_workspace_info({owners: [user]})
                         .then(function(d) {
-                            var ids = [];
-                            for (var i in d) ids.push(d[i][0]);
-                            return ids;
+                            var workspaces = [];
+                            for (var i in d) workspaces.push({id: d[i][0], name: d[i][1]});
+                            return workspaces;
                         })
             }
 
-            function getAllData() {
+            function events() {
+                var selected = [];
+                $('.kb-import-checkbox').unbind('change');
+                $('.kb-import-checkbox').change(function(){
+                    $(this).is(":checked");
 
+                    var item = $(this).parent('.kb-import-item');
+                    var ref = item.data('ref').replace(/\./g, '/');
+                    var name = item.data('obj-name');
+
+                    // update model for selected items
+                    if ($(this).is(":checked")) {
+                        selected.push({ref: ref, name: name});
+                    }
+                    else {
+                        for (var i=0; i<selected.length; i++) {
+                            if (selected[0].ref == ref) 
+                                selected.splice(i, 1);
+                        }
+                    }
+
+                    // disable/enable button
+                    if (selected.length > 0) btn.prop('disabled', false);
+                    else btn.prop('disabled', true);
+                });
+
+                // import items on button click
+                btn.unbind('click');
+                btn.click(function() {
+                    if (selected.length == 0) return;
+
+                    var proms = copyObjects(selected, narWSName);
+                    $.when.apply($, proms).done(function(data) {
+                        importStatus.html('');
+                        var status = $('<span class="text-success">done.</span>');
+                        importStatus.append(status);
+                        status.delay(1000).fadeOut();
+
+                        // update sidebar data list
+                        self.trigger('updateDataList.Narrative');
+                    });
+                });                
             }
 
+            function renderFilteredData(typeFilter, wsFilter, queryFilter) {
+                content.html('');
+                console.log('filtering on', typeFilter, wsFilter, queryFilter)
+
+                // add each item to view
+                for (var i in myData) {
+                    var obj = myData[i];
+
+                    var mod_type = obj[2].split('-')[0];
+                    var item = {id: obj[0],
+                                name: obj[1],
+                                mod_type: mod_type,
+                                version: obj[4],
+                                kind: mod_type.split('.')[1],
+                                module: mod_type.split('.')[0],
+                                wsID: obj[6],
+                                ws: obj[7],
+                                relativeTime: self.prettyTimestamp(obj[3]) }   
+
+
+                    // filter conditions
+                    if (queryFilter && item.name.toLowerCase().indexOf(queryFilter.toLowerCase()) == -1) 
+                        continue;
+                    if (typeFilter && typeFilter.split('.')[1] != item.kind) 
+                        continue;
+                    if (wsFilter && wsFilter != item.ws)
+                        continue;
+
+                    var item = rowTemplate(item);
+
+                    // update DOM
+                    content.append(item);
+                    events();
+                }
+            }
+
+            function renderAllData() {
+                content.html('');
+                // add each item to view
+                for (var i in myData) {
+                    var obj = myData[i];
+
+                    var mod_type = obj[2].split('-')[0];
+                    var item = {id: obj[0],
+                                name: obj[1],
+                                mod_type: mod_type,
+                                version: obj[4],
+                                kind: mod_type.split('.')[1],
+                                module: mod_type.split('.')[0],
+                                wsID: obj[6],
+                                ws: obj[7],
+                                relativeTime: self.prettyTimestamp(obj[3]) }                             
+
+                    // update model for types dropdown
+                    if (types.indexOf(mod_type) == -1) types.push(mod_type);
+
+                    var item = rowTemplate(item);
+
+                    // update DOM
+                    content.append(item);
+                    events();                    
+                }
+            }
+
+            function rowTemplate(obj) {
+                var item = $('<div class="kb-import-item">')
+                                .data('ref', obj.wsID+'.'+obj.id)
+                                .data('obj-name', obj.name);                                    
+                item.append('<input type="checkbox" value="" class="pull-left kb-import-checkbox">');
+                item.append('<a class="h4" href="'+
+                                objURL(obj.module, obj.kind, obj.ws, obj.name)+
+                                '" target="_blank">'+obj.name+'</a>'+
+                            '<span class="kb-data-list-version">v'+obj.version+'</span>');
+
+                item.append('<br>');
+
+                item.append('<div class="kb-import-info">'+
+                                '<span>TYPE</span><br>'+
+                                '<b>'+obj.kind+'</b>'+
+                            '</div>');
+                item.append('<div class="kb-import-info">'+
+                                '<span>NARRATIVE</span><br>'+
+                                '<b>'+obj.ws+'<b>'+   //<a class="" href="'+wsURL(obj.ws)+'">'
+                            '</div>');
+                item.append('<div class="kb-import-info">'+
+                                '<span>LAST MODIFIED</span><br>'+
+                                '<b>'+obj.relativeTime+'</b>'+
+                            '</div>');
+                item.append('<br><hr>')
+
+                return item;
+            }
+
+
+            function objURL(module, type, ws, name) {
+                if (self.landingPageMap[module])
+                    return self.options.landingPageURL+self.landingPageMap[module][type]+'/'+ws+'/'+name;
+                else 
+                    console.error('could not find a landing page mapping for', module);
+            }
+
+            function wsURL(ws) {
+                return self.options.landingPageURL+'ws/'+ws;
+            }            
 
 
         }, 

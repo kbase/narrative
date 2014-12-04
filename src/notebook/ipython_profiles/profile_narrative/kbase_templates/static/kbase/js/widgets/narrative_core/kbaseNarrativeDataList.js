4231f65d40c0e8f6588bc7bfe0f6ca9f045dc45f
msneddon <mwsneddon@lbl.gov>
2014-12-04 12:22:36 -0800

    use preloaded landing page map

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/narrative_core/kbaseNarrativeDataList.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/narrative_core/kbaseNarrativeDataList.js
@@ -961,11 +961,11 @@
         },
         
         getLandingPageMap: function() {
+            this.ws_landing_page_map = window.kbconfig.landing_page_map;
             /**
              * Get the landing page map.
              * First, try getting it from /functional-site/landing_page_map.json.
              * If that fails, try /static/kbase/js/widgets/landing_page_map.json.
-             */
             $.ajax({
                 url: '/functional-site/landing_page_map.json',
                 async: true,
@@ -987,7 +987,7 @@
                             this.ws_landing_page_map = null;
                         }, this)
                     })
-                }, this)});
+                }, this)});*/
         },
         
         /**

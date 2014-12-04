80b0857a7246ad8fe1c941ac00ba3d409c6b6171
Bill Riehl <wjriehl@lbl.gov>
2014-12-04 13:48:00 -0800

    Added a pile of debugging console logs

--- src/biokbase/narrative/common/kbjob_manager.py
+++ src/biokbase/narrative/common/kbjob_manager.py
@@ -111,8 +111,9 @@ class KBjobManager():
         for app_info in app_jobs:
             try:
                 app_state_list.append(self.get_app_state(app_info[1], app_info[2], app_info[3], app_info[0]))
-            except Exception:
-                app_state_list.append({'app_job_id' : app_info[0]})
+            except Exception as e:
+                import traceback
+                app_state_list.append({'app_job_id' : app_info[0], 'error' : e.__str__(), 'traceback' : traceback.format_exc()})
 
         jobs_info = {'methods' : meth_state_list, 'apps' : app_state_list}
 

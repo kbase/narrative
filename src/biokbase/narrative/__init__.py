be85240eccdba58435abe847daaf1798ef49fd8c
Dan Gunter <dkgunter@lbl.gov>
2014-12-11 08:13:56 -0800

    added docs for git workflow, and helper fn for version bump

--- src/biokbase/narrative/__init__.py
+++ src/biokbase/narrative/__init__.py
@@ -4,6 +4,23 @@ from semantic_version import Version
 __version__ = Version("0.2.1")
 version = lambda: __version__
 
-# if run directly, print current version
+# if run directly:
+#   no args = print current version
+#   1 arg = self-modify version to arg value
 if __name__ == '__main__':
-    print(version())
+    import sys
+    if len(sys.argv) == 1:
+        print(version())
+    elif len(sys.argv) == 2:
+        ver = sys.argv[1]
+        try:
+            Version(ver)
+        except:
+            print("Invalid version: {}".format(ver))
+            sys.exit(1)
+        oldver = '"' + str(version()) + '"'
+        newver = '"' + ver + '"'
+        oldself = open(__file__).read()
+        newself = oldself.replace(oldver, newver)
+        open(__file__, 'w').write(newself)
+    sys.exit(0)

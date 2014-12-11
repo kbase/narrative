df35e584662d22b06cf01101bfed44ab14eea578
Dan Gunter <dkgunter@lbl.gov>
2014-12-11 04:26:58 -0800

    print version from __init__

--- src/biokbase/narrative/__init__.py
+++ src/biokbase/narrative/__init__.py
@@ -3,3 +3,7 @@ __all__ = ['magics', 'mongonbmanager', 'ws_util', 'common', 'kbasewsmanager', 's
 from semantic_version import Version
 __version__ = Version("0.2.0")
 version = lambda: __version__
+
+# if run directly, print current version
+if __name__ == '__main__':
+    print(version())

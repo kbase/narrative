1b5d9dd40c7a2585a261ceddf0d4170ceaa4cc0f
Dan Gunter <dkgunter@lbl.gov>
2015-01-15 05:32:14 -0800

    added KBFail, KBError, KBFatal and some usages

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/kbaseNarrative.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/kbaseNarrative.js
@@ -1,7 +1,7 @@
 /**
  * This is the entry point for the Narrative's front-end. It initializes
  * the login session, fires up the data and function widgets, and creates
- * the kbaseNarrativeWorkspace wrapper around the IPython notebook that 
+ * the kbaseNarrativeWorkspace wrapper around the IPython notebook that
  * does fun things like manage widgets and cells and kernel events to talk to them.
  */
 "use strict";
@@ -45,7 +45,7 @@
 
     var $dataList = $('<div>');
     var $shareWidget = $dataList["kbaseNarrativeSharePanel"]({});
-    $('#kb-share-btn').popover({ 
+    $('#kb-share-btn').popover({
         html : true,
         placement : "bottom",
         //title: function() {
@@ -60,7 +60,7 @@
                     return "<br><br>Please name your Narrative before sharing.<br><br>"
                 }
             }
-            
+
             //!! arg!! I have to refresh to get reattach the events, which are lost when
             //the popover is hidden!!!  makes it a little slower because we refetch permissions from ws each time
             $shareWidget.refresh();
@@ -92,23 +92,74 @@ EndpointTester.prototype.test = function(url) {
         data: '{"params":{}, "version":"1.1", "method":"", "id":"' + Math.random() + '"}',
         url: this.url,
         success: $.proxy(function() { this.$target.html(this.okayText); }, this),
-        error: $.proxy(function(error) { 
+        error: $.proxy(function(error) {
             console.log('error!');
             console.log(error);
-            this.$target.html(this.downText); 
+            this.$target.html(this.downText);
         }, this),
     };
     var getTestParams = {
         type: 'GET',
         url: this.url,
         success: $.proxy(function() { this.$target.html(this.okayText); }, this),
-        error: $.proxy(function(error) { 
+        error: $.proxy(function(error) {
             $.ajax(postTestParams);
-            this.$target.html(this.downText); 
-        }, this),        
+            this.$target.html(this.downText);
+        }, this),
     }
     $.ajax(getTestParams);
 };
+
+/**
+ * Error logging for detectable failure conditions.
+ * Logs go through the kernel and thus are sent to the
+ * main KBase logging facility (Splunk, as of this writing).
+ *
+ * Usage:
+ *    KBFail(<is_it_fatal>, "what you were doing", "what happened");
+ * Returns: false if IPython not initialized yet, true otherwise
+ */
+var _kb_failed_once = false;
+var KBFail = function(is_fatal, where, what) {
+  if (!IPython || !IPython.notebook) {
+    return false;
+  }
+  var code = "";
+  if (_kb_failed_once == false) {
+    code += "from biokbase.narrative.common import kblogging\n";
+    _kb_failed_once = true;
+  }
+  code += "kblogging.NarrativeUIError(";
+  if (is_fatal) {
+    code += "True,";
+  }
+  else {
+    code += "False,";
+  }
+  if (where) {
+    code += 'where="' + where + '"';
+  }
+  if (what) {
+    if (where) { code += ", "; }
+    code += 'what="' + what + '"';
+  }
+  code += ")\n";
+  // Log the failure
+  IPython.notebook.kernel.execute(code);
+  return true;
+}
+/**
+ * Syntactic sugar for logging error vs. fatal error.
+ *
+ * Same as KBFail() with boolean flag replaced by different names
+ * for the function.
+ */
+var KBError = function(where, what) {
+  return KBFail(false, where, what);
+}
+var KBFatal = function(where, what) {
+  return KBFail(true, where, what);
+}
 
 var narrative = {};
 narrative.init = function() {
@@ -126,8 +177,8 @@ narrative.init = function() {
 
     var versionHtml = 'KBase Narrative<br>Alpha version';
     var endpointTesters = [];
-    if (window.kbconfig && 
-        window.kbconfig.name && 
+    if (window.kbconfig &&
+        window.kbconfig.name &&
         window.kbconfig.version) {
         var $versionDiv = $('<div>')
                           .append('<b>Version:</b> ' + window.kbconfig.version);
@@ -143,7 +194,7 @@ narrative.init = function() {
             var urlList = Object.keys(window.kbconfig.urls).sort();
             var $versionTable = $('<table>')
                                 .addClass('table table-striped table-bordered');
-            $.each(urlList, 
+            $.each(urlList,
                 function(idx, val) {
                     var url = window.kbconfig.urls[val].toString();
                     // if url looks like a url (starts with http), include it.
@@ -303,7 +354,7 @@ narrative.init = function() {
             // setTimeout(function() { $jobsWidget.refresh(); }, 750);
         }
         else {
-            /* ??? */
+            // ?
         }
     });
 };

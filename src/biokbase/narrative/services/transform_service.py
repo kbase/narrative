abb842c694ed7dcbb53f8b6961723fe632111a19
Gavin Price <gaprice@lbl.gov>
2015-01-20 14:28:15 -0800

    Add xform service call to backend for AssyFile->ContigSet
    - add transform service client
    - add call to backend
    - improve the url struct so that the urls aren't mixed in with the class
    dict

--- src/biokbase/narrative/services/transform_service.py
+++ src/biokbase/narrative/services/transform_service.py
@@ -9,24 +9,14 @@ __date__ = '1/15/15'
 # Imports
 
 import json
-# Third-party
-import IPython.utils.traitlets as trt
 # Local
 import biokbase.narrative.common.service as service
 from biokbase.narrative.common.service import init_service
 from biokbase.narrative.common.service import finalize_service
 from biokbase.narrative.common.service import method
-from biokbase.narrative_method_store.client import NarrativeMethodStore
-from biokbase.NarrativeJobService.Client import NarrativeJobService
-from biokbase.workspace.client import Workspace as workspaceService
-from biokbase.narrative.common.generic_service_calls import correct_method_specs_json
-from biokbase.narrative.common.generic_service_calls import create_app_for_njs
+from biokbase.Transform.Client import Transform
 
-## Globals
-
-_CT = 'content-type'
-_AJ = 'application/json'
-_URL_SCHEME = frozenset(['http', 'https'])
+#  Global vars
 
 VERSION = (0, 0, 1)
 NAME = "Transform Service"
@@ -49,33 +39,20 @@ def _assy_to_cs(meth, input_assyfile, output_contigset):
     :return: running job info
     """
     token, workspace = meth.token, meth.workspace_id
-    meth.debug(input_assyfile)
-    meth.debug(output_contigset)
-#     appSpec = json.loads(app_spec_json)
-#     paramValues = json.loads(param_values_json)
-#
-#     methIdToSpec = json.loads(correct_method_specs_json(method_specs_json))
+#     meth.debug(str(service.URLS))
+    transform_url = service.URLS.get_url('transform')
+    trans = Transform(transform_url, token=token)
+    _, ujs_job = trans.convert(
+        {'source_kbase_type': 'KBaseFile.Assembly',
+         'source_workspace_name': workspace,
+         'source_object_name': input_assyfile,
+         'destination_kbase_type': 'KBaseGenomes.ContigSet',
+         'destination_workspace_name': workspace,
+         'destination_object_name': output_contigset}
+    )
 
-    # raise ValueError("========\nExternal=" + method_specs_json +
-    # "\n=======================\nInternal=" + json.dumps(methIdToSpec))
-
-#     app = create_app_for_njs(
-#         workspace, token, service.URLS, appSpec['info']['id'],
-#         appSpec['steps'], methIdToSpec, paramValues)
-
-    # raise ValueError("App sending to NJS: " + json.dumps(app))
-#     meth.debug(json.dumps(app))
-#     njsClient = NarrativeJobService(service.URLS.__dict__['job_service'],
-#         token=token)
-#     appState = njsClient.run_app(app)
-
-    # assuming we get a job ID out of this, do the following:
-#     job_id = "njs:" + appState["job_id"]
-#     meth.register_app(job_id)
-
-#     return json.dumps({'job_id': job_id, 'app': app, 'app_state': appState})
-    meth.register_job("foo")
-    return json.dumps({'jobID': 'foo'})
+    meth.register_job(ujs_job)
+    return json.dumps({'jobID': ujs_job})
 
 # Finalize (registers service)
 finalize_service()

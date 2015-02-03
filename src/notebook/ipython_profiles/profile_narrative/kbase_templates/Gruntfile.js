b968d05849aaddcadb4f6cac76704ae32f4cabb0
Bill Riehl <wjriehl@lbl.gov>
2015-02-02 18:43:19 -0800

    Big commit! Made the gruntfile compile, minify, and version most of the KBase javascript in notebook.html. Fixed some issues with kbaseAssembly and kbaseSimulationSet that were causing problems when compiled.

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/Gruntfile.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/Gruntfile.js
@@ -1,62 +1,125 @@
-module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+'use strict';
+var path = require('path');
 
+// the actual "static" directory path, relative to this Gruntfile.
+// should be updated as necessary, if this moves
+var staticDir = 'static';
+
+/** 
+ * We need a config for the first step - concat - that plays nice with the Jinja directive-wrapped
+ * script src tag.
+ * All src strings are wrapped with the static_url function like this:
+ *     <script src="{{ static_url("path/to/script.js") }}"></script>"
+ * 
+ * This needs to be stripped so we actually have
+ *     static/path/to/script.js
+ * instead, and returned as the config object. This is ripped out of the block
+ * variable where all the raw stuff lives and parsed via regex, then we use
+ * a vaguely simple version of the rest of the createConfig code from 
+ * grunt-usemin's default config function, here:
+ * https://github.com/yeoman/grunt-usemin/blob/master/lib/config/concat.js
+ *
+ * This only needs to be done for the first step, since the minifier just uses the concatted 
+ * version.
+ */
+var createIPythonCompatibleConfig = function(context, block) {
+    var parsedFiles = [];
+    // regex for ripping out the static_url bit
+    // strings that match this regex will have the actual path and script in
+    // the second element of the match array
+    var pattern = /\<script\s*src=\"\{\{\s*static_url\(\"(\S+\.js)\"\)\s*\}\}\"/;
+    for (var i=0; i<block.raw.length; i++) {
+        var m = block.raw[i].match(pattern);
+        if (m && m.length === 2) {
+            parsedFiles.push(m[1]);
+        }
+    }
+    block.src = parsedFiles;
+    context.inFiles = parsedFiles;
+
+    // file paths are now parsed and fixed, set up the config
+    var cfg = {
+        files: []
+    };
+
+    var outfile = path.join(context.outDir, block.dest);
+    var files = {
+        dest: outfile,
+        src: []
+    };
+
+    context.inFiles.forEach(function (f) {
+        files.src.push(path.join(staticDir, f));
+    });
+    cfg.files.push(files);
+    context.outFiles = [block.dest];
+    return cfg;
+};
+
+module.exports = function(grunt) {
     // Project configuration
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-filerev');
+    grunt.loadNpmTasks('grunt-usemin');
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
+        useminPrepare: {
+            html: 'notebook.html',
             options: {
-                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-            },
-            my_target: {
-                src : ['static/kbase/js/widgets/function_input/kbaseNarrativeInput.js',
-                       'static/kbase/js/widgets/function_input/kbaseNarrativeMethodInput.js',
-                       'static/kbase/js/widgets/function_input/parameter_input/kbaseNarrativeParameterInput.js',
-                       'static/kbase/js/widgets/function_input/parameter_input/kbaseNarrativeParameterTextInput.js',
-                       'static/kbase/js/widgets/function_input/parameter_input/kbaseNarrativeParameterDropdownInput.js',
-                       'static/kbase/js/widgets/function_input/parameter_input/kbaseNarrativeParameterCheckboxInput.js',
-                       'static/kbase/js/widgets/function_input/parameter_input/kbaseNarrativeParameterTextareaInput.js',
-                       'static/kbase/js/widgets/function_input/parameter_input/kbaseNarrativeParameterFileInput.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeSidePanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeSideImportTab.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeSidePublicTab.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeExampleDataTab.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeCellMenu.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeMethodCell.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeAppCell.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeOutputCell.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeControlPanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeMethodPanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeDataPanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeManagePanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeDownloadPanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeDataList.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeAppPanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeJobsPanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeSharePanel.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeWorkspace.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeCell.js',
-                       'static/kbase/js/widgets/narrative_core/kbaseNarrativeDataCell.js',
-                       'static/kbase/js/kbaseNarrative.js',
-                       'static/kbase/js/widgets/function_output/kbaseNarrativeError.js',
-                       'static/kbase/js/widgets/function_output/kbaseDefaultNarrativeOutput.js',],
-                dest : '<%= pkg.name %>-<%= pkg.version %>.js',
+                staging: 'static/kbase/js/staging',
+                dest: 'static',
+                flow: {
+                    steps: {
+                        js: [{
+                            name: 'concat',
+                            createConfig: createIPythonCompatibleConfig
+                        }, 'uglifyjs' ]
+                    },
+                    post: {}
+                }
             }
         },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                sourceMap: true,
+            }
+        },
+        filerev: {
+            options: {
+                algorithm: 'md5',
+                length: 8
             },
-            my_target: {
-                src : '<%= pkg.name %>-<%= pkg.version %>.js',
-                dest : '<%= pkg.name %>-<%= pkg.version %>.min.js'
+            source: {
+                files: [{
+                    src: [
+                        'static/kbase/js/kbase-narrative.min.js',
+                    ],
+                    dest: 'static/kbase/js'
+                }]
+            }
+        },
+        usemin: {
+            html: 'notebook.html',
+            options: {
+                assetsDirs: ['static'],
+                blockReplacements: {
+                    js: function (block) {
+                        return '<script src="{{ static_url("' + block.dest + '") }}" type="text/javascript" charset="utf-8"></script>';
+                    }
+                }
             }
         }
     });
-/*
 
 
-*/
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('build', [
+        'useminPrepare',
+        'concat:generated',
+        'uglify:generated',
+        'filerev',
+        'usemin'
+    ]);
 };

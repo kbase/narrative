c741321a3dc7a820be8bf90f8d9af9bdfebc5f45
Bill Riehl <wjriehl@lbl.gov>
2015-04-16 17:09:23 -0700

    Added updated build.js file.

---
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/build.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: "./static",
+    mainConfigFile: "static/narrative_paths.js",
+    findNestedDependencies: true,
+    // optimize: "none",
+    name: "narrative_paths",
+    out: "static/dist/kbase-narrative-built.js"
+})

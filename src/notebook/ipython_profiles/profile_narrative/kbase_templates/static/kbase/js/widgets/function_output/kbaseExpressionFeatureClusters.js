0d83f199fe0839464fb526ec440b8c09ce98baa5
Pavel Novichkov <psnovichkov@lbl.gov>
2015-08-18 01:26:17 -0700

    A column with action buttons is added to a table of clusters. From the context menu user can launch several types of vizs.

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseExpressionFeatureClusters.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/widgets/function_output/kbaseExpressionFeatureClusters.js
@@ -26,6 +26,7 @@ define(['jquery',
 		// Extracted data for vizualization
 		clusterSet: null,
 		expMatrixRef: null,
+		expMatrixName: null,
 		genomeRef: null,
 		matrixRowIds: null,
 		matrixColIds: null,
@@ -54,7 +55,7 @@ define(['jquery',
 			this.ws = new Workspace(this.options.workspaceURL, auth);
 		   
 			// Let's go...
-			this.loadAndrender();           
+			this.loadAndRender();           
 		   
 			return this;
 		},
@@ -65,7 +66,7 @@ define(['jquery',
 			return this;
 		},
 
-		loadAndrender: function(){
+		loadAndRender: function(){
 
 			var self = this;
 
@@ -74,13 +75,12 @@ define(['jquery',
 			var kbws = this.ws;
 			var clusterSetRef = self.buildObjectIdentity(this.options.workspaceID, this.options.clusterSetID);
 
-			console.log("kbws.get_objects([clusterSetRef]", clusterSetRef);
 			kbws.get_objects([clusterSetRef], 
 				function(data) {
 					self.clusterSet = data[0].data;
 					self.expMatrixRef = self.clusterSet.original_data;
 
-					console.log("kbws.get_object_subset", self.expMatrixRef);
+					console.log("expMatrixRef", self.expMatrixRef);
 
 					kbws.get_object_subset([					
 							{ 'ref':self.expMatrixRef, 'included':['/genome_ref'] },
@@ -92,8 +92,8 @@ define(['jquery',
 							self.matrixRowIds = data[1].data.data.row_ids;
 							self.matrixColIds = data[2].data.data.col_ids;	
 
-							console.log("kbws.get_object_subset", self.genomeRef);
-							kbws.get_object_subset([
+							console.log("genomeRef", self.genomeRef);
+							var jobGetGenomeData = kbws.get_object_subset([
 									{ 'ref':self.genomeRef, 'included':['/id'] },
 									{ 'ref':self.genomeRef, 'included':['/scientific_name'] },
 									{ 'ref':self.genomeRef, 'included':['/features'] }								
@@ -102,13 +102,31 @@ define(['jquery',
 									self.genomeID = data[0].data.id;
 									self.genomeName = data[1].data.scientific_name;
 									self.features = data[2].data.features;
-
-									// Now we are ready to visualize it
-									self.render();
+									console.log("genomeName", self.genomeName);
 								}, 
 								function(error){
+									self.clientError(error);
 								}
 							);
+
+							var jobGetMatrixName = kbws.get_object_info_new({
+									'objects' : [{'ref' : self.expMatrixRef}],
+									'includeMetadata' :0
+								},
+								function(data){
+									self.expMatrixName = data[0][1];
+									console.log("expMatrixName", self.expMatrixName);
+								}
+							);
+
+							// Wait until all  jobs are done
+							$.when.apply($, [jobGetMatrixName, jobGetGenomeData]).done( 
+								function(){
+									// Now we are ready to visualize it
+									self.render();
+								}
+							);							
+
 						}, 
 						function(error){
 							self.clientError(error);
@@ -122,18 +140,17 @@ define(['jquery',
 
 		render: function(){
 			var self = this;
-            var container = this.$elem;
+            var $container = this.$elem;
 			var pref = this.uuid();
 			self.pref = pref;
-			//console.log("self.pref = ", self.pref);
 
 
 			self.loading(false);
 
 			///////////////////////////////////// Instantiating Tabs ////////////////////////////////////////////
-			container.empty();
+			$container.empty();
 			var tabPane = $('<div id="'+pref+'tab-content">');
-			container.append(tabPane);
+			$container.append(tabPane);
 
 			tabPane.kbaseTabs({canDelete : true, tabs : []});                    
 			///////////////////////////////////// Overview table ////////////////////////////////////////////           
@@ -167,6 +184,14 @@ define(['jquery',
 
 			///////////////////////////////////// Clusters table ////////////////////////////////////////////          
 
+			self.buildActionMenu($container);
+			$(document).mousedown( function(e){
+				// Hide menu on mousedown only if we are not inside the menu
+				if(e.toElement.getAttribute('methodInput') == null){
+					self.$menu.hide();
+				}
+			});			
+
 			var tableClusters = $('<table id="'+pref+'clusters-table" \
 				class="table table-bordered table-striped" style="width: 100%; margin-left: 0px; margin-right: 0px;">\
 				</table>')
@@ -175,21 +200,80 @@ define(['jquery',
 			   "sDom": 'lftip',
 				"aaData": self.buildClustersTableData(),
 				 "aoColumns": [
-				//     {
-				//         width: "1em",
-				//         sortable: false,    
-				//         title: '<input type="checkbox" id="'+pref+'conditions_check_all"/>',
-				//         data: null,
-				//         render: function ( data, type, row ) {
-				//             return '<input type="checkbox" class="'+pref+'conditions_checkbox"/>';
-				//         }
-				//     }, 
 					{ sTitle: "Cluster", mData:"clusterId" },
 					{ sTitle: "Number of genes", mData:"size" },
 					{ sTitle: "Mean correlation", mData:"meancor" },
-				]
+                    { sTitle: "", mData: "rowIndex",
+                        mRender: function ( rowIndex ) {
+                        	return '<button class="btn btn-default ' + pref + 'action_button" rowIndex="' + rowIndex +'" >Action  <span class="caret"></span></button>';
+                        }
+                    }
+				],
+				"fnDrawCallback" :function(){
+					// It should be done here because otherwise initially hidden buttons will not work
+					self.registerActionButtonClick();
+				}
 			} );
 		},
+
+		registerActionButtonClick : function(){
+			var self = this;
+			var pref = self.pref;
+			$('.' + pref + 'action_button').on('click', function(e){
+				var $actionButton = $(e.toElement);
+				var x = $actionButton.offset().left - $('#notebook-container').offset().left + e.toElement.offsetLeft;
+				var y = $actionButton.offset().top - $('#notebook-container').offset().top + e.toElement.offsetHeight + e.toElement.offsetTop;				
+				self.$menu
+                    .data("invokedOn", $(e.target))
+                    .css({
+                        position: "absolute",                        
+                        left: x,
+                        top: y
+                    })
+                    .show()
+                    .off('click')
+                    .on('click', 'a', function (e) {
+                        self.$menu.hide();
+                
+                        var $invokedOn = self.$menu.data("invokedOn");
+                        var $selectedMenu = $(e.target);
+                        var rowIndex = $invokedOn[0].getAttribute('rowIndex');
+                        var methodInput = $selectedMenu[0].getAttribute('methodInput');
+
+                        var geneIds = self.getClusterGeneIds(rowIndex);
+
+						IPython.narrative.createAndRunMethod(methodInput, 
+							{
+								'input_expression_matrix':self.expMatrixName, 
+								'input_gene_ids': geneIds.join(",")
+							}
+						);
+                    });                
+			});
+		},
+
+		getClusterGeneIds: function(rowIndex){
+			var geneIds = [];
+			for(var geneId in this.clusterSet.feature_clusters[rowIndex].id_to_pos){
+				geneIds.push(geneId);
+			}
+			return geneIds;
+		},
+
+        getMenuPosition: function(mouse, direction, scrollDir) {
+        	var self = this;
+
+            var win = $(window)[direction](),
+                scroll = $(window)[scrollDir](),
+                menu = $(settings.menuSelector)[direction](),
+                position = mouse + scroll;
+                        
+            // opening menu would pass the side of the page
+            if (mouse + menu > win && menu < mouse) 
+                position -= menu;
+            
+            return position;
+        },
 
 		buildClustersTableData: function(){
 			var self = this;
@@ -204,7 +288,8 @@ define(['jquery',
 				tableData.push({
 					clusterId: "cluster_" + i,
 					size: Object.keys(feature_clusters[i].id_to_pos).length,
-					meancor : feature_clusters[i].meancor
+					meancor : feature_clusters[i].meancor.toFixed(2),
+					rowIndex : i
 				})
 			}
 
@@ -212,6 +297,22 @@ define(['jquery',
 		},
 
 	
+		buildActionMenu: function($container){
+			var $menu = $(' \
+				<ul id="contextMenu" class="dropdown-menu" role="menu" style="display:none" > \
+				    <li><a tabindex="-1" href="#" methodInput="view_expression_profile">View expression profile</a></li> \
+				    <li><a tabindex="-1" href="#" methodInput="view_expression_pairwise_correlation">Show pairwise correltaion</a></li> \
+				    <li><a tabindex="-1" href="#" methodInput="view_expression_heatmap">Browse conditions</a></li> \
+				</ul> \
+			');
+
+				    // <li class="divider"></li> \
+				    // <li><a tabindex="-1" href="#" methodInput="build_feature_set">Create a FeatureSet</a></li> \
+
+			$container.append($menu);	
+			this.$menu = $menu;
+		},
+
 		buildFeatureId2FeatureHash: function(){
 			var self = this;
 			var features = self.features;
@@ -227,15 +328,6 @@ define(['jquery',
 					   .append($("<th />").css('width','20%').append(name))
 					   .append($("<td />").append(value));
 			return $row;
-		},
-
-		getData: function() {
-			return {
-				type: 'ExpressionMatrix',
-				id: this.options.expressionMatrixID,
-				workspace: this.options.workspaceID,
-				title: 'Expression Matrix'
-			};
 		},
 
 		loading: function(isLoading) {

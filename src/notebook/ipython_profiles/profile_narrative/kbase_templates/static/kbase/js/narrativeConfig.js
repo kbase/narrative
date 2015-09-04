e68783a4718c1ba82382b24be5e305e861b4100f
Bill Riehl <wjriehl@lbl.gov>
2015-09-04 11:41:09 -0700

    Added an option in the config.json to use only locally deployed widgets, not those in a remote ui-common deployment.

--- src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/narrativeConfig.js
+++ src/notebook/ipython_profiles/profile_narrative/kbase_templates/static/kbase/js/narrativeConfig.js
@@ -1,6 +1,5 @@
 /**
  * Loads the required narrative configuration files.
- * This returns a Promise that will eventually hold the results.
  * This should mainly be invoked by the starting app, then
  * that result should be injected where necessary.
  *
@@ -17,11 +16,13 @@ define(['jquery',
             iconsSet) {
     "use strict";
 
+    // Get the workspace id from the URL
     var workspaceId = null;
     var m = window.location.href.match(/ws\.(\d+)\.obj\.(\d+)/);
     if (m && m.length > 1)
         workspaceId = parseInt(m[1]);
-    console.log(configSet);
+
+    // Build the config up from the configSet (from config.json)
     var config = {
         urls:            configSet[configSet['config']],
         version:         configSet['version'],
@@ -33,26 +34,45 @@ define(['jquery',
         icons:           iconsSet,
         workspaceId:     workspaceId,
         loading_gif:     configSet['loading_gif'],
+        use_local_widgets: configSet['use_local_widgets']
     };
+    // Add a remote UI-common to the Require.js config
     require.config({
         paths: {
             'uiCommonPaths': config.urls.ui_common_root + "widget-paths"
         }
     });
 
+    /**
+     * Updates the RequireJS config with additional locations from 
+     * a config given by the ui-common repo. This file is expected to be 
+     * called "widget-paths.js" and should be deployed in the configured
+     * ui-common location.
+     *
+     * Note that this is optional. If we're expected to use local widgets,
+     * as configured with the use_local_widgets flag, then skip this step
+     * and just run the callback.
+     */
     var updateConfig = function(callback) {
-        // var uiCommonPaths = config.urls.ui_common_root + "widget-paths.json";
-        require(['uiCommonPaths'], function(pathConfig) {
-            for (var name in pathConfig.paths) {
-                pathConfig.paths[name] = config.urls.ui_common_root + pathConfig.paths[name];
-            }
-            require.config(pathConfig);
-            config.new_paths = pathConfig;
-            callback(config);
-        }, function(error) { 
-            console.log("Unable to get updated widget paths. Sticking with what we've got.");
-            callback(config);
-        });
+        if (config.use_local_widgets != true) {
+            require(['uiCommonPaths'], function(pathConfig) {
+                for (var name in pathConfig.paths) {
+                    pathConfig.paths[name] = config.urls.ui_common_root + pathConfig.paths[name];
+                }
+                require.config(pathConfig);
+                config.new_paths = pathConfig;
+                if (callback)
+                    callback(config);
+            }, function(error) { 
+                console.log("Unable to get updated widget paths. Sticking with what we've got.");
+                if (callback)
+                    callback(config);
+            });
+        }
+        else {
+            if (callback)
+                callback(config);
+        }
     };
 
     return {

82927ed40f7f86c6d46ffb31b1c178e66512a414
Bill Riehl <wjriehl@lbl.gov>
2015-11-19 17:32:02 -0800

    Updated start script to accept command line variables

--- test/spec/kbase/js/kbaseNarrativeSpec.js
+++
@@ -1,7 +0,0 @@
-define(['jquery', 'kbwidget', 'narrativeLogin'], function($, KBWidget, Narrative) {
-    describe('Test the kbaseNarrative module', function() {
-        it('Should do things', function() {
-
-        });
-    });
-});

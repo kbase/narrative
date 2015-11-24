9cdb4f62918babe29618657dfb4835c52c59ada0
Erik Pearson <eapearson@lbl.gov>
2015-11-24 12:03:39 -0800

    add events to Markdown cells to support defocusing and rendering; override page resize method, the height calculation for the main body panel is incorrect (using size() not css('size')), fixes permanent scrollbar

--- kbase-extension/static/custom/custom.js
+++ kbase-extension/static/custom/custom.js
@@ -80,51 +80,62 @@
  * @class customjs
  * @static
  */
-define(['jquery', 
-        'base/js/namespace', 
-        'base/js/security',
-        'base/js/utils',
-        'notebook/js/notebook',
-        'notebook/js/textcell',
-        'notebook/js/savewidget',
-        'base/js/dialog',
-        'base/js/keyboard',
-        'notebook/js/cell',
-        'services/config',
-        'notebook/js/mathjaxutils',
-        'components/marked/lib/marked',
-        'components/requirejs/require',
-        'narrative_paths',
-        ],
-    function($, 
-             IPython, 
-             security,
-             utils,
-             notebook,
-             textCell,
-             saveWidget,
-             dialog,
-             keyboard,
-             cell,
-             config,
-             mathjaxutils,
-             marked) {
+define(['jquery',
+    'base/js/namespace',
+    'base/js/security',
+    'base/js/utils',
+    'base/js/page',
+    'notebook/js/notebook',
+    'notebook/js/textcell',
+    'notebook/js/savewidget',
+    'base/js/dialog',
+    'base/js/keyboard',
+    'notebook/js/cell',
+    'services/config',
+    'notebook/js/mathjaxutils',
+    'components/marked/lib/marked',
+    'components/requirejs/require',
+    'narrative_paths'    
+],
+    function ($,
+        IPython,
+        security,
+        utils,
+        page,
+        notebook,
+        textCell,
+        saveWidget,
+        dialog,
+        keyboard,
+        cell,
+        config,
+        mathjaxutils,
+        marked) {
         "use strict";
 
         // Patch the security mechanisms to allow any JavaScript to run for now.
         // TODO: update this so only the few KBase commands run.
-        security.sanitize_html = function(html, allow_css) { return html; };
-        security.sanitize_css = function(css, tagPolicy) { return css };
-        security.sanitize_stylesheets = function(html, tagPolicy) { return html };
+        security.sanitize_html = function (html, allow_css) {
+            return html;
+        };
+        security.sanitize_css = function (css, tagPolicy) {
+            return css;
+        };
+        security.sanitize_stylesheets = function (html, tagPolicy) {
+            return html;
+        };
 
         // Patch the MarkdownCell renderer to run the Javascript we need.
-        textCell.MarkdownCell.prototype.render = function() {
+        textCell.MarkdownCell.prototype.render = function () {
             var cont = textCell.TextCell.prototype.render.apply(this);
             if (cont) {
-                var that = this;
-                var text = this.get_text();
-                var math = null;
-                if (text === "") { text = this.placeholder; }
+                var that = this,
+                    text = this.get_text(),
+                    math = null,
+                    title, $html;
+                if (text === "") {
+                    text = this.placeholder;
+                }
                 var text_and_math = mathjaxutils.remove_math(text);
                 text = text_and_math[0];
                 math = text_and_math[1];
@@ -132,26 +143,43 @@ define(['jquery',
                     html = mathjaxutils.replace_math(html, math);
                     html = security.sanitize_html(html);
                     try {
-                        html = $($.parseHTML(html, undefined, true));
-                    }
-                    catch (error) {
-                        html = "Error while parsing markdown cell: " + error;
-                    }
-                    // add anchors to headings
-                    html.find(":header").addBack(":header").each(function (i, h) {
-                        h = $(h);
-                        var hash = h.text().replace(/ /g, '-');
-                        h.attr('id', hash);
-                        h.append(
-                            $('<a/>')
+                        $html = $($.parseHTML(html, undefined, true));
+                        
+                        // Extract title from h1, if any. otheriwse, first 50 characters
+                        var title = $html.filter('h1').first().first().text();
+                        if (!title) {
+                            title = $html.first().html().substr(0,50) || '';
+                        }
+                        
+                        // add anchors to headings
+                        $html.find(":header").addBack(":header").each(function (i, h) {
+                            h = $(h);
+                            var hash = h.text().replace(/ /g, '-');
+                            h.attr('id', hash);
+                            h.append(
+                                $('<a/>')
                                 .addClass('anchor-link')
                                 .attr('href', '#' + hash)
                                 .text('¶')
-                        );
-                    });
-                    // links in markdown cells should open in new tabs
-                    html.find("a[href]").not('[href^="#"]').attr("target", "_blank");
-                    that.set_rendered(html);
+                                );
+                        });
+                        // links in markdown cells should open in new tabs
+                        $html.find("a[href]").not('[href^="#"]').attr("target", "_blank");
+                        
+                        
+                    } catch (error) {
+                        title = 'Markdown Error';
+                        $html = "Error while parsing markdown cell: " + error;
+                    }
+
+                    $(that.element)
+                        .trigger('set-title', [title || '?']);
+                    
+                    console.log('setting icon for markdown');
+                    $(that.element)
+                        .trigger('set-icon', ['<i class="fa fa-2x fa-paragraph markdown-icon"></i>']);
+                    
+                    that.set_rendered($html);
                     that.typeset();
                     that.events.trigger("rendered.MarkdownCell", {cell: that});
                 });
@@ -159,23 +187,60 @@ define(['jquery',
             return cont;
         };
 
+        /** @method bind_events **/
+        textCell.MarkdownCell.prototype.bind_events = function () {
+            textCell.TextCell.prototype.bind_events.apply(this);
+            var that = this;
+
+            this.code_mirror.on('blur', function () {
+                that.render();
+            });
+            
+            var $cellNode = $(this.element);
+
+            $cellNode
+                .on('set-title', function (e, title) {
+                    if (title === undefined) {
+                        return;
+                    } 
+                    $cellNode.data('title', title);
+                    $cellNode             
+                        .find('.celltoolbar > .button_container')
+                        .trigger('set-title', [title || '?']);
+                });
+            $cellNode               
+                .on('set-icon', function (e, icon) {
+                   $cellNode.data('icon', icon);
+                    $cellNode
+                    .find('.celltoolbar > .button_container')
+                    .trigger('set-icon', [icon]);
+            });
+                
+
+//            this.element.click(function () {
+//                var cont = that.unrender();
+//                if (cont) {
+//                    that.focus_editor();
+//                }
+//            });
+        };
+
         // Patch the MarkdownCell renderer to throw an error when failing to render Javascript we need.
-        textCell.TextCell.prototype.set_rendered = function(text) {
+        textCell.TextCell.prototype.set_rendered = function (text) {
             try {
                 this.element.find('div.text_cell_render').html(text);
-            }
-            catch (error) {
+            } catch (error) {
                 this.element.find('div.text_cell_render').html("Error while parsing markdown cell: " + error);
             }
         };
 
         // Patch the Notebook to return the right name
-        notebook.Notebook.prototype.get_notebook_name = function() {
+        notebook.Notebook.prototype.get_notebook_name = function () {
             return this.metadata.name;
         };
 
         // Patch the Notebook to not wedge a file extension on a new Narrative name
-        notebook.Notebook.prototype.rename = function(new_name) {
+        notebook.Notebook.prototype.rename = function (new_name) {
             var that = this;
             var parent = utils.url_path_split(this.notebook_path)[0];
             var new_path = utils.url_path_join(parent, new_name);
@@ -191,24 +256,24 @@ define(['jquery',
         };
 
         // Patch the save widget to take in options at save time
-        saveWidget.SaveWidget.prototype.rename_notebook = function(options) {
+        saveWidget.SaveWidget.prototype.rename_notebook = function (options) {
             options = options || {};
             var that = this;
             var dialog_body = $('<div/>').append(
                 $("<p/>").addClass("rename-message")
-                    .text('Enter a new Narrative name:')
-            ).append(
+                .text('Enter a new Narrative name:')
+                ).append(
                 $("<br/>")
-            ).append(
-                $('<input/>').attr('type','text').attr('size','25').addClass('form-control')
+                ).append(
+                $('<input/>').attr('type', 'text').attr('size', '25').addClass('form-control')
                 .val(options.notebook.get_notebook_name())
-            );
+                );
             var d = dialog.modal({
                 title: "Rename Narrative",
                 body: dialog_body,
                 notebook: options.notebook,
                 keyboard_manager: this.keyboard_manager,
-                buttons : {
+                buttons: {
                     "OK": {
                         class: "btn-primary",
                         click: function () {
@@ -221,16 +286,16 @@ define(['jquery',
                                     that.notebook.metadata.name = new_name;
                                     that.element.find('span.filename').text(new_name);
                                 }, function (error) {
-                                    d.find('.rename-message').text(error.message || 'Unknown error');
-                                    d.find('input[type="text"]').prop('disabled', false).focus().select();
-                                }
+                                d.find('.rename-message').text(error.message || 'Unknown error');
+                                d.find('input[type="text"]').prop('disabled', false).focus().select();
+                            }
                             );
                             return false;
                         }
                     },
                     "Cancel": {}
-                    },
-                open : function () {
+                },
+                open: function () {
                     /**
                      * Upon ENTER, click the OK button.
                      */
@@ -249,11 +314,16 @@ define(['jquery',
         // Kickstart the Narrative loading routine once the notebook is loaded.
         $([IPython.events]).on('notebook_loaded.Notebook',
             function narrativeStart() {
-                require(['kbaseNarrative'], function(Narrative) {
+                require(['kbaseNarrative'], function (Narrative) {
                     IPython.narrative = new Narrative();
                     IPython.narrative.init();
                 });
             }
         );
+    
+        page.Page.prototype._resize_site = function() {
+        // Update the site's size.
+        $('div#site').height($(window).height() - $('#header').css('height'));
+    };
     }
 );

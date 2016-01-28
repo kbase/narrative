1c19df28954c8ca7d7d8a8b3e8409d5c7dfe04c7
Bill Riehl <wjriehl@lbl.gov>
2016-01-27 19:17:42 -0800

    Modified the data list to use Bluebird for loading.

--- kbase-extension/static/kbase/js/util/timeFormat.js
+++ kbase-extension/static/kbase/js/util/timeFormat.js
@@ -8,6 +8,8 @@
 define([], function() {
     'use strict';
 
+    var monthLookup = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
     /**
      * @method makePrettyTimestamp
      * Makes a span containing the 'started time' in units of time ago, with a Bootstrap 3 tooltip
@@ -227,12 +229,62 @@ define([], function() {
         return timeDiff.toFixed(1) + unit;
     }
 
+    /**
+     * Ported out from data list widget... needs updating.
+     * edited from: http://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+     */
+    function getTimeStampStr (objInfoTimeStamp) {
+        var date = new Date(objInfoTimeStamp);
+        var seconds = Math.floor((new Date() - date) / 1000);
+
+        // f-ing safari, need to add extra ':' delimiter to parse the timestamp
+        if (isNaN(seconds)) {
+            var tokens = objInfoTimeStamp.split('+');  // this is just the date without the GMT offset
+            var newTimestamp = tokens[0] + '+' + tokens[0].substr(0, 2) + ":" + tokens[1].substr(2, 2);
+            date = new Date(newTimestamp);
+            seconds = Math.floor((new Date() - date) / 1000);
+            if (isNaN(seconds)) {
+                // just in case that didn't work either, then parse without the timezone offset, but
+                // then just show the day and forget the fancy stuff...
+                date = new Date(tokens[0]);
+                return monthLookup[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+            }
+        }
+
+        var interval = Math.floor(seconds / 31536000);
+        if (interval > 1) {
+            return monthLookup[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            if (interval < 4) {
+                return interval + " months ago";
+            } else {
+                return monthLookup[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+            }
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days ago";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours ago";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes ago";
+        }
+        return Math.floor(seconds) + " seconds ago";
+    }
+
     return {
         parseDate: parseDate,
         prettyTimestamp: prettyTimestamp,
         calcTimeFromNow: calcTimeFromNow,
         calcTimeDifference: calcTimeDifference,
         reformatDate: reformatDate,
-        reformatISOTimeString: reformatISOTimeString
+        reformatISOTimeString: reformatISOTimeString,
+        getTimeStampStr: getTimeStampStr
     };
 });

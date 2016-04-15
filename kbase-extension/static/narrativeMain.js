b9b4273ffa3cda5fd36e0cec9b96117fcac304a6
Bill Riehl <wjriehl@lbl.gov>
2016-04-15 16:42:13 -0700

    Made changes to config module to fix static pages.

--- kbase-extension/static/narrativeMain.js
+++ kbase-extension/static/narrativeMain.js
@@ -1,21 +1,21 @@
 /*global define*/
 /*jslint white: true*/
 /**
- * A little wrapper script for making sure that Narrative code 
+ * A little wrapper script for making sure that Narrative code
  * is injected at the right time before loading the Notebook running
  * code.
- * 
+ *
  * Mainly, we need to load up a configuration, then make
- * sure that the other Narrative JS widgets are loaded *first*, 
+ * sure that the other Narrative JS widgets are loaded *first*,
  * because they're referenced from within Markdown cells at Notebook
  * load time.
- * 
+ *
  * I'm not thrilled about this, but if this isn't done, then our GUI
  * method/app/output widget cells are replaced by ReferenceErrors.
  *
  * Once this step is complete, the notebook main is run, and the
  * actual KBase Narrative startup code is instantiated in custom.js,
- * where is all ought to be. This is a ***temporary*** solution, 
+ * where is all ought to be. This is a ***temporary*** solution,
  * until we migrate our Frankenstein'd markdown cells to proper
  * output cells.
  *
@@ -39,10 +39,11 @@ require(['narrative_paths'], function (paths) {
         "use strict";
         console.log('Loading KBase Narrative setup routine.');
 
-        Config.loadConfig()
-            .then(function () {
-                return Config.updateConfig();
-            })
+        // Config.loadConfig()
+        //     .then(function () {
+        //         return Config.updateConfig();
+        //     })
+        Config.updateConfig()
             .then(function (config) {
                 require(['kbaseNarrative'], function (Narrative) {
                     Login.init($('#signin-button'));

bc12fc496bd5589b101f21a1a40344082c08cfa6
Bill Riehl <wjriehl@lbl.gov>
2016-05-06 17:39:35 -0700

    Moved biokbase.narrative.jobmanager to biokbase.narrative.jobs

---
+++ src/biokbase/narrative/jobs/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['job', 'method_util', 'methodmanager', 'jobmanager']
---
+++ src/biokbase/narrative/jobs/method_util.py
@@ -0,0 +1,69 @@
+"""
+Some utility functions for running KBase Apps or Methods or whatever they are this week.
+"""
+__author__ = "Bill Riehl <wjriehl@lbl.gov>"
+
+import os
+import re
+
+method_version_tags = ['release', 'beta', 'dev']
+
+def check_tag(tag, raise_exception=False):
+    """
+    Checks if the given tag is one of "release", "beta", or "dev".
+    Returns a boolean.
+    if raise_exception == True and the tag is bad, raises a ValueError
+    """
+    tag_exists = tag in method_version_tags
+    if not tag_exists and raise_exception:
+        raise ValueError("Can't find tag %s - allowed tags are %s" % (tag, ", ".join(method_version_tags)))
+    else:
+        return tag_exists
+
+def system_variable(var):
+    """
+    Returns a KBase system variable. Just a little wrapper.
+
+    Parameters
+    ----------
+    var: string, one of "workspace", "token", "user_id"
+        workspace - returns the KBase workspace name
+        token - returns the current user's token credential
+        user_id - returns the current user's id
+
+    if anything is not found, returns None
+    """
+    var = var.lower()
+    if var == 'workspace':
+        return os.environ.get('KB_WORKSPACE_ID', None)
+    elif var == 'token':
+        return os.environ.get('KB_AUTH_TOKEN', None)
+    elif var == 'user_id':
+        token = os.environ.get('KB_AUTH_TOKEN', None)
+        if token is None:
+            return None
+        m = re.match("un=(\w+)|", token)
+        if m is not None and len(m.groups()) == 1:
+            return m.group(1)
+        else:
+            return None
+
+def map_inputs_from_state(state, method_spec):
+    """
+    returns a dict of readable (ish) method inputs - those used for the run_method function - to the input values.
+    narrative_system_variables are ignored, so workspace names and tokens shouldn't show up.
+    """
+    input_dict = dict()
+    inputs = state['original_app']['steps'][0]['input_values'][0]
+    spec_inputs = method_spec['behavior']['kb_service_input_mapping']
+    # preprocess so it's O(1) lookup
+    targets_to_inputs = dict()
+    for spec_input in spec_inputs:
+        if 'input_parameter' in spec_input:
+            targets_to_inputs[spec_input['target_property']] = spec_input['input_parameter']
+
+    for target_name in inputs:
+        if target_name in targets_to_inputs:
+            input_dict[targets_to_inputs[target_name]] = inputs[target_name]
+
+    return input_dict

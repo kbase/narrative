34443851f6f435f9a474816bb353ecb2e70a32a1
Erik Pearson <eapearson@lbl.gov>
2016-05-10 07:24:52 -0700

    er, job-status not log-status.

--- kbase-extension/static/kbase/js/widgets/narrative_core/ipythonCellMenu.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/ipythonCellMenu.js
@@ -149,10 +149,10 @@ define([
         celltoolbar.CellToolbar.register_callback('kbase-edit-notebook-metadata', editNotebookMetadata);
         celltoolbar.CellToolbar.register_callback('kbase.menu', makeKBaseMenu);
         celltoolbar.CellToolbar.register_callback('kbase-status', status);
-        celltoolbar.CellToolbar.register_callback('kbase-log-status', jobStatus);
+        celltoolbar.CellToolbar.register_callback('kbase-job-status', jobStatus);
 
         // default.rawedit for the metadata editor
-        celltoolbar.CellToolbar.register_preset('KBase', ['kbase.menu', 'default.rawedit', 'kbase-toggle-input', 'kbase-edit-notebook-metadata', 'kbase-status', 'kbase-log-status']);
+        celltoolbar.CellToolbar.register_preset('KBase', ['kbase.menu', 'default.rawedit', 'kbase-toggle-input', 'kbase-edit-notebook-metadata', 'kbase-status', 'kbase-job-status']);
     };
     return {register: register};
 });

9a52f9250fdcb0a577ebbc3dcd81f4caf40efcd0
Bill Riehl <wjriehl@lbl.gov>
2016-05-12 16:35:10 -0700

    Made some updates, did mock non-ws testing!

--- src/biokbase/narrative/exporter/tests/test_narrativeexporting.py
+++ src/biokbase/narrative/exporter/tests/test_narrativeexporting.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 """
 Some tests for narrative exporting.
 """
@@ -8,39 +10,67 @@ from biokbase.narrative.exporter.exporter import NarrativeExporter
 import unittest
 import os
 import ConfigParser
+import mock
+import sys
+import json
+
+def read_narrative_file(path):
+    try:
+        with open(path, 'r') as f:
+            narr = json.loads(f.read())
+            f.close()
+            return narr
+    except Exception, e:
+        print("Unable to open file {}: {}".format(path, e))
+        sys.exit(1)
+
+def mock_read_narrative(bar):
+    if bar == test_narrative_ref:
+        return narr_file
+    elif bar == bad_narrative_ref:
+        raise ValueError('bad!')
+    elif bar == private_narrative_ref:
+        raise PermissionsError('private!')
 
 output_file = "test.html"
+config = ConfigParser.ConfigParser()
+config.read('test.cfg')
+
+test_narrative_ref = config.get('narrative_refs', 'public')
+private_narrative_ref = config.get('narrative_refs', 'private')
+bad_narrative_ref = config.get('narrative_refs', 'bad')
+narr_file = read_narrative_file(config.get('narrative_refs', 'narr_file'))
 
 class NarrativeExportTesting(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        config = ConfigParser.ConfigParser()
-        config.read('test.cfg')
-
-        self.test_narrative_ref = config.get('narrative_refs', 'public')
-        self.private_narrative_ref = config.get('narrative_refs', 'private')
-        self.bad_narrative_ref = config.get('narrative_refs', 'bad')
-
-        self.exporter = NarrativeExporter()
-
     @classmethod
     def tearDownClass(self):
         # delete generated file
         pass
-        # if os.path.isfile(output_file):
-        #     os.remove(output_file)
+        if os.path.isfile(output_file):
+            os.remove(output_file)
 
-    def test_export(self):
-        self.exporter.export_narrative(self.test_narrative_ref, output_file)
-        self.assertTrue(os.path.isfile(output_file))
+    @mock.patch('biokbase.narrative.exporter.exporter.NarrativeIO')
+    def test_export(self, mock_io):
+        mock_io.return_value.read_narrative = mock_read_narrative
+        mock_io.return_value.test_connection.return_value = ""
+        exporter = NarrativeExporter()
+        exporter.export_narrative(test_narrative_ref, output_file)
 
-    def test_export_bad_ref(self):
+    @mock.patch('biokbase.narrative.exporter.exporter.NarrativeIO')
+    def test_export_bad_ref(self, mock_io):
+        mock_io.return_value.test_connection.return_value = ""
+        mock_io.return_value.read_narrative = mock_read_narrative
+        exporter = NarrativeExporter()
         with self.assertRaises(ValueError) as err:
-            self.exporter.export_narrative(self.bad_narrative_ref, output_file)
+            exporter.export_narrative(bad_narrative_ref, output_file)
 
-    def test_export_private_ref(self):
+    @mock.patch('biokbase.narrative.exporter.exporter.NarrativeIO')
+    def test_export_private_ref(self, mock_io):
+        mock_io.return_value.test_connection.return_value = ""
+        mock_io.return_value.read_narrative = mock_read_narrative
+        exporter = NarrativeExporter()
         with self.assertRaises(PermissionsError) as err:
-            self.exporter.export_narrative(self.private_narrative_ref, output_file)
+            exporter.export_narrative(private_narrative_ref, output_file)
 
 if __name__ == "__main__":
     unittest.main()

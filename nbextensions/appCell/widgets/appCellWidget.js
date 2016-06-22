af61650595bff103c8c4e2570c0fe91b601f9a5f
Erik Pearson <eapearson@lbl.gov>
2016-06-22 11:02:54 -0700

    rename cell bus, start using it for all cell-wide events rename 'run' and 're-run' events to 'run-app' and 're-run-app' to avoid confusion with the ubiquitous 'run' event.

--- nbextensions/appCell/widgets/appCellWidget.js
+++ nbextensions/appCell/widgets/appCellWidget.js
@@ -54,7 +54,7 @@ define([
                 ui: {
                     buttons: {
                         enabled: ['remove'],
-                        disabled: ['run', 'cancel', 're-run']
+                        disabled: ['run-app', 'cancel', 're-run-app']
                     },
                     elements: {
                         show: ['parameters-group', 'output-group'],
@@ -81,8 +81,8 @@ define([
                 },
                 ui: {
                     buttons: {
-                        enabled: ['run', 'remove'],
-                        disabled: ['cancel', 're-run']
+                        enabled: ['run-app', 'remove'],
+                        disabled: ['cancel', 're-run-app']
                     },
                     elements: {
                         show: ['parameters-group', 'output-group'],
@@ -139,7 +139,7 @@ define([
                 ui: {
                     buttons: {
                         enabled: ['cancel', 'remove'],
-                        disabled: ['run', 're-run']
+                        disabled: ['run-app', 're-run-app']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group'],
@@ -208,7 +208,7 @@ define([
                 ui: {
                     buttons: {
                         enabled: ['cancel', 'remove'],
-                        disabled: ['run', 're-run']
+                        disabled: ['run-app', 're-run-app']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group'],
@@ -246,7 +246,7 @@ define([
                 ui: {
                     buttons: {
                         enabled: ['cancel', 'remove'],
-                        disabled: ['run', 're-run']
+                        disabled: ['run-app', 're-run-app']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group'],
@@ -278,8 +278,8 @@ define([
                 },
                 ui: {
                     buttons: {
-                        enabled: ['re-run', 'remove'],
-                        disabled: ['run', 'cancel']
+                        enabled: ['re-run-app', 'remove'],
+                        disabled: ['run-app', 'cancel']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group', 'output-group'],
@@ -313,8 +313,8 @@ define([
                 },
                 ui: {
                     buttons: {
-                        enabled: ['re-run', 'remove'],
-                        disabled: ['run', 'cancel']
+                        enabled: ['re-run-app', 'remove'],
+                        disabled: ['run-app', 'cancel']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group', 'output-group'],
@@ -341,8 +341,8 @@ define([
                 },
                 ui: {
                     buttons: {
-                        enabled: ['re-run', 'remove'],
-                        disabled: ['run', 'cancel']
+                        enabled: ['re-run-app', 'remove'],
+                        disabled: ['run-app', 'cancel']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group', 'output-group'],
@@ -369,8 +369,8 @@ define([
                 },
                 ui: {
                     buttons: {
-                        enabled: ['re-run', 'remove'],
-                        disabled: ['run', 'cancel']
+                        enabled: ['re-run-app', 'remove'],
+                        disabled: ['run-app', 'cancel']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group', 'output-group'],
@@ -396,8 +396,8 @@ define([
                 },
                 ui: {
                     buttons: {
-                        enabled: ['re-run', 'remove'],
-                        disabled: ['run', 'cancel']
+                        enabled: ['re-run-app', 'remove'],
+                        disabled: ['run-app', 'cancel']
                     },
                     elements: {
                         show: ['parameters-display-group', 'exec-group', 'output-group'],
@@ -420,9 +420,14 @@ define([
     function factory(config) {
         var container, places, dom,
             workspaceInfo = config.workspaceInfo,
+            runtime = Runtime.make(),
             cell = config.cell,
             parentBus = config.bus,
-            runtime = Runtime.make(),
+            // TODO: the cell bus should be created and managed through main.js,
+            // that is, the extension.
+            cellBus = runtime.bus().makeChannelBus({
+                    cell: utils.getMeta(cell, 'attributes', 'id')
+                }, 'A cell channel'),
             bus = runtime.bus().makeChannelBus(null, 'A app cell widget'),
             env = {},
             model,
@@ -783,13 +788,13 @@ define([
                                     body: [
                                         div({class: 'btn-toolbar'}, [
                                             div({class: 'btn-group'}, [
-                                                dom.makeButton('Run', 'run', {events: events, type: 'primary'})
+                                                dom.makeButton('Run', 'run-app', {events: events, type: 'primary'})
                                             ]),
                                             div({class: 'btn-group'}, [
                                                 dom.makeButton('Cancel', 'cancel', {events: events, type: 'danger'})
                                             ]),
                                             div({class: 'btn-group'}, [
-                                                dom.makeButton('Run Again', 're-run', {events: events, type: 'primary'})
+                                                dom.makeButton('Run Again', 're-run-app', {events: events, type: 'primary'})
                                             ]),
                                             div({class: 'btn-group'}, [
                                                 dom.makeButton('Remove', 'remove', {events: events, type: 'danger'})
@@ -1237,9 +1242,14 @@ define([
 
             // Remove all of the execution state when we reset the app.
             model.deleteItem('exec');
+            
+            // Also ensure that the exec widget is reset
+            // widgets.execWidget.bus.emit('reset');
 
             // TODO: evaluate the params again before we do this.
             fsm.newState({mode: 'editing', params: 'complete', code: 'built'});
+            
+            clearOutput();
 
             renderUI();
         }
@@ -1321,9 +1331,11 @@ define([
                         case 'in-progress':
                             return {mode: 'processing', stage: 'running'};
                         case 'completed':
+                            stopListeningForJobMessages();
                             return {mode: 'success'};
                         case 'suspend':
                         case 'error':
+                            stopListeningForJobMessages();
                             if (currentState.state.stage) {
                                 return {mode: 'error', stage: currentState.state.stage};
                             }
@@ -1369,10 +1381,10 @@ define([
             });
         }
 
+        var jobListener = null;
         function startListeningForJobMessages(jobId) {
             // console.log('Starting to listen for job messages', jobChannelId);
-
-            runtime.bus().listen({
+            jobListener = runtime.bus().listen({
                 channel: {
                     jobId: jobId
                 },
@@ -1381,62 +1393,30 @@ define([
                 },
                 handle: function (message) {
                     // Store the most recent job status (jobInfo) in the model and thus metadata.
-
                     var existingState = model.getItem('exec.jobState');
-                    if (!existingState || existingState.job_state !== message.job.state.job_state) {
-                        model.setItem('exec.jobState', message.job.state);
-                        // Forward the job info to the exec widget if it is available. (it should be!)
-                        if (widgets.execWidget) {
-                            widgets.execWidget.bus.emit('job-state', {
-                                jobState: message.job.state
-                            });
-                        }
+                    if (!existingState || existingState.job_state !== message.jobState.job_state) {
+                        model.setItem('exec.jobState', message.jobState);
+                        
+                        // Now we send the job state on the cell bus, generally.
+                        // The model is that a cell can only have one job active at a time.
+                        // Thus we can just emit the state of the current job globally
+                        // on the cell bus for thos widgets interested.
+                        cellBus.emit('job-state', {
+                            jobState: message.jobState
+                        });
                     } else {
-                        if (widgets.execWidget) {
-                            widgets.execWidget.bus.emit('job-state-updated', {
-                                jobId: message.job.state.job_id
-                            });
-                        }
+                        cellBus.emit('job-state-updated', {
+                            jobId: message.jobState.job_id
+                        })
                     }
                     model.setItem('exec.jobStateUpdated', new Date().getTime());
-                    updateFromJobState(message.job.state);
+                    updateFromJobState(message.jobState);
                 }
             });
-//
-//            runtime.bus().listen({
-//                channel: {
-//                    jobId: jobId
-//                },
-//                key: {
-//                    type: 'job-status'
-//                },
-//                handle: function (message) {
-//                    // Store the most recent job status (jobInfo) in the model and thus metadata.
-//                    updateFromJobState(message.job.state);
-//
-//                    var existingState = model.getItem('exec.jobState');
-//                    if (!existingState || existingState.job_state !== message.job.state.job_state) {
-//                        model.setItem('exec.jobState', message.job.state);
-//                        // Forward the job info to the exec widget if it is available. (it should be!)
-//                        if (widgets.execWidget) {
-//                            widgets.execWidget.bus.emit('job-state', {
-//                                jobState: message.job.state
-//                            });
-//                        }
-//                    } else {
-//                        if (widgets.execWidget) {
-//                            widgets.execWidget.bus.emit('job-state-updated', {
-//                                jobId: message.job.state.job_id
-//                            });
-//                        }
-//                    }
-//                    model.setItem('exec.jobStateUpdated', new Date().getTime());
-//                }
-//            });
         }
 
         function stopListeningForJobMessages() {
-            // TODO
+            runtime.bus().removeListener(jobListener);
         }
 
         /*
@@ -1593,6 +1573,17 @@ define([
 
             return newCellId;
         }
+        
+        function clearOutput() {
+            // cell.set_text('from biokbase.narrative.jobs import AppManager\nAppManager().clear_app()');
+            // cell.execute();
+            var cellNode = cell.element.get(0),
+                textNode = document.querySelector('.output_area.output_text');
+            
+            if (textNode) {
+                textNode.innerHTML = '';
+            }
+        }
 
         function findCellForId(id) {
             var matchingCells = Jupyter.notebook.get_cells().filter(function (cell) {
@@ -1650,7 +1641,7 @@ define([
                 params: model.copyItem('params')
             });
 
-            widgets.outputWidget.instance.bus.emit('update', {
+            widgets.outputWidget.instance.bus().emit('update', {
                 jobState: model.getItem('exec.jobState'),
                 output: model.getItem('output')
             });
@@ -1688,10 +1679,10 @@ define([
                         buttonNode.classList.remove('active');
                     }
                 });
-                bus.on('run', function () {
+                bus.on('run-app', function () {
                     doRun();
                 });
-                bus.on('re-run', function () {
+                bus.on('re-run-app', function () {
                     doRerun();
                 });
                 bus.on('cancel', function () {
@@ -1760,61 +1751,33 @@ define([
                 }
 
 
-                // console.log('INITIAL STATE IS ', state);
-
-
                 // TODO: only turn this on when we need it!
-                runtime.bus().makeChannelBus({
-                    cell: utils.getMeta(cell, 'attributes', 'id')
-                }, 'A cell channel');
-                runtime.bus().listen({
-                    channel: {
-                        cell: utils.getMeta(cell, 'attributes', 'id')
-                    },
-                    key: {
-                        type: 'run-status'
-                    },
-                    handle: function (message) {
-                        console.log('RUN-STATUS', message);
-                        updateFromLaunchEvent(message);
-                        utils.pushMeta(cell, 'appCell.exec.log', {
-                            timestamp: new Date(),
-                            event: 'runstatus',
-                            data: {
-                                jobId: message.jobId,
-                                runId: message.runId,
-                                status: message.event
-                            }
-                        });
-
-                        // Forward to the exec widget
-                        if (widgets.execWidget) {
-                            widgets.execWidget.bus.emit('launch-event', message);
+                cellBus.on('run-status', function (message) {
+                    updateFromLaunchEvent(message);
+                    utils.pushMeta(cell, 'appCell.exec.log', {
+                        timestamp: new Date(),
+                        event: 'runstatus',
+                        data: {
+                            jobId: message.jobId,
+                            runId: message.runId,
+                            status: message.event
                         }
-
-                    }
+                    });
                 });
-                runtime.bus().listen({
-                    channel: {
-                        cell: utils.getMeta(cell, 'attributes', 'id')
-                    },
-                    key: {
-                        type: 'output-cell-removed'
-                    },
-                    handle: function (message) {
-                        var output = model.getItem('output');
+                
+                cellBus.on('output-cell-removed', function (message) {
+                    var output = model.getItem('output');
 
-                        // console.log('HANDLE', message, output);
-                        if (!output.byJob[message.jobId]) {
-                            return;
-                        }
-                        delete output.byJob[message.jobId];
-                        model.setItem('output', output);
-                        widgets.outputWidget.instance.bus.emit('update', {
-                            jobState: model.getItem('exec.jobState'),
-                            output: output
-                        });
+                    // console.log('HANDLE', message, output);
+                    if (!output.byJob[message.jobId]) {
+                        return;
                     }
+                    delete output.byJob[message.jobId];
+                    model.setItem('output', output);
+                    widgets.outputWidget.instance.bus().emit('update', {
+                        jobState: model.getItem('exec.jobState'),
+                        output: output
+                    });
                 });
 
 //                runtime.bus().listen({
@@ -1975,6 +1938,7 @@ define([
                 }
 
                 require([selectedWidget], function (Widget) {
+                    // TODO: widget should make own bus.
                     var bus = runtime.bus().makeChannelBus(null, 'Parent comm bus for input widget'),
                         widget = Widget.make({
                             bus: bus,
@@ -2055,6 +2019,7 @@ define([
                 require([
                     'nbextensions/appCell/widgets/appParamsViewWidget'
                 ], function (Widget) {
+                    // TODO: widget should make own bus
                     var bus = runtime.bus().makeChannelBus(null, 'Parent comm bus for load input view widget'),
                         widget = Widget.make({
                             bus: bus,
@@ -2119,21 +2084,18 @@ define([
                 require([
                     'nbextensions/appCell/widgets/appExecWidget'
                 ], function (Widget) {
-                    var bus = runtime.bus().makeChannelBus(null, 'Parent comm bus for load exec widget'),
-                        widget = Widget.make({
-                            bus: bus,
+                    var widget = Widget.make({
+                            bus: cellBus,
                             workspaceInfo: workspaceInfo
                         });
                     widgets.execWidget = {
                         path: ['exec-group', 'widget'],
-                        bus: bus,
                         instance: widget
                     };
                     widget.start();
-                    bus.emit('run', {
+                    widget.bus().emit('run', {
                         node: dom.getElement('exec-group.widget'),
                         jobState: model.getItem('exec.jobState')
-                            // jobInfo:
                     });
                     resolve();
                 }, function (err) {
@@ -2156,7 +2118,7 @@ define([
                         instance: widget
                     };
                     widget.start();
-                    widget.bus.emit('run', {
+                    widget.bus().emit('run', {
                         node: dom.getElement('output-group.widget'),
                         jobState: model.getItem('exec.jobState'),
                         output: model.getItem('output')

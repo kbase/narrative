f13de69e4d7dc0c378e3381ffe83a32873c35448
Bill Riehl <wjriehl@lbl.gov>
2016-07-01 15:05:26 -0700

    Fixed issue with list-position of input parameters

--- src/biokbase/narrative/jobs/appmanager.py
+++ src/biokbase/narrative/jobs/appmanager.py
@@ -21,7 +21,6 @@ import json
 import re
 from biokbase.narrative.common import kblogging
 import logging
-from ipykernel.comm import Comm
 import datetime
 import traceback
 
@@ -112,9 +111,93 @@ class AppManager(object):
         """
         return self.spec_manager.available_apps(tag)
 
+    def run_local_app(self, app_id, tag="release", version=None, cell_id=None, run_id=None, **kwargs):
+        """
+        Attempts to run a local app. These do not return a Job object, but just the result of the app.
+        In most cases, this will be a Javascript display of the result, but could be anything.
+
+        If the app_spec looks like it makes a service call, then this raises a ValueError.
+        Otherwise, it validates each parameter in **kwargs against the app spec, executes it, and
+        returns the result.
+
+        Parameters:
+        -----------
+        app_id - should be from the app spec, e.g. 'view_expression_profile'
+        tag - optional, one of [release|beta|dev] (default=release)
+        version - optional, a semantic version string. Only released modules have
+                  versions, so if the tag is not 'release', and a version is given,
+                  a ValueError will be raised.
+        **kwargs - these are the set of parameters to be used with the app.
+                   They can be found by using the app_usage function. If any
+                   non-optional apps are missing, a ValueError will be raised.
+
+        Example:
+        run_local_app('NarrativeViewers/view_expression_profile', version='0.0.1', input_expression_matrix="MyMatrix", input_gene_ids="1234")
+        """
+
+        self._send_comm_message('run_status', {
+            'event': 'validating_app',
+            'event_at': datetime.datetime.utcnow().isoformat() + 'Z',
+            'cell_id': cell_id,
+            'run_id': run_id
+        })
+
+        ### TODO: this needs restructuring so that we can send back validation failure
+        ### messages. Perhaps a separate function and catch the errors, or return an
+        ### error structure.
+
+        # Intro tests:
+        self.spec_manager.check_app(app_id, tag, raise_exception=True)
+
+        if version is not None and tag != "release":
+            raise ValueError("App versions only apply to released app modules!")
+
+        # Get the spec & params
+        spec = self.spec_manager.get_spec(app_id, tag)
+
+        if not 'behavior' in spec:
+            raise ValueError("This app appears invalid - it has no defined behavior")
+
+        behavior = spec['behavior']
+
+        if 'kb_service_input_mapping' in behavior:
+            # it's a service! Should run this with run_app!
+            raise ValueError('This app appears to be a long-running job! Please start it using the run_app function instead.')
+
+        if 'script_module' in behavior or 'script_name' in behavior:
+            # It's an old NJS script. These don't work anymore.
+            raise ValueError('This app relies on a service that is now obsolete. Please contact the administrator.')
+
+        # Here, we just deal with two behaviors:
+        # 1. None of the above - it's a viewer.
+        # 2. python_class / python_function. Import and exec the python code.
+
+
+
 
 
     def run_app(self, *args, **kwargs):
+        """
+        Attempts to run the app, returns a Job with the running app info.
+        If this is given a cell_id, then returns None. If not, it returns the generated
+        Job object.
+
+        Parameters:
+        -----------
+        app_id - should be from the app spec, e.g. 'build_a_metabolic_model'
+                    or 'MegaHit/run_megahit'.
+        tag - optional, one of [release|beta|dev] (default=release)
+        version - optional, a semantic version string. Only released modules have
+                  versions, so if the tag is not 'release', and a version is given,
+                  a ValueError will be raised.
+        **kwargs - these are the set of parameters to be used with the app.
+                   They can be found by using the app_usage function. If any
+                   non-optional apps are missing, a ValueError will be raised.
+
+        Example:
+        --------
+        run_app('MegaHit/run_megahit', version=">=1.0.0", read_library_name="My_PE_Library", output_contigset_name="My_Contig_Assembly")
+        """
         try:
             return self._run_app_internal(*args, **kwargs)
         except Exception as e:
@@ -164,8 +247,6 @@ class AppManager(object):
             'run_id': run_id
         })
 
-        viewer_app = False
-
         ### TODO: this needs restructuring so that we can send back validation failure
         ### messages. Perhaps a separate function and catch the errors, or return an
         ### error structure.
@@ -188,10 +269,7 @@ class AppManager(object):
             raise Exception("This app appears invalid - it has no defined behavior")
 
         if not 'kb_service_input_mapping' in spec['behavior']:
-            if 'output_mapping' in spec['behavior']:
-                viewer_app = True
-            else:
-                raise Exception("This app is not SDK-compatible!")
+            raise Exception("This app does not appear to be a long-running job! Please use 'run_local_app' to start this instead.")
 
         # Preflight check the params - all required ones are present, all values are the right type, all numerical values are in given ranges
         spec_params = self.spec_manager.app_params(spec)
@@ -259,95 +337,59 @@ class AppManager(object):
             'run_id': run_id
         })
 
+        input_vals = self._map_inputs(spec['behavior']['kb_service_input_mapping'], params)
 
-        # Now we branch. If it's a viewer, make and register a transient 'ViewerJob' that'll handle the rest.
-        # Otherwise, make a real Job
-        if viewer_app:
-            log_info = {
-                'app_id': app_id,
-                'tag': tag,
-                'version': 'old_viewer',
-                'username': system_variable('user_id'),
-                'wsid': ws_id
-            }
-            self._log.setLevel(logging.INFO)
-            kblogging.log_event(self._log, "run_app", log_info)
-            self._send_comm_message('run_status', {
-                'event': 'launching_job',
-                'event_at': datetime.datetime.utcnow().isoformat() + 'Z',
-                'cell_id': cell_id,
-                'run_id': run_id
-            })
-            new_job = ViewerJob(self.viewer_count, app_id, [params], tag=tag, cell_id=cell_id)
-            self.viewer_count += 1
+        service_method = spec['behavior']['kb_service_method']
+        service_name = spec['behavior']['kb_service_name']
+        service_ver = spec['behavior'].get('kb_service_version', None)
+        service_url = spec['behavior']['kb_service_url']
 
-        else:
-            # Okay, NOW we can start the show
-            input_vals = dict()
-            for param in spec['behavior']['kb_service_input_mapping']:
-                p_value = None
-                p_id = param.get('target_property', 'invalid_mapping')
-                if 'input_parameter' in param:
-                    p_value = params.get(param['input_parameter'], None)
-                elif 'narrative_system_variable' in param:
-                    p_value = system_variable(param['narrative_system_variable'])
-                input_vals[p_id] = p_value
+        # This is what calls the function in the back end - Module.method
+        # This isn't the same as the app spec id.
+        function_name = service_name + '.' + service_method
+        job_meta = {'tag': tag}
+        if cell_id is not None:
+            job_meta['cell_id'] = cell_id
+        if run_id is not None:
+            job_meta['run_id'] = run_id
 
-            service_method = spec['behavior']['kb_service_method']
-            service_name = spec['behavior']['kb_service_name']
-            service_ver = spec['behavior'].get('kb_service_version', None)
-            #if (service_ver == None):
-            #    raise ValueError('Invalid method - service version (behavior.kb_service_version) not found in spec')
+        job_runner_inputs = {
+            'method' : function_name,
+            'service_ver' : service_ver,
+            'params' : input_vals,
+            'app_id' : app_id,
+            'wsid': ws_id,
+            'meta': job_meta
+        }
+        if len(ws_input_refs) > 0:
+            job_runner_inputs['source_ws_objects'] = ws_input_refs
 
-            service_url = spec['behavior']['kb_service_url']
+        log_info = {
+            'app_id': app_id,
+            'tag': tag,
+            'version': service_ver,
+            'username': system_variable('user_id'),
+            'wsid': ws_id
+        }
+        self._log.setLevel(logging.INFO)
+        kblogging.log_event(self._log, "run_app", log_info)
 
-            # This is what calls the function in the back end - Module.method
-            # This isn't the same as the app spec id.
-            function_name = service_name + '.' + service_method
-            job_meta = {'tag': tag}
-            if cell_id is not None:
-                job_meta['cell_id'] = cell_id
-            if run_id is not None:
-                job_meta['run_id'] = run_id
+        self._send_comm_message('run_status', {
+            'event': 'launching_job',
+            'event_at': datetime.datetime.utcnow().isoformat() + 'Z',
+            'cell_id': cell_id,
+            'run_id': run_id
+        })
 
-            job_runner_inputs = {
-                'method' : function_name,
-                'service_ver' : service_ver,
-                'params' : [input_vals],
-                'app_id' : app_id,
-                'wsid': ws_id,
-                'meta': job_meta
-            }
-            if len(ws_input_refs) > 0:
-                job_runner_inputs['source_ws_objects'] = ws_input_refs
+        try:
+            job_id = self.njs.run_job(job_runner_inputs)
+        except Exception as e:
+            log_info.update({'err': str(e)})
+            self._log.setLevel(logging.ERROR)
+            kblogging.log_event(self._log, "run_app_error", log_info)
+            raise
 
-            log_info = {
-                'app_id': app_id,
-                'tag': tag,
-                'version': service_ver,
-                'username': system_variable('user_id'),
-                'wsid': ws_id
-            }
-            self._log.setLevel(logging.INFO)
-            kblogging.log_event(self._log, "run_app", log_info)
-
-            self._send_comm_message('run_status', {
-                'event': 'launching_job',
-                'event_at': datetime.datetime.utcnow().isoformat() + 'Z',
-                'cell_id': cell_id,
-                'run_id': run_id
-            })
-
-            try:
-                job_id = self.njs.run_job(job_runner_inputs)
-            except Exception as e:
-                log_info.update({'err': str(e)})
-                self._log.setLevel(logging.ERROR)
-                kblogging.log_event(self._log, "run_app_error", log_info)
-                raise
-
-            new_job = Job(job_id, app_id, [params], tag=tag, app_version=service_ver, cell_id=cell_id)
-
+        new_job = Job(job_id, app_id, [params], tag=tag, app_version=service_ver, cell_id=cell_id)
 
         self._send_comm_message('run_status', {
             'event': 'launched_job',
@@ -361,6 +403,94 @@ class AppManager(object):
             return
         else:
             return new_job
+
+    def _map_inputs(self, input_mapping, params):
+        """
+        input_mapping is a list of dicts, as defined by NarrativeMethodStore.ServiceMethodInputMapping.
+        params is a dict of key-value-pairs, each key is the input_parameter field of some parameter.
+        """
+        inputs_dict = dict()
+        for p in input_mapping:
+            # 2 steps - figure out the proper value, then figure out the proper position.
+            # value first!
+            p_value = None
+            if 'input_parameter' in p:
+                p_value = params.get(p['input_parameter'], None)
+            elif 'narrative_system_variable' in p:
+                p_value = system_variable(p['narrative_system_variable'])
+            if 'constant_value' in p and p_value is None:
+                p_value = p['constant_value']
+            if 'generated_value' in p and p_value is None:
+                p_value = self._generate_input(generated_value)
+            if 'target_type_transform' in p:
+                p_value = self._transform_input(transform_type, p_value)
+
+            # get position!
+            arg_position = p.get('target_argument_position', 0)
+            target_prop = p.get('target_property', None)
+            if target_prop is not None:
+                final_input = inputs_dict.get(arg_position, dict())
+                final_input[target_prop] = p_value
+                inputs_dict[arg_position] = final_input
+            else:
+                inputs_dict[arg_position] = p_value
+
+        inputs_list = list()
+        keys = sorted(inputs_dict.keys())
+        for k in keys:
+            inputs_list.append(inputs_dict[k])
+        return inputs_list
+
+    def _transform_input(self, transform_type, value):
+        if transform_type == "none" or transform_type is None:
+            return value
+
+        elif transform_type == "ref":
+            # make a workspace ref
+            if value is not None:
+                value = system_variable('workspace') + '/' + value
+            return value
+
+        elif transform_type == "int":
+            # make it an integer, OR 0.
+            if value is None or len(str(value).strip()) == 0:
+                return None
+            return int(value)
+
+        elif transform_type.startswith("list<") and transform_type.endswith(">"):
+            # make it a list of transformed types.
+            list_type = transform_type[5:-1]
+            if isinstance(value, list):
+                ret = []
+                for pos in range(0, len(value)):
+                    ret.append(self._transform_input(list_type, value[pos]))
+                return ret
+            else:
+                return [self._transform_input(list_type, value)]
+
+        else:
+            raise ValueError("Unsupported Transformation type: " + transform_type)
+
+
+    def _generate_input(self, generator):
+        """
+        Generates an input value using rules given by NarrativeMethodStore.AutoGeneratedValue.
+        generator - dict
+            has 3 optional properties:
+            prefix - if present, is prepended to the generated string.
+            symbols - if present is the number of symbols to autogenerate (if not present, default=8)
+            suffix - if present, is appended to the generated string.
+        So, if generator is None or an empty dict, returns an 8-symbol string.
+        """
+        symbols = 8
+        if 'symbols' in generator:
+            symbols = int(generator['symbols'])
+        ret = ''.join([chr(random.randrange(0, 26) + ord('A')) for _ in xrange(symbols)])
+        if 'prefix' in generator:
+            ret = str(generator['prefix']) + ret;
+        if 'suffix' in generator:
+            ret = ret + str(generator['suffix']);
+        return ret
 
     def _check_parameter(self, param, value, workspace):
         """

e9cf0a6baffddcb6b3141e50599ddd8aa2a2ccb3
Erik Pearson <eapearson@lbl.gov>
2016-07-13 12:49:02 -0700

    just formatting

--- kbase-extension/static/kbase/js/common/runtime.js
+++ kbase-extension/static/kbase/js/common/runtime.js
@@ -5,13 +5,17 @@ define([
     'narrativeConfig',
     'common/props',
     './monoBus'
-], function (Jupyter, Config, Props, Bus) {
+], function (
+    Jupyter,
+    Config,
+    Props,
+    Bus) {
     'use strict';
     var narrativeConfig = Props.make({data: Config.getConfig()});
 
     function factory(config) {
-        
-        
+
+
         function createRuntime() {
             var bus = Bus.make();
             return {
@@ -26,14 +30,14 @@ define([
         if (!window.kbaseRuntime) {
             window.kbaseRuntime = createRuntime();
         }
-        
+
         // Global scope
-        
+
         function bus() {
             return window.kbaseRuntime.bus;
         }
-        
-        
+
+
         // These are still module scope
 
         function authToken() {
@@ -42,7 +46,7 @@ define([
 
         function getConfig(key, defaultValue) {
             return narrativeConfig.getItem(key, defaultValue);
-            
+
 //            var path = key.split('.'),
 //                root = path[0],
 //                rest = path.slice(1),
@@ -61,7 +65,7 @@ define([
 //            }
 //            return configRoot;
         }
-        
+
         return {
             authToken: authToken,
             config: getConfig,

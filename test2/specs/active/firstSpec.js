3af92bd7102ad0ada3f7436227d1b4dc9bf3e7bc
Erik Pearson <eapearson@lbl.gov>
2016-07-14 16:19:25 -0700

    moved test spec file

---
+++ test2/specs/active/firstSpec.js
@@ -0,0 +1,10 @@
+define([
+], function () {
+    'use strict';
+    describe('Sanity check', function () {
+      it('Should be true', function () {
+        var theTruth = true;
+        expect(theTruth).toBeTruthy();
+      });
+    });
+})

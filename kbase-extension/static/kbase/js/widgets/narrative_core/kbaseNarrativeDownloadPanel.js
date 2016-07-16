75aab29c5f0560fb345c651ed6b7d3199b17af1a
Roman Sutormin <rsutormin@lbl.gov>
2016-07-15 18:12:26 -0700

    First part of SDK function support in download panel (not ready yet).

--- kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeDownloadPanel.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeDownloadPanel.js
@@ -10,12 +10,16 @@ define (
 		'kbwidget',
 		'bootstrap',
 		'jquery',
-		'narrativeConfig'
+		'narrativeConfig',
+		'kbase-client-api',
+		'kbase-generic-client-api'
 	], function(
 		KBWidget,
 		bootstrap,
 		$,
-		Config
+		Config,
+		kbase_client_api,
+		kbase_generic_client_api
 	) {
     'use strict';
     return KBWidget({
@@ -26,10 +30,11 @@ define (
         	token: null,
         	type: null,
         	wsId: null,
-        	objId: null
+        	objId: null,
+        	downloadSpecCache: null  // value is structure {'tag': <tag>, 'lastUpdateTime': <milliseconds>, 'types': {<type>: <type-spec>}}
         },
         token: null,
-        type: null,
+        type: null,  // Type of workspace object to show downloaders for
         wsId: null,
         objId: null,
         loadingImage: Config.get('loading_gif'),
@@ -38,7 +43,10 @@ define (
         ujsURL: Config.url('user_and_job_state'),
         shockURL: Config.url('shock'),
         exportURL: Config.url('data_import_export'),
+        nmsURL: Config.url('narrative_method_store'),
+        eeURL: Config.url('job_service'),
         timer: null,
+        downloadSpecCache: null,
         
         downloaders: {  // type -> {name: ..., external_type: ...[, transform_options: ...[, unzip: <file_ext>]}
         	'KBaseGenomes.ContigSet': [{name: 'FASTA', external_type: 'FASTA.DNA.Assembly', transform_options: {"output_file_name": "?.fasta"}}],
@@ -119,11 +127,43 @@ define (
 
         init: function(options) {
             this._super(options);
+            var self = this;
             this.token = this.options.token;
             this.type = this.options.type;
             this.wsId = this.options.wsId;
             this.objId = this.options.objId;
-            this.render();
+            this.downloadSpecCache = options['downloadSpecCache'];
+            console.log(this.downloadSpecCache);
+            var lastUpdateTime = this.downloadSpecCache['lastUpdateTime'];
+            if (lastUpdateTime) {
+                this.render();
+            } else {
+                console.log("Loading type specs...");
+                //downloadSpecCache: {'currentTag': <current-tag>, <tag>: {'types': {<type>: <type-spec>}}}
+                var nms = new NarrativeMethodStore(this.nmsURL, { token: this.token });
+                nms.list_categories({'load_methods': 0, 'load_apps' : 0, 'load_types' : 1},
+                        $.proxy(function(data) {
+                            console.log(data);
+                            var aTypes = data[3];
+                            var types = {};
+                            for (var key in aTypes) {
+                                if (aTypes[key]["loading_error"]) {
+                                    console.log("Error loading type [" + key + "] for tag [" + currentTag + "]: " + 
+                                            aTypes[key]["loading_error"]);
+                                    continue;
+                                }
+                                types[key] = aTypes[key];
+                            }
+                            self.downloadSpecCache['types'] = types;
+                            self.downloadSpecCache['lastUpdateTime'] = Date.now();
+                            console.log("2", self.downloadSpecCache);
+                            self.render();
+                        }, this),
+                        $.proxy(function(error) {
+                            self.showError(error);
+                        }, this)
+                );
+            }
             return this;
         },
         
@@ -131,22 +171,23 @@ define (
             var self = this;
     		var downloadPanel = this.$elem;
 		
-		var $labeltd = $('<td>').css({'white-space':'nowrap','padding':'1px'}).append('Export as:');
-		var $btnTd = $('<td>').css({'padding':'1px'});
-		downloadPanel.append($('<table>').css({width:'100%'})
-					.append('<tr>')
-					   .append($labeltd)
-					   .append($btnTd));
-		
+    		var $labeltd = $('<td>').css({'white-space':'nowrap','padding':'1px'}).append('Export as:');
+    		var $btnTd = $('<td>').css({'padding':'1px'});
+    		downloadPanel.append($('<table>').css({width:'100%'})
+    		        .append('<tr>')
+    		        .append($labeltd)
+    		        .append($btnTd));
+
 		
     		var addDownloader = function(descr) {
-		    $btnTd.append($('<button>').addClass('kb-data-list-btn')
-    					.append(descr.name)
-    					.click(function() {
-						$btnTd.find('.kb-data-list-btn').prop('disabled', true);
-    						self.runDownloader(self.type, self.wsId, self.objId, descr);
-    					}));
+    		    $btnTd.append($('<button>').addClass('kb-data-list-btn')
+    		            .append(descr.name)
+    		            .click(function() {
+    		                $btnTd.find('.kb-data-list-btn').prop('disabled', true);
+    		                self.runDownloader(self.type, self.wsId, self.objId, descr);
+    		            }));
     		};
+    		
     		var downloaders = self.prepareDownloaders(self.type, self.wsId, self.objId);
     		for (var downloadPos in downloaders)
     			addDownloader(downloaders[downloadPos]);
@@ -161,63 +202,88 @@ define (
                     	self.downloadFile(url);
                     }));
     		$btnTd.append($('<button>').addClass('kb-data-list-cancel-btn')
-                    .append('Cancel')
-                    .click(function() {
-			self.stopTimer();
-			downloadPanel.empty();
-		    } ));
+    		        .append('Cancel')
+    		        .click(function() {
+    		            self.stopTimer();
+    		            downloadPanel.empty();
+    		        } ));
 		
-	    self.$statusDiv = $('<div>').css({'margin':'15px'});
-	    self.$statusDivContent = $('<div>');
-	    self.$statusDiv.append(self.$statusDivContent);
-	    downloadPanel.append(self.$statusDiv.hide());
+    		self.$statusDiv = $('<div>').css({'margin':'15px'});
+    		self.$statusDivContent = $('<div>');
+    		self.$statusDiv.append(self.$statusDivContent);
+    		downloadPanel.append(self.$statusDiv.hide());
         },
         
         prepareDownloaders: function(type, wsId, objId) {
-        	var descrList = this.downloaders[type];
-        	var ret = [];
-        	for (var descrPos in descrList) {
-        		var descr = descrList[descrPos];
-        		var retDescr = {name: descr.name, external_type: descr.external_type, unzip: descr.unzip};
-        		ret.push(retDescr);
-        		if (descr.transform_options) {
-        			retDescr.transform_options = {};
-        			for (var key in descr.transform_options) {
-        				if (!descr.transform_options.hasOwnProperty(key))
-        					continue;
-        				var value = descr.transform_options[key];
-        				if (value.indexOf('?') == 0)
-        					value = objId + value.substring(1);
-        				retDescr.transform_options[key] = value;
-        			}
-        		}
-        	}
-        	return ret;
+            var ret = [];
+            var typeSpec = this.downloadSpecCache['types'] ? 
+                    this.downloadSpecCache['types'][type] : null;
+            if (typeSpec && typeSpec['export_functions']) {
+                for (var name in typeSpec['export_functions'])
+                    if (typeSpec['export_functions'].hasOwnProperty(name))
+                        ret.push({name: name + " (SDK)", local_function: typeSpec['export_functions'][name]});
+            } else {
+                var descrList = this.downloaders[type];
+                for (var descrPos in descrList) {
+                    var descr = descrList[descrPos];
+                    var retDescr = {name: descr.name, external_type: descr.external_type, unzip: descr.unzip};
+                    ret.push(retDescr);
+                    if (descr.transform_options) {
+                        retDescr.transform_options = {};
+                        for (var key in descr.transform_options) {
+                            if (!descr.transform_options.hasOwnProperty(key))
+                                continue;
+                            var value = descr.transform_options[key];
+                            if (value.indexOf('?') == 0)
+                                value = objId + value.substring(1);
+                            retDescr.transform_options[key] = value;
+                        }
+                    }
+                }
+            }
+            console.log(ret);
+            return ret;
         },
         
-        runDownloader: function(type, wsId, objId, descr) { // descr is {name: ..., external_type: ...[, transform_options: ...[, unzip: ...]]}
+        runDownloader: function(type, wsId, objId, descr) {
+            // descr is {name: ..., external_type: ...[, transform_options: ...[, unzip: ...]] [, local_function: ...]}
             var self = this;
             self.showMessage('<img src="'+self.loadingImage+'" /> Export status: Preparing data');
             self.$statusDiv.show();
-        	var transform_options = descr.transform_options;
-        	if (!transform_options)
-        		transform_options = {};
-        	var args = {external_type: descr.external_type, kbase_type: type, workspace_name: wsId, object_name: objId, optional_arguments: {transform: transform_options}};
-    		console.log("Downloader data to be sent to transform service:");
-    		console.log(JSON.stringify(args));
-    		var nameSuffix = '.' + descr.name.replace(/[^a-zA-Z0-9|\.\-_]/g,'_');
-            var transformSrv = new Transform(this.transformURL, {token: this.token});
-            transformSrv.download(args,
-            		$.proxy(function(data) {
-            			console.log(data);
-            			var jobId = data[1];
-            			self.waitForJob(jobId, objId + nameSuffix, descr.unzip);
-            		}, this),
-            		$.proxy(function(data) {
-            			console.log(data.error.error);
-            			self.showError(data.error.error);
-            		}, this)
-            );
+            var wsObjectName = objId + '.' + descr.name.replace(/[^a-zA-Z0-9|\.\-_]/g,'_');
+            if (descr.local_function) {
+                var method = descr.local_function.replace('/', '.');
+                var genericClient = new GenericClient(this.eeURL, { token: this.token }, null, false);
+                genericClient.sync_call("NarrativeJobService.run_job",
+                        [{method: method, params: [{input_ref: wsId + "/" + objId}],
+                            service_ver: this.downloadSpecCache['tag']}], function(data){
+                    var jobId = data[0];
+                    console.log("Running " + descr.local_function + ", job ID: " + jobId);
+                },
+                function(error){
+                    self.showError(error);
+                });
+            } else {
+                var transform_options = descr.transform_options;
+                if (!transform_options)
+                    transform_options = {};
+                var args = {external_type: descr.external_type, kbase_type: type, workspace_name: wsId, 
+                        object_name: objId, optional_arguments: {transform: transform_options}};
+                console.log("Downloader data to be sent to transform service:");
+                console.log(JSON.stringify(args));
+                var transformSrv = new Transform(this.transformURL, {token: this.token});
+                transformSrv.download(args,
+                        $.proxy(function(data) {
+                            console.log(data);
+                            var jobId = data[1];
+                            self.waitForJob(jobId, wsObjectName, descr.unzip);
+                        }, this),
+                        $.proxy(function(data) {
+                            console.log(data.error.error);
+                            self.showError(data.error.error);
+                        }, this)
+                );
+            }
         },
 
         waitForJob: function(jobId, wsObjectName, unzip) {
@@ -238,7 +304,8 @@ define (
 								self.$statusDiv.hide();
 								self.$elem.find('.kb-data-list-btn').prop('disabled', false);
 								console.log(data);
-								self.downloadUJSResults(data, wsObjectName, unzip);
+								self.downloadUJSResults(data.shocknodes[0], data.shockurl, 
+								        wsObjectName, unzip);
 							}, function(data) {
             					console.log(data.error.message);
                     			self.showError(data.error.message);
@@ -261,9 +328,8 @@ define (
 			timeLst();
         },
         
-        downloadUJSResults: function(ujsResults, wsObjectName, unzip) {
+        downloadUJSResults: function(shockNode, remoteShockUrl, wsObjectName, unzip) {
         	var self = this;
-			var shockNode = ujsResults.shocknodes[0];
 			var elems = shockNode.split('/');
 			if (elems.length > 1)
 				shockNode = elems[elems.length - 1];
@@ -280,7 +346,6 @@ define (
     			} else {
     				url += '&name='+encodeURIComponent(name);
     			}
-    			var remoteShockUrl = ujsResults.shockurl;
     			if (remoteShockUrl)
     				url += '&url='+encodeURIComponent(remoteShockUrl);
     			self.downloadFile(url);

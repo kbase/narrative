c179918bde2ed0d272108c43951f1b2379a9bbc6
Bill Riehl <wjriehl@lbl.gov>
2016-08-30 18:27:47 -0700

    Updates to test_auth (soon to become the new Lua Auth module...)

--- docker/proxy_mgr.lua
+++ docker/proxy_mgr.lua
@@ -449,7 +449,7 @@ end
 -- The intent is that this is a fast, specific reaping process, accessible from outside.
 -- It's set up as a REST call, but a valid user token is required, and that token must come
 -- from the user who's instance it's trying to shut down.
--- Only the GET and DELETE methods are implemented. GET returns some info, and DELETE 
+-- Only the GET and DELETE methods are implemented. GET returns some info, and DELETE
 -- will shutdown the container under 2 conditions:
 -- 1. A valid KBase auth token is given in the cookie given by auth_cookie_name
 -- 2. The user specified by that cookie is shutting down their own Narrative instance
@@ -725,7 +725,7 @@ set_proxy = function(self)
                     ngx.status = ngx.HTTP_NOT_FOUND
                 end
             end
-        else 
+        else
             response["error"] = "No valid session key specified"
             ngx.status = ngx.HTTP_NOT_FOUND
         end
@@ -1034,7 +1034,7 @@ use_proxy = function(self)
     -- get session
     -- If if fails for any reason (there are several possible) redirect to
     -- an end point which can authenticate and hopefully send them back here
-    -- NB although the key for the container is called various things through this 
+    -- NB although the key for the container is called various things through this
     -- file it is important that it is the USERNAME, and thus it is named in this
     -- function.
     local username = get_session()
@@ -1049,12 +1049,12 @@ use_proxy = function(self)
     if target == nil then
         session_lock = locklib:new(M.lock_name, lock_opts)
         elapsed, err = session_lock:lock(username)
-        if err then 
+        if err then
             ngx.log(ngx.ERR, string.format("Error obtaining key %s", err))
             return ngx.exit(ngx.HTTP_REQUEST_TIMEOUT, string.format("Error obtaining key %s", err))
         end
         target = session_map:get(username)
-        -- still missing, but we would expect that, as it is unlikely that 
+        -- still missing, but we would expect that, as it is unlikely that
         -- a session for this user would be created between the two calls.
         if target == nil then
             -- this updates docker_map with session info
@@ -1084,7 +1084,7 @@ use_proxy = function(self)
             -- route to the "loading" page which will wait until the container
             -- is ready before loading the narrative.
             local scheme = ngx.var.src_scheme and ngx.var.src_scheme or 'http'
-            local returnurl = string.format("%s://%s%s", scheme, ngx.var.http_host, ngx.var.request_uri)            
+            local returnurl = string.format("%s://%s%s", scheme, ngx.var.http_host, ngx.var.request_uri)
             return ngx.redirect(string.format(M.load_redirect, ngx.escape_uri(returnurl)))
         end
         session_lock:unlock()
--- docker/test_auth.lua
+++ docker/test_auth.lua
@@ -1,6 +1,8 @@
 local M = {}
 local auth_cookie_name = "kbase_session"
 
+local json = require("json")
+local locklib = require("resty.lock")
 local httplib = require("resty.http")
 local httpclient = httplib:new()
 
@@ -9,10 +11,13 @@ local token_cache = nil
 -- tokens live for up to 5 minutes
 local max_token_lifespan = 5 * 60
 
-local nexus_url = 'http://127.0.0.1:65001/users/'
+-- local auth_url = 'http://127.0.0.1:65001/users/'
+local auth_url = 'https://kbase.us/services/authorization/Sessions/Login'
 
 local initialize
 local initialized = nil
+
+M.lock_name = "token_lock"
 
 initialize = function(self, conf)
     if conf then
@@ -32,8 +37,60 @@ initialize = function(self, conf)
     end
 end
 
+-- If the given token is stored in the cache, and hasn't expired, this
+-- returns that user id.
+-- If it has expired, or the token doesn't exist, returns nil
+get_user_from_cache = function(self, token)
+    if token then
+        -- TODO: hash that token
+        return token_cache:get(token)
+    else
+        return nil
+    end
+end
+
+add_valid_token = function(self, token, user)
+    -- TODO: hash that token
+    if token and user then
+        token_cache:set(token_cache, token, user, M.max_token_lifespan)
+    end
+end
+
+get_user = function(self, token)
+    if token then
+        user = get_user_from_cache(token)
+        if user then
+            return user
+        else
+            d = {token=token, fields='user_id'}
+            local user_request = {
+                url = auth_url,
+                method = "POST",
+                body = d
+            }
+            local ok, code, headers, status, body = httpclient:request(user_request)
+            if code >= 200 and code < 300 then
+                local profile = json.decode(body)
+                if profile.user_id then
+                    M.add_valid_token(token, profile.user_id)
+                else
+                    --error - missing user id from token lookup
+                    ngx.log(ngx.ERR, "Error: auth token lookup doesn't return a user id")
+                end
+            else
+                ngx.log(ngx.ERR, "Error during token validation: "..status.." "..body)
+            end
+        end
+    end
+end
+
 test_auth = function(self)
-    ngx.say("testing auth!")
+    local table = {
+        "hello, ",
+        {"world: ", true, " or ", false,
+            {": ", nil}}
+    }
+    ngx.say(table)
 end
 
 M.initialize = initialize

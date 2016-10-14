04d660768fa4cfc8a676ee66dd6742dffe279173
Bill Riehl <wjriehl@lbl.gov>
2016-10-13 18:47:49 -0700

    Added test specs for all narrative_core modules

---
+++ test/unit/spec/narrative_core/kbaseCellToolbarMenu-spec.js
@@ -0,0 +1,14 @@
+/*global define*/
+/*global describe, it, expect*/
+/*global jasmine*/
+/*global beforeEach, afterEach*/
+/*jslint white: true*/
+define([
+    'kbaseCellToolbarMenu'
+], function(Widget) {
+    describe('Test the kbaseCellToolbarMenu widget', function() {
+        it('Should do things', function() {
+
+        });
+    });
+});
---
+++ test/unit/spec/narrative_core/kbaseNarrativeDownloadPanel-spec.js
@@ -0,0 +1,14 @@
+/*global define*/
+/*global describe, it, expect*/
+/*global jasmine*/
+/*global beforeEach, afterEach*/
+/*jslint white: true*/
+define([
+    'kbaseNarrativeDownloadPanel'
+], function(Widget) {
+    describe('Test the kbaseNarrativeDownloadPanel widget', function() {
+        it('Should do things', function() {
+
+        });
+    });
+});

d96879940309ba647aa82a598e1bc34936845336
Bill Riehl <wjriehl@lbl.gov>
2016-11-15 17:44:34 -0800

    Sort by last updated datestame in dropdown.

--- kbase-extension/static/kbase/js/widgets/appWidgets/input/singleObjectInput.js
+++ kbase-extension/static/kbase/js/widgets/appWidgets/input/singleObjectInput.js
@@ -222,13 +222,13 @@ define([
                 })
                 .then(function (objects) {
                     objects.sort(function (a, b) {
-                        if (a.name < b.name) {
-                            return -1;
+                        if (a.saveDate < b.saveDate) {
+                            return 1;
                         }
-                        if (a.name === b.name) {
+                        if (a.saveDate === b.saveDate) {
                             return 0;
                         }
-                        return 1;
+                        return -1;
                     });
                     return objects;
                 });

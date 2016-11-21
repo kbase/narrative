4f1d1c43f94b8c4b626eb51e689fa59d232d9393
Bill Riehl <wjriehl@lbl.gov>
2016-11-21 12:48:27 -0600

    functionally working prototype. time for styling.

--- kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeStagingDataTab.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeStagingDataTab.js
@@ -3,13 +3,15 @@ define([
     'dropzone',
     'kbwidget',
     'narrativeConfig',
-    'common/runtime'
+    'common/runtime',
+    'util/string'
 ], function(
     $,
     Dropzone,
     KBWidget,
     Config,
-    Runtime
+    Runtime,
+    StringUtil
 ) {
     return new KBWidget({
         name: 'kbaseNarrativeStagingDataTab',
@@ -19,7 +21,30 @@ define([
         },
 
         render: function() {
-            this.$elem.empty().append('I am staging zone. Drop stuff on me.');
+            var dzId = StringUtil.uuid() + '-dropzone';
+            var $mainElem = $('<div>')
+                .css({
+                    'height': '604px'
+                });
+            var $dropzoneElem = $('<div>')
+                .attr('id', dzId)
+                .css({
+                    'border': '2px dashed #2196F3',
+                    'height': '50px',
+                    'margin': '5px',
+                    'text-align': 'center'
+                })
+                .append('I am staging zone. Drop stuff on me.');
+            this.$elem
+                .empty()
+                .append($mainElem.append($dropzoneElem));
+
+            var dz = new Dropzone($dropzoneElem.get(0), {
+                url: 'file/post',
+                accept: function(file, done) {
+                    done("Haha, nope");
+                }
+            });
         }
     });
 });

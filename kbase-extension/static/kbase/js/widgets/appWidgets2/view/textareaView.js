a46ec3760c1047a01e807a693b29ebb97238a2ba
Erik Pearson <eapearson@lbl.gov>
2017-01-26 17:16:20 -0800

    Fix common paramter view widgets - ensure read-only - remove any change-detection and updating code

--- kbase-extension/static/kbase/js/widgets/appWidgets2/view/textareaView.js
+++ kbase-extension/static/kbase/js/widgets/appWidgets2/view/textareaView.js
@@ -95,88 +95,16 @@ define([
                 });
         }
 
-        var autoChangeTimer;
-
-        function cancelTouched() {
-            if (autoChangeTimer) {
-                window.clearTimeout(autoChangeTimer);
-                autoChangeTimer = null;
-            }
-        }
-
-        function handleTouched(interval) {
-            var editPauseInterval = interval || 100;
-            return {
-                type: 'keyup',
-                handler: function(e) {
-                    bus.emit('touched');
-                    cancelTouched();
-                    autoChangeTimer = window.setTimeout(function() {
-                        autoChangeTimer = null;
-                        e.target.dispatchEvent(new Event('change'));
-                    }, editPauseInterval);
-                }
-            };
-        }
-
-        function handleChanged() {
-            return {
-                type: 'change',
-                handler: function() {
-                    cancelTouched();
-                    importControlValue()
-                        .then(function(value) {
-                            model.setItem('value', value);
-                            bus.emit('changed', {
-                                newValue: value
-                            });
-                            return validate(value);
-                        })
-                        .then(function(result) {
-                            if (result.isValid) {
-                                if (config.showOwnMessages) {
-                                    ui.setContent('input-container.message', '');
-                                }
-                            } else if (result.diagnosis === 'required-missing') {
-                                // nothing??
-                            } else {
-                                if (config.showOwnMessages) {
-                                    // show error message -- new!
-                                    var message = inputUtils.buildMessageAlert({
-                                        title: 'ERROR',
-                                        type: 'danger',
-                                        id: result.messageId,
-                                        message: result.errorMessage
-                                    });
-                                    ui.setContent('input-container.message', message.content);
-                                    message.events.attachEvents();
-                                }
-                            }
-                            bus.emit('validation', result);
-                        })
-                        .catch(function(err) {
-                            bus.emit('validation', {
-                                isValid: false,
-                                diagnosis: 'invalid',
-                                errorMessage: err.message
-                            });
-                        });
-                }
-            };
-        }
-
         /*
          * Creates the markup
          * Places it into the dom node
          * Hooks up event listeners
          */
-        function makeInputControl(events) {
+        function makeViewControl(events) {
             return textarea({
-                id: events.addEvents({
-                    events: [handleChanged(), handleTouched()]
-                }),
                 class: 'form-control',
                 dataElement: 'input',
+                readonly: true,
                 rows: options.rowCount
             });
         }
@@ -186,7 +114,7 @@ define([
                 dataElement: 'main-panel'
             }, [
                 div({ dataElement: 'input-container' }, [
-                    makeInputControl(events)
+                    makeViewControl(events)
                 ])
             ]);
             return {

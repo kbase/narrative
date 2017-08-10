0b7bbe3fd02f0a6d10b099f5c3731d7f287278d8
Erik Pearson <eapearson@lbl.gov>
2017-08-10 06:39:58 -0700

    remove unused functions and clean up [TASK-938]

--- kbase-extension/static/kbase/js/widgets/narrative_core/ipythonCellMenu.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/ipythonCellMenu.js
@@ -10,20 +10,22 @@ define([
     'kb_common/html',
     'kbaseNarrativeCellMenu',
     'kbaseCellToolbarMenu'
-], function ($, celltoolbar, Jupyter, html, KBaseNarrativeCellMenu, KBaseMenu) {
-    "use strict";
+], function (
+    $,
+    celltoolbar,
+    Jupyter,
+    html,
+    KBaseNarrativeCellMenu,
+    KBaseMenu
+) {
+    'use strict';
 
     var t = html.tag,
-        span = html.tag('span');
+        span = t('span');
 
     /*
      * Dealing with metadata
      */
-    function createMeta(cell, initial) {
-        var meta = cell.metadata;
-        meta.kbase = initial;
-        cell.metadata = meta;
-    }
     function getMeta(cell, group, name) {
         if (!cell.metadata.kbase) {
             return;
@@ -33,67 +35,29 @@ define([
         }
         return cell.metadata.kbase[group][name];
     }
-    function setMeta(cell, group, name, value) {
-        /*
-         * This funny business is because the trigger on setting the metadata
-         * property (via setter and getter in core Cell object) is only invoked
-         * when the metadata preoperty is actually set -- doesn't count if
-         * properties of it are.
-         */
-        var temp = cell.metadata;
-        if (!temp.kbase) {
-            temp.kbase = {};
-        }
-        if (!temp.kbase[group]) {
-            temp.kbase[group] = {};
-        }
-        temp.kbase[group][name] = value;
-        cell.metadata = temp;
-    }
 
     function makeKBaseMenux(div, cell) {
-        new KBaseNarrativeCellMenu(div, {cell: cell});
+        new KBaseNarrativeCellMenu(div, { cell: cell });
     }
+
     function makeKBaseMenu($toolbarNode, cell) {
         var kbaseMenu = KBaseMenu.make();
         kbaseMenu.register_callback($toolbarNode, cell);
     }
-    function toggleInput(toolbarDiv, cell) {
-        if (!cell.metadata.kbase) {
-            return;
-        }
-        if (cell.metadata.kbase.type !== 'method') {
-            return;
-        }
-        var button = html.tag('button'),
-            inputAreaShowing = getMeta(cell, 'user-settings', 'showCodeInputArea'),
-            label = inputAreaShowing ? 'Hide code' : 'Show code',
-            toggleButton = button({
-                type: 'button',
-                class: 'btn btn-default btn-xs',
-                dataElement: 'kbase-method-cell-hideshow'}, [
-                label
-            ]);
-        toolbarDiv.append(toggleButton);
-        toolbarDiv.find('[data-element="kbase-method-cell-hideshow"]').on('click', function (e) {
-            toggleCodeInputArea(cell);
-        });
-    }
 
     function status(toolbarDiv, cell) {
         var status = getMeta(cell, 'attributes', 'status'),
-            content = span({style: {fontWeight: 'bold'}}, status);
-        toolbarDiv.append(span({style: {padding: '4px'}}, content));
+            content = span({ style: { fontWeight: 'bold' } }, status);
+        toolbarDiv.append(span({ style: { padding: '4px' } }, content));
     }
 
     function jobStatus(toolbarDiv, cell) {
         var jobStatus = getMeta(cell, 'attributes', 'jobStatus'),
-            content = span({style: {fontWeight: 'bold'}}, jobStatus);
-        $(toolbarDiv).append(span({style: {padding: '4px'}}, content));
+            content = span({ style: { fontWeight: 'bold' } }, jobStatus);
+        $(toolbarDiv).append(span({ style: { padding: '4px' } }, content));
     }
 
     var register = function (notebook) {
-
         celltoolbar.CellToolbar.register_callback('kbase.menu', makeKBaseMenux);
         celltoolbar.CellToolbar.register_callback('kbase-status', status);
         celltoolbar.CellToolbar.register_callback('kbase-job-status', jobStatus);
@@ -102,5 +66,5 @@ define([
         // default.rawedit for the metadata editor
         celltoolbar.CellToolbar.register_preset('KBase', ['kbase-menu']);
     };
-    return {register: register};
+    return { register: register };
 });

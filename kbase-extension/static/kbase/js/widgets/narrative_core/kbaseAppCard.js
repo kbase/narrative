b8e76d41ae1d392eb71ecd3d3013b970ce7c6c20
Bill Riehl <wjriehl@lbl.gov>
2017-11-13 16:02:30 -0800

    tweak border positions in cards

--- kbase-extension/static/kbase/js/widgets/narrative_core/kbaseAppCard.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/kbaseAppCard.js
@@ -1,157 +1,155 @@
 /**
  *  kbaseAppCard.js -- used making App cards in narrative
+ *  Authors: zzheng@lbl.gov, wjriehl@lbl.gov
  *
- *  Authors: zzheng@lbl.gov
+ *  Example and expected output:
  *
- *   Example and expected out put:
+ *  var $card = new kbaseAppCard({
+ *      //expected values
+ *      app: app (object with info object and favorite field),
+ *      favorite: app.favorite (number),
  *
- *     var $card = new kbaseAppCard(
-                {   
-                    //expected values
-                    app: app (object with info object and favorite field),
-                    favorite: app.favorite (number),
-
-                    //values with default, passing in value will override default
-                    createdBy: array of strs,
-                    type: str,
-
-                    //optional values
-                    moreContent: jquery object,
-                });
+ *      //values with default, passing in value will override default
+ *      createdBy: array of strs,
+ *      type: str,
  *
- *             
-        app.info:
-        authors:["rsutormin"], categories:["annotation"], icon:{url:}, id: str,
-        input_types: ['KbaseGenomeAnnotations.Assembly'], module_name: str,
-        name: str, namespace: str, output_types:["KBaseGenomes.Genome"],
-        subtitle: str, tooltip: str, ver: str
-             
-            
-*/
+ *      //optional values
+ *      moreContent: jquery object,
+ *  });
+ *
+ *
+ *        app.info:
+ *        authors:["rsutormin"], categories:["annotation"], icon:{url:}, id: str,
+ *        input_types: ['KbaseGenomeAnnotations.Assembly'], module_name: str,
+ *        name: str, namespace: str, output_types:["KBaseGenomes.Genome"],
+ *        subtitle: str, tooltip: str, ver: str
+ *
+ *
+ */
 
-define (
-    [
-        'bootstrap',
-        'util/icon',
-        'bluebird',
-        'util/bootstrapDialog',
-        'util/display',
-        'kbase/js/widgets/narrative_core/kbaseCardLayout',
-        'narrativeConfig',
-        'jquery'
-    ], function(
-        bootstrap,
-        Icon,
-        Promise,
-        BootstrapDialog,
-        DisplayUtil,
-        kbaseCardLayout,
-        Config,
-        $
-    ) {
-        function KbaseAppCard(entry) {
-            var self = this;
-            var favorite = entry.app.favorite;
-            var app = entry.app.info;
-            
-            var shortName = entry.name ? entry.name : app.name;
-            var authors = entry.createdBy; 
-            var version = entry.version ? entry.version : ('v' + app.ver);
-            var type;
+define ([
+    'bootstrap',
+    'util/icon',
+    'bluebird',
+    'util/bootstrapDialog',
+    'util/display',
+    'kbase/js/widgets/narrative_core/kbaseCardLayout',
+    'narrativeConfig',
+    'jquery'
+], function(
+    bootstrap,
+    Icon,
+    Promise,
+    BootstrapDialog,
+    DisplayUtil,
+    kbaseCardLayout,
+    Config,
+    $
+) {
+    'use strict';
+    function KbaseAppCard(entry) {
+        var self = this;
+        var favorite = entry.app.favorite;
+        var app = entry.app.info;
 
-            if(entry.createdBy === undefined){
-                if(app.authors.length >2){
-                    authors = app.authors.slice(0,2).join(', ') + '+ ' + (app.authors.length-2) + ' more';
-                }else{
-                    authors = app.authors.join(', ');
-                }
+        var shortName = entry.name ? entry.name : app.name;
+        var authors = entry.createdBy;
+        var version = entry.version ? entry.version : ('v' + app.ver);
+        var type;
+
+        if(entry.createdBy === undefined){
+            if(app.authors.length >2){
+                authors = app.authors.slice(0,2).join(', ') + '+ ' + (app.authors.length-2) + ' more';
+            }else{
+                authors = app.authors.join(', ');
             }
-            if (app.module_name && (entry.version === undefined)) {
-                type = '<a href="' + self.options.moduleLink + '/' + app.module_name + '" target="_blank">' +
-                    app.namespace + '</a> ';
-            }
-
-            var $star = $('<i>').addClass('fa fa-star kbcb-star-default');
-            if (favorite) {
-                $star.addClass('fa fa-star kbcb-star-favorite').append('&nbsp;');
-            }
-
-            $star.click(function(e){
-                e.stopPropagation();
-                var params = {};
-                if (app.module_name) {
-                    params['module_name'] = app.module_name;
-                    params['id'] = app.id.split('/')[1];
-                } else {
-                    params['id'] = app.id;
-                }
-
-                if (favorite) {
-                    Promise.resolve(self.catalog.remove_favorite(params))
-                        .then(function () {
-                            $star.removeClass('kbcb-star-favorite');
-                            favorite = null; // important to set this if we don't refresh the panel
-                        })
-                        .catch(function (error) {
-                            console.error(error);
-                        });
-                } else {
-                    Promise.resolve(self.catalog.add_favorite(params))
-                        .then(function () {
-                            $star.addClass('kbcb-star-favorite');
-                            favorite = new Date().getTime(); // important to set this if we don't refresh the panel
-                        })
-                        .catch(function (error) {
-                            console.error(error);
-                        });
-                }            
-            })
-                .tooltip({
-                    title: 'Add or remove from your favorites',
-                    container: 'body',
-                    placement: 'bottom',
-                    delay: {
-                        show: Config.get('tooltip').showDelay,
-                        hide: Config.get('tooltip').hideDelay
-                    }
-                });
-
-            var $logo = $('<div>');
-            if (app.icon && app.icon.url) {
-                var url = Config.url('narrative_method_store_image')+ app.icon.url;
-                $logo.append(DisplayUtil.getAppIcon({ url: url, cursor: 'pointer', setColor: true, size: '50px' }));
-            } else {
-                $logo.append(DisplayUtil.getAppIcon({ cursor: 'pointer', setColor: true }));
-            }
-                   
-            var $name = $('<div>').addClass('kb-data-list-name').append(shortName);
-            var $version = $('<span>').addClass('kb-data-list-version').append(version);
-            var $type = $('<span>').addClass('kb-data-list-type').append(type).append($version);
-            var $date = $('<span>').addClass('kb-data-list-date');
-            var $authors = $('<span>').addClass('kb-data-list-edit-by').append(authors);
-
-            var $title = $('<div>').append($name).append($star);
-
-            var $subcontent = $('<div>')
-                .addClass('narrative-data-list-subcontent')
-                .append($('<span/>').append($star))
-                .append($type)
-                .append($date);
-            
-            if (entry.createdBy === undefined || entry.createdBy) {
-                $subcontent.append($authors);
-            }
-          
-            var layout = {
-                logo: $logo,
-                title: $title,
-                subcontent: $subcontent,
-                moreContent : entry.moreContent
-            };
-
-            var $card = new kbaseCardLayout(layout);
-
-            return $card;
         }
-        return KbaseAppCard;  //end init
-    });
+        if (app.module_name && (entry.version === undefined)) {
+            type = '<a href="' + self.options.moduleLink + '/' + app.module_name + '" target="_blank">' +
+                app.namespace + '</a> ';
+        }
+
+        var $star = $('<i>').addClass('fa fa-star kbcb-star-default');
+        if (favorite) {
+            $star.addClass('fa fa-star kbcb-star-favorite').append('&nbsp;');
+        }
+
+        $star.click(function(e){
+            e.stopPropagation();
+            var params = {};
+            if (app.module_name) {
+                params['module_name'] = app.module_name;
+                params['id'] = app.id.split('/')[1];
+            } else {
+                params['id'] = app.id;
+            }
+
+            if (favorite) {
+                Promise.resolve(self.catalog.remove_favorite(params))
+                    .then(function () {
+                        $star.removeClass('kbcb-star-favorite');
+                        favorite = null; // important to set this if we don't refresh the panel
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            } else {
+                Promise.resolve(self.catalog.add_favorite(params))
+                    .then(function () {
+                        $star.addClass('kbcb-star-favorite');
+                        favorite = new Date().getTime(); // important to set this if we don't refresh the panel
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            }
+        })
+            .tooltip({
+                title: 'Add or remove from your favorites',
+                container: 'body',
+                placement: 'bottom',
+                delay: {
+                    show: Config.get('tooltip').showDelay,
+                    hide: Config.get('tooltip').hideDelay
+                }
+            });
+
+        var $logo = $('<div>');
+        if (app.icon && app.icon.url) {
+            var url = Config.url('narrative_method_store_image')+ app.icon.url;
+            $logo.append(DisplayUtil.getAppIcon({ url: url, cursor: 'pointer', setColor: true, size: '50px' }));
+        } else {
+            $logo.append(DisplayUtil.getAppIcon({ cursor: 'pointer', setColor: true }));
+        }
+
+        var $name = $('<div>').addClass('kb-data-list-name').append(shortName);
+        var $version = $('<span>').addClass('kb-data-list-version').append(version);
+        var $type = $('<span>').addClass('kb-data-list-type').append(type).append($version);
+        var $date = $('<span>').addClass('kb-data-list-date');
+        var $authors = $('<span>').addClass('kb-data-list-edit-by').append(authors);
+
+        var $title = $('<div>').append($name).append($star);
+
+        var $subcontent = $('<div>')
+            .addClass('narrative-data-list-subcontent')
+            .append($('<span/>').append($star))
+            .append($type)
+            .append($date);
+
+        if (entry.createdBy === undefined || entry.createdBy) {
+            $subcontent.append($authors);
+        }
+
+        var layout = {
+            logo: $logo,
+            title: $title,
+            subcontent: $subcontent,
+            moreContent : entry.moreContent
+        };
+
+        var $card = new kbaseCardLayout(layout);
+
+        return $card;
+    }
+    return KbaseAppCard;  //end init
+});

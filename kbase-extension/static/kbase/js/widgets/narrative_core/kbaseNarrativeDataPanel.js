88205c8d867511d83a205b98217df15dce2fdc14
Bill Riehl <wjriehl@lbl.gov>
2018-01-11 17:53:36 -0800

    semicolon

--- kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeDataPanel.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeDataPanel.js
@@ -203,7 +203,7 @@ define([
                     this.trigger('toggleSidePanelOverlay.Narrative', this.$overlayPanel);
                     //once we've clicked it 10 times, meaning we've open and shut the browser 5x, we reveal its TRUE NAME.
                     if (++numDataBrowserClicks >= 10) {
-                        this.$slideoutBtn.attr('data-original-title', 'Hide / Show Slidey McSliderface')
+                        this.$slideoutBtn.attr('data-original-title', 'Hide / Show Slidey McSliderface');
                     }
                 }.bind(this));
 

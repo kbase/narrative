a217b1960af18a687039ec0d25a8e99fcaf3b592
Jim Thomason <jim@japanesewartuba.com>
2018-03-29 16:13:17 -0500

    tried adding in an es6-set polyfill

--- kbase-extension/static/narrative_paths.js
+++ kbase-extension/static/narrative_paths.js
@@ -36,6 +36,7 @@ require.config({
         // not under bower control
         colorbrewer: 'ext_packages/colorbrewer/colorbrewer.min',
         'igv' : 'ext_packages/igv.js/igv.min',
+        'es6set' : 'ext_packages/es6-set/index',
         // copied from the cdn
         'google-code-prettify': 'ext_packages/google-code-prettify/1.2.0/',
         jqueryCookie: 'ext_packages/jquery-extensions/js/jquery.cookie.min',
@@ -473,7 +474,7 @@ require.config({
             deps: ['jquery', 'jqueryui']
         },
         igv : {
-          deps : ['jquery', 'jqueryui', 'bootstrap']
+          deps : ['jquery', 'jqueryui', 'bootstrap', 'es6set']
         }
     }
 });

ef8d644caab1469f1d80e4107a3b56ed284c3ca6
Erik Pearson <eapearson@lbl.gov>
2018-06-01 10:28:49 -0700

    disbled report viewer button is disabled text file not ban [SCT-930]

--- kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeDataList.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/kbaseNarrativeDataList.js
@@ -1216,7 +1216,9 @@ define([
                 .then(function (result) {
                     if (result.length === 0) {
                         $reportButton.addClass('disabled');
-                        $reportButton.empty().append($('<span>').addClass('fa fa-ban').css('width', '0.75em'));
+                        $reportButton.empty().append($('<span>')
+                            .addClass('fa fa-file-text')
+                            .css('width', '0.75em'));
                         $reportButton
                             .tooltip({
                                 title: 'No report associated with this object',
@@ -1229,7 +1231,11 @@ define([
                         objData.reportRef = null;
                         return;
                     } else if (result.length === 1) {
-                        $reportButton.empty().append($('<span>').addClass('fa fa-file-text').css('width', '0.75em'));
+                        $reportButton
+                            .empty()
+                            .append($('<span>')
+                                .addClass('fa fa-file-text')
+                                .css('width', '0.75em'));
                         objData.reportRef = result[0].ref;
                         $reportButton
                             .tooltip({

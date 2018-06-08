cb4329ea645d8d173d35e094cc706511eb422f65
Bill Riehl <wjriehl@lbl.gov>
2018-06-08 14:06:32 -0700

    bump to version 3.7.3

--- src/biokbase/narrative/__init__.py
+++ src/biokbase/narrative/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['magics', 'ws_util', 'common', 'handlers', 'contents', 'services', 'widgetmanager']
 
 from semantic_version import Version
-__version__ = Version("3.7.2")
+__version__ = Version("3.7.3")
 version = lambda: __version__
 
 # if run directly:

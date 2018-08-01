f500cfdaf5824cb9b956cc385a65ebd8b148cdf1
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-08-01 14:15:26 -0500

    Add some docstrings and make pandas & clustergram widgets dependencies

--- src/biokbase/narrative/viewers.py
+++ src/biokbase/narrative/viewers.py
@@ -7,6 +7,18 @@ ws = clients.get('workspace')
 
 
 def view_as_clustergrammer(ws_ref, col_categories=(), row_categories=(), normalize_on=None):
+    """
+    This function returns an interactive clustergrammer widget for a specified object. Data type
+    must contain a 'data' key with a FloatMatrix2D type value
+    :param ws_ref: Object workspace reference
+    :param col_categories: iterable with the permitted factors from the col_conditionset.
+        Defaults to all factors.
+    :param row_categories: iterable with the permitted categories from the row_conditionset.
+        Defaults to all factors.
+    :param normalize_on: If provided, the matrix will be converted to zscores normalized on the
+        'row' or 'column' axis
+    :return:
+    """
     assert isinstance(col_categories, (tuple, set, list))
     assert isinstance(row_categories, (tuple, set, list))
     assert normalize_on in {None, "row", "column"}
@@ -22,8 +34,11 @@ def view_as_clustergrammer(ws_ref, col_categories=(), row_categories=(), normali
 
 
 def _get_df(ws_ref, col_categories, row_categories):
+    """Gets a dataframe from the WS object"""
     generic_data = ws.get_objects2({'objects': [{'ref': ws_ref}]})['data'][0]['data']
-    _is_compatible_matrix(generic_data)
+    if not _is_compatible_matrix(generic_data):
+        raise ValueError("{} is not a compatible data type for this viewer. Data type must "
+                         "contain a 'data' key with a FloatMatrix2D type value".format(ws_ref))
     cols = _get_categories(generic_data['data']['col_ids'],
                            generic_data.get('col_conditionset_ref'),
                            generic_data.get('col_mapping'),
@@ -36,13 +51,18 @@ def _get_df(ws_ref, col_categories, row_categories):
 
 
 def _is_compatible_matrix(obj):
-    assert 'data' in obj
-    assert 'col_ids' in obj['data']
-    assert 'row_ids' in obj['data']
-    assert 'values' in obj['data']
+    try:
+        assert 'data' in obj
+        assert 'col_ids' in obj['data']
+        assert 'row_ids' in obj['data']
+        assert 'values' in obj['data']
+    except AssertionError:
+        return False
+    return True
 
 
 def _get_categories(ids, conditionset_ref=None, mapping=None, whitelist=()):
+    """Creates the correct kind of multi-factor index for clustergrammer display"""
     if not conditionset_ref:
         return ids
     cat_list = []
@@ -50,6 +70,7 @@ def _get_categories(ids, conditionset_ref=None, mapping=None, whitelist=()):
 
     if not mapping:
         mapping = {x:x for x in ids}
+    whitelist = set(whitelist)
 
     for _id in ids:
         condition_values = condition_data['conditions'][mapping[_id]]

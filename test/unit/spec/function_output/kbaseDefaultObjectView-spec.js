3c19606c118b7415df841e00864a36bd2ecda170
Bill Riehl <wjriehl@lbl.gov>
2018-08-08 14:25:02 -0700

    add test stub

---
+++ test/unit/spec/function_output/kbaseDefaultObjectView-spec.js
@@ -0,0 +1,14 @@
+/*global define*/
+/*global describe, it, expect*/
+/*global jasmine*/
+/*global beforeEach, afterEach*/
+/*jslint white: true*/
+define([
+    'kbaseDefaultObjectView'
+], function(Widget) {
+    describe('Test the kbaseDefaultObjectView widget', function() {
+        it('Should load properly', function() {
+
+        });
+    });
+});

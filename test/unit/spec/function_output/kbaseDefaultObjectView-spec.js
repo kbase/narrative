d09fe30bdd549692d06be761fd5c9d24fced7306
Bill Riehl <wjriehl@lbl.gov>
2018-08-08 17:29:02 -0700

    more tests

--- test/unit/spec/function_output/kbaseDefaultObjectView-spec.js
+++ test/unit/spec/function_output/kbaseDefaultObjectView-spec.js
@@ -55,7 +55,7 @@ define([
                 });
         });
 
-        it('Should properly load with a valid upa', (done) => {
+        it('Should properly load with a valid upa without metadata', (done) => {
             let ws = 1111,
                 oid = 2222,
                 ver = 3333,
@@ -98,10 +98,119 @@ define([
             let w = new KBaseDefaultObjectView($div, {upas: {upas: [upa]}});
             w.render([upa])
                 .then(() => {
-                    expect($div.html()).toContain('fakeUser');
+                    // simple string matching
+                    [
+                        'Overview',
+                        'Metadata',
+                        'Objects of this type don\'t have a viewer associated with them. Showing default information.',
+                        'No metadata found for this object'
+                    ].forEach((str) => {
+                        expect($div.html()).toContain(str);
+                    });
+                    // more complex structure matching
+                    let tabs = $div.find('.tabbable');
+                    expect(tabs).not.toBeNull();
+                    let tabsContent = $div.find('.tab-pane');
+                    expect(tabsContent.length).toEqual(2);
+                    [
+                        upa,
+                        name,
+                        userId,
+                        size.toLocaleString(),
+                        'Explore data landing page',
+                        'View data provenance and relationships',
+                        objType,
+                        'module info',
+                        'type spec'
+                    ].forEach((str) => {
+                        expect($(tabsContent[0]).html()).toContain(str);
+                    });
+                    expect($(tabsContent[1]).html()).toContain('No metadata found for this object.');
+                    expect($(tabsContent[1]).html()).not.toContain('<table>');
                     done();
                 });
+        });
 
+        it('Should properly load with a valid upa with metadata', (done) => {
+            let ws = 1111,
+                oid = 2222,
+                ver = 3333,
+                name = 'fake_test_object',
+                objType = 'FakeModule.FakeType-7.7',
+                saveDate = '2018-08-03T00:17:04+0000',
+                userId = 'fakeUser',
+                wsName = 'fakeWs',
+                checksum = '12345',
+                meta = {
+                    'key1': 'value1',
+                    'key2': 'value2'
+                },
+                size = 1234567,
+                upa = String(ws) + '/' + String(oid) + '/' + String(ver);
+
+            jasmine.Ajax.stubRequest('https://ci.kbase.us/services/ws').andReturn({
+                status: 200,
+                statusText: 'success',
+                contentType: 'application/json',
+                responseHeaders: '',
+                responseText: JSON.stringify({
+                    version: '1.1',
+                    result: [{
+                        infos: [[
+                            oid,
+                            name,
+                            objType,
+                            saveDate,
+                            ver,
+                            userId,
+                            ws,
+                            wsName,
+                            checksum,
+                            size,
+                            meta
+                        ]],
+                        paths: [[upa]]
+                    }]
+                })
+            });
+
+            let w = new KBaseDefaultObjectView($div, {upas: {upas: [upa]}});
+            w.render([upa])
+                .then(() => {
+                    // simple string matching
+                    [
+                        'Overview',
+                        'Metadata',
+                        'Objects of this type don\'t have a viewer associated with them. Showing default information.'
+                    ].forEach((str) => {
+                        expect($div.html()).toContain(str);
+                    });
+                    // more complex structure matching
+                    let tabs = $div.find('.tabbable');
+                    expect(tabs).not.toBeNull();
+                    let tabsContent = $div.find('.tab-pane');
+                    expect(tabsContent.length).toEqual(2);
+                    [
+                        upa,
+                        name,
+                        userId,
+                        size.toLocaleString(),
+                        'Explore data landing page',
+                        'View data provenance and relationships',
+                        objType,
+                        'module info',
+                        'type spec'
+                    ].forEach((str) => {
+                        expect($(tabsContent[0]).html()).toContain(str);
+                    });
+                    expect($(tabsContent[1]).html()).not.toContain('No metadata found for this object.');
+                    expect($(tabsContent[1]).html()).toContain('table');
+                    Object.keys(meta).forEach((key) => {
+                        expect($(tabsContent[1]).html()).toContain(key);
+                        expect($(tabsContent[1]).html()).toContain(meta[key]);
+                    });
+                    done();
+                });
         });
     });
 });

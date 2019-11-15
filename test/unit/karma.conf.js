667b0a4bf73a05970dd26dd3142a901f8a84d7cf
Bill Riehl <wjriehl@lbl.gov>
2019-11-14 16:00:50 -0800

    turn on all front end tests again

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -20,8 +20,8 @@ module.exports = function (config) {
         },
         files: [
             'kbase-extension/static/narrative_paths.js',
-            // {pattern: 'test/unit/spec/**/*.js', included: false},
-            {pattern: 'test/unit/spec/common/validate-Spec.js', included: false},
+            {pattern: 'test/unit/spec/**/*.js', included: false},
+            // {pattern: 'test/unit/spec/common/validate-Spec.js', included: false},
             // {pattern: 'test/unit/spec/narrative_core/upload/fileUploadWidget-spec.js', included: false},
             {pattern: 'node_modules/string.prototype.startswith/startswith.js', included: true},
             {pattern: 'node_modules/string.prototype.endswith/endswith.js', included: true},

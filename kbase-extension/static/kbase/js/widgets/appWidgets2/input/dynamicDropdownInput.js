f5a13cb75c7332a4010c2c34a26d342415012403
Bill Riehl <wjriehl@lbl.gov>
2019-11-20 11:11:37 -0800

    revert dynamic dropdown "fix"

--- kbase-extension/static/kbase/js/widgets/appWidgets2/input/dynamicDropdownInput.js
+++ kbase-extension/static/kbase/js/widgets/appWidgets2/input/dynamicDropdownInput.js
@@ -171,7 +171,7 @@ define([
                         max_length: spec.data.constraints.max_length,
                         required: spec.data.constraints.required
                     };
-                return Validation.validateText(String(selectedItem), validationConstraints);
+                return Validation.validateText(selectedItem, validationConstraints);
             });
         }
 

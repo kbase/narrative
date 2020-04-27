7f7b2d6486014e1c948a0a8bcbb5471944838b37
slebras <slebras@lbl.gov>
2020-04-27 16:17:14 -0700

    addressing some feedback from code review

--- kbase-extension/static/kbase/js/widgets/function_output/kbaseSampleSet.js
+++ kbase-extension/static/kbase/js/widgets/function_output/kbaseSampleSet.js
@@ -35,42 +35,42 @@ define ([
             default_blank_value: ""
         },
 
-        init: function init(options) {
+        init: function (options) {
             this._super(options);
 
-            var self = this;
-            self.obj_ref = self.options.upas.id;
-            self.link_ref = self.obj_ref;
+            // var self = this;
+            this.obj_ref = this.options.upas.id;
+            this.link_ref = this.obj_ref;
 
             if(options._obj_info) {
-                self.ss_info = options._obj_info;
-                self.obj_ref = self.ss_info['ws_id'] + '/' + self.ss_info['id'] + '/' + self.ss_info['version'];
-                self.link_ref = self.ss_info['ws_id'] + '/' + self.ss_info['name'] + '/' + self.ss_info['version'];
+                this.ss_info = options._obj_info;
+                this.obj_ref = this.ss_info['ws_id'] + '/' + this.ss_info['id'] + '/' + this.ss_info['version'];
+                this.link_ref = this.ss_info['ws_id'] + '/' + this.ss_info['name'] + '/' + this.ss_info['version'];
             }
 
-            self.client = new GenericClient(Config.url('service_wizard'), {token: self.authToken()});
-            self.ws = new Workspace(Config.url('workspace'),{'token':self.authToken()});
+            this.client = new GenericClient(Config.url('service_wizard'), {token: this.authToken()});
+            this.ws = new Workspace(Config.url('workspace'),{'token': this.authToken()});
 
-            self.$elem.append($('<div>').attr('align', 'center').append($('<i class="fa fa-spinner fa-spin fa-2x">')));
+            this.$elem.append($('<div>').attr('align', 'center').append($('<i class="fa fa-spinner fa-spin fa-2x">')));
 
             // 1) get stats, and show the panel
             var basicInfoCalls = [];
             basicInfoCalls.push(
-                Promise.resolve(self.ws.get_objects2({objects: [{'ref': this.obj_ref}]}))
-                    .then(function(obj) {
-                        self.ss_obj_data = obj['data'][0]['data']
-                        self.ss_obj_info = obj['data'][0]['info']
-                        self.link_ref = self.ss_obj_info[6] + '/' + self.ss_obj_info[0] + '/' + self.ss_obj_info[4];
+                Promise.resolve(this.ws.get_objects2({objects: [{'ref': this.obj_ref}]}))
+                    .then((obj) => {
+                        this.ss_obj_data = obj['data'][0]['data']
+                        this.ss_obj_info = obj['data'][0]['info']
+                        this.link_ref = this.ss_obj_info[6] + '/' + this.ss_obj_info[0] + '/' + this.ss_obj_info[4];
                     }));
 
             Promise.all(basicInfoCalls)
-                .then(function() {
-                    self.renderBasicTable();
+                .then(() => {
+                    this.renderBasicTable();
                 })
-                .catch(function(err) {
+                .catch((err) => {
                     console.error('an error occurred! ' + err);
-                    self.$elem.empty();
-                    self.$elem.append('Error' + JSON.stringify(err));
+                    this.$elem.empty();
+                    this.$elem.append('An unexpected error occured: \nPlease contact KBase <a href="https://kbase.us/new-help-board/">here<a>');
                 });
 
             return this;
@@ -105,29 +105,31 @@ define ([
             self.client.sync_call('SampleService.get_sample', [{
                 id: self.ss_obj_data['samples'][0]['id']
             }]).then(function(sample){
-                var node_tree = sample[0]['node_tree'][0]
-                Object.keys(node_tree['meta_controlled']).concat(Object.keys(node_tree['meta_user'])).forEach( function(metakey){
-                    self.metadata_headers.push({
-                        id: metakey.split(" ").join('_'),
-                        text: metakey,
-                        isSortable: true
+                if (sample.length > 0 && 'node_tree' in sample[0] && sample[0]['node_tree'].length > 0){
+                    var node_tree = sample[0]['node_tree'][0]
+                    Object.keys(node_tree['meta_controlled']).concat(Object.keys(node_tree['meta_user'])).forEach( function(metakey){
+                        self.metadata_headers.push({
+                            id: metakey.split(" ").join('_'),
+                            text: metakey,
+                            isSortable: true
+                        })
                     })
-                })
+                } else {
+                    console.error('Error: Could not load the first sample for metadata headers: ' + err);
+                }
             })
 
             // Build the tabs
             var $tabs = new kbaseTabs($tabPane, {
                 tabPosition : 'top',
-                canDelete : true, //whether or not the tab can be removed.
+                canDelete : false, //whether or not the tab can be removed.
                 tabs : [
                     {
                         tab : 'Summary',   //name of the tab
                         content : $('<div>').css('margin-top','15px').append($overviewTable),
-                        canDelete : false, //override the canDelete param on a per tab basis
                         show : true,
                     }, {
                         tab : 'Samples',
-                        canDelete : false,
                         showContentCallback: function() {
                             return self.addSamplesList();
                         }
@@ -187,19 +189,12 @@ define ([
                             var row = rows[j];
                             for (const meta_idx in self.metadata_headers){
                                 var meta_header = self.metadata_headers[meta_idx]
-                                console.log('in promise all')
-                                console.log(sample_data[j])
-                                console.log(meta_header)
-                                console.log(meta_header.id)
-                                console.log(meta_header.text)
-                                console.log(sample_data[j][meta_header.text])
                                 var row_str = self.options.default_blank_value;
                                 if (meta_header.text in sample_data[j]){
                                     row_str = sample_data[j][meta_header.text]
                                 }
                                 row.push(row_str);
                             }
-                            console.log(self.metadata_headers)
                             rows[j] = row
                         }
                         return {
@@ -216,9 +211,9 @@ define ([
         },
 
         unpack_metadata_to_string: function(metadata){
-            var metastr = metadata['value'];
+            var metastr = String(metadata['value']);
             if ('units' in metadata){
-                metastr =  String(metadata['value']) + " " + String(metadata['units']);
+                metastr += " " + String(metadata['units']);
             }
             return metastr
         },

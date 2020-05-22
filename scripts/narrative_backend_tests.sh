d92d0f4354ef96c2a1d5eb85a7ed09e667a7a31f
Bill Riehl <wjriehl@lbl.gov>
2020-05-22 12:24:15 -0700

    try with codecov

--- scripts/narrative_backend_tests.sh
+++ scripts/narrative_backend_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export NARRATIVE_DIR=$(pwd)
-python -m nose $@ --nocapture --with-coverage --cover-html --cover-package=biokbase.narrative src/biokbase/narrative/tests
+python -m nose $@ --nocapture --with-coverage --cover-html --cover-xml --cover-package=biokbase.narrative src/biokbase/narrative/tests

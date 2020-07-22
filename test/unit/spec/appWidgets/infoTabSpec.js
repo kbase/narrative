ae3945fb2fe949fc0a062fc7d08da76856c8d141
Dakota Blair <dblair@bnl.gov>
2020-07-22 13:53:42 -0400

    Incorporated feedback on app cell info tab.
    This commit adjusts the app cell info tab according to user feedback, fixes
    some unit tests and updates some linter settings. Note that this commit changes
    the order of the info tab among other app cell tabs. This is not immediately
    obvious as JS did not always guarantee key order in objects, but now it does.
    It removes some instances of Bluebird in favor of the global built in Promise
    object. It fixes some unit tests to test for what their spec says they are
    testing for, and includes some new tests.

--- test/unit/spec/appWidgets/infoTabSpec.js
+++ test/unit/spec/appWidgets/infoTabSpec.js
@@ -1,6 +1,6 @@
-/*global define*/
-/*global describe, expect, it*/
 /*global beforeEach */
+/*global define*/ // eslint-disable-line no-redeclare
+/*global describe, expect, it*/
 /*jslint white: true*/
 
 define([
@@ -22,11 +22,13 @@ define([
     });
 
     describe('The App Info Tab instance', function() {
-        let node;
         const model = {
             getItem: (item) => model[item],
             'app': {'tag': 'Mock App'},
             'app.spec': {
+                'full_info' : {
+                    'description': 'Details about this mock app'
+                },
                 'info': {
                     'authors': ['Abraham', 'Martin', 'John'],
                     'id': 0,
@@ -35,19 +37,26 @@ define([
                 },
                 'parameters': [{
                     'text_options': {
-                        'valid_ws_types': ['genome']},
-                    'ui_name': 'Genome',
+                        'valid_ws_types': ['KBaseRNASeq.RNASeqSampleSet']},
+                    'ui_name': 'RNA sequence object <font color=red>*</font>',
+                }, {
+                    'ui_name': 'Adapters',
                 }],
             },
             'executionStats': {
                 'number_of_calls': 1729,
                 'total_exec_time': 9001,
-            }
+            },
         };
+        const appSpec = model.getItem('app.spec');
         const mockInfoTab = infoTabWidget.make({model});
+        let node, infoTabPromise, infoTab;
 
-        beforeEach(function () {
+        beforeEach(async function () {
             node = document.createElement('div');
+            infoTabPromise = mockInfoTab.start({node});
+            infoTab = await infoTabPromise;
+            return infoTab; // to use infoTab for linter
         });
 
         it('has a factory which can be invoked', function() {
@@ -60,31 +69,43 @@ define([
         });
 
         it('has a method "start" which returns a Promise',
-            async function() {
-                const result = await mockInfoTab.start({node});
-                expect(result).toBe(node);
+            function() {
+                expect(infoTabPromise instanceof Promise).toBeTrue();
             }
         );
 
         it('has a method "stop" which returns a Promise',
-            async function() {
-                await mockInfoTab.start({node});
-                const result = await mockInfoTab.stop();
-                expect(result).toBeUndefined();
+            function() {
+                const result = mockInfoTab.stop();
+                expect(result instanceof Promise).toBeTrue();
             }
         );
 
-        it('returns the defined description', async function() {
-            const infoTab = await mockInfoTab.start({node});
+        it('returns the defined description', function() {
             expect(infoTab.firstChild.textContent).toBe(
-                model['app.spec']['info']['subtitle']
+                appSpec.full_info.description
             );
         });
 
-        it('returns an item for each parameter', async function() {
-            const infoTab = await mockInfoTab.start({node});
+        it('returns an item for each parameter', function() {
             const listItems = Array.from(infoTab.querySelectorAll('li li'));
-            expect(listItems.length).toBe(1);
+            expect(listItems.length).toBe(appSpec.parameters.length);
+        });
+
+        it('renders parameter with formatting correctly', function() {
+            const RNASeqFormat = infoTab.querySelectorAll(
+                'li li:nth-child(1) font'
+            );
+            expect(RNASeqFormat.length).toBeGreaterThan(0);
+        });
+
+        it('renders parameter with no formatting correctly', function() {
+            const AdaptersFormat = infoTab.querySelectorAll(
+                'li li:nth-child(2)'
+            )[0];
+            expect(AdaptersFormat.innerText).toBe(
+                appSpec.parameters[1].ui_name
+            );
         });
     });
 });

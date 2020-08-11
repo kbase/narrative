253e30de6bfd7ebe6c209c139364d662357cef24
Bill Riehl <wjriehl@lbl.gov>
2020-08-11 09:09:56 -0700

    fix whitespace

--- scripts/local-dev-run.sh
+++ scripts/local-dev-run.sh
@@ -14,7 +14,7 @@ echo "Starting Narrative for environment '${ENV}'"
 mount_local_dirs="${mount:-t}"
 
 if [ "${mount_local_dirs}" == "t" ]; then
-    echo "Mounting local dirs ${mount_local_dirs}"
+	echo "Mounting local dirs ${mount_local_dirs}"
 	docker run \
 		--dns=8.8.8.8 \
 		-e "CONFIG_ENV=${ENV}" \
@@ -22,8 +22,8 @@ if [ "${mount_local_dirs}" == "t" ]; then
 		--network=kbase-dev \
 		--name=narrative  \
 		--mount type=bind,src=${root}/${kbase_dir},dst=${container_root}/${kbase_dir} \
-        --mount type=bind,src=${root}/${test_dir},dst=${container_root}/${test_dir} \
-        --mount type=bind,src=${root}/${src_dir},dst=${container_root}/${src_dir} \
+		--mount type=bind,src=${root}/${test_dir},dst=${container_root}/${test_dir} \
+		--mount type=bind,src=${root}/${src_dir},dst=${container_root}/${src_dir} \
 		--mount type=bind,src=${root}/${nbextension_dir},dst=${container_root}/kbase-extension/static/${nbextension_dir} \
 		--rm -it \
 		kbase/narrative:dev

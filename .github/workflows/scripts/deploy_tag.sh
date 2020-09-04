c00e04a240d66cd3bb1956e88dac110f0fe1e8bd
JS Fillman <jsfillman@lbl.gov>
2020-09-04 12:56:27 -0700

    Updated deploy_tag.sh with new repo-agnostic version

--- .github/workflows/scripts/deploy_tag.sh
+++ .github/workflows/scripts/deploy_tag.sh
@@ -1,25 +1,34 @@
 #! /usr/bin/env bash
 
-# Usage: ./deploy_tag.sh IMAGE_TAG TARGET DEV_PROD
+# Usage: ./deploy_tag.sh -e TARGET -o ORG -r REPO -s DEV_PROD -t IMAGE_TAG
 #
-# Example 1: ./deploy_tag.sh latest ci dev
-# Example 2: ./deploy_tag.sh pr-9001 next prod
+# Example 1: ./deploy_tag.sh -o "kbase" -r "narrative-traefiker" -s "dev" -t "pr-9001" -e "ci"
+# Example 2: ./deploy_tag.sh -o "kbase" -r "narrative" -s "prod" -t "latest" -e "next"
 #
 # Where:
-#   IMAGE_TAG is either the pull request (e.g. `pr-9001`) or `latest`
-#   TARGET is one of: `appdev`, `ci`, or `next`
-#   DEV_PROD is the source of the above IMAGE_TAG:
-#     If set to `dev`, then the image will be pulled from the `APPNAME-develop` image
-#     Otherwise, it'll pull the (pre)production `APPNAME` image
+#   -o ORG is the organization (`kbase`, `kbaseapps`, etc.)
+#   -r REPO is the repository (e.g. `narrative`)
+#   -s DEV_PROD determines whether to pull the development {APPNAME}-develop or production {APPNAME} image.
+#   -t IMAGE_TAG is the *current* Docker image tag, typically `pr-#` or `latest`
+#   -e TARGET is one of: `appdsshev`, `ci`, or `next`
 #
 # Be sure to set $TOKEN first!
 # See: https://docs.github.com/en/packages/getting-started-with-github-container-registry/migrating-to-github-container-registry-for-docker-images#authenticating-with-the-container-registry
 
-IMAGE_TAG=$1
-TARGET=$2
-DEV_PROD=$3
 
-curl -v -H "Authorization: token $TOKEN" \
+while getopts e:o:r:s:t: option
+  do
+   case "${option}"
+    in
+      e) TARGET=${OPTARG};;
+      o) ORG=${OPTARG};;
+      r) REPO=${OPTARG};;
+      s) DEV_PROD=${OPTARG};;
+      t) IMAGE_TAG=${OPTARG};;
+    esac
+done
+
+curl -H "Authorization: token $TOKEN" \
     -H 'Accept: application/vnd.github.everest-preview+json' \
-    "https://api.github.com/repos/kbase/narrative/dispatches" \
-    -d '{"event_type":"Tag '"$IMAGE_TAG"' for '"$TARGET"'", "client_payload": {"image_tag": "'"$IMAGE_TAG"'","target": "'"$TARGET"'","dev_prod": "'"$DEV_PROD"'"}}'
+    "https://api.github.com/repos/$ORG/$REPO/dispatches" \
+    -d '{"event_type":"Tag '"$DEV_PROD"' '"$IMAGE_TAG"' for '"$TARGET"'", "client_payload": {"image_tag": "'"$IMAGE_TAG"'","target": "'"$TARGET"'","dev_prod": "'"$DEV_PROD"'"}}'

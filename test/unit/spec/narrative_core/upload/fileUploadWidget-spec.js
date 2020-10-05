6a21219fb0df8e8968946a0162b9c59b749060ea
leia-sefkin <leia.sefkin@gmail.com>
2020-10-05 15:26:49 -0700

    removing globus tests from file upload widget

--- test/unit/spec/narrative_core/upload/fileUploadWidget-spec.js
+++ test/unit/spec/narrative_core/upload/fileUploadWidget-spec.js
@@ -62,46 +62,7 @@ define([
             jasmine.Ajax.uninstall();
         });
 
-        it('Should render properly when linked to globus', () => {
-            const $node = $('<div>'),
-                fuw = new FileUploadWidget($node, {
-                    path: '/',
-                    userInfo: {
-                        user: fakeUser,
-                        globusLinked: true
-                    }
-                }),
-                renderedMsg = $node.find('.dz-message').html();
-            expect(renderedMsg).toContain('<p>Three ways to add data to the staging area:</p>');
-            expect(renderedMsg).toContain('<li>Click in this box.</li>');
-            expect(renderedMsg).toContain('<li>Drag and drop data files.</li>');
-            expect(renderedMsg).toContain('For large files (over 20GB) or a large number of files use');
-            expect(renderedMsg).toContain(Config.get('upload').globus_upload_url);
-            expect(renderedMsg).toContain(fakeUser);
-            expect(renderedMsg).not.toContain('For large files (over 20GB), use Globus. Click');
-            expect(renderedMsg).not.toContain('https://docs.kbase.us/data/globus');
-        });
-
-        it('Should render properly when not linked to globus', () => {
-            const $node = $('<div>'),
-                fuw = new FileUploadWidget($node, {
-                    path: '/',
-                    userInfo: {
-                        user: fakeUser,
-                        globusLinked: false
-                    }
-                }),
-                renderedMsg = $node.find('.dz-message').html();
-            expect(renderedMsg).toContain('<p>Three ways to add data to the staging area:</p>');
-            expect(renderedMsg).toContain('<li>Click in this box.</li>');
-            expect(renderedMsg).toContain('<li>Drag and drop data files.</li>');
-            expect(renderedMsg).toContain('For large files (over 20GB), use Globus. Click');
-            expect(renderedMsg).toContain('https://docs.kbase.us/data/globus');
-            expect(renderedMsg).not.toContain('For large files (over 20GB) or a large number of files use');
-            expect(renderedMsg).not.toContain(Config.get('upload').globus_upload_url);
-        });
-
-        it('Should be able to set and retrieve the path', () => {
+       it('Should be able to set and retrieve the path', () => {
             const $node = $('<div>'),
                 fuw = new FileUploadWidget($node, {
                     path: '/',
@@ -155,24 +116,6 @@ define([
                 done();
             });
             fuWidget.dropzone.addFile(mockFile);
-        });
-
-        it('Should open a Globus window on click', () => {
-            // first pass = no globus link.
-            const unlinkedLink = 'https://docs.kbase.us/data/globus';
-            spyOn(window, 'open').and.callThrough();
-            fuWidget.$elem.find('a.globus_link').click();
-            expect(window.open).toHaveBeenCalledWith(unlinkedLink, '_blank');
-            const $node = $('<div>'),
-                fuw = new FileUploadWidget($node, {
-                    path: '/',
-                    userInfo: {
-                        user: fakeUser,
-                        globusLinked: true
-                    }
-                });
-            fuw.$elem.find('a.globus_link').click();
-            expect(window.open).toHaveBeenCalledWith('', 'dz-globus');
         });
 
     });

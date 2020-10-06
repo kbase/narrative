b50aa0f463fe4113dd1e831cd47bcd78f00c479f
eamahanna <emahanna@truss.works>
2020-10-06 15:56:31 -0800

    get a test appCell working

--- test/unit/spec/nbextensions/appCell2/widgets/appCellWidget-spec.js
+++ test/unit/spec/nbextensions/appCell2/widgets/appCellWidget-spec.js
@@ -6,11 +6,11 @@
 define([
     'jquery',
     '../../../../../../../narrative/nbextensions/appCell2/widgets/appCellWidget',
-    'kbase/js/common/monoBus'
+    'common/runtime',
 ], function(
     $,
     AppCell,
-    MonoBus
+    Runtime
 ) {
     'use strict';
     describe('The appCell widget module', function() {
@@ -48,27 +48,98 @@ define([
             user_permission: "a"
         };
 
-        var bus = MonoBus.make();
+        var cell = {
+            metadata: {
+                kbase: {
+                    attributes:{
+                        created: "Fri, 27 Mar 2020 17:39:10 GMT",
+                        id: "71e12dca-3a12-4dd7-862b-125f4337e723",
+                        info: {
+                            label: "more...",
+                            url: "/#appcatalog/app/simpleapp/example_method/beta"
+                        },
+                        lastLoaded: "Tue, 06 Oct 2020 23:28:26 GMT",
+                        status: "new",
+                        subtitle: "Perform some kind of method",
+                        title: "SimpleApp Simple Add"
+                    },
+                    appCell: {
+                        app: {
+                            spec: {
+                                parameters: [{
+                                    advanced: 0,
+                                    allow_multiple: 0,
+                                    default_values: ["0"],
+                                    description: "The first parameter that needs to be entered to drive the method. This might be the first of many.",
+                                    disabled: 0,
+                                    field_type: "text",
+                                    id: "base_number",
+                                    optional: 1,
+                                    short_hint: "The first parameter",
+                                    text_options:{
+                                        is_output_name: 0,
+                                        placeholder: "",
+                                        regex_constraint: [],
+                                        valid_ws_types: [],
+                                        validate_as: "int",
+                                        ui_class: "parameter",
+                                        ui_name: "base_number",
+                                    }
+                                }]
+                            }
+                        }
+                    }
+                }
+            }
+        };
 
-        beforeEach( () => {
+        // Can't create a cell each time. I don't know how to kill the cellBus...
+        // beforeEach( () => {
+            var bus = Runtime.make().bus()
             $node = $("<div>");
             mockAppCell = AppCell.make({
                 workspaceInfo: workspaceInfo,
                 bus: bus,
-                cell: {},
+                cell: cell,
 
             });
             appCell = mockAppCell.start({$node});
-        });
+        // });
 
-        afterEach(() => {
-            $node = null;
-            appCell = null;
-            mockAppCell = null;
-        });
+        // afterEach(() => {
+        //     $node = null;
+        //     appCell = null;
+        //     mockAppCell = null;
+        //     bus.stop();
+        // });
 
-        it('Should return a make function', function() {
+        it('Should render an app cell', function() {
             expect(true).toEqual(true);
         });
+
+        it('Should render the runtime control panel', function() {
+            expect(true).toEqual(true);
+        });
+
+        it('Should an action button in the control panel', function() {
+            expect(true).toEqual(true);
+        });
+
+        it('When the mode: execution-requsted, the cancel button should be enabled', function() {
+            expect(true).toEqual(true);
+        });
+
+        it('When the mode: execution-requsted, the cancel button should be enabled', function() {
+            expect(true).toEqual(true);
+        });
+
+        it('When the cancel button is clicked a confimation modal should appear', function() {
+            expect(true).toEqual(true);
+        });
+
+        it('When the cancel button is clicked a confimation modal should appear', function() {
+            expect(true).toEqual(true);
+        });
+
     });
 });

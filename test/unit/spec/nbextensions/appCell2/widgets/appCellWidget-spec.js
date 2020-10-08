f32c3025b654cb96795091af9bd27367cc202593
eamahanna <emahanna@truss.works>
2020-10-08 14:51:06 -0800

    make codacy happy

--- test/unit/spec/nbextensions/appCell2/widgets/appCellWidget-spec.js
+++ test/unit/spec/nbextensions/appCell2/widgets/appCellWidget-spec.js
@@ -1,6 +1,4 @@
-/*global define*/
 /*global describe, it, expect*/
-/*global jasmine*/
 /*global beforeEach, afterEach*/
 /*jslint white: true*/
 define([
@@ -20,24 +18,22 @@ define([
     let mockAppCell, $node;
 
     var workspaceInfo = {
-        globalread: "n",
+        globalread: 'n',
         id: 54745,
-        lockstat: "unlocked",
+        lockstat: 'unlocked',
         metadata: {
-            cell_count: "1",
-            narrative_nice_name: "Test Narrative",
-            searchtags: "narrative",
-            is_temporary: "false",
-            narrative: "1"
+            cell_count: '1',
+            narrative_nice_name: 'Test Narrative',
+            searchtags: 'narrative',
+            is_temporary: 'false',
+            narrative: '1'
         },
-        moddate: "2020-10-06T03:30:52+0000",
-        name: "testUser:narrative_1601948894239",
+        moddate: '2020-10-06T03:30:52+0000',
+        name: 'testUser:narrative_1601948894239',
         object_count: 1,
-        owner: "testUser",
-        user_permission: "a"
+        owner: 'testUser',
+        user_permission: 'a'
     };
-
-    $inputElement = $('<div>');
 
     var cell = {
         cell_type: 'code',
@@ -45,16 +41,16 @@ define([
             kbase: {
                 type: 'app',
                 attributes:{
-                    created: "Fri, 27 Mar 2020 17:39:10 GMT",
-                    id: "71e12dca-3a12-4dd7-862b-125f4337e723",
+                    created: 'Fri, 27 Mar 2020 17:39:10 GMT',
+                    id: '71e12dca-3a12-4dd7-862b-125f4337e723',
                     info: {
-                        label: "more...",
-                        url: "/#appcatalog/app/simpleapp/example_method/beta"
+                        label: 'more...',
+                        url: '/#appcatalog/app/simpleapp/example_method/beta'
                     },
-                    lastLoaded: "Tue, 06 Oct 2020 23:28:26 GMT",
-                    status: "new",
-                    subtitle: "Perform some kind of method",
-                    title: "SimpleApp Simple Add"
+                    lastLoaded: 'Tue, 06 Oct 2020 23:28:26 GMT',
+                    status: 'new',
+                    subtitle: 'Perform some kind of method',
+                    title: 'SimpleApp Simple Add'
                 },
                 appCell: {
                     app: {
@@ -62,27 +58,26 @@ define([
                             parameters: [{
                                 advanced: 0,
                                 allow_multiple: 0,
-                                default_values: ["0"],
-                                description: "The first parameter that needs to be entered to drive the method. This might be the first of many.",
+                                default_values: ['0'],
+                                description: 'The first parameter that needs to be entered to drive the method. This might be the first of many.',
                                 disabled: 0,
-                                field_type: "text",
-                                id: "base_number",
+                                field_type: 'text',
+                                id: 'base_number',
                                 optional: 1,
-                                short_hint: "The first parameter",
+                                short_hint: 'The first parameter',
                                 text_options:{
                                     is_output_name: 0,
-                                    placeholder: "",
+                                    placeholder: '',
                                     regex_constraint: [],
                                     valid_ws_types: [],
-                                    validate_as: "int",
-                                    ui_class: "parameter",
-                                    ui_name: "base_number",
+                                    validate_as: 'int',
+                                    ui_class: 'parameter',
+                                    ui_name: 'base_number',
                                 }
                             }]
                         }
                     }
                 },
-                input: [inputElement],
             }
         }
     };
@@ -97,7 +92,7 @@ define([
     // Can only test the public functions...
     describe('The appCell widget', () => {
 
-        beforeEach( async () => {
+        beforeEach( () => {
             $node = $('<div>');
             var bus = Runtime.make().bus();
             mockAppCell = AppCell.make({
@@ -132,7 +127,7 @@ define([
             expect(mockAppCell.detach).toBeDefined();
         });
 
-        it('has a method "start" which returns a Promise', async () => {
+        it('has a method start which returns a Promise', async () => {
             var startPromise = mockAppCell.start();
             expect(startPromise instanceof Promise).toBeTrue();
 
@@ -140,12 +135,12 @@ define([
             expect(result).toBeNull();
         });
 
-        it('has a method "stop" which returns a Promise', () => {
+        it('has a method stop which returns a Promise', () => {
             var stopPromise = mockAppCell.stop();
             expect(stopPromise instanceof Promise).toBeTrue();
         });
 
-        it('has a method "detach" which returns a Promise', () => {
+        it('has a method detach which returns a Promise', () => {
             var detachPromise = mockAppCell.stop();
             expect(detachPromise instanceof Promise).toBeTrue();
         });

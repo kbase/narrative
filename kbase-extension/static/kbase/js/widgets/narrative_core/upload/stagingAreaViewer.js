722eb05f434d8e1b070091832752eabc356fe89c
leia-sefkin <leia.sefkin@gmail.com>
2020-10-15 13:44:53 -0700

    should actually (hopefully) fix it, renaming class on <a> target so that jquery can find it

--- kbase-extension/static/kbase/js/widgets/narrative_core/upload/stagingAreaViewer.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/upload/stagingAreaViewer.js
@@ -167,7 +167,7 @@ define([
 
             // Add ACL before going to the staging area
             // If it fails, it'll just do so silently.
-            var $globusLink = this.$elem.find('.globus_linked');
+            var $globusLink = this.$elem.find('.globus_acl_link');
             $globusLink.click((e) => {
                 var globusWindow = window.open('', 'globus');
                 globusWindow.document.write('<html><body><h2 style="text-align:center; font-family:\'Oxygen\', arial, sans-serif;">Loading Globus...</h2></body></html>');

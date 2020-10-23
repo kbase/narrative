09816dca11a00c112ee4d7fa454e2b3458826f31
eamahanna <emahanna@truss.works>
2020-10-22 20:04:38 -0800

    fix typo

--- kbase-extension/static/kbase/js/widgets/narrative_core/upload/fileUploadWidget.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/upload/fileUploadWidget.js
@@ -96,7 +96,7 @@ define([
                 .on('success', (file, serverResponse) => {
                     var $successElem = $(file.previewElement);
                     $successElem.find('#upload_progress_and_cancel').remove();
-                    $successElem.find('#dz_file_row_1'.css({"display": "flex", "align-items": "center"}));
+                    $successElem.find('#dz_file_row_1').css({"display": "flex", "align-items": "center"});
                     $successElem.find('#success_icon').css('display', 'flex');
                     $successElem.find('#success_message').css('display', 'inline');
                     $dropzoneElem.find('#upload-message').text(this.makeUploadMessage());
@@ -130,7 +130,7 @@ define([
                 .on('error', (erroredFile) => {
                     var $errorElem = $(erroredFile.previewElement);
                     $errorElem.find('#upload_progress_and_cancel').remove();
-                    $errorElem.find('#dz_file_row_1'.css({"display": "flex", "align-items": "center"}));
+                    $errorElem.find('#dz_file_row_1').css({"display": "flex", "align-items": "center"});
                     $errorElem.css('color', '#DF0002');
                     $errorElem.find('#error_icon').css('display', 'flex');
 

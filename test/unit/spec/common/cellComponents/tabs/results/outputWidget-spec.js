65fad263c25115ad5b1d30e60ef971f00d94c125
Bill Riehl <wjriehl@lbl.gov>
2020-12-21 17:23:23 -0800

    remove comments from tests

--- test/unit/spec/common/cellComponents/tabs/results/outputWidget-spec.js
+++ test/unit/spec/common/cellComponents/tabs/results/outputWidget-spec.js
@@ -7,26 +7,6 @@ define([
 ], (OutputWidget, Jupyter, Config, Workspace, ResultsData) => {
     'use strict';
 
-    // const reports = ['1/2/3', '4/5/6'];  // just some simple happy path report ids
-    // const reportsToObjects = {
-    //     '1/2/3': {
-    //         'objects_created': [{
-    //             'description': 'An Object',
-    //             'ref': '7/8/9'
-    //         }]
-    //     },
-    //     '4/5/6': {
-    //         'objects_created': [{
-    //             'description': 'Another Object',
-    //             'ref': '10/11/12'
-    //         }]
-    //     }
-    // };
-    // const objectInfos = {
-    //     '7/8/9': [8, 'SomeObject', 'KBaseGenomes.Genome-1.0'],
-    //     '10/11/12': [11, 'SomeOtherObject', 'KBaseGenomeAnnotations.Assembly-2.0']
-    // };
-
     /**
      * Sets up happy path workspace mock AJAX calls using jasmine.Ajax.
      * This mocks the calls to get_objects2 and get_object_info_new

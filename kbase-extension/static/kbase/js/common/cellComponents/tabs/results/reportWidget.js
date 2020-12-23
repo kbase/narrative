f4fdd83b618f30c21e26251d46950b37ac41e89e
Bill Riehl <wjriehl@lbl.gov>
2020-12-23 11:15:53 -0800

    refactoring report widget

--- kbase-extension/static/kbase/js/common/cellComponents/tabs/results/reportWidget.js
+++ kbase-extension/static/kbase/js/common/cellComponents/tabs/results/reportWidget.js
@@ -1,6 +1,8 @@
-define(['bluebird', 'jquery', 'common/html', 'common/ui', 'common/events'], function (
+/**
+ * This renders a list of KBase Report objects. Each one is expandable and loads up under a caret.
+ */
+define(['bluebird', 'common/html', 'common/ui', 'common/events'], function (
     Promise,
-    $,
     html,
     UI,
     Events
@@ -18,6 +20,12 @@ define(['bluebird', 'jquery', 'common/html', 'common/ui', 'common/events'], func
                 console.log('ready to render report with data: ', data);
                 resolve(true);
             });
+        }
+
+        function fetchReportData(reports, workspaceClient) {
+            const reportLookupParam = reports.map(report => ({ref: report}));
+
+            return workspaceClient.get_object_info_new({objects: reportLookupParam});
         }
 
         function renderLayout() {
@@ -41,26 +49,41 @@ define(['bluebird', 'jquery', 'common/html', 'common/ui', 'common/events'], func
             };
         }
 
-        function doAttach(node) {
-            container = node;
+        /**
+         *
+         * @param {object} arg
+         * - node - the DOM node to attach to
+         * - reports - an array of report references to render from
+         * - workspaceClient - a workspace client to use to fetch report info
+         */
+        function doAttach(arg) {
+            container = arg.node;
             ui = UI.make({
                 node: container,
             });
 
-            let layout = renderLayout();
+            return fetchReportData(arg.reports, arg.workspaceClient)
+                .then((reportData) => {
+                    let layout = renderLayout();
 
-            container.innerHTML = layout.content;
-            layout.events.attachEvents(container);
+                    container.innerHTML = layout.content;
+                    layout.events.attachEvents(container);
+                });
         }
 
+        /**
+         *
+         * @param {object} arg
+         * - node - the DOM node to build this under
+         * - reports - a list of report ids to fetch the relevant info from
+         * - workspaceClient - a workspace client to use
+         */
         function start(arg) {
             // send parent the ready message
-            doAttach(arg.node);
-
-            return renderReport(arg.data).catch(function (err) {
-                // do somethig with the error.
-                console.error('ERROR in start', err);
-            });
+            return doAttach(arg)
+                .catch((err) => {
+                    console.error('Error while starting the created objects view', err);
+                });
         }
 
         function stop() {

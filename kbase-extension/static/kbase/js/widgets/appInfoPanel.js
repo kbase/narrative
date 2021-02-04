8afe6ab4207f053346861a4ea258884fcbddd507
Bill Riehl <wjriehl@lbl.gov>
2021-02-04 13:27:20 -0500

    fix app info panel widget and tests

--- kbase-extension/static/kbase/js/widgets/appInfoPanel.js
+++ kbase-extension/static/kbase/js/widgets/appInfoPanel.js
@@ -1,4 +1,3 @@
-/*global define*/
 /*jslint white:true,browser:true*/
 /**
  * Creates an informational panel for Apps, based on some general app info.
@@ -15,79 +14,72 @@ define([
     'kb_service/client/narrativeMethodStore',
     'handlebars',
     'text!kbase/templates/app_info_panel.html'
-], function(
+], (
     Promise,
     Runtime,
     Catalog,
     NarrativeMethodStore,
     Handlebars,
     appInfoPanelTmpl
-) {
+) => {
     'use strict';
 
     function factory(config) {
-        var container,
-            runtime = Runtime.make(),
+        const runtime = Runtime.make(),
             nms = new NarrativeMethodStore(runtime.config('services.narrative_method_store.url')),
             catalog = new Catalog(runtime.config('services.catalog.url')),
             appId = config.appId,
             appModule = config.appModule,
             tag = config.tag || 'release',
-            infoPanel = Handlebars.compile(appInfoPanelTmpl),
+            infoPanel = Handlebars.compile(appInfoPanelTmpl);
+        let container,
             info = {};
 
         function start(arg) {
-            return Promise.try(function() {
-                    container = arg.node;
+            container = arg.node;
 
-                    var infoProms = [
-                        /* Get the method full info so we can populate the description */
-                        nms.get_method_full_info({ 'ids': [appId], 'tag': tag })
-                        .then(function(methodInfo) {
-                            methodInfo = methodInfo[0] || {};
-                            var desc = methodInfo.description || '';
-                            info.description = new Handlebars.SafeString(desc);
+            var infoProms = [
+                /* Get the method full info so we can populate the description */
+                nms.get_method_full_info({ 'ids': [appId], 'tag': tag })
+                    .then(function(methodInfo) {
+                        methodInfo = methodInfo[0] || {};
+                        var desc = methodInfo.description || '';
+                        info.description = new Handlebars.SafeString(desc);
 
-                            var authorList = methodInfo.authors || [];
-                            info.authorList = authorList.join(', ');
-                            info.multiAuthors = authorList.length > 1;
-                        }),
+                        var authorList = methodInfo.authors || [];
+                        info.authorList = authorList.join(', ');
+                        info.multiAuthors = authorList.length > 1;
+                    }),
 
-                        /* Get the method stats so we know how many times it was run. */
-                        catalog.get_exec_aggr_stats({ 'full_app_ids': [appId] })
-                        .then(function(appStats) {
-                            console.log('app stats', appStats);
-                            appStats = appStats[0] || {};
-                            info.runCount = appStats.number_of_calls || 'unknown';
-                        }),
+                /* Get the method stats so we know how many times it was run. */
+                catalog.get_exec_aggr_stats({ 'full_app_ids': [appId] })
+                    .then(function(appStats) {
+                        appStats = appStats[0] || {};
+                        info.runCount = appStats.number_of_calls || 'unknown';
+                    }),
 
-                        /* Get the module info so we know when it was last updated. */
-                        catalog.get_module_info({ 'module_name': appModule })
-                        .then(function(moduleInfo) {
-                            moduleInfo = moduleInfo[tag] || {};
-                            var timestamp = moduleInfo.timestamp || 'unknown';
-                            var dateString = 'unknown';
-                            try {
-                                dateString = new Date(timestamp).toLocaleDateString();
-                            } catch (e) {
-                                //pass
-                            }
-                            info.updateDate = dateString;
-                        })
-                    ];
-                    return Promise.all(infoProms);
-                })
-                .then(function() {
-                    return Promise.try(function() {
-                        container.html(infoPanel(info));
-                    });
+                /* Get the module info so we know when it was last updated. */
+                catalog.get_module_info({ 'module_name': appModule })
+                    .then(function(moduleInfo) {
+                        moduleInfo = moduleInfo[tag] || {};
+                        var timestamp = moduleInfo.timestamp || 'unknown';
+                        var dateString = 'unknown';
+                        try {
+                            dateString = new Date(timestamp).toLocaleDateString();
+                        } catch (e) {
+                            //pass
+                        }
+                        info.updateDate = dateString;
+                    })
+            ];
+            return Promise.all(infoProms)
+                .then(() => {
+                    container.html(infoPanel(info));
                 });
         }
 
         function stop() {
-            return Promise.try(function() {
-                container.html('');
-            });
+            return Promise.resolve(container.html(''));
         }
 
         return {

752f9bc4b2e4ff513e03a8044f88e08838056e16
ialarmedalien <ialarmedalien@gmail.com>
2021-02-09 09:55:55 -0800

    Add in JSON spec reporter for more info on test failures

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -1,5 +1,3 @@
-/*jslint white: true*/
-/*global module */
 // process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
     'use strict';
@@ -21,7 +19,8 @@ module.exports = function (config) {
             'karma-requirejs',
             'karma-coverage',
             'karma-mocha-reporter',
-            'karma-es6-shim'
+            'karma-es6-shim',
+            'karma-json-result-reporter',
         ],
         preprocessors: {
             'kbase-extension/static/kbase/js/**/!(api)/*.js': ['coverage'],
@@ -57,7 +56,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha', 'coverage'],
+        reporters: ['mocha', 'coverage', 'json-result'],
         coverageReporter: {
             type: 'html',
             dir: 'js-coverage/',
@@ -72,6 +71,9 @@ module.exports = function (config) {
         },
         mochaReporter: {
             ignoreSkipped: true,
+        },
+        jsonResultReporter: {
+            outputFile: 'karma-result.json',
         },
         // web server port
         port: 9876,

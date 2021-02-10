57982652d1ca3197dfa239c9e352c8d6d1aad23d
Bill Riehl <wjriehl@lbl.gov>
2021-02-10 11:24:07 -0500

    update bootstrapsearch tests

--- test/unit/spec/Util/BootstrapSearchSpec.js
+++ test/unit/spec/Util/BootstrapSearchSpec.js
@@ -1,59 +1,50 @@
-/*global define*/
-/*global describe, it, expect*/
-/*global jasmine*/
-/*global beforeEach, afterEach*/
-/*jslint white: true*/
-
 define ([
     'jquery',
     'util/bootstrapSearch',
-    'base/js/namespace',
-    'kbaseNarrative'
+    'base/js/namespace'
 ], function(
     $,
     BootstrapSearch,
-    Jupyter,
-    Narrative
+    Jupyter
 ) {
     'use strict';
     var $targetElem;
 
-    describe('Test the BootstrapSearch module', function() {
-        beforeEach(function() {
+    describe('Test the BootstrapSearch module', () => {
+        beforeEach(() => {
             $targetElem = $('<div>');
-            Jupyter.narrative = new Narrative();
-            Jupyter.narrative.disableKeyboardManager = () => {};
+            Jupyter.narrative = {
+                disableKeyboardManager: () => {}
+            };
         });
 
-        afterEach(function() {
+        afterEach(() => {
             $targetElem.empty();
         });
 
-        it('Should create a new search object', function() {
-            var bsSearch = new BootstrapSearch($targetElem, {});
+        it('Should create a new search object', () => {
+            const bsSearch = new BootstrapSearch($targetElem, {});
             expect(bsSearch).not.toBeNull();
         });
 
-        it('Should fire an input function when triggered by input', function() {
-            var passed = false;
-            var bsSearch = new BootstrapSearch($targetElem, {
-                inputFunction: function() {
-                    passed = true;
+        it('Should fire an input function when triggered by input', (done) => {
+            const bsSearch = new BootstrapSearch($targetElem, {
+                inputFunction: () => {
+                    done();
                 }
             });
             bsSearch.val('stuff');
-            expect(passed).toBe(true);
         });
 
-        it('Should have empty and filled icons at the right time', function() {
-            var emptyFa = 'fa-battery-0',
+        it('Should have empty and filled icons at the right time', () => {
+            const emptyFa = 'fa-battery-0',
                 filledFa = 'fa-battery-4';
 
-            var bsSearch = new BootstrapSearch($targetElem, {
+            const bsSearch = new BootstrapSearch($targetElem, {
                 emptyIcon: emptyFa,
                 filledIcon: filledFa
             });
-            var addon = $targetElem.find('span.input-group-addon > span');
+            const addon = $targetElem.find('span.input-group-addon > span');
             expect(addon.hasClass(emptyFa)).toBe(true);
             bsSearch.val('stuff');
             expect(addon.hasClass(filledFa)).toBe(true);
@@ -61,17 +52,17 @@ define ([
             expect(addon.hasClass(emptyFa)).toBe(true);
         });
 
-        it('Should auto-set fa- in front of icons without it', function() {
-            var empty = 'battery-0',
+        it('Should auto-set fa- in front of icons without it', () => {
+            const empty = 'battery-0',
                 emptyFa = 'fa-battery-0',
                 filled = 'battery-4',
                 filledFa = 'fa-battery-4';
 
-            var bsSearch = new BootstrapSearch($targetElem, {
+            const bsSearch = new BootstrapSearch($targetElem, {
                 emptyIcon: empty,
                 filledIcon: filled
             });
-            var addon = $targetElem.find('span.input-group-addon > span');
+            const addon = $targetElem.find('span.input-group-addon > span');
             expect(addon.hasClass(emptyFa)).toBe(true);
             bsSearch.val('stuff');
             expect(addon.hasClass(filledFa)).toBe(true);
@@ -79,63 +70,58 @@ define ([
             expect(addon.hasClass(emptyFa)).toBe(true);
         });
 
-        it('Should clear input by default when clicking addon', function() {
-            var bsSearch = new BootstrapSearch($targetElem);
+        it('Should clear input by default when clicking addon', () => {
+            const bsSearch = new BootstrapSearch($targetElem);
             bsSearch.val('stuff');
             $targetElem.find('span.input-group-addon').click();
             expect(bsSearch.val()).toBeFalsy();
         });
 
-        it('Should fire a function when clicking the addon - overrides default clear', function() {
-            var passed = false;
-            var bsSearch = new BootstrapSearch($targetElem, {
-                addonFunction: function() {
-                    passed = true;
+        it('Should fire a function when clicking the addon - overrides default clear', (done) => {
+            const bsSearch = new BootstrapSearch($targetElem, {
+                addonFunction: () => {
+                    expect(bsSearch.val()).toEqual('stuff');
+                    done();
                 }
             });
             bsSearch.val('stuff');
             $targetElem.find('span.input-group-addon').click();
-            expect(passed).toBe(true);
-            expect(bsSearch.val()).toEqual('stuff');
         });
 
-        it('Should have a val function that works as in vanilla JS', function() {
-            var bsSearch = new BootstrapSearch($targetElem);
+        it('Should have a val function that works as in vanilla JS', () => {
+            const bsSearch = new BootstrapSearch($targetElem);
             bsSearch.val('stuff');
             expect(bsSearch.val()).toEqual('stuff');
         });
 
-        it('Should have a working focus function', function() {
-            var bsSearch = new BootstrapSearch($targetElem);
-            var passed = false;
+        it('Should have a working focus function', (done) => {
+            const bsSearch = new BootstrapSearch($targetElem);
             $('body').append($targetElem);
-            $targetElem.find('input.form-control').on('focus', function() {
-                passed = true;
+            $targetElem.find('input.form-control').on('focus', () => {
+                done();
             });
             bsSearch.focus();
-            expect(passed).toBe(true);
         });
 
-        it('Should set placeholder text', function() {
-            var placeholder = 'some text';
-            var bsSearch = new BootstrapSearch($targetElem, {
+        it('Should set placeholder text', () => {
+            const placeholder = 'some text';
+            const bsSearch = new BootstrapSearch($targetElem, {
                 placeholder: placeholder
             });
             expect($targetElem.find('input.form-control').attr('placeholder')).toEqual(placeholder);
         });
 
-        it('Should trigger an escape function', function() {
-            var passed = false;
-            var bsSearch = new BootstrapSearch($targetElem, {
-                escFunction: function() {
-                    passed = true;
+        it('Should trigger an escape function', (done) => {
+            const passed = false;
+            const bsSearch = new BootstrapSearch($targetElem, {
+                escFunction: () => {
+                    done();
                 }
             });
-            var e = $.Event('keyup');
+            const e = $.Event('keyup');
             e.which = 27;
             e.keyCode = 27;
             $targetElem.find('input.form-control').trigger(e);
-            expect(passed).toBe(true);
         });
     });
 });

dbcf111a82babc6555fb480c2699161ec1adf6bc
Bill Riehl <wjriehl@lbl.gov>
2021-02-10 08:55:42 -0500

    fix linting and other minor things with genome categorizer

--- test/unit/spec/function_output/GenomeCategorizer-spec.js
+++ test/unit/spec/function_output/GenomeCategorizer-spec.js
@@ -1,14 +1,13 @@
-/*jslint white: true*/
 define([
     'jquery',
     'GenomeCategorizer',
-    'base/js/namespace',
-    'kbaseNarrative'
+    'narrativeConfig',
+    'base/js/namespace'
 ], (
     $,
     GenomeCategorizer,
-    Jupyter,
-    Narrative
+    Config,
+    Jupyter
 ) => {
     'use strict';
     describe('Test the GenomeCategorizer widget', () => {
@@ -16,8 +15,9 @@ define([
         beforeEach(() => {
             jasmine.Ajax.install();
             $div = $('<div>');
-            Jupyter.narrative = new Narrative();
-            Jupyter.narrative.getAuthToken = () => { return 'NotARealToken!' };
+            Jupyter.narrative = {
+                getAuthToken: () => 'NotARealToken!'
+            };
         });
 
         afterEach(() => {
@@ -25,53 +25,54 @@ define([
             $div.remove();
         });
 
-        it('Should properly render data', (done) => {
+        it('Should properly render data', () => {
             let genomecatgorizerdata = {
-                "attribute_data":["EamA family transporter RarD","hydrogenase nickel incorporation protein HypA"],
-                "attribute_type":"functional_roles",
-                "class_list_mapping":{"N":1,"P":0},
-                "classifier_description":"this is my description",
-                "classifier_handle_ref":"c061a832-1d64-4119-a2fb-75a360fa1f53",
-                "classifier_id":"",
-                "classifier_name":"DTCFL_DecisionTreeClassifier_entropy",
-                "classifier_type":"DecisionTreeClassifier",
-                "lib_name":"sklearn",
-                "number_of_attributes":3320,
-                "number_of_genomes":2,
-                "training_set_ref":"35279/17/1"
+                'attribute_data':['EamA family transporter RarD','hydrogenase nickel incorporation protein HypA'],
+                'attribute_type':'functional_roles',
+                'class_list_mapping':{'N':1,'P':0},
+                'classifier_description':'this is my description',
+                'classifier_handle_ref':'c061a832-1d64-4119-a2fb-75a360fa1f53',
+                'classifier_id':'',
+                'classifier_name':'DTCFL_DecisionTreeClassifier_entropy',
+                'classifier_type':'DecisionTreeClassifier',
+                'lib_name':'sklearn',
+                'number_of_attributes':3320,
+                'number_of_genomes':2,
+                'training_set_ref':'35279/17/1'
             };
             let trainingsetdata = {
-                "classes":["P","N"],
-                "classification_data":[
+                'classes':['P','N'],
+                'classification_data':[
                     {
-                        "evidence_types":["another","some","list"],
-                        "genome_classification":"N","genome_id": "my_genome_id",
-                        "genome_name":"Acetivibrio_ethanolgignens",
-                        "genome_ref":"35279/3/1",
-                        "references":["some","list"]
+                        'evidence_types':['another','some','list'],
+                        'genome_classification':'N','genome_id': 'my_genome_id',
+                        'genome_name':'Acetivibrio_ethanolgignens',
+                        'genome_ref':'35279/3/1',
+                        'references':['some','list']
                     },{
-                        "evidence_types":["another","some","list"],
-                        "genome_classification":"P",
-                        "genome_id":"my_genome_id",
-                        "genome_name":"Aggregatibacter_actinomycetemcomitans_serotype_b_str._SCC4092",
-                        "genome_ref":"35279/4/1",
-                        "references":["some","list"]
+                        'evidence_types':['another','some','list'],
+                        'genome_classification':'P',
+                        'genome_id':'my_genome_id',
+                        'genome_name':'Aggregatibacter_actinomycetemcomitans_serotype_b_str._SCC4092',
+                        'genome_ref':'35279/4/1',
+                        'references':['some','list']
                     },{
-                        "evidence_types":["another","some","list"],
-                        "genome_classification":"N",
-                        "genome_id":"my_genome_id",
-                        "genome_name":"Afipia_felis_ATCC_53690",
-                        "genome_ref":"35279/5/1",
-                        "references":["some","list"]
-                }],
-                "classification_type":"my_classification_type",
-                "description":"my_description",
-                "name":"my_name",
-                "number_of_classes":2,
-                "number_of_genomes":3
+                        'evidence_types':['another','some','list'],
+                        'genome_classification':'N',
+                        'genome_id':'my_genome_id',
+                        'genome_name':'Afipia_felis_ATCC_53690',
+                        'genome_ref':'35279/5/1',
+                        'references':['some','list']
+                    }
+                ],
+                'classification_type':'my_classification_type',
+                'description':'my_description',
+                'name':'my_name',
+                'number_of_classes':2,
+                'number_of_genomes':3
             };
             // this code is more of less ignored, because two WS calls are made both can't be stubbed
-            jasmine.Ajax.stubRequest('https://ci.kbase.us/services/ws').andReturn({
+            jasmine.Ajax.stubRequest(Config.url('workspace')).andReturn({
                 status: 200,
                 statusText: 'success',
                 contentType: 'application/json',
@@ -108,7 +109,6 @@ define([
             });
             expect($(tabsContent[1]).html()).toContain('Acetivibrio_ethanolgignens');
             expect($(tabsContent[1]).html()).not.toContain('<table>');
-            done();
         });
-    })
-})
+    });
+});

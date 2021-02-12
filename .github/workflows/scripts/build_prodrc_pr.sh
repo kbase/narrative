b3e9b4613cf2f1736f258a90c76be502ab1722eb
Steve Chan <sychan168@gmail.com>
2021-02-12 11:13:43 -0800

    Initial changes to get the narrative_version container built/pushed

--- .github/workflows/scripts/build_prodrc_pr.sh
+++ .github/workflows/scripts/build_prodrc_pr.sh
@@ -5,6 +5,7 @@ export MY_APP=$(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}')
 export DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export COMMIT=$(echo "$SHA" | cut -c -7)
+export NARRATIVE_VERSION_NUM=`grep '\"version\":' src/config.json.templ | awk '{print $2}' | sed 's/"//g'`
 
 docker login -u "$DOCKER_ACTOR" -p "$DOCKER_TOKEN" ghcr.io
 docker build --build-arg BUILD_DATE="$DATE" \
@@ -13,4 +14,19 @@ docker build --build-arg BUILD_DATE="$DATE" \
              --build-arg PULL_REQUEST="$PR" \
              --label us.kbase.vcs-pull-req="$PR" \
              -t ghcr.io/"$MY_ORG"/"$MY_APP":"pr-""$PR" .
+docker push ghcr.io/"$MY_ORG"/"$MY_APP":"pr-""$PR"
+
+docker tag ghcr.io/"$MY_ORG"/"$MY_APP":"pr-""$PR" kbase/narrative:tmp
+export MY_APP=narrative_version
+
+docker build -t ghcr.io/"$MY_ORG"/"$MY_APP":"pr-""$PR" \
+                --build-arg BUILD_DATE=$DATE \
+                --build-arg VCS_REF=$COMMIT \
+                --build-arg BRANCH="$GITHUB_HEAD_REF" \
+                --build-arg PULL_REQUEST="$PR" \
+                --label us.kbase.vcs-pull-req="$PR" \
+                --build-arg NARRATIVE_VERSION=$NARRATIVE_VERSION_NUM \
+                -f Dockerfile2 \
+                .
+docker rmi kbase/narrative:tmp
 docker push ghcr.io/"$MY_ORG"/"$MY_APP":"pr-""$PR"

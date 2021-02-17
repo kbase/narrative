0705089f64a5113545acf96ad6bb13de915bf5eb
Erik Pearson <eapearson@lbl.gov>
2021-02-17 06:35:18 -0800

    add randomized tab selection to test data

--- test/integration/specs/kbaseNarrativeDataPanel_test.js
+++ test/integration/specs/kbaseNarrativeDataPanel_test.js
@@ -2,7 +2,7 @@
 /* eslint {strict: ['error', 'global']} */
 'use strict';
 
-const {login, openNarrative, clickWhenReady, waitForClass} = require('../wdioUtils.js');
+const { login, openNarrative, clickWhenReady, waitForClass } = require('../wdioUtils.js');
 
 // Ideally the test data should be the same, except for narrative id, in each env.
 // But currently CI and prod are indexed differently.
@@ -10,31 +10,46 @@ const {login, openNarrative, clickWhenReady, waitForClass} = require('../wdioUti
 // Note that the narrativeIds used below must be owned or shared with full rights (at least edit) with the narrativetest user.
 // Note that narrativetest is not yet set up in narrative-dev/prod.
 const allTestCases = {
-
-    ci: {
-        TEST_CASE_1: {
-            narrativeId: 53983,
-            tabs: [{
-                name: 'sharedwithme'
-            }, {
-                name: 'public'
-            }, {
-                name: 'example'
-            }, {
-                name: 'import'
-            }, { 
-                name: 'mydata'
-            }]
-        }
+    common: {
+        randomTabs: [
+            1, 4, 3, 2, 0, 1, 0, 4, 1, 4, 0, 3,
+            1, 3, 4, 3, 4, 0, 2, 0, 1, 3, 2, 0,
+            2, 4, 3, 0, 4, 2, 3, 2, 0, 4, 1, 0,
+            1, 3, 2, 1, 4, 3, 1, 4, 1, 2, 4, 1,
+            2, 1, 2, 1, 4, 2, 4, 0, 1, 3, 4, 3,
+            2, 1, 2, 3, 0, 3, 1, 2, 3, 0, 1, 4,
+            1, 0, 4, 0, 1, 0, 1, 4, 2, 0, 3, 4
+        ],
+        tabs: [{
+            name: 'sharedwithme'
+        }, {
+            name: 'public'
+        }, {
+            name: 'example'
+        }, {
+            name: 'import'
+        }, {
+            name: 'mydata'
+        }]
     },
-    'narrative-dev':  {
-        TEST_CASE_1: {
-            narrativeId: 78050,
+    envs: {
+        ci: {
+            TEST_CASE_1: {
+                narrativeId: 53983
+            },
+        },
+        'narrative-dev': {
+            TEST_CASE_1: {
+                narrativeId: 78050,
+            }
         }
     }
 };
 
-const testCases = allTestCases[browser.config.testParams.ENV];
+function getTestCase(name) {
+    const envCases = allTestCases.envs[browser.config.testParams.ENV];
+    return Object.assign({}, allTestCases.common, envCases[name]);
+}
 
 async function openDataSlideout() {
     // Open the data slideout by clicking it's toggle buttoon
@@ -66,7 +81,7 @@ async function ensureTabIsActive(slideoutPanel, name) {
 async function selectTab(slideoutPanel, name) {
     const tab = await slideoutPanel.$(`[data-test-id="tab-${name}"]`);
     await clickWhenReady(tab);
-    return ensureTabIsActive(slideoutPanel, name); 
+    return ensureTabIsActive(slideoutPanel, name);
 }
 
 async function selectTabByLabel(slideoutPanel, name) {
@@ -87,10 +102,10 @@ describe('Tabbing within the data panel should work', () => {
     });
 
     it('opens the "Shared With Me" tab', async () => {
-        const testCase = testCases.TEST_CASE_1;
+        const testCase = getTestCase('TEST_CASE_1');
         await login();
         await openNarrative(testCase.narrativeId);
-        
+
         const slideoutPanel = await openDataSlideout();
 
         const [, tabPanel] = await selectTab(slideoutPanel, 'sharedwithme');
@@ -105,14 +120,14 @@ describe('Tabbing within the data panel should work', () => {
     });
 
     it('opens the "Public" tab', async () => {
-        const testCase = testCases.TEST_CASE_1;
+        const testCase = getTestCase('TEST_CASE_1');
         await login();
         await openNarrative(testCase.narrativeId);
-        
+
         const slideoutPanel = await openDataSlideout();
 
         const [, tabPanel] = await selectTab(slideoutPanel, 'public');
-
+        
         // Ensure the main elements of the public data tab have been rendered.
         await browser.waitUntil(async () => {
             const resultContainer = await tabPanel.$('[data-test-id="result"]');
@@ -122,31 +137,28 @@ describe('Tabbing within the data panel should work', () => {
     });
 
     it('opens the "Example" tab', async () => {
-        const testCase = testCases.TEST_CASE_1;
+        const testCase = getTestCase('TEST_CASE_1');
         await login();
         await openNarrative(testCase.narrativeId);
-        
         const slideoutPanel = await openDataSlideout();
 
         const [, tabPanel] = await selectTab(slideoutPanel, 'example');
-
         // Make sure the main elements of the example tab have been rendered.
         await browser.waitUntil(async () => {
             const resultContainer = await tabPanel.$('[data-test-id="example-data-objects"]');
             const kids = await resultContainer.$$('[data-test-id="type-container"]');
             return kids.length > 0;
         });
-    }); 
+    });
 
     it('opens the "Import" tab', async () => {
-        const testCase = testCases.TEST_CASE_1;
+        const testCase = getTestCase('TEST_CASE_1');
         await login();
         await openNarrative(testCase.narrativeId);
 
         const slideoutPanel = await openDataSlideout();
 
         const [, tabPanel] = await selectTab(slideoutPanel, 'import');
-        
         // Make sure the main elements of the import tab have been rendered.
         const fileDropZone = await tabPanel.$('.kb-dropzone');
         await fileDropZone.waitForExist();
@@ -155,41 +167,39 @@ describe('Tabbing within the data panel should work', () => {
     });
 
     it('Switches between all tabs in sequence', async () => {
-        const testCase = testCases.TEST_CASE_1;
+        const testCase = getTestCase('TEST_CASE_1');
         await login();
         await openNarrative(testCase.narrativeId);
 
         const slideoutPanel = await openDataSlideout();
 
-        for (const {name} of testCase.tabs) {
+        for (const { name } of testCase.tabs) {
             await selectTab(slideoutPanel, name);
         }
     });
 
     it('Switches between many tabs randomly', async () => {
-        const testCase = testCases.TEST_CASE_1;
+        const testCase = getTestCase('TEST_CASE_1');
         await login();
         await openNarrative(testCase.narrativeId);
 
         const slideoutPanel = await openDataSlideout();
 
-        for (let i = 0; i < 100; i += 1) {
-            const tabIndex = Math.floor(Math.random() * testCase.tabs.length);
-            const {name} = testCase.tabs[tabIndex]
+        for (const tabIndex of testCase.randomTabs) {
+            const { name } = testCase.tabs[tabIndex]
             await selectTab(slideoutPanel, name);
         }
     });
 
-    it('Switches between many tab labels randomly by clicking on the label', async () => {
-        const testCase = testCases.TEST_CASE_1;
+    xit('Switches between many tab labels randomly by clicking on the label', async () => {
+        const testCase = getTestCase('TEST_CASE_1');
         await login();
         await openNarrative(testCase.narrativeId);
 
         const slideoutPanel = await openDataSlideout();
 
-        for (let i = 0; i < 100; i += 1) {
-            const tabIndex = Math.floor(Math.random() * testCase.tabs.length);
-            const {name} = testCase.tabs[tabIndex]
+        for (const tabIndex of testCase.randomTabs) {
+            const { name } = testCase.tabs[tabIndex]
             await selectTabByLabel(slideoutPanel, name);
         }
     });

a1706e82a5fe66274fe72ff0d13d6d8b1a94c61b
Erik Pearson <eapearson@lbl.gov>
2021-02-20 15:14:31 -0800

    html module trimmed down and tests beefed up

--- kbase-extension/static/kbase/js/common/html.js
+++ kbase-extension/static/kbase/js/common/html.js
@@ -14,57 +14,9 @@ define([
     const div = tag('div'),
         span = tag('span'),
         i = tag('i'),
-        table = tag('table'),
-        thead = tag('thead'),
-        tbody = tag('tbody'),
-        tr = tag('tr'),
-        th = tag('th'),
-        td = tag('td'),
         ul = tag('ul'),
         li = tag('li'),
         a = tag('a');
-
-    function jsonToHTML(node) {
-        const nodeType = typeof node;
-        let out;
-        if (nodeType === 'string') {
-            return node;
-        }
-        if (nodeType === 'boolean') {
-            if (node) {
-                return 'true';
-            }
-            return 'false';
-        }
-        if (nodeType === 'number') {
-            return String(node);
-        }
-        if (Array.isArray(node)) {
-            out = '';
-            node.forEach((item) => {
-                out += jsonToHTML(item);
-            });
-            return out;
-        }
-        if (nodeType === 'object') {
-            if (node === null) {
-                return '';
-            }
-            out = '';
-            out += '<' + nodeType.tag;
-            if (node.attributes) {
-                node.attributes.keys().forEach((key) => {
-                    out += key + '="' + node.attributes[key] + '"';
-                });
-            }
-            out += '>';
-            if (node.children) {
-                out += jsonToHTML(node.children);
-            }
-            out += '</' + node.tag + '>';
-            return out;
-        }
-    }
 
     /**
      * Given a simple object of keys and values, create a string which 
@@ -179,48 +131,44 @@ define([
     }
 
     function renderContent(children) {
-        if (children) {
-            if (typeof children === 'string') {
-                return children;
-            }
-            if (typeof children === 'number') {
-                return String(children);
-            }
-            if (Array.isArray(children)) {
-                return children.map((item) => {
-                    return renderContent(item);
-                }).join('');
-            }
-        } else {
-            return '';
+        switch (typeof children) {
+            case 'string': return children;
+            case 'number': return String(children);
+            case 'boolean': return '';
+            case 'undefined': return '';
+            case 'object':
+                if (Array.isArray(children)) {
+                    return children.map((item) => {
+                        return renderContent(item);
+                    }).join('');
+                } else if (children === null) {
+                    return '';
+                } else {
+                    return '';
+                }
         }
     }
     function merge(obj1, obj2) {
-        function isObject(x) {
-            if (typeof x === 'object' &&
-                x !== null &&
-                !(x instanceof Array)) {
-                return true;
-            }
-            return false;
-        }
         function merger(aObj, bObj) {
             Object.keys(bObj).forEach((key) => {
-                if (isObject(aObj) && isObject(bObj)) {
+                if (isSimpleObject(aObj[key]) && isSimpleObject(bObj[key])) {
                     aObj[key] = merger(aObj[key], bObj[key]);
+                } else {
+                    aObj[key] = bObj[key];
                 }
-                aObj[key] = bObj[key];
             });
             return aObj;
         }
         return merger(obj1, obj2);
     }
+
     function isSimpleObject(obj) {
         if (typeof obj !== 'object' || obj === null) {
             return false;
         }
         return Object.getPrototypeOf(obj) === Object.getPrototypeOf({});
     }
+
     function tag(tagName, options) {
         options = options || {};
         let tagAttribs;
@@ -229,46 +177,14 @@ define([
         }
         const tagFun = function (attribs, children) {
             let node = '<' + tagName;
-            switch (typeof attribs) {
-                case 'string':
-                    // skip attribs, just go to children.
-                    children = attribs;
-                    attribs = null;
-                    break;
-                case 'number':
-                    children = String(attribs);
-                    attribs = null;
-                    break;
-                case 'boolean':
-                    if (attribs) {
-                        children = 'true';
-                    } else {
-                        children = 'false';
-                    }
-                    attribs = null;
-                    break;
-                case 'undefined':
-                    if (!children) {
-                        children = '';
-                    }
-                    break;
-                case 'object':
-                    if (Array.isArray(attribs)) {
-                        // skip attribs, just go to children.
-                        children = attribs;
-                        attribs = null;
-                    } else if (attribs === null) {
-                        if (!children) {
-                            children = '';
-                        }
-                    } else if (isSimpleObject(attribs)) {
-                        if (options.attribs) {
-                            attribs = merge(merge({}, options.attribs), attribs);
-                        }
-                    } else {
-                        throw 'Cannot make tag ' + tagName + ' from a ' + (typeof attribs);
-                    }
-                    break;
+
+            if (isSimpleObject(attribs)) {
+                if (options.attribs) {
+                    attribs = merge(merge({}, options.attribs), attribs);
+                }
+            } else {
+                children = children || attribs;
+                attribs = null;
             }
 
             attribs = attribs || options.attribs;
@@ -291,51 +207,13 @@ define([
         }
         return tagFun;
     }
+
     function genId() {
         return 'kb_html_' + (new Uuid(4)).format();
     }
-    function makeTable(arg) {
-        let id;
-        arg = arg || {};
-        if (arg.id) {
-            id = arg.id;
-        } else {
-            id = genId();
-            arg.generated = { id: id };
-        }
-        const attribs = { id: id };
-        if (arg.class) {
-            attribs.class = arg.class;
-        } else if (arg.classes) {
-            attribs.class = arg.classes.join(' ');
-        }
-        return table(attribs, [
-            thead(tr(arg.columns.map((x) => {
-                return th(x);
-            }))),
-            tbody(arg.rows.map((row) => {
-                return tr(row.map((x) => {
-                    return td(x);
-                }));
-            }))
-        ]);
-    }
-
-    function bsPanel(title, content) {
-        return div({ class: 'panel panel-default' }, [
-            div({ class: 'panel-heading' }, [
-                span({ class: 'panel-title' }, title)
-            ]),
-            div({ class: 'panel-body' }, [
-                content
-            ])
-        ]);
-    }
 
     function makePanel(arg) {
-
         const klass = arg.class || 'default';
-
         return div({ class: 'panel panel-' + klass }, [
             div({ class: 'panel-heading' }, [
                 span({ class: 'panel-title' }, arg.title)
@@ -347,273 +225,11 @@ define([
     }
 
     function loading(msg) {
-        const prompt = msg ? `${msg}... &nbsp &nbsp` : '';
+        const prompt = msg ? `${msg} &nbsp;&nbsp;` : '';
         return span([
             prompt,
             i({ class: 'fa fa-spinner fa-pulse fa-2x fa-fw margin-bottom' })
         ]);
-    }
-
-    /*
-     * 
-     */
-    function makeTableRotated(arg) {
-        function columnLabel(column) {
-            let key;
-            if (typeof column === 'string') {
-                key = column;
-            } else {
-                if (column.label) {
-                    return column.label;
-                }
-                key = column.key;
-            }
-            return key
-                .replace(/(id|Id)/g, 'ID')
-                .split(/_/g).map((word) => {
-                    return word.charAt(0).toUpperCase() + word.slice(1);
-                })
-                .join(' ');
-        }
-        function formatValue(rawValue, column) {
-            if (typeof column === 'string') {
-                return rawValue;
-            }
-            if (column.format) {
-                return column.format(rawValue);
-            }
-            if (column.type) {
-                switch (column.type) {
-                    case 'bool':
-                        // yuck, use truthiness
-                        if (rawValue) {
-                            return 'True';
-                        }
-                        return 'False';
-                    default:
-                        return rawValue;
-                }
-            }
-            return rawValue;
-        }
-
-        const id = genId(),
-            attribs = { id: id };
-        if (arg.class) {
-            attribs.class = arg.class;
-        } else if (arg.classes) {
-            attribs.class = arg.classes.join(' ');
-        }
-
-        return table(attribs,
-            arg.columns.map((column, index) => {
-                return tr([
-                    th(columnLabel(column)),
-                    arg.rows.map((row) => {
-                        return td(formatValue(row[index], column));
-                    })
-                ]);
-            }));
-    }
-
-    function makeRotatedTable(data, columns) {
-        function columnLabel(column) {
-            let key;
-            if (column.label) {
-                return column.label;
-            }
-            if (typeof column === 'string') {
-                key = column;
-            } else {
-                key = column.key;
-            }
-            return key
-                .replace(/(id|Id)/g, 'ID')
-                .split(/_/g).map((word) => {
-                    return word.charAt(0).toUpperCase() + word.slice(1);
-                })
-                .join(' ');
-        }
-        function columnValue(row, column) {
-            const rawValue = row[column.key];
-            if (column.format) {
-                return column.format(rawValue);
-            }
-            if (column.type) {
-                switch (column.type) {
-                    case 'bool':
-                        // yuck, use truthiness
-                        if (rawValue) {
-                            return 'True';
-                        }
-                        return 'False';
-                    default:
-                        return rawValue;
-                }
-            }
-            return rawValue;
-        }
-
-        return table({ class: 'table table-stiped table-bordered' },
-            columns.map((column) => {
-                return tr([
-                    th(columnLabel(column)), data.map((row) => {
-                        return td(columnValue(row, column));
-                    })
-                ]);
-            }));
-    }
-
-    function properCase(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    function makeObjTable(data, options) {
-        const tableData = (data instanceof Array && data) || [data],
-            columns = (options && options.columns) || Object.keys(tableData[0]).map((key) => {
-                return {
-                    key: key,
-                    label: properCase(key)
-                };
-            }),
-            classes = (options && options.classes) || ['table-striped', 'table-bordered'];
-
-        function columnValue(row, column) {
-            const rawValue = row[column.key];
-            if (column.format) {
-                return column.format(rawValue);
-            }
-            if (column.type) {
-                switch (column.type) {
-                    case 'bool':
-                        // yuck, use truthiness
-                        if (rawValue) {
-                            return 'True';
-                        }
-                        return 'False';
-                    default:
-                        return rawValue;
-                }
-            }
-            return rawValue;
-        }
-        if (options && options.rotated) {
-            return table({ class: 'table ' + classes.join(' ') },
-                columns.map((column) => {
-                    return tr([
-                        th(column.label),
-                        tableData.map((row) => {
-                            return td({ dataElement: column.key }, columnValue(row, column));
-                        })
-                    ]);
-                }));
-        }
-        return table({ class: 'table ' + classes.join(' ') },
-            [tr(columns.map((column) => {
-                return th(column.label);
-            }))].concat(tableData.map((row) => {
-                return tr(columns.map((column) => {
-                    return td({ dataElement: column.key }, columnValue(row, column));
-                }));
-            })));
-    }
-
-    function makeObjectTable(data, options) {
-        function columnLabel(column) {
-            let key;
-            if (column.label) {
-                return column.label;
-            }
-            if (typeof column === 'string') {
-                key = column;
-            } else {
-                key = column.key;
-            }
-            return key
-                .replace(/(id|Id)/g, 'ID')
-                .split(/_/g).map((word) => {
-                    return word.charAt(0).toUpperCase() + word.slice(1);
-                })
-                .join(' ');
-        }
-        function columnValue(row, column) {
-            const rawValue = row[column.key];
-            if (column.format) {
-                return column.format(rawValue);
-            }
-            if (column.type) {
-                switch (column.type) {
-                    case 'bool':
-                        // yuck, use truthiness
-                        if (rawValue) {
-                            return 'True';
-                        }
-                        return 'False';
-                    default:
-                        return rawValue;
-                }
-            }
-            return rawValue;
-        }
-        let columns, classes;
-        if (!options) {
-            options = {};
-        } else if (options.columns) {
-            columns = options.columns;
-        } else {
-            columns = options;
-            options = {};
-        }
-        if (!columns) {
-            columns = Object.keys(data).map((columnName) => {
-                return {
-                    key: columnName
-                };
-            });
-        } else {
-            columns = columns.map((column) => {
-                if (typeof column === 'string') {
-                    return {
-                        key: column
-                    };
-                }
-                return column;
-            });
-        }
-        if (options.classes) {
-            classes = options.classes;
-        } else {
-            classes = ['table-striped', 'table-bordered'];
-        }
-
-        return table({ class: 'table ' + classes.join(' ') },
-            columns.map((column) => {
-                return tr([
-                    th(columnLabel(column)),
-                    td(columnValue(data, column))
-                ]);
-            }));
-    }
-
-    function flatten(html) {
-        if (typeof html === 'string') {
-            return html;
-        }
-        if (Array.isArray(html)) {
-            return html.map((h) => {
-                return flatten(h);
-            }).join('');
-        }
-        throw new Error('Not a valid html representation -- must be string or list');
-    }
-
-    function makeList(arg) {
-        if (Array.isArray(arg.items)) {
-            return ul(arg.items.map((item) => {
-                return li(item);
-            }));
-        }
-        return 'Sorry, cannot make a list from that';
     }
 
     /**
@@ -696,21 +312,13 @@ define([
     }
 
     return Object.freeze({
-        html: jsonToHTML,
         camelToKebab,
-        tag,
-        makeTable,
-        makeTableRotated,
-        makeRotatedTable,
-        makeObjectTable,
-        makeObjTable,
         genId,
-        bsPanel,
-        panel: bsPanel,
-        makePanel,
+        isSimpleObject,
         loading,
-        flatten,
-        makeList,
-        makeTabs
+        makePanel,
+        makeTabs,
+        merge,
+        tag
     });
 });
--- test/unit/spec/common/html-Spec.js
+++ test/unit/spec/common/html-Spec.js
@@ -5,6 +5,138 @@ define([
     'use strict';
 
     describe('html utility library', () => {
+        // camelToKebab
+        it('Converts camelCaseToHyphen', () => {
+            expect(html.camelToKebab('camelCase')).toEqual('camel-case');
+            expect(html.camelToKebab('CamelCase')).toEqual('-camel-case');
+            expect(html.camelToKebab('camel')).toEqual('camel');
+            expect(html.camelToKebab('CASE')).toEqual('-c-a-s-e');
+        });
+
+        // genId
+        it('Generates an id', () => {
+            expect(html.genId()).toMatch(/^kb_html_.*$/);
+        });
+
+        // Looading
+        it('Makes a loading message', () => {
+            let loading = html.loading();
+            expect(loading).toEqual('<span><i class="fa fa-spinner fa-pulse fa-2x fa-fw margin-bottom"></i></span>');
+
+            loading = html.loading('Loading...');
+            expect(loading).toEqual('<span>Loading... &nbsp;&nbsp;<i class="fa fa-spinner fa-pulse fa-2x fa-fw margin-bottom"></i></span>');
+        });
+
+        // makePanel
+        it('Makes a panel', () => {
+            const panelDef = [{
+                title: 'Panel 1',
+                body: 'This is panel 1'
+            }, {
+                title: 'Panel2',
+                body: 'This is panel 2'
+            }];
+            const panelContent = '<div class="panel panel-default"><div class="panel-heading"><span class="panel-title"></span></div><div class="panel-body"></div></div>';
+            const panel = html.makePanel(panelDef);
+            expect(panel).toEqual(panelContent);
+        });
+
+        // makeTabs
+        it('Makes tabs', () => {
+            const arg = {
+                id: '123',
+                alignRight: false,
+                tabs: [{
+                    name: 'tab1',
+                    label: 'Tab One',
+                    content: 'This is tab one'
+                }, {
+                    name: 'tab2',
+                    label: 'Tab Two',
+                    content: 'This is tab two'
+                }, null]
+            };
+            const expected = new RegExp('<div id="123"><ul class="nav nav-tabs" role="tablist"><li role="presentation" class="active" style=""><a href="#kb_html_[^"]+" aria-controls="home" role="tab" data-toggle="tab">Tab One</a></li><li role="presentation" style=""><a href="#kb_html_[^"]+" aria-controls="home" role="tab" data-toggle="tab">Tab Two</a></li></ul><div class="tab-content"><div role="tabpanel" class="tab-pane active" id="kb_html_[^"]+" data-name="tab1">This is tab one</div><div role="tabpanel" class="tab-pane" id="kb_html_[^"]+" data-name="tab2">This is tab two</div></div></div>');
+            const tabs = html.makeTabs(arg);
+            expect(tabs).toMatch(expected);
+        });
+
+        it('Makes tabs right aligned', () => {
+            const arg = {
+                id: '123',
+                alignRight: true,
+                tabs: [{
+                    name: 'tab1',
+                    label: 'Tab One',
+                    content: 'This is tab one'
+                }, {
+                    name: 'tab2',
+                    label: 'Tab Two',
+                    content: 'This is tab two'
+                }]
+            };
+            const expected = new RegExp('<div id="123"><ul class="nav nav-tabs" role="tablist"><li role="presentation" style="float: right"><a href="#kb_html_[^"]+" aria-controls="home" role="tab" data-toggle="tab">Tab Two</a></li><li role="presentation" class="active" style="float: right"><a href="#kb_html[^"]+" aria-controls="home" role="tab" data-toggle="tab">Tab One</a></li></ul><div class="tab-content"><div role="tabpanel" class="tab-pane active" id="kb_html_[^"]+" data-name="tab1">This is tab one</div><div role="tabpanel" class="tab-pane" id="kb_html_[^"]+" data-name="tab2">This is tab two</div></div></div>');
+
+            const tabs = html.makeTabs(arg);
+
+            expect(tabs).toMatch(expected);
+        });
+
+        // merge
+        it('Merges two simple objects', () => {
+            const x = { foo: 'bar' };
+            const y = { bing: 'bong' };
+            const z = html.merge(x, y);
+            expect(z).toEqual({ foo: 'bar', bing: 'bong' });
+        });
+
+        it('Merges two nested objects', () => {
+            const x = { foo: 'bar', 'name': { middle: 'M' } };
+            const y = { bing: 'bong', 'name': { first: 'Mickey', last: 'Mouse' } };
+            const z = html.merge(x, y);
+            expect(z).toEqual({ foo: 'bar', bing: 'bong', name: { first: 'Mickey', middle: 'M', last: 'Mouse' } });
+        });
+
+        // isSimpleObject
+        it('Determine if a value is a simple object', () => {
+            const cases = [{
+                value: 'hi',
+                expected: false
+            }, {
+                value: 42,
+                expected: false
+            }, {
+                value: true,
+                expected: false
+            }, {
+                value: false,
+                expected: false
+            }, {
+                value: 'hi',
+                expected: false
+            }, {
+                value: null,
+                expected: false
+            }, {
+                value: undefined,
+                expected: false
+            }, {
+                value: new Date(),
+                expected: false
+            }, {
+                value: {},
+                expected: true
+            }, {
+                value: { foo: 'bar' },
+                expected: true
+            }]
+            for (const { value, expected } of cases) {
+                expect(html.isSimpleObject(value)).toEqual(expected);
+            }
+        });
+
+        // tag
+
         it('Creates a simple tag', () => {
             const span = html.tag('span');
             expect(span('hi')).toEqual('<span>hi</span>');
@@ -12,7 +144,7 @@ define([
 
         it('Creates a tag with attributes', () => {
             const div = html.tag('div');
-            expect(div({ class: 'foo' }, 'hello')).toEqual('<div class="foo">hello</div>');
+            expect(div({ class: 'foo', bar: 42 }, 'hello')).toEqual('<div class="foo" bar="42">hello</div>');
         });
 
         it('Creates a tag with empty attributes', () => {
@@ -22,17 +154,17 @@ define([
 
         it('Creates a tag with a string as attributes', () => {
             const div = html.tag('div');
-            expect(div('hi', 'hello')).toEqual('<div>hi</div>');
+            expect(div('hi', 'hello')).toEqual('<div>hello</div>');
         });
 
         it('Creates a tag with false as attributes', () => {
             const div = html.tag('div');
-            expect(div(false, 'hello')).toEqual('<div>false</div>');
+            expect(div(false, 'hello')).toEqual('<div>hello</div>');
         });
 
         it('Creates a tag with true as attributes', () => {
             const div = html.tag('div');
-            expect(div(true, 'hello')).toEqual('<div>true</div>');
+            expect(div(true, 'hello')).toEqual('<div>hello</div>');
         });
 
         it('Creates a tag with null as attributes', () => {
@@ -47,12 +179,34 @@ define([
 
         it('Creates a tag with a  number as attributes', () => {
             const div = html.tag('div');
-            expect(div(42, 'hello')).toEqual('<div>42</div>');
+            expect(div(42, 'hello')).toEqual('<div>hello</div>');
+        });
+
+        it('Creates a tag without a closing tag', () => {
+            const z = html.tag('z', { close: false });
+            expect(z()).toEqual('<z>');
         });
 
         it('Creates a tag with attribute with  number value', () => {
-            const div = html.tag('div');
-            expect(div({ foo: 123 }, 'hello')).toEqual('<div foo="123">hello</div>');
+            const x = html.tag('x', { attribs: { foo: 'bar' } });
+            expect(x({ ping: 'pong' }, 'hello')).toEqual('<x foo="bar" ping="pong">hello</x>');
+        });
+
+        it('Creates a tag with attributes, overrides with and without cached tag', () => {
+            const y = html.tag('y', { attribs: { foo: 'bar' } });
+            expect(y()).toEqual('<y foo="bar"></y>');
+
+            const y1 = html.tag('y');
+            expect(y1()).toEqual('<y foo="bar"></y>');
+
+            const y2 = html.tag('y', { attribs: { foo: 'baz' }, ignoreCache: true });
+            expect(y2()).toEqual('<y foo="baz"></y>');
+
+            const y3 = html.tag('y', { ignoreCache: true });
+            expect(y3({ bee: 'buzz' })).toEqual('<y bee="buzz"></y>');
+
+            const y4 = html.tag('y');
+            expect(y4()).toEqual('<y foo="bar"></y>');
         });
 
         it('Creates a tag with attribute with  null value', () => {
@@ -101,12 +255,45 @@ define([
             expect(div(div(div('hi')))).toEqual('<div><div><div>hi</div></div></div>');
         });
 
-        it('Converts camelCaseToHyphen', () => {
-            expect(html.camelToKebab('camelCase')).toEqual('camel-case');
-            expect(html.camelToKebab('CamelCase')).toEqual('-camel-case');
-            expect(html.camelToKebab('camel')).toEqual('camel');
-            expect(html.camelToKebab('CASE')).toEqual('-c-a-s-e');
+        it('Creates a tag with an array content', () => {
+            const div = html.tag('div');
+            expect(div([])).toEqual('<div></div>');
+            expect(div(['hello', 'there'])).toEqual('<div>hellothere</div>');
+            expect(div([123, 456])).toEqual('<div>123456</div>');
         });
 
+        it('Creates a tag with an array tag content', () => {
+            const div = html.tag('div');
+            expect(div([div('hi'), div('hello')])).toEqual('<div><div>hi</div><div>hello</div></div>');
+        });
+
+        it('Creates a tag with non-string content', () => {
+            const div = html.tag('div');
+            const content = [{
+                value: 42,
+                expected: '42'
+            }, {
+                value: 123.456,
+                expected: '123.456'
+            }, {
+                value: true,
+                expected: ''
+            }, {
+                value: false,
+                expected: ''
+            }, {
+                value: null,
+                expected: ''
+            }, {
+                value: undefined,
+                expected: ''
+            }, {
+                value: new Date(),
+                expected: ''
+            }];
+            for (const { value, expected } of content) {
+                expect(div(value)).toEqual(`<div>${expected}</div>`);
+            }
+        });
     });
 });

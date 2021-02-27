3d635ca29e45fea9ef5940f35746b8af3dcb3e27
ialarmedalien <ialarmedalien@gmail.com>
2021-02-26 16:38:05 -0800

    Update JS minification rituals to use modern uglification tools (terser) and remove the unnecessary grunt steps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,17 @@
 module.exports = function (grunt) {
-    // Project configuration
     'use strict';
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-regex-replace');
-    grunt.loadNpmTasks('grunt-contrib-uglify-es');
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        // Compile the requirejs stuff into a single, uglified file.
-        // the options below are taken verbatim from a standard build.js file
-        // used for r.js (if we were doing this outside of a grunt build)
-        requirejs: {
-            compile: {
-                options: {
-                    name: 'narrative_paths',
-                    baseUrl: 'kbase-extension/static',
-                    include: ['narrativeMain', 'buildTools/loadAppWidgets'],
-                    mainConfigFile: 'kbase-extension/static/narrative_paths.js',
-                    findNestedDependencies: true,
-                    optimize: 'none',
-                    generateSourceMaps: true,
-                    preserveLicenseComments: false,
-                    out: 'kbase-extension/static/kbase-narrative.js',
-                    paths: {
-                        jqueryui: 'empty:',
-                        bootstrap: 'empty:',
-                        'jquery-ui': 'empty:',
-                        narrativeConfig: 'empty:',
-                        'base/js/utils': 'empty:',
-                        'base/js/namespace': 'empty:',
-                        bootstraptour: 'empty:',
-                        'services/kernels/comm': 'empty:',
-                        'common/ui': 'empty:',
-                        'notebook/js/celltoolbar': 'empty:',
-                        'base/js/events': 'empty:',
-                        'base/js/keyboard': 'empty:',
-                        'base/js/dialog': 'empty:',
-                        'notebook/js/notebook': 'empty:',
-                        'notebook/js/main': 'empty:',
-                        'custom/custom': 'empty:',
-                    },
-                    inlineText: false,
-                    buildCSS: false,
-                    optimizeAllPluginResources: false,
-                    done: function (done, output) {
-                        console.log(output);
-                        done();
-                    },
-                },
-            },
-        },
-
-        uglify: {
-            dist: {
-                options: {
-                    sourceMap: true,
-                },
-                files: {
-                    'kbase-extension/static/kbase-narrative-min.js': [
-                        'kbase-extension/static/kbase-narrative.js',
-                    ],
-                },
-            },
-        },
-
-        // Once we have a revved file, this inserts that reference into page.html at
-        // the right spot (near the top, the narrative_paths reference)
+        // This inserts the reference to the compiled, minified JS file into page.html at
+        // the right spot (in place of `narrativeMain`, which is used locally)
         'regex-replace': {
             dist: {
                 src: ['kbase-extension/kbase_templates/notebook.html'],
                 actions: [
                     {
                         name: 'requirejs-onefile',
-                        // search: 'narrativeMain',
                         search: 'narrativeMain.js',
-
                         replace: function () {
                             return 'kbase-narrative-min.js';
                         },
@@ -85,8 +21,4 @@ module.exports = function (grunt) {
             },
         },
     });
-
-    grunt.registerTask('minify', ['requirejs', 'uglify', 'regex-replace']);
-
-    grunt.registerTask('build', ['requirejs', 'regex-replace']);
 };

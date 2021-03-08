2abdd8720f173551048f5e0818ffc22b27b7649b
ialarmedalien <ialarmedalien@gmail.com>
2021-03-08 08:13:25 -0800

    Refactoring the ParamResolver module to remove some of the dynamic module loading and make the big module chooser tree more succinct.

--- kbase-extension/static/kbase/js/widgets/appWidgets2/paramResolver.js
+++ kbase-extension/static/kbase/js/widgets/appWidgets2/paramResolver.js
@@ -1,5 +1,110 @@
-define(['bluebird', 'require'], (Promise, require) => {
+define([
+    'bluebird',
+    'require',
+    './input/checkboxInput',
+    './input/customInput',
+    './input/customSubdataInput',
+    './input/dynamicDropdownInput',
+    './input/fileInput',
+    './input/floatInput',
+    './input/intInput',
+    './input/newObjectInput',
+    './input/select2ObjectInput',
+    './input/selectInput',
+    './input/subdataInput',
+    './input/taxonomyRefInput',
+    './input/textareaInput',
+    './input/textInput',
+    './input/toggleButtonInput',
+    './input/undefinedInput',
+    './view/checkboxView',
+    './view/customView',
+    './view/customSubdataView',
+    './view/dynamicDropdownView',
+    './view/fileView',
+    './view/floatView',
+    './view/intView',
+    './view/newObjectView',
+    './view/select2ObjectView',
+    './view/selectView',
+    './view/subdataView',
+    './view/taxonomyRefView',
+    './view/textareaView',
+    './view/textView',
+    './view/toggleButtonView',
+    './view/undefinedView',
+], (
+    Promise,
+    require,
+    checkboxInput,
+    customInput,
+    customSubdataInput,
+    dynamicDropdownInput,
+    fileInput,
+    floatInput,
+    intInput,
+    newObjectInput,
+    select2ObjectInput,
+    selectInput,
+    subdataInput,
+    taxonomyRefInput,
+    textareaInput,
+    textInput,
+    toggleButtonInput,
+    undefinedInput,
+    checkboxView,
+    customView,
+    customSubdataView,
+    dynamicDropdownView,
+    fileView,
+    floatView,
+    intView,
+    newObjectView,
+    select2ObjectView,
+    selectView,
+    subdataView,
+    taxonomyRefView,
+    textareaView,
+    textView,
+    toggleButtonView,
+    undefinedView
+) => {
     'use strict';
+
+    const inputModules = {
+        './input/checkboxInput': checkboxInput,
+        './input/customInput': customInput,
+        './input/customSubdataInput': customSubdataInput,
+        './input/dynamicDropdownInput': dynamicDropdownInput,
+        './input/fileInput': fileInput,
+        './input/floatInput': floatInput,
+        './input/intInput': intInput,
+        './input/newObjectInput': newObjectInput,
+        './input/select2ObjectInput': select2ObjectInput,
+        './input/selectInput': selectInput,
+        './input/subdataInput': subdataInput,
+        './input/taxonomyRefInput': taxonomyRefInput,
+        './input/textareaInput': textareaInput,
+        './input/textInput': textInput,
+        './input/toggleButtonInput': toggleButtonInput,
+        './input/undefinedInput': undefinedInput,
+        './view/checkboxView': checkboxView,
+        './view/customView': customView,
+        './view/customSubdataView': customSubdataView,
+        './view/dynamicDropdownView': dynamicDropdownView,
+        './view/fileView': fileView,
+        './view/floatView': floatView,
+        './view/intView': intView,
+        './view/newObjectView': newObjectView,
+        './view/select2ObjectView': select2ObjectView,
+        './view/selectView': selectView,
+        './view/subdataView': subdataView,
+        './view/taxonomyRefView': taxonomyRefView,
+        './view/textareaView': textareaView,
+        './view/textView': textView,
+        './view/toggleButtonView': toggleButtonView,
+        './view/undefinedView': undefinedView,
+    };
 
     function factory() {
         function getWidgetModule(spec) {
@@ -9,126 +114,52 @@ define(['bluebird', 'require'], (Promise, require) => {
             switch (dataType) {
                 case 'string':
                     switch (controlType) {
-                        case 'dropdown':
-                            return {
-                                input: 'selectInput',
-                                view: 'selectView',
-                            };
                         case 'textarea':
-                            return {
-                                input: 'textareaInput',
-                                view: 'textareaView',
-                            };
                         case 'file':
-                            return {
-                                input: 'fileInput',
-                                view: 'fileView',
-                            };
+                            return controlType;
+
                         case 'custom_textsubdata':
-                            return {
-                                input: 'customSubdataInput',
-                                view: 'customSubdataView',
-                            };
+                            return 'customSubdata';
+                        case 'dropdown':
+                            return 'select';
                         case 'autocomplete':
-                            // TODO: refactor this from the sdk on up.
-                            return {
-                                input: 'taxonomyRefInput',
-                                view: 'taxonomyRefView',
-                            };
+                            return 'taxonomyRef';
                         case 'dynamic_dropdown':
-                            return {
-                                input: 'dynamicDropdownInput',
-                                view: 'dynamicDropdownView',
-                            };
+                            return 'dynamicDropdown';
                         case 'text':
                         default:
-                            // A string type is normally entered as a
-                            // simple text input.
-                            return {
-                                input: 'textInput',
-                                view: 'textView',
-                            };
+                            return 'text';
                     }
                 case 'sequence':
-                    return {
-                        input: 'sequenceInput',
-                        view: 'sequenceView',
-                    };
-                case 'int':
-                    switch (controlType) {
-                        case 'checkbox':
-                            return {
-                                input: 'checkboxInput',
-                                view: 'checkboxView',
-                            };
-                        case 'text':
-                        default:
-                            return {
-                                input: 'intInput',
-                                view: 'intView',
-                            };
-                    }
                 case 'float':
-                    return {
-                        input: 'floatInput',
-                        view: 'floatView',
-                    };
+                case 'subdata':
+                case 'customSubdata':
+                case 'struct':
+                case 'custom':
+                    return dataType;
+                case 'boolean':
+                    return 'toggleButton';
+                case 'int':
+                    return controlType === 'checkbox' ? controlType : dataType;
                 case 'workspaceObjectRef':
                     switch (spec.ui.class) {
                         case 'input':
                         case 'parameter':
-                            return {
-                                input: 'select2ObjectInput',
-                                view: 'select2ObjectView',
-                            };
+                            return 'select2Object';
                         default:
-                            return {
-                                input: 'undefinedInput',
-                                view: 'undefinedView',
-                            };
+                            return 'undefined';
                     }
                 // IS THIS used anywhere other than in output areas??
                 case 'workspaceObjectName':
                     switch (spec.ui.class) {
                         case 'parameter':
                         case 'output':
-                            return {
-                                input: 'newObjectInput',
-                                view: 'newObjectView',
-                            };
+                            return 'newObject';
                         default:
-                            return {
-                                input: 'undefinedInput',
-                                view: 'undefinedView',
-                            };
+                            return 'undefined';
                     }
-                case 'subdata':
-                    return {
-                        input: 'subdataInput',
-                        view: 'subdataView',
-                    };
-                case 'customSubdata':
-                    return {
-                        input: 'customSubdataInput',
-                        view: 'customSubdataView',
-                    };
-                case 'boolean':
-                    return {
-                        input: 'toggleButtonInput',
-                        view: 'toggleButtonView',
-                    };
-                case 'struct':
-                    return {
-                        input: 'structInput',
-                        view: 'structView',
-                    };
-                case 'custom':
-                    return {
-                        input: 'customInput',
-                        view: 'customView',
-                    };
                 default:
-                    console.error('ERROR could not detremine control modules for this spec', spec);
+                    console.error('ERROR could not determine control modules for this spec', spec);
                     throw new Error('Could not determine control modules for this spec');
             }
         }
@@ -145,17 +176,27 @@ define(['bluebird', 'require'], (Promise, require) => {
 
         function loadInputControl(parameterSpec) {
             const module = getWidgetModule(parameterSpec);
-            return loadModule('input', module.input);
+            // the sequence(Input|View) and struct(Input|View) modules
+            // require ParamResolver, so have to be loaded dynamically
+            if (module === 'sequence' || module === 'struct') {
+                return loadModule('input', `${module}Input`);
+            }
+            return Promise.resolve(inputModules[`./input/${module}Input`]);
         }
 
         function loadViewControl(parameterSpec) {
             const module = getWidgetModule(parameterSpec);
-            return loadModule('view', module.view);
+            // the sequence(Input|View) and struct(Input|View) modules
+            // require ParamResolver, so have to be loaded dynamically
+            if (module === 'sequence' || module === 'struct') {
+                return loadModule('view', `${module}View`);
+            }
+            return Promise.resolve(inputModules[`./view/${module}View`]);
         }
 
         return {
-            loadInputControl: loadInputControl,
-            loadViewControl: loadViewControl,
+            loadInputControl,
+            loadViewControl,
         };
     }
     return {

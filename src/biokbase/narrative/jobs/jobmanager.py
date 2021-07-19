3053708d54058cdfbe7858ed2b54cb0b39ad068e
Sumin <sumin.w.wang@gmail.com>
2021-07-19 15:17:23 -0700

    add app spec test data

--- src/biokbase/narrative/jobs/jobmanager.py
+++ src/biokbase/narrative/jobs/jobmanager.py
@@ -424,6 +424,10 @@ class JobManager(object):
                 "job_id": {"state": {"job_id": job_id, "status": status, ...} ...},
                 "retry_id": {"state": {"job_id": job_id, "status": status, ...} ...}
             },
+            {
+                "job_id": {"state": {"job_id": job_id, "status": status, ...} ...},
+                "error": "..."
+            }
             ...
             {
                 "job_id": {"state": {"job_id": job_id, "status": "does_not_exist"}},

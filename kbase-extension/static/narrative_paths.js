054f72bbef9d837ea2b284c03f307725fd9716e1
Erik Pearson <eapearson@lbl.gov>
2021-08-09 19:24:33 -0700

    remove duplicate key

--- kbase-extension/static/narrative_paths.js
+++ kbase-extension/static/narrative_paths.js
@@ -113,7 +113,6 @@ require.config({
         common: 'kbase/js/common',
         widgets: 'kbase/js/widgets',
         appWidgets: 'kbase/js/widgets/appWidgets2',
-        jsonrpc: 'kbase/js/jsonrpc',
 
         /***
          * CORE NARRATIVE WIDGETS

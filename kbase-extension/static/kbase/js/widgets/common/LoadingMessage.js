3e7e66fa6c597eb071d00adcaccf529cfa295e2f
Erik Pearson <eapearson@lbl.gov>
2021-08-20 06:15:50 -0700

    Fix loading message (probably merge error)

--- kbase-extension/static/kbase/js/widgets/common/LoadingMessage.js
+++ kbase-extension/static/kbase/js/widgets/common/LoadingMessage.js
@@ -1,9 +1,15 @@
 define(['jquery', 'css!./LoadingMessage.css'], ($) => {
     'use strict';
     function $LoadingMessage(message) {
+        let $message;
+        if (typeof message === 'string') {
+            $message = $('<span>').text(message);
+        } else {
+            $message = message;
+        }
         return $('<div>')
             .addClass('alert alert-info LoadingMessage')
-            .text(message)
+            .append($message)
             .append($('<i>').addClass('fa fa-spinner fa-spin fa-2x LoadingMessage-icon'));
     }
     return $LoadingMessage;

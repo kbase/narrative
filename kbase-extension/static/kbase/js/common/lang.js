33ae0cf638326a62d813361e22c498b989a15901
Bill Riehl <wjriehl@lbl.gov>
2021-09-20 22:46:02 -0400

    remove isEqual from common/lang

--- kbase-extension/static/kbase/js/common/lang.js
+++ kbase-extension/static/kbase/js/common/lang.js
@@ -23,64 +23,8 @@ define(['bluebird'], (Promise) => {
         });
     }
 
-    function isEqual(v1, v2) {
-        const path = [];
-        function iseq(v1, v2) {
-            const t1 = typeof v1;
-            const t2 = typeof v2;
-            if (t1 !== t2) {
-                return false;
-            }
-            switch (t1) {
-                case 'string':
-                case 'number':
-                case 'boolean':
-                    if (v1 !== v2) {
-                        return false;
-                    }
-                    break;
-                case 'object':
-                    if (v1 instanceof Array) {
-                        if (v1.length !== v2.length) {
-                            return false;
-                        } else {
-                            for (let i = 0; i < v1.length; i++) {
-                                path.push(i);
-                                if (!iseq(v1[i], v2[i])) {
-                                    return false;
-                                }
-                                path.pop();
-                            }
-                        }
-                    } else if (v1 === null) {
-                        if (v2 !== null) {
-                            return false;
-                        }
-                    } else if (v2 === null) {
-                        return false;
-                    } else {
-                        const k1 = Object.keys(v1);
-                        const k2 = Object.keys(v2);
-                        if (k1.length !== k2.length) {
-                            return false;
-                        }
-                        for (let i = 0; i < k1.length; i++) {
-                            path.push(k1[i]);
-                            if (!iseq(v1[k1[i]], v2[k1[i]])) {
-                                return false;
-                            }
-                            path.pop();
-                        }
-                    }
-            }
-            return true;
-        }
-        return iseq(v1, v2);
-    }
-
     return Object.freeze({
         copy: copyValue,
         pRequire,
-        isEqual,
     });
 });
--- test/unit/spec/common/lang-Spec.js
+++ test/unit/spec/common/lang-Spec.js
@@ -3,7 +3,7 @@ define(['common/lang'], (Utils) => {
 
     describe('Lang Util functions', () => {
         it('should have expected functions', () => {
-            ['copy', 'isEqual', 'pRequire'].forEach((fn) => {
+            ['copy', 'pRequire'].forEach((fn) => {
                 expect(Utils[fn]).toEqual(jasmine.any(Function));
             });
         });
@@ -37,88 +37,6 @@ define(['common/lang'], (Utils) => {
                 copied.foo.bar = 'wat';
                 expect(copied).not.toEqual(obj);
                 expect(obj.foo.bar).toEqual('baz');
-            });
-        });
-
-        describe('the isEqual function', () => {
-            const testCases = [
-                'a',
-                1,
-                [],
-                {},
-                [1, 2, 3],
-                { a: 1 },
-                { a: { b: { c: ['d', 'e', 'f'] } } },
-                undefined,
-                true,
-                false,
-                null,
-            ];
-            const complicatedObj = {
-                b: null,
-                c: 1,
-                d: 'a',
-                e: ['a', 'b', 'c'],
-                f: {
-                    g: null,
-                    i: {
-                        j: 'yes!',
-                        k: ['x', 'y', 'z'],
-                        l: true,
-                    },
-                },
-            };
-            it('should return equal when doing an identity comparison', () => {
-                testCases.forEach((testCase) => {
-                    expect(Utils.isEqual(testCase, testCase)).toBeTrue();
-                });
-            });
-
-            it('should return equal for simple copies of objects', () => {
-                testCases.forEach((testCase) => {
-                    const copied = Utils.copy(testCase);
-                    expect(Utils.isEqual(testCase, copied)).toBeTrue();
-                });
-            });
-
-            it('should return equal for complex structures', () => {
-                const copied = Utils.copy(complicatedObj);
-                expect(Utils.isEqual(complicatedObj, copied)).toBeTrue();
-            });
-
-            it('should return unequal when things are different', () => {
-                testCases.forEach((testCase) => {
-                    expect(Utils.isEqual(testCase, 1000)).toBeFalse();
-                    expect(Utils.isEqual(1000, testCase)).toBeFalse(); // reverse should also be false
-                });
-            });
-
-            it('should return unequal with a deep nested difference', () => {
-                const obj = Utils.copy(complicatedObj);
-                obj.f.i.l = false;
-                expect(Utils.isEqual(complicatedObj, obj)).toBeFalse();
-            });
-
-            const testArray = ['a', 'b', 'c'];
-            const arrayCases = [['a', 'b'], [], ['a', 'b', 'd'], ['a', 'b', 'c', undefined]];
-            it('should properly compare arrays', () => {
-                arrayCases.forEach((arrayCase) => {
-                    expect(Utils.isEqual(testArray, arrayCase)).toBeFalse();
-                });
-            });
-
-            it('should compare nulls with objects to be false', () => {
-                expect(Utils.isEqual(null, { a: 1 })).toBeFalse();
-                expect(Utils.isEqual({ a: 1 }, null)).toBeFalse();
-            });
-
-            it('should similar, non-identical objects to be false', () => {
-                const mainObj = { a: 1, b: 2 };
-                const testCases = [{ a: 1 }, { b: 2 }, { a: 1, b: 2, c: 3 }, { a: 1, b: 3 }];
-                testCases.forEach((testCase) => {
-                    expect(Utils.isEqual(mainObj, testCase)).toBeFalse();
-                    expect(Utils.isEqual(testCase, mainObj)).toBeFalse();
-                });
             });
         });
 

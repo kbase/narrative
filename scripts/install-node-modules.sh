7d49f126b2a6d697a9927713578dd83a12845d1a
Erik Pearson <eapearson@lbl.gov>
2021-10-06 16:10:59 -0700

    add peer dependency stylelint; change name of install-npm -> install-node-modules

--- scripts/install-node-modules.sh
+++ scripts/install-node-modules.sh
@@ -1,4 +1,4 @@
 mkdir -p kbase-extension/static/ext_modules/react
-cp node_modules/react/umd/react.production.min.js kbase-extension/static/ext_modules/react
+cp node_modules/react/umd/react.development.js kbase-extension/static/ext_modules/react
 mkdir -p kbase-extension/static/ext_modules/react-dom
-cp node_modules/react-dom/umd/react-dom.production.min.js kbase-extension/static/ext_modules/react-dom
+cp node_modules/react-dom/umd/react-dom.development.js kbase-extension/static/ext_modules/react-dom

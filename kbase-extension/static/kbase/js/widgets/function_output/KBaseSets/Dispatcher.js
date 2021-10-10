11e1f54f9fdf48a8b59834ccc781dc59940aed7b
Erik Pearson <eapearson@lbl.gov>
2021-10-10 09:11:59 -0700

    removed comments, have been moved to a README

--- kbase-extension/static/kbase/js/widgets/function_output/KBaseSets/Dispatcher.js
+++ kbase-extension/static/kbase/js/widgets/function_output/KBaseSets/Dispatcher.js
@@ -16,59 +16,6 @@ an error message, or are handled by other viewers.
 Note that this widget is a wrapper dispatching to the specific viewer, most of the
 implementation is in the React components referred two in the AMD dependencies. The only
 UI supported herein is the loading indicator and an error message, if necessary.
-
-The original intention of this viewer was to support all of the types implemented in KBaseSets,
-but that work has not yet been completed:
-
-Also, see: https://github.com/kbase/NarrativeViewers/blob/dd1eeeba0ba1faacd6c3596a9413109aeb82e32e/ui/narrative/methods/view_generic_set/spec.json#L21
-
-Below is the status of each KBaseSet type:
-
-Implemented in this viewer:
-
-KBaseSets.ReadsAlignmentSet
-SetAPI.get_reads_alignment_set_v1
-
-KBaseSets.AssemblySet
-SetAPI.get_assembly_set_v1
-
-Implemented in other viewers:
-
-KBaseSets.DifferentialExpressionMatrixSet
-SetAPI.get_differential_expression_matrix_set_v1
-Implemented by:
-https://github.com/kbase/NarrativeViewers/blob/dd1eeeba0ba1faacd6c3596a9413109aeb82e32e/ui/narrative/methods/view_differential_expression_matrix_set/spec.json
-and this viewer does not work
-
-KBaseSets.ExpressionSet
-SetAPI.get_expression_set_v1
-Implemented by:
-https://github.com/kbase/NarrativeViewers/blob/dd1eeeba0ba1faacd6c3596a9413109aeb82e32e/ui/narrative/methods/view_rnaseq_sample_expression/spec.json
-and that viewer doesn't work
-
-KBaseSets.ReadsSet
-SetAPI.get_reads_set_v1
-Implemented by:
-https://github.com/kbase/NarrativeViewers/blob/dd1eeeba0ba1faacd6c3596a9413109aeb82e32e/ui/narrative/methods/view_reads_set/spec.json
-and the current viewer works
-
-KBaseSets.SampleSet
-SetAPI.sample_set_to_samples_info (I think)
-Implemented by:
-https://github.com/kbase/NarrativeViewers/tree/master/ui/narrative/methods/view_sample_set
-but not that this viewer does not utilize the SetAPI, which was implemented against the search, which
-has never been fully implemented for search. Also Samples and RNASeqSamples seem to be conflated
-a bit in SetAPI.
-
-Not implemented anywhere:
-
-KBaseSets.FeatureSetSet
-SetAPI.get_feature_set_set_v1
-can't find any data, or any app which outputs this type, or accepts as input (according to the app browser)
-
-KBaseSets.GenomeSet
-SetAPI.get_genome_set_v1
-
 */
 define([
     'react',

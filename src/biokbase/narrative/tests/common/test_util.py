5b4a457e7297ff0a38e34d8af07413d0b6e88766
Erik Pearson <eapearson@lbl.gov>
2021-10-11 12:00:35 -0700

    make codacy happier through simplification?

--- src/biokbase/narrative/tests/common/test_util.py
+++ src/biokbase/narrative/tests/common/test_util.py
@@ -1,45 +1,31 @@
-"""
-Standalone utility methods for Python tests
-"""
 import unittest
 from biokbase.narrative.common.util import make_ranges
 
-__author__ = "Bill Riehl <wjriehl@lbl.gov>"
-
-
 class UtilTestCase(unittest.TestCase):
-    # Before test:
-    # - Log in (for tests that need a valid login)
-    # also sets the token in the environment variable.
-    test_token = None
-    private_token = None
-
-    # Test make_batches
-
     def test_make_batches_normal_cases(self):
         # Args: total_size, range_size
         cases = [
-            {"input": [0, 1], "expected": []},
-            {"input": [1, 1], "expected": [[0, 1]]},
-            {"input": [10, 10], "expected": [[0, 10]]},
-            {"input": [10, 5], "expected": [[0, 5], [5, 10]]},
-            {"input": [10, 3], "expected": [[0, 3], [3, 6], [6, 9], [9, 10]]},
+            {"args": [0, 1], "expected": []},
+            {"args": [1, 1], "expected": [[0, 1]]},
+            {"args": [10, 10], "expected": [[0, 10]]},
+            {"args": [10, 5], "expected": [[0, 5], [5, 10]]},
+            {"args": [10, 3], "expected": [[0, 3], [3, 6], [6, 9], [9, 10]]},
         ]
         for case in cases:
-            self.assertEqual(make_ranges(*case["input"]), case["expected"])
+            self.assertEqual(make_ranges(*case["args"]), case["expected"])
 
     def test_make_batches_arg_error_cases(self):
         # Args: total_size, range_size
         cases = [
-            {"input": [1, 0]},
-            {"input": [0, 0]},
-            {"input": [1, -1]},
-            {"input": [-1, 1]},
-            {"input": [-1, -1]},
+            {"args": [1, 0]},
+            {"args": [0, 0]},
+            {"args": [1, -1]},
+            {"args": [-1, 1]},
+            {"args": [-1, -1]},
         ]
         for case in cases:
             with self.assertRaises(ValueError):
-                make_ranges(*case["input"])
+                make_ranges(*case["args"])
 
 
 if __name__ == "__main__":

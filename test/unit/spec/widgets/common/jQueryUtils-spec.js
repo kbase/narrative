07fd24e7dbf4f9c9fdc8eb6990ff740e56fec336
Erik Pearson <eapearson@lbl.gov>
2021-10-21 16:44:06 -0700

    add tests

--- test/unit/spec/widgets/common/jQueryUtils-spec.js
+++ test/unit/spec/widgets/common/jQueryUtils-spec.js
@@ -1,6 +1,6 @@
 define(['jquery', 'widgets/common/jQueryUtils'], ($, jQueryUtils) => {
     'use strict';
-    const { $el } = jQueryUtils;
+    const { $el, $row, $col } = jQueryUtils;
 
     describe('The $el function', () => {
         it('should produce a jQuery object', () => {
@@ -17,6 +17,40 @@ define(['jquery', 'widgets/common/jQueryUtils'], ($, jQueryUtils) => {
         it('should display text within', () => {
             const message = 'I am loading';
             const $testDiv = $el('div').text(message);
+            expect($testDiv).toBeDefined();
+            expect($testDiv.text()).toContain(message);
+        });
+    });
+
+    describe('The $row function', () => {
+        it('should produce a jQuery object', () => {
+            expect($row()).toBeInstanceOf($);
+        });
+
+        it('should generate a div element', () => {
+            expect($row().get(0).tagName).toEqual('DIV');
+        });
+
+        it('should display text within', () => {
+            const message = 'I am content';
+            const $testDiv = $row().text(message);
+            expect($testDiv).toBeDefined();
+            expect($testDiv.text()).toContain(message);
+        });
+    });
+
+    describe('The $col function', () => {
+        it('should produce a jQuery object', () => {
+            expect($col()).toBeInstanceOf($);
+        });
+
+        it('should generate a div element', () => {
+            expect($col().get(0).tagName).toEqual('DIV');
+        });
+
+        it('should display text within', () => {
+            const message = 'I am content';
+            const $testDiv = $col().text(message);
             expect($testDiv).toBeDefined();
             expect($testDiv.text()).toContain(message);
         });

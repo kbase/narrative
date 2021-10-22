9b504ff45f739163791d18aa914313c3c9bcd6a6
Erik Pearson <eapearson@lbl.gov>
2021-10-22 12:43:58 -0700

    sonarcloud fixes

--- kbase-extension/static/kbase/js/widgets/common/jQueryUtils.js
+++ kbase-extension/static/kbase/js/widgets/common/jQueryUtils.js
@@ -11,10 +11,11 @@ define([
 
     function $row() {
         return $el('div').addClass('flex-row');
-    };
+    }
+
     function $col() {
         return $el('div').addClass('flex-col');
-    };
+    }
 
     return { $el, $row, $col };
 });

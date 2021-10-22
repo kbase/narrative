172af063ec9fa52dc869120cca4780a54c4b4104
Erik Pearson <eapearson@lbl.gov>
2021-10-22 12:20:15 -0700

    add test for expression sample

---
+++ test/unit/spec/function_output/rna-seq/kbaseExpressionSample-spec.js
@@ -0,0 +1,8 @@
+define(['widgets/function_output/rna-seq/kbaseExpressionSample'], (Widget) => {
+    'use strict';
+    describe('The kbaseExpressionSample widget', () => {
+        it('should be defined', () => {
+            expect(Widget).toBeDefined();
+        });
+    });
+});

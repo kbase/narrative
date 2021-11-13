96448898fba024a6b160c7fc7e37fdd5c24b4342
Bill Riehl <wjriehl@lbl.gov>
2021-11-13 09:06:28 -0500

    fix bulk import tests again

--- test/unit/karma.bulkImport.conf.js
+++ test/unit/karma.bulkImport.conf.js
@@ -4,18 +4,28 @@ module.exports = function (config) {
     'use strict';
     const karmaConfig = require('./karma.conf');
     karmaConfig(config);
-    const bulkImportTestPath = 'test/unit/spec/nbextensions/bulkImportCell/bulkImportCell-spec.js';
+    const bulkImportTestPaths = [
+        'test/unit/spec/nbextensions/bulkImportCell/main-spec.js',
+        'test/unit/spec/nbextensions/bulkImportCell/bulkImportCell-spec.js',
+    ];
     config.files.forEach((line) => {
         if (typeof line === 'object' && line.pattern.indexOf('.js') >= 0) {
             line.nocache = true;
             if (line.pattern === 'test/unit/spec/**/*.js') {
-                line.pattern = bulkImportTestPath;
+                line.pattern = bulkImportTestPaths.shift();
             }
         }
     });
+    bulkImportTestPaths.forEach((path) => {
+        config.files.push({
+            pattern: path,
+            included: false,
+            nocache: true,
+        });
+    });
 
     config.exclude = config.exclude.filter((line) => {
-        return line !== bulkImportTestPath;
+        return !bulkImportTestPaths.includes(line);
     });
 
     config.reporters = ['mocha', 'json-result'];

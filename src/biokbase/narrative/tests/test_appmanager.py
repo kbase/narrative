913eb3a5d995f4c92c8720f8ad3f097005385e1e
ialarmedalien <ialarmedalien@gmail.com>
2021-12-01 07:50:34 -0800

    Python changes: appmanager.py: delete unused code job.py: reorganise file: put static methods together, move narrative output/print content to the bottom jobcomm.py: small refactor to group common core code for each method; simplify logs call by keeping var names the same jobmanager.py: change ee2_error implementation to contain cached code with status ee2_error and updated timestamp   alter retry_jobs to return results as a dict indexed by the original job_id   simplify logs call   rm unnecessary method update tests accordingly

--- src/biokbase/narrative/tests/test_appmanager.py
+++ src/biokbase/narrative/tests/test_appmanager.py
@@ -147,7 +147,9 @@ class AppManagerTestCase(unittest.TestCase):
         except Exception:
             pass
 
-    def run_app_expect_error(self, comm_mock, run_func, func_name, print_error, cell_id=None):
+    def run_app_expect_error(
+        self, comm_mock, run_func, func_name, print_error, cell_id=None
+    ):
         """
         A wrapper for various versions of run_app* that'll test and verify that errors get
         1. printed to stdout and
@@ -168,7 +170,9 @@ class AppManagerTestCase(unittest.TestCase):
             )
             self.assertIn(print_error, output_str)
         else:
-            self.assertEqual('', output_str)  # if nothing gets written to a StringIO, getvalue returns an empty string
+            self.assertEqual(
+                "", output_str
+            )  # if nothing gets written to a StringIO, getvalue returns an empty string
         self._verify_comm_error(comm_mock, cell_id=cell_id)
 
     def test_reload(self):
@@ -333,9 +337,11 @@ class AppManagerTestCase(unittest.TestCase):
         # should print an error if there is no cell id
         self.run_app_expect_error(
             comm_mock,
-            lambda: self.am.run_app(self.bad_app_id, self.test_app_params, tag=self.test_tag),
+            lambda: self.am.run_app(
+                self.bad_app_id, self.test_app_params, tag=self.test_tag
+            ),
             "run_app",
-            "Unknown app id"
+            "Unknown app id",
         )
 
         comm_mock2 = MagicMock()
@@ -345,10 +351,15 @@ class AppManagerTestCase(unittest.TestCase):
         cell_id = "some_app_cell"
         self.run_app_expect_error(
             comm_mock2,
-            lambda: self.am.run_app(self.bad_app_id, self.test_app_params, tag=self.test_tag, cell_id=cell_id),
+            lambda: self.am.run_app(
+                self.bad_app_id,
+                self.test_app_params,
+                tag=self.test_tag,
+                cell_id=cell_id,
+            ),
             "run_app",
             None,
-            cell_id=cell_id
+            cell_id=cell_id,
         )
 
     ############# End tests for run_app #############
@@ -521,9 +532,11 @@ class AppManagerTestCase(unittest.TestCase):
         # should print an error if there is no cell id
         self.run_app_expect_error(
             comm_mock,
-            lambda: self.am.run_app_batch(self.bad_app_id, [self.test_app_params], tag=self.test_tag),
+            lambda: self.am.run_app_batch(
+                self.bad_app_id, [self.test_app_params], tag=self.test_tag
+            ),
             "run_app_batch",
-            "Unknown app id"
+            "Unknown app id",
         )
 
         comm_mock2 = MagicMock()
@@ -533,10 +546,15 @@ class AppManagerTestCase(unittest.TestCase):
         cell_id = "some_batch_cell"
         self.run_app_expect_error(
             comm_mock2,
-            lambda: self.am.run_app_batch(self.bad_app_id, [self.test_app_params], tag=self.test_tag, cell_id=cell_id),
+            lambda: self.am.run_app_batch(
+                self.bad_app_id,
+                [self.test_app_params],
+                tag=self.test_tag,
+                cell_id=cell_id,
+            ),
             "run_app_batch",
             None,
-            cell_id=cell_id
+            cell_id=cell_id,
         )
 
     ############# End tests for run_app_batch #############
@@ -617,9 +635,11 @@ class AppManagerTestCase(unittest.TestCase):
         # should print an error if there is no cell id
         self.run_app_expect_error(
             comm_mock,
-            lambda: self.am.run_local_app(self.bad_app_id, self.test_app_params, tag=self.test_tag),
+            lambda: self.am.run_local_app(
+                self.bad_app_id, self.test_app_params, tag=self.test_tag
+            ),
             "run_local_app",
-            "Unknown app id"
+            "Unknown app id",
         )
 
         comm_mock2 = MagicMock()
@@ -629,10 +649,15 @@ class AppManagerTestCase(unittest.TestCase):
         cell_id = "some_local_app_cell"
         self.run_app_expect_error(
             comm_mock2,
-            lambda: self.am.run_local_app(self.bad_app_id, self.test_app_params, tag=self.test_tag, cell_id=cell_id),
+            lambda: self.am.run_local_app(
+                self.bad_app_id,
+                self.test_app_params,
+                tag=self.test_tag,
+                cell_id=cell_id,
+            ),
             "run_local_app",
             None,
-            cell_id=cell_id
+            cell_id=cell_id,
         )
 
     ############# End tests for run_local_app #############
@@ -838,7 +863,7 @@ class AppManagerTestCase(unittest.TestCase):
             comm_mock,
             lambda: self.am.run_app_bulk(test_case),
             "run_app_bulk",
-            "an app_id must be of the format module_name/app_name"
+            "an app_id must be of the format module_name/app_name",
         )
 
         comm_mock2 = MagicMock()
@@ -851,7 +876,7 @@ class AppManagerTestCase(unittest.TestCase):
             lambda: self.am.run_app_bulk(test_case, cell_id=cell_id),
             "run_app_bulk",
             None,
-            cell_id=cell_id
+            cell_id=cell_id,
         )
 
     @mock.patch("biokbase.narrative.jobs.appmanager.clients.get", get_mock_client)

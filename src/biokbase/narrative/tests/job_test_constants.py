bef76c8f3b49a7e4bcfe5b6e46f153cfe1bc00f4
ialarmedalien <ialarmedalien@gmail.com>
2022-01-18 09:21:55 -0800

    Move generation of data-dependent constants to `generate_test_results.py` substitute in up-to-date ee2 job data structures

--- src/biokbase/narrative/tests/job_test_constants.py
+++ src/biokbase/narrative/tests/job_test_constants.py
@@ -1,19 +1,10 @@
 from .util import ConfigTests
 import copy
+from biokbase.narrative.jobs.job import TERMINAL_STATUSES
 
 
 config = ConfigTests()
 TEST_JOBS = config.load_json_file(config.get("jobs", "ee2_job_test_data_file"))
-
-
-def get_retried_jobs(all_jobs):
-    # collect retried jobs
-    retried_jobs = {}
-    for job in all_jobs.values():
-        # save the first retry ID with the retried job
-        if "retry_ids" in job and len(job["retry_ids"]) > 0:
-            retried_jobs[job["job_id"]] = job["retry_ids"][0]
-    return retried_jobs
 
 
 def generate_error(job_id, err_type):
@@ -45,8 +36,6 @@ def get_test_job(job_id):
 
 
 CLIENTS = "biokbase.narrative.clients.get"
-TEST_JOB_IDS = list(TEST_JOBS.keys())
-RETRIED_JOBS = get_retried_jobs(TEST_JOBS)
 
 # test_jobs contains jobs in the following states
 JOB_COMPLETED = "JOB_COMPLETED"
@@ -62,6 +51,22 @@ BATCH_ERROR_RETRIED = "BATCH_ERROR_RETRIED"
 BATCH_RETRY_COMPLETED = "BATCH_RETRY_COMPLETED"
 BATCH_RETRY_RUNNING = "BATCH_RETRY_RUNNING"
 BATCH_RETRY_ERROR = "BATCH_RETRY_ERROR"
+
+ALL_JOBS = [
+    JOB_COMPLETED,
+    JOB_CREATED,
+    JOB_RUNNING,
+    JOB_TERMINATED,
+    JOB_ERROR,
+    BATCH_PARENT,
+    BATCH_COMPLETED,
+    BATCH_TERMINATED,
+    BATCH_TERMINATED_RETRIED,
+    BATCH_ERROR_RETRIED,
+    BATCH_RETRY_COMPLETED,
+    BATCH_RETRY_ERROR,
+    BATCH_RETRY_RUNNING
+]
 
 JOB_NOT_FOUND = "job_not_found"
 BAD_JOB_ID = "a_bad_job_id"
@@ -81,23 +86,8 @@ BATCH_CHILDREN = [
 
 BATCH_PARENT_CHILDREN = [BATCH_PARENT] + BATCH_CHILDREN
 
-JOBS_TERMINALITY = {
-    JOB_COMPLETED: True,
-    JOB_CREATED: False,
-    JOB_RUNNING: False,
-    JOB_TERMINATED: True,
-    JOB_ERROR: True,
-    BATCH_PARENT: False,
-    BATCH_COMPLETED: True,
-    BATCH_TERMINATED: True,
-    BATCH_TERMINATED_RETRIED: True,
-    BATCH_ERROR_RETRIED: True,
-    BATCH_RETRY_COMPLETED: True,
-    BATCH_RETRY_RUNNING: False,
-    BATCH_RETRY_ERROR: True,
-}
+JOBS_TERMINALITY = {id: TEST_JOBS[id]["status"] in TERMINAL_STATUSES for id in TEST_JOBS.keys()}
 
-ALL_JOBS = list(JOBS_TERMINALITY.keys())
 TERMINAL_JOBS = []
 ACTIVE_JOBS = []
 for key, value in JOBS_TERMINALITY.items():
@@ -106,37 +96,6 @@ for key, value in JOBS_TERMINALITY.items():
     else:
         ACTIVE_JOBS.append(key)
 
-TEST_CELL_ID_LIST = [
-    "NARRATIVE_CELL_1",
-    "NARRATIVE_CELL_2",
-    # batch cell
-    "NARRATIVE_BATCH_CELL_1",
-    # batch cell two
-    "NARRATIVE_BATCH_CELL_2",
-    "invalid_cell_id",
-]
-# expected _jobs_by_cell_id mapping in JobManager
-JOBS_BY_CELL_ID = {
-    TEST_CELL_ID_LIST[0]: {JOB_COMPLETED, JOB_CREATED},
-    TEST_CELL_ID_LIST[1]: {JOB_RUNNING, JOB_TERMINATED, JOB_ERROR},
-    TEST_CELL_ID_LIST[2]: {
-        BATCH_PARENT,
-        BATCH_COMPLETED,
-        BATCH_TERMINATED,
-        BATCH_TERMINATED_RETRIED,
-        BATCH_ERROR_RETRIED,
-    },
-    TEST_CELL_ID_LIST[3]: {
-        BATCH_PARENT,
-        BATCH_RETRY_COMPLETED,
-        BATCH_RETRY_RUNNING,
-        BATCH_RETRY_ERROR,
-    },
-}
-
-# mapping expected as output from get_job_states_by_cell_id
-TEST_CELL_IDs = {id: list(JOBS_BY_CELL_ID[id]) for id in JOBS_BY_CELL_ID.keys()}
-TEST_CELL_IDs[TEST_CELL_ID_LIST[4]] = []
 
 READS_OBJ_1 = "rhodobacterium.art.q20.int.PE.reads"
 READS_OBJ_2 = "rhodobacterium.art.q10.PE.reads"

1da0446ac37b2eab60c7278a3066e86cf3a18120
ialarmedalien <ialarmedalien@gmail.com>
2022-04-06 09:39:33 -0700

    Big reorganisation of GitHub Actions: |     - use GHAs to build and tag releases instead of shell scripts |     - use variables instead of repeating script content |     - update runners in unit/integration tests |     - edit pre-commit hook to remove css processing, move it to lint-staged |     - add .dockerignore file |     - misc edits and tidying on installation scripts |     - update dockerfile to use pip for installation of python modules where possible |     - update requirements.txt with modules moved out of dockerfile

--- scripts/narrative_backend_tests.sh
+++ scripts/narrative_backend_tests.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 export NARRATIVE_DIR=$(pwd)
+
+docker_container=${DOCKER_CONTAINER:-false}
+
+# if the test is being run in a docker container, ensure
+# that the conda base environment is activated
+if [ "$docker_container" == "true" ]; then
+    source activate base
+fi
+
 pytest \
     --cov=biokbase.narrative \
     --cov-config=.coveragerc \
@@ -7,3 +16,11 @@ pytest \
     --cov-report=xml \
     --ignore=src/biokbase/narrative/tests/util.py \
     src/biokbase/narrative/tests
+
+exit_status=$?
+
+if [ "$docker_container" == "true" ]; then
+    cp ./python-coverage/coverage.xml /tmp/output/coverage.xml
+fi
+
+exit $exit_status

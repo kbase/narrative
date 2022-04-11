85bc38f3db1b8e31910bcc640a2fdb856d679e98
Bill Riehl <wjriehl@lbl.gov>
2022-04-11 18:05:15 -0400

    version bump to 5.0.3

--- src/biokbase/narrative/__init__.py
+++ src/biokbase/narrative/__init__.py
@@ -2,7 +2,7 @@ __all__ = ["magics", "common", "handlers", "contents", "services", "widgetmanage
 
 from semantic_version import Version
 
-__version__ = Version("5.0.2")
+__version__ = Version("5.0.3")
 
 
 def version():
@@ -13,22 +13,4 @@ def version():
 #   no args = print current version
 #   1 arg = self-modify version to arg value
 if __name__ == "__main__":
-    import os
-    import sys
-
-    if len(sys.argv) == 1:
-        print(version())
-    elif len(sys.argv) == 2:
-        ver = sys.argv[1]
-        try:
-            Version(ver)
-        except BaseException:
-            print("Invalid version: {}".format(ver))
-            sys.exit(1)
-        oldver = '"' + str(version()) + '"'
-        newver = '"' + ver + '"'
-        myfile = os.path.abspath(sys.argv[0])
-        oldself = open(myfile).read()
-        newself = oldself.replace(oldver, newver)
-        open(myfile, "w").write(newself)
-    sys.exit(0)
+    print(version())

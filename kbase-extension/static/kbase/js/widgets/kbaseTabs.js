c5c6a11c98ef9c6cd38896839e47dc8ea4d5adbb
Erik Pearson <eapearson@lbl.gov>
2022-10-24 09:27:49 -0700

    add comments, address tab initialization issue, add additional usage examples to gallery [PTV-1620][PTV-1845]

--- kbase-extension/static/kbase/js/widgets/kbaseTabs.js
+++ kbase-extension/static/kbase/js/widgets/kbaseTabs.js
@@ -42,7 +42,6 @@ define([
     'bootstrap',
 ], (KBWidget, $, kbaseDeletePrompt, $ErrorMessage) => {
     'use strict';
-
     return KBWidget({
         name: 'kbaseTabs',
 
@@ -69,14 +68,17 @@ define([
                 tabs = this.options.tabs;
             }
 
-            const $block = $('<div>').addClass('tabbable');
+            const $tabset = $('<div>').addClass('tabbable');
+
             const $tabs = $('<div>').addClass('tab-content').attr('id', 'tabs-content');
+
             const $nav = $('<ul role="tablist" />').addClass('nav nav-tabs').attr('id', 'tabs-nav');
-            $block.append($nav).append($tabs);
 
-            this._rewireIds($block, this);
+            $tabset.append($nav).append($tabs);
 
-            $elem.append($block);
+            this._rewireIds($tabset, this);
+
+            $elem.append($tabset);
 
             if (tabs && tabs.length > 0) {
                 // defaults to first tab being active if none are set as active by default.
@@ -93,17 +95,99 @@ define([
             }
         },
 
+        renderDeleteTabButton: function (tab) {
+            return $('<button>')
+                .addClass('btn btn-default btn-xs')
+                .append($('<span>').append('&#x2716;'))
+                .css('padding', '0px')
+                .css('width', '12px')
+                .css('height', '12px')
+                .css('margin-left', '10px')
+                .css('font-size', '10px')
+                .css('margin-bottom', '3px')
+                .css('outline', 'none')
+                .css('border', '0')
+                .bind(
+                    'click',
+                    $.proxy(function (e) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        if (typeof tab.deleteCallback !== 'undefined') {
+                            tab.deleteCallback(tab.tab);
+                        } else if (typeof tab.confirmDelete === 'undefined') {
+                            // In this case, there is no confirmDelete set on the tab,
+                            // so we default to the global behavior.
+                            if (
+                                typeof this.options.confirmDelete === 'undefined' ||
+                                this.options.confirmDelete
+                            ) {
+                                this.deletePrompt(tab.tab);
+                            } else {
+                                this.removeTab(tab.tab);
+                            }
+                        } else if (tab.confirmDelete) {
+                            this.deletePrompt(tab.tab);
+                        } else {
+                            this.removeTab(tab.tab);
+                        }
+                    }, this)
+                );
+        },
+
+        renderTabPanel: function (tab, $tabPanel) {
+            // This may be called before the tab is added
+            $tabPanel = $tabPanel || this.data('tabs')[tab.tab];
+            if ($tabPanel.hasContent) {
+                return;
+            }
+            if (tab.content) {
+                $tabPanel.append(tab.content);
+                $tabPanel.hasContent = true;
+            } else if (tab.showContentCallback) {
+                try {
+                    $tabPanel.html(tab.showContentCallback($tabPanel));
+                } catch (ex) {
+                    $tabPanel.html($ErrorMessage(ex));
+                }
+                if (!tab.dynamicContent) {
+                    $tabPanel.hasContent = true;
+                }
+            }
+        },
+
         addTab: function (tab) {
             if (typeof tab.canDelete === 'undefined') {
                 tab.canDelete = this.options.canDelete;
             }
 
-            const $tab = $('<div role="tabpanel"></div>').addClass('tab-pane fade');
+            const $tabPanel = $('<div role="tabpanel"></div>').addClass('tab-pane fade');
 
-            $tab.hasContent = false;
+            $tabPanel.hasContent = false;
 
-            const $that = this;
+            // TODO: this is a hack, remove it!
+            // NOTE: So, I was wondering why this code is here? It is not necessary for the
+            // kbaseTabs logic. However,  there are tab usages which presume that after the tab
+            // is created, that all tab content is rendered and inserted into the DOM.
+            // E.g. in kbaseExpressionFeatureClusters the content contains links which are treated as buttons,
+            // whose event handlers are added as the tabset is rendered.
+            // Any post-tab-render, per-tab logic should be handled by the onShown or whenShown callbacks.
+            // Note that this approach will fail if a tab uses "showContentCallback".
+            // However, if we use this.showTabPanel() here to possibly invoke showContentCallback, it obviates
+            // the whole lazy nature of showContentCallback!
+            // In other words, we need to keep this here as it is, until we can fix code which relies upon
+            // this behavior.
+            // Probably better is to fork this widget and it can incrementally spread throughout the codebase
+            // until there are no usages of the old one left, then remove it.
+            if (tab.content) {
+                $tabPanel.append(tab.content);
+                $tabPanel.hasContent = true;
+            }
 
+            // We need to preserve "this" for the jquery "click" handling below, since that code
+            // uses the jquery logic for "this".
+            const $widget = this;
+
+            // Create the tab button itself.
             const $navButton = $('<a>')
                 .attr('href', '#')
                 .text(tab.tab)
@@ -113,8 +197,7 @@ define([
                     e.stopPropagation();
 
                     // NB this function mimics bootstrap tab's "show" method.
-
-                    const previous = $that.data('tabs-nav').find('.active:last a')[0];
+                    const previous = $widget.data('tabs-nav').find('.active:last a')[0];
 
                     // Danger: this calls the internal api for bootstrap tabs; that is
                     // where the mysterious "activate" lives.
@@ -123,101 +206,84 @@ define([
                     $.fn.tab.Constructor.prototype.activate.call(
                         $(this),
                         $(this).parent('li'),
-                        $that.data('tabs-nav')
+                        $widget.data('tabs-nav')
                     );
+
+                    // Rendering of the tab pane
+                    $widget.renderTabPanel(tab, $tabPanel);
 
                     // Activate this tab, and emit events.
                     $.fn.tab.Constructor.prototype.activate.call(
                         $(this),
-                        $tab,
-                        $tab.parent(),
+                        $tabPanel,
+                        $tabPanel.parent(),
                         () => {
-                            $tab.trigger({
+                            $tabPanel.trigger({
                                 type: 'shown',
                                 relatedTarget: previous,
                             });
                         }
                     );
 
-                    // Rendering of the tab pane
-                    if (!$tab.hasContent) {
-                        if (tab.content) {
-                            $tab.append(tab.content);
-                            $tab.hasContent = true;
-                        } else if (tab.showContentCallback) {
-                            try {
-                                $tab.html(tab.showContentCallback($tab));
-                            } catch (ex) {
-                                $tab.html($ErrorMessage(ex));
-                            }
-                            if (!tab.dynamicContent) {
-                                $tab.hasContent = true;
-                            }
+                    // Take some action after the tab is rendered.
+                    if (tab.onShown) {
+                        try {
+                            tab.onShown();
+                        } catch (ex) {
+                            console.error('Error in "onShown"', ex);
                         }
                     }
                 });
 
+            // Add the delete button if this is a deletable tab.
             if (tab.canDelete) {
-                $navButton.append(
-                    $('<button>')
-                        .addClass('btn btn-default btn-xs')
-                        .append($('<span>').append('&#x2716;'))
-                        .css('padding', '0px')
-                        .css('width', '12px')
-                        .css('height', '12px')
-                        .css('margin-left', '10px')
-                        .css('font-size', '10px')
-                        .css('margin-bottom', '3px')
-                        .css('outline', 'none')
-                        .css('border', '0')
-                        .bind(
-                            'click',
-                            $.proxy(function (e) {
-                                e.preventDefault();
-                                e.stopPropagation();
-                                if (typeof tab.deleteCallback !== 'undefined') {
-                                    tab.deleteCallback(tab.tab);
-                                } else if (typeof tab.confirmDelete === 'undefined') {
-                                    // In this case, there is no confirmDelete set on the tab,
-                                    // so we default to the global behavior.
-                                    if (
-                                        typeof this.options.confirmDelete === 'undefined' ||
-                                        this.options.confirmDelete
-                                    ) {
-                                        this.deletePrompt(tab.tab);
-                                    } else {
-                                        this.removeTab(tab.tab);
-                                    }
-                                } else if (tab.confirmDelete) {
-                                    this.deletePrompt(tab.tab);
-                                } else {
-                                    this.removeTab(tab.tab);
-                                }
-                            }, this)
-                        )
-                );
+                $navButton.append(this.renderDeleteTabButton(tab));
             }
 
             const $nav = $('<li role="tab"></li>').css('white-space', 'nowrap').append($navButton);
 
-            this.data('tabs')[tab.tab] = $tab;
+            this.data('tabs')[tab.tab] = $tabPanel;
             this.data('nav')[tab.tab] = $nav;
 
-            this.data('tabs-content').append($tab);
+            this.data('tabs-content').append($tabPanel);
             this.data('tabs-nav').append($nav);
 
-            const tabCount = this.data('tabs').length;
-            if (tab.show || tabCount === 1) {
+            // A tab requests that it be shown.
+            // Note that the use case for this is more complicated than it may appear at first glance.
+            //
+            // Primary use cases:
+            //
+            // For tabsets which provide all tabs via the constructor and don't specify "show" on a tab,
+            // the initial tab will be set to "show" in the constructor.
+            //
+            // For tabs which are dynamically added, the "show" property on that tab will indicate that it
+            // should be shown as it is added.
+            //
+            // Less common use cases:
+            //
+            // The ui is invoked with the initial tab parameterized, and the code which creates the tabset
+            // uses that to select a tab other than the first one as selected. I'm not sure that there is
+            // any usage of this in the narrative codebase.
+            //
+            if (tab.show) {
                 this.showTab(tab.tab);
             }
+        },
+
+        closeIcon: function () {
+            return 'icon-remove';
+        },
+
+        deleteTabToolTip: function (tabName) {
+            return 'Remove ' + tabName;
         },
 
         hasTab: function (tabName) {
             return this.data('tabs')[tabName];
         },
 
-        showTab: function (tab) {
-            this.data('nav')[tab].find('a').trigger('click');
+        showTab: function (tabName) {
+            this.data('nav')[tabName].find('a').trigger('click');
         },
 
         removeTab: function (tabName) {

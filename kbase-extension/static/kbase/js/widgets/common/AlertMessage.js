8c2cae5a397e96d18dbbdf843390c8560d868eed
Erik Pearson <eapearson@lbl.gov>
2023-07-06 16:05:37 -0700

    bootstrap alert wrapper default to 'info' type [PTV-1886]

--- kbase-extension/static/kbase/js/widgets/common/AlertMessage.js
+++ kbase-extension/static/kbase/js/widgets/common/AlertMessage.js
@@ -2,17 +2,16 @@ define([
     'jquery',
 
     // for effect
-    'bootstrap'
+    'bootstrap',
 ], ($) => {
     'use strict';
 
-    function $AlertMessage(message, { type } = {}) {
-        if (typeof type === 'string' && !['info', 'warning', 'danger', 'success'].includes(type)) {
+    function $AlertMessage(message, options = {}) {
+        const type = options.type || 'info';
+        if (!['info', 'warning', 'danger', 'success'].includes(type)) {
             throw new Error(`Alert type "${type}" not recognized`);
         }
-        return $('<div>')
-            .addClass(`alert alert-${type}`)
-            .text(message);
+        return $('<div>').addClass(`alert alert-${type}`).text(message);
     }
 
     return $AlertMessage;

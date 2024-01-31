53d1632674beee74d438d27aa34bb2b799b2b493
Erik Pearson <eapearson@lbl.gov>
2024-01-31 11:32:09 -0800

    convert more object spreads to Object.assign [PTV-1905] to make esprima happy in GHA build

--- kbase-extension/static/kbase/js/widgets/narrative_core/kbaseCellToolbarMenu.js
+++ kbase-extension/static/kbase/js/widgets/narrative_core/kbaseCellToolbarMenu.js
@@ -127,10 +127,9 @@ define([
                 return '';
             }
             if (cell.cell_type === 'code') {
-                const attribs = {
-                    ...buttonBase,
+                const attribs = Object.assign({}, buttonBase, {
                     id: handleClick(events, doToggleCodeView),
-                };
+                });
                 if (isCodeShowing(cell)) {
                     attribs.title = 'Hide code';
                     attribs.class += ' active';

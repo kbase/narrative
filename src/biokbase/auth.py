0abd60fb98eabce5abe9c26baab760cb2ad8a1cc
ialarmedalien <ialarmedalien@gmail.com>
2024-05-07 06:29:48 -0700

    Updatng python deps for Apr 30; also some misc code cleanup, adding type hints, and autofixing some  commands

--- src/biokbase/auth.py
+++ src/biokbase/auth.py
@@ -1,9 +1,9 @@
-"""
-Narrative authentication tools.
+"""Narrative authentication tools.
+
 This uses the KBase auth2 API to get and manage auth tokens.
 """
 
-from typing import Optional
+from typing import Any
 
 import requests
 
@@ -11,14 +11,15 @@ from biokbase.narrative.common.url_config import URLS
 from biokbase.narrative.common.util import kbase_env
 
 token_api_url = URLS.auth + "/api/V2"
-endpt_token = "/token"
-endpt_token_revoke = "/tokens/revoke"
+endpt_token = "/token"  # noqa: S105
+endpt_token_revoke = "/tokens/revoke"  # noqa: S105
 endpt_user_display = "/users/?list="
 endpt_me = "/me"
 
 
 class TokenInfo:
-    """
+    """Container for token information.
+
     Converts an information dictionary from the Auth service to a
     simple class that contains default values.
 
@@ -29,7 +30,7 @@ class TokenInfo:
     the info dict, if present.
     """
 
-    def __init__(self, info_dict: dict, token: str = None):
+    def __init__(self: "TokenInfo", info_dict: dict, token: str | None = None) -> None:
         self.cachefor = info_dict.get("cachefor", 0)
         self.created = info_dict.get("created", 0)
         self.custom = info_dict.get("custom", {})
@@ -42,12 +43,12 @@ class TokenInfo:
 
 
 class UserInfo:
-    """
-    A container for user information that comes from the Auth service. Does not
-    hold the token itself.
+    """A container for user information that comes from the Auth service.
+
+    Does not hold the token itself.
     """
 
-    def __init__(self, user_dict: dict):
+    def __init__(self: "UserInfo", user_dict: dict[str, Any]) -> None:
         self.anon_user_id = user_dict.get("anonid")
         self.custom_roles = user_dict.get("customroles", [])
         self.display_name = user_dict.get("display")
@@ -55,8 +56,9 @@ class UserInfo:
 
 
 def validate_token() -> bool:
-    """
-    Validates the currently set auth token. Returns True if valid, False otherwise.
+    """Validates the currently set auth token.
+
+    Returns True if valid, False otherwise.
     """
     headers = {"Authorization": get_auth_token()}
     r = requests.get(token_api_url + endpt_token, headers=headers)
@@ -64,43 +66,38 @@ def validate_token() -> bool:
 
 
 def set_environ_token(token: str | None) -> None:
-    """
-    Sets a login token in the local environment variable.
-    """
+    """Sets a login token in the local environment variable."""
     kbase_env.auth_token = token
 
 
 def get_auth_token() -> str | None:
-    """
-    Returns the current login token being used, or None if one isn't set.
-    """
+    """Returns the current login token being used, or None if one isn't set."""
     return kbase_env.auth_token
 
 
 def get_user_info(token: str) -> UserInfo:
-    """
-    Returns the UserInfo object with information about the user who
-    owns the valid token. If the token is invalid (i.e. the status_code
+    """Returns the UserInfo object with information about the user who owns the valid token.
+
+    If the token is invalid (i.e. the status_code
     of the request is not ok), an HTTPError exception is raised.
     """
     headers = {"Authorization": token}
     r = requests.get(token_api_url + endpt_me, headers=headers)
     r.raise_for_status()
-    user_info = UserInfo(r.json())
-    return user_info
+    return UserInfo(r.json())
 
 
 def get_token_info(token: str) -> TokenInfo:
+    """Retrieve token information as a TokenInfo object."""
     headers = {"Authorization": token}
     r = requests.get(token_api_url + endpt_token, headers=headers)
     r.raise_for_status()
-    token_info = TokenInfo(r.json(), token=token)
-    return token_info
+    return TokenInfo(r.json(), token=token)
 
 
 def init_session_env(token_info: TokenInfo, user_info: UserInfo, ip: str) -> None:
-    """
-    Initializes the internal session environment.
+    """Initializes the internal session environment.
+
     Parameters:
       token_info: TokenInfo object, uses id, token, and user attributes
       ip: the client IP address
@@ -127,8 +124,7 @@ def get_agent_token(login_token: str, token_name: str = "NarrativeAgent") -> Tok
     data = {"name": token_name}
     r = requests.post(token_api_url + endpt_token, headers=headers, json=data)
     r.raise_for_status()
-    agent_token_info = TokenInfo(r.json())
-    return agent_token_info
+    return TokenInfo(r.json())
 
 
 def get_display_names(auth_token: str, user_ids: list) -> dict:

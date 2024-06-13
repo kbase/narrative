25744531a385e2f3f1b1f669aa5124e4533c2387
ialarmedalien <ialarmedalien@gmail.com>
2024-06-13 09:33:13 -0700

    Reverting notebook extensions

--- kbase-extension/jupyter_notebook_config.py
+++ kbase-extension/jupyter_notebook_config.py
@@ -33,8 +33,8 @@ c = get_config()  # noqa: F821
 # Python modules to load as notebook server extensions. This is an experimental
 # API, and may change in future releases.
 # c.NotebookApp.server_extensions = []
-# c.NotebookApp.server_extensions = ["biokbase.narrative.handlers.narrativehandler"]
-c.NotebookApp.nbserver_extensions = {"biokbase.narrative.handlers.narrativehandler": True}
+c.NotebookApp.server_extensions = ["biokbase.narrative.handlers.narrativehandler"]
+# c.NotebookApp.nbserver_extensions = {"biokbase.narrative.handlers.narrativehandler": True}
 # The random bytes used to secure cookies. By default this is a new random
 # number every time you start the Notebook. Set it to a value in a config file
 # to enable logins to persist across server sessions.

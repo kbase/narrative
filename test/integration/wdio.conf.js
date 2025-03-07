25e2bb132d38b14dd657ec463013f7d27acac8f8
Bill Riehl <wjriehl@lbl.gov>
2025-03-07 17:02:55 -0500

    tweak wdio options

--- test/integration/wdio.conf.js
+++ test/integration/wdio.conf.js
@@ -169,16 +169,16 @@ const authToken = (() => {
 // the most recent version of Firefox is supported.
 //
 
-const serviceConfigs = {
-    'selenium-standalone': {
-        logPath: 'selenium-standalone-logs',
-        // drivers
-    },
-    browserstack: {
-        browserstackLocal: true,
-        opts: {},
-    },
-};
+// const serviceConfigs = {
+//     'selenium-standalone': {
+//         logPath: 'selenium-standalone-logs',
+//         // drivers
+//     },
+//     browserstack: {
+//         browserstackLocal: true,
+//         opts: {},
+//     },
+// };
 
 const testParams = makeConfig();
 
@@ -193,9 +193,11 @@ function makeCapabilities(config) {
         case 'chromedriver':
             return (() => {
                 const args = [
-                    'headless',
-                    'disable-gpu',
-                    `window-size=${config.WIDTH},${config.HEIGHT}`,
+                    '--headless',
+                    '--no-sandbox',
+                    '--disable-gpu',
+                    '--disable-dev-shm-usage',
+                    `--window-size=${config.WIDTH},${config.HEIGHT}`,
                 ];
                 return {
                     browserName: 'chrome',
@@ -205,7 +207,7 @@ function makeCapabilities(config) {
                     'goog:chromeOptions': {
                         args,
                     },
-                    'wdio:enforceWebDriverClassic': true
+                    'wdio:enforceWebDriverClassic': true,
                 };
             })();
         case 'selenium-standalone':
@@ -421,7 +423,6 @@ const wdioConfig = {
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
     reporters: ['spec'],
-
 
     //
     // Options to be passed to Mocha.
